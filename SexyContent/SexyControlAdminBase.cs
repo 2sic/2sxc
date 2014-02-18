363ef0788e6d7263b4cc38ca6ab047c977202e75
raphael-m <raaphii@gmail.com>
2014-02-18 15:01:26 +0100

    Added control's base classes SexyControlAdminBase and SexyControlEditBase

---
+++ SexyContent/SexyControlAdminBase.cs
@@ -0,0 +1,28 @@
+﻿using DotNetNuke.Entities.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToSic.SexyContent
+{
+    /// <summary>
+    /// Contains properties that all controls use that edit the current module's data (not global data like admin controls)
+    /// It delivers a context that uses the current modules App and the current portal's Zone.
+    /// </summary>
+    public abstract class SexyControlAdminBase : SexyControlEditBase
+    {
+        protected new int? AppId
+        {
+            get
+            {
+                var appIdString = Request.QueryString[SexyContent.AppIDString];
+                int appId;
+                if (appIdString != null && int.TryParse(appIdString.ToString(), out appId))
+                    return appId;
+                return null;
+            }
+        }
+
+    }
+}
