2a27a37581e26bfe6445dc56bf5463d09a1ca6d1
tobias2sic <tobias.krattinger@2sic.com>
2014-04-02 08:39:54 +0200

    Show details on unknown error.

--- SexyContent/DataImportExport/DataXmlImport.cs
+++ SexyContent/DataImportExport/DataXmlImport.cs
@@ -218,7 +218,7 @@ namespace ToSic.SexyContent.DataImportExport
             }
             catch (Exception exception)
             {
-                errorProtocol.AppendError(DataImportErrorCode.Unknown, exception.Message);
+                errorProtocol.AppendError(DataImportErrorCode.Unknown, exception.ToString());
             }
         }
 
