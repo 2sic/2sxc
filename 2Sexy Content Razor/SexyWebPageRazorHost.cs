5a1eba2150c544c02a1e31c68c2ddb3b9d25c8db
raphael-m <raaphii@gmail.com>
2014-04-17 17:50:11 +0200

    Some tests...

---
+++ 2Sexy Content Razor/SexyWebPageRazorHost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToSic.SexyContent.Razor
+{
+    class SexyWebPageRazorHost : System.Web.WebPages.Razor.WebPageRazorHost
+    {
+        public SexyWebPageRazorHost(string virtualPath)
+            : base(virtualPath, null)
+        {
+            this.RegisterSpecialFile();
+        }
+    }
+}
