4c3f56a714788a8fe774b25f95bb26eb925aa6b6
raphael-m <raaphii@gmail.com>
2014-05-15 16:26:39 +0200

    Changed internal assembly version to 6.0.6.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("6.0.6.3")]
-[assembly: AssemblyFileVersion("6.0.6.3")]
+[assembly: AssemblyVersion("6.0.6.4")]
+[assembly: AssemblyFileVersion("6.0.6.4")]
