e921a8a8f1e04586ae09a6f08c79c63dcf98bfed
raphael-m <raaphii@gmail.com>
2014-05-20 09:55:11 +0200

    Changed version numbers to 06.00.08

--- SexyContent/SexyContent.cs
+++ SexyContent/SexyContent.cs
@@ -31,7 +31,7 @@ namespace ToSic.SexyContent
     {
         #region Constants
 
-        public const string ModuleVersion = "06.00.07";
+        public const string ModuleVersion = "06.00.08";
         public const string TemplateID = "TemplateID";
         public const string ContentGroupIDString = "ContentGroupID";
         public const string AppIDString = "AppId";
