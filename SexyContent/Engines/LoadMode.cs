b785767d237f65a174cfbfab6fff949be81f5359
raphael-m <raaphii@gmail.com>
2014-05-22 13:32:59 +0200

    Added LoadMode

---
+++ SexyContent/Engines/LoadMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToSic.SexyContent.Engines
+{
+    public enum LoadMode
+    {
+        WebView,
+        IndexingForSearch,
+        PublishData
+    }
+}
