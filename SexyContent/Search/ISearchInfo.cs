27cda9fef741cba497b59b8323e5d4c7f4f4efb9
raphael-m <raaphii@gmail.com>
2014-05-23 15:07:08 +0200

    Added needed properties to ISearchInfo

--- SexyContent/Search/ISearchInfo.cs
+++ SexyContent/Search/ISearchInfo.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using ToSic.Eav;
 
 namespace ToSic.SexyContent.Search
 {
     public interface ISearchInfo
     {
+        string UniqueKey { get; set; }
         string Title { get; set; }
+        string Description { get; set; }
+        string Body { get; set; }
+        string Url { get; set; }
+        DateTime ModifiedTimeUtc { get; set; }
+        bool IsActive { get; set; }
+        string QueryString { get; set; }
+        string CultureCode { get; set; }
+        IEntity Entity { get; set; }
     }
 }
