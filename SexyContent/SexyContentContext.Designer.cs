4ffc5d2087a84542142e499043e1b75b4129fd59
BeniGemperle <Benjamin.Gemperle@2sic.com>
2014-10-31 11:22:33 +0100

    Updated Entity Framework Model with columns from Master-Branch

--- SexyContent/SexyContentContext.Designer.cs
+++ SexyContent/SexyContentContext.Designer.cs
@@ -1049,55 +1049,7 @@ namespace ToSic.SexyContent
         private global::System.Int32 _AppID;
         partial void OnAppIDChanging(global::System.Int32 value);
         partial void OnAppIDChanged();
-
-        /// <summary>
-        /// No Metadata Documentation available.
-        /// </summary>
-        [EdmScalarPropertyAttribute(EntityKeyProperty=false, IsNullable=false)]
-        [DataMemberAttribute()]
-        public global::System.Boolean PublishData
-        {
-            get
-            {
-                return _PublishData;
-            }
-            set
-            {
-                OnPublishDataChanging(value);
-                ReportPropertyChanging("PublishData");
-                _PublishData = StructuralObject.SetValidValue(value);
-                ReportPropertyChanged("PublishData");
-                OnPublishDataChanged();
-            }
-        }
-        private global::System.Boolean _PublishData;
-        partial void OnPublishDataChanging(global::System.Boolean value);
-        partial void OnPublishDataChanged();
     
-        /// <summary>
-        /// No Metadata Documentation available.
-        /// </summary>
-        [EdmScalarPropertyAttribute(EntityKeyProperty=false, IsNullable=false)]
-        [DataMemberAttribute()]
-        public global::System.String StreamsToPublish
-        {
-            get
-            {
-                return _StreamsToPublish;
-            }
-            set
-            {
-                OnStreamsToPublishChanging(value);
-                ReportPropertyChanging("StreamsToPublish");
-                _StreamsToPublish = StructuralObject.SetValidValue(value, false);
-                ReportPropertyChanged("StreamsToPublish");
-                OnStreamsToPublishChanged();
-            }
-        }
-        private global::System.String _StreamsToPublish;
-        partial void OnStreamsToPublishChanging(global::System.String value);
-        partial void OnStreamsToPublishChanged();
-
         /// <summary>
         /// No Metadata Documentation available.
         /// </summary>
@@ -1145,6 +1097,54 @@ namespace ToSic.SexyContent
         private global::System.String _ViewNameInUrl;
         partial void OnViewNameInUrlChanging(global::System.String value);
         partial void OnViewNameInUrlChanged();
+    
+        /// <summary>
+        /// No Metadata Documentation available.
+        /// </summary>
+        [EdmScalarPropertyAttribute(EntityKeyProperty=false, IsNullable=false)]
+        [DataMemberAttribute()]
+        public global::System.Boolean PublishData
+        {
+            get
+            {
+                return _PublishData;
+            }
+            set
+            {
+                OnPublishDataChanging(value);
+                ReportPropertyChanging("PublishData");
+                _PublishData = StructuralObject.SetValidValue(value);
+                ReportPropertyChanged("PublishData");
+                OnPublishDataChanged();
+            }
+        }
+        private global::System.Boolean _PublishData;
+        partial void OnPublishDataChanging(global::System.Boolean value);
+        partial void OnPublishDataChanged();
+    
+        /// <summary>
+        /// No Metadata Documentation available.
+        /// </summary>
+        [EdmScalarPropertyAttribute(EntityKeyProperty=false, IsNullable=false)]
+        [DataMemberAttribute()]
+        public global::System.String StreamsToPublish
+        {
+            get
+            {
+                return _StreamsToPublish;
+            }
+            set
+            {
+                OnStreamsToPublishChanging(value);
+                ReportPropertyChanging("StreamsToPublish");
+                _StreamsToPublish = StructuralObject.SetValidValue(value, false);
+                ReportPropertyChanged("StreamsToPublish");
+                OnStreamsToPublishChanged();
+            }
+        }
+        private global::System.String _StreamsToPublish;
+        partial void OnStreamsToPublishChanging(global::System.String value);
+        partial void OnStreamsToPublishChanged();
 
         #endregion
 
