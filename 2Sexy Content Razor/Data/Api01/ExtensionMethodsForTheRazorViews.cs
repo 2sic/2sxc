6c08f73d599975a64e68b4c3702788188cccf62e
iJugleboy <daniel.mettler@2sic.com>
2014-10-31 10:18:05 +0100

    created initial structures

---
+++ 2Sexy Content Razor/Data/Api01/ExtensionMethodsForTheRazorViews.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Web.UI;
+using ToSic.SexyContent.Data.Api01;
+
+namespace ToSic.SexyContent.Razor.Data.Api01
+{
+    /// <summary>
+    /// Place the data extension methods here for the razor-views.
+    /// It's important to keep this in an own namespace, to allow us to switch to a different API in the future without breaking the old one.
+    /// </summary>
+    public static class ExtensionMethodsForTheRazorViews
+    {
+        /// <summary>
+        /// this will simply return a correctly instantiated instance of the simple data controller.
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns>A instantiaced DataController which can Create/Update/Delete entities</returns>
+        public static SimpleDataController DataController(this SexyContentWebPage page)
+        {
+            // todo: ensure I have the language & appid
+            return new SimpleDataController(page.App.AppId, "");
+        }
+
+
+        public static bool Create(this SexyContentWebPage page, string contentTypeName, Dictionary<string, object> values)
+        {
+            return page.DataController().Create(contentTypeName, values);
+        }
+        public static bool Update(this SexyContentWebPage page, int entityId, Dictionary<string, object> values)
+        {
+            return page.DataController().Update(entityId, values);
+        }
+        public static bool Create(this SexyContentWebPage page, int entityId)
+        {
+            return page.DataController().Delete(entityId);
+        }
+    }
+
+}
---
+++ SexyContent/Data/Api01/SimpleDataController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Web;
+
+namespace ToSic.SexyContent.Data.Api01
+{
+    /// <summary>
+    /// This is a simple controller with some Create/Update/Delete commands. 
+    /// It needs to be initialized with AppId, DefaultLanguage and maybe ZoneId.
+    /// </summary>
+    public class SimpleDataController
+    {
+        public SimpleDataController(int appId, string defaultLanguageCode)
+        {
+            // todo:
+        }
+
+        // todo: develop methods "Create", "Update", "Delete"
+        public bool Create(string contentTypeName, Dictionary<string, object> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int entityId, Dictionary<string, object> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Guid entityGuid, Dictionary<string, object> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid entityGuid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
