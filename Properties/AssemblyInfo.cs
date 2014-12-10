29b9416e75a19268125074df1a9237cedb277f99
raphael-m <raaphii@gmail.com>
2014-12-10 09:40:15 +0100

    Increased version number (assembly only)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("6.5.1.0")]
-[assembly: AssemblyFileVersion("6.5.1.0")]
+[assembly: AssemblyVersion("6.5.1.1")]
+[assembly: AssemblyFileVersion("6.5.1.1")]
