a3da75628aa4fb1f4b8ebf0090f4e18fee71f57b
iJugleboy <daniel.mettler@2sic.com>
2015-05-13 11:16:13 +0200

    Refactored Serializer - moved to EAV

--- SexyContent/Serializers/Serializer.cs
+++ SexyContent/Serializers/Serializer.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Web;
-using Newtonsoft.Json;
 using ToSic.Eav;
-using ToSic.Eav.DataSources;
 using ToSic.SexyContent.EAVExtensions;
 
 namespace ToSic.SexyContent.Serializers
 {
-	public class Serializer
+	public class Serializer: Eav.Serializers.Serializer
 	{
 		private SexyContent Sxc { get; set; }
 
@@ -20,90 +15,34 @@ namespace ToSic.SexyContent.Serializers
 			Sxc = sexy;
 		}
 
-		/// <summary>
-		/// Returns an object that represents an IDataSource, but is serializable. If streamsToPublish is null, it will return all streams.
-		/// </summary>
-		public object Prepare(IDataSource source, IEnumerable<string> streamsToPublish = null)
-		{
-			var language = Thread.CurrentThread.CurrentCulture.Name;
-			
-			if (streamsToPublish == null)
-				streamsToPublish = source.Out.Select(p => p.Key);
 
-			var y = streamsToPublish.Where(k => source.Out.ContainsKey(k))
-				.ToDictionary(k => k, s => source.Out[s].List.Select(c => GetDictionaryFromEntity(c.Value, language))
-			);
-
-			return y;
-		}
-
-		/// <summary>
-		/// Returns an object that represents an IDataSource, but is serializable. If streamsToPublish is null, it will return all streams.
-		/// </summary>
-		public object Prepare(IDataSource source, string streamsToPublish)
-		{
-			return Prepare(source, streamsToPublish.Split(','));
-		}
-
-		/// <summary>
-		/// Return an object that represents an IDataStream, but is serializable
-		/// </summary>
-		public object Prepare(IDataStream stream)
-		{
-			// var language = Thread.CurrentThread.CurrentCulture.Name;
-		    return Prepare(stream.List);  // stream.List.Select(c => GetDictionaryFromEntity(c.Value, language));
-		}
+        #region Prepare statements expecting dynamic objects - extending the EAV Prepare variations
 
         /// <summary>
         /// Return an object that represents an IDataStream, but is serializable
         /// </summary>
-        public object Prepare(IDictionary<int, IEntity> list)
+        public IEnumerable<Dictionary<string, object>> Prepare(IEnumerable<dynamic> dynamicList)
         {
             var language = Thread.CurrentThread.CurrentCulture.Name;
-            return list.Select(c => GetDictionaryFromEntity(c.Value, language));
+            return dynamicList.Select(c => GetDictionaryFromEntity(c.Entity, language) as Dictionary<string, object>).ToList();
         }
+
 
         /// <summary>
         /// Return an object that represents an IDataStream, but is serializable
         /// </summary>
-        public object Prepare(IEnumerable<dynamic> dynamicList)
-        {
-            var language = Thread.CurrentThread.CurrentCulture.Name;
-            return dynamicList.Select(c => GetDictionaryFromEntity(c.Entity, language));
-        }
-        
-        /// <summary>
-        /// Return an object that represents an IDataStream, but is serializable
-        /// </summary>
-        public object Prepare(IEntity entity)
-        {
-            var language = Thread.CurrentThread.CurrentCulture.Name;
-            return GetDictionaryFromEntity(entity, language);
-        }
-
-        /// <summary>
-        /// Return an object that represents an IDataStream, but is serializable
-        /// </summary>
-        public object Prepare(DynamicEntity dynamicEntity)
+        public Dictionary<string, object> Prepare(DynamicEntity dynamicEntity)
         {
             return Prepare(dynamicEntity.Entity);
         }
+        #endregion
 
 
 
-		private Dictionary<string, object> GetDictionaryFromEntity(IEntity entity, string language)
+        public override Dictionary<string, object> GetDictionaryFromEntity(IEntity entity, string language)
 		{
-			var dynamicEntity = new DynamicEntity(entity, new[] { language }, Sxc);
-
-			// Convert DynamicEntity to dictionary
-			var dictionary = (from d in entity.Attributes select d.Value).ToDictionary(k => k.Name, v =>
-			{
-				bool propertyNotFound;
-				var value = dynamicEntity.GetEntityValue(v.Name, out propertyNotFound);
-				if (v.Type == "Entity" && value is List<DynamicEntity>)
-					return ((List<DynamicEntity>)value).Select(p => new { Id = p.EntityId, Title = p.EntityTitle });
-				return value;
-			}, StringComparer.OrdinalIgnoreCase);
+            // Do groundwork
+            var dictionary = base.GetDictionaryFromEntity(entity, language);
 
             // Add full presentation object if it has one...because there we need more than just id/title
 			if (entity is EntityInContentGroup && !dictionary.ContainsKey("Presentation"))
@@ -112,12 +51,6 @@ namespace ToSic.SexyContent.Serializers
 				if (entityInGroup.Presentation != null)
 					dictionary.Add("Presentation", GetDictionaryFromEntity(entityInGroup.Presentation, language));
 			}
-
-            // If ID is not used by the entity itself as an internal value, give the object a Id property as well since it's nicer to use in JS
-            // Note that for editing purposes or similar, there is always the extended info-object, so this is purely for "normal" working with the data
-            dictionary.Add((dictionary.ContainsKey("Id") ? "EntityId": "Id"), entity.EntityId);
-            if(!dictionary.ContainsKey("Title"))
-                dictionary.Add("Title", entity.GetBestValue("EntityTitle"));
 
             // Check w/2rm - this should only happen in edit mode...
 		    if (true) // todo: edit-mode-only...?
