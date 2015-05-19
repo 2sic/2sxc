8f4749179fa229966d5150e0357f1dcb80fc2d57
iJugleboy <daniel.mettler@2sic.com>
2015-05-19 09:44:24 +0200

    Include files which accidentally were forgotten till now

---
+++ SexyContent/EAV/PipelineDesigner/PipelineManagement.ascx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.UI;
+
+namespace ToSic.Eav.ManagementUI.EAV
+{
+	public partial class PipelineManagement : UserControl
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ SexyContent/EAV/Security/PermissionsDnnWrapper.ascx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Framework;
+
+namespace ToSic.SexyContent.EAV.Security
+{
+	public partial class PermissionsDnnWrapper : SexyControlEditBase
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			ServicesFramework.Instance.RequestAjaxAntiForgerySupport();
+		}
+	}
+}
