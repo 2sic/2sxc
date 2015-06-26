10a2274f6ddf552e99e7ead2382ca016de3d7d7f
iJugleboy <daniel.mettler@2sic.com>
2015-06-26 15:15:24 +0200

    add untracked file

---
+++ i18n/inpage-en.js
@@ -0,0 +1,14 @@
+{
+    "TemplatePicker": {
+        "AppPickerDefault": "<Choose App>",
+        "ContentTypePickerDefault": "<Choose Content Type>",
+        "Save": "Save and close",
+        "Cancel": "Cancel change",
+		"Close": "Close"
+    },
+    "Template": "Subj De",
+    "Category": "Cat de",
+    "Send": "Schick", 
+    "SendingMessage": "sendend...",
+    "ThanksMessage": "danke...2"
+}
