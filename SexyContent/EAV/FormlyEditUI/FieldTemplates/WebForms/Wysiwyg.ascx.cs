f0542c34afddb4533ae2c457e1c4019178c299f4
Raphael Müller <raaphii@gmail.com>
2015-08-26 13:37:03 +0200

    Working on Fomly EditUI

---
+++ SexyContent/EAV/FormlyEditUI/FieldTemplates/WebForms/Wysiwyg.ascx.cs
@@ -0,0 +1,20 @@
+﻿using DotNetNuke.Entities.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ToSic.SexyContent.EAV.FormlyEditUI.FieldTemplates.WebForms
+{
+	public partial class Wysiwyg : PortalModuleBase
+	{
+		public int PortalId { get; set; }
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			Texteditor1.ChooseMode = false;
+		}
+	}
+}
