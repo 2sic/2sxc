a80e667f97668f05f83bc1b758b43308a3e6d7e7
iJugleboy <daniel.mettler@2sic.com>
2015-09-20 21:25:28 +0200

    changed in-page language-loader to use the partial-loader and the languages delivered by the EavConfiguration

--- src/inpage/template-selector/template-selector.js
+++ src/inpage/template-selector/template-selector.js
@@ -1,21 +1,32 @@
 ﻿(function () {
-    var module = angular.module("2sxc.view", ["2sxc4ng", "pascalprecht.translate", "SxcInpageTemplates"]);
+    var module = angular.module("2sxc.view", [
+        "2sxc4ng",
+        "pascalprecht.translate",
+        "SxcInpageTemplates",
+        "EavConfiguration"
+    ]);
 
-    module.config(function ($translateProvider, AppInstanceId) {
+    module.config(function ($translateProvider, AppInstanceId, $translatePartialLoaderProvider, languages) {
         
         var globals = $2sxc(AppInstanceId).manage._manageInfo;
         
         // add translation table
         $translateProvider
             .preferredLanguage(globals.lang)
-            .useSanitizeValueStrategy("escape")
+            .useSanitizeValueStrategy("escapeParameters") // this is very important to allow html in the JSON files
             .fallbackLanguage(globals.fallbackLang)
-            .useLoaderCache(true)
-            .useStaticFilesLoader({
-                // todo: path...
-                prefix: globals.applicationRoot + "desktopmodules/tosic_sexycontent/dist/i18n/inpage-",
-                suffix: ".js"
-            });
+            .useLoader("$translatePartialLoader", {
+                urlTemplate: languages.i18nRoot + "{part}-{lang}.js"
+            })
+            .useLoaderCache(true);
+
+            //.useStaticFilesLoader({
+            //    // todo: path...
+            //    prefix: globals.applicationRoot + "desktopmodules/tosic_sexycontent/dist/i18n/inpage-",
+            //    suffix: ".js"
+        //})
+
+        $translatePartialLoaderProvider.addPart("inpage");
     });
 
     module.controller("TemplateSelectorCtrl", function($scope, $attrs, moduleApiService, $filter, $q, $window) {
