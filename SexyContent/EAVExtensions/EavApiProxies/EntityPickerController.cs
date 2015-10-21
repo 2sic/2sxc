5f3d09960596a58c3e10ceb75620119ebe811a54
unknown <2dm@NBK-DEV-01.2sic.com>
2015-10-21 18:17:25 +0200

    fixed #377 where the entity-picker didn't apply the type filter

--- SexyContent/EAVExtensions/EavApiProxies/EntityPickerController.cs
+++ SexyContent/EAVExtensions/EavApiProxies/EntityPickerController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Web.Http;
 using DotNetNuke.Security;
 using DotNetNuke.Web.Api;
 using ToSic.SexyContent.WebApi;
@@ -9,12 +8,11 @@ namespace ToSic.SexyContent.EAVExtensions.EavApiProxies
     [SupportedModules("2sxc,2sxc-app")]
     public class EntityPickerController : SxcApiController
     {
-        [HttpGet]
-		[DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
-		public IEnumerable<dynamic> GetAvailableEntities(string entityType = null, int? dimensionId = null)
-		{
-			return new ToSic.Eav.WebApi.EntityPickerController().GetAvailableEntities(App.AppId, entityType, dimensionId);
-		}
-
+ 		[DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
+        public IEnumerable<dynamic> GetAvailableEntities(int appId, string contentTypeName = null,
+            int? dimensionId = null)
+        {
+			return new Eav.WebApi.EntityPickerController().GetAvailableEntities(App.AppId, contentTypeName, dimensionId);
+        }
     }
 }
