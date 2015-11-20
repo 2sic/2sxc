b6da842cc884ecf53425cfef785fc17a6e17d785
unknown <2dm@NBK-DEV-01.2sic.com>
2015-11-20 18:15:26 +0100

    ensure that in-page scripts in edit-mode are always minified - fix #453

--- SexyContent/SexyViewContentOrApp.cs
+++ SexyContent/SexyViewContentOrApp.cs
@@ -37,7 +37,7 @@ namespace ToSic.SexyContent
 				if (UserMayEditThisModule)
 				{
 				    var root = "~/desktopmodules/tosic_sexycontent/";
-				    var ext = ".js"; // ".min.js";
+				    var ext = ".min.js";// ".js"; // ".min.js";
 
                     // add edit-mode CSS
                     ClientResourceManager.RegisterStyleSheet(Page, root + "edit.css");
