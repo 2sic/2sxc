f659de69d9fd717ab3389f05b8dfb94eef980752
unknown <2dm@NBK-DEV-01.2sic.com>
2015-11-21 16:07:23 +0100

    Fix #456 - ensure that replace works even if an item has a null-title

--- SexyContent/WebApi/ContentGroupController.cs
+++ SexyContent/WebApi/ContentGroupController.cs
@@ -72,7 +72,7 @@ namespace ToSic.SexyContent.WebApi
 
 
 	        var dataSource = App.Data[ct.Name];// attributeSetName];
-            var results = dataSource.List.ToDictionary(p => p.Value.EntityId, p => p.Value.GetBestValue("EntityTitle").ToString());
+            var results = dataSource.List.ToDictionary(p => p.Value.EntityId, p => (p.Value.GetBestValue("EntityTitle")??"").ToString());
 
             var selectedId = set[index]?.EntityId;
 
@@ -107,7 +107,7 @@ namespace ToSic.SexyContent.WebApi
                 Index = index,
 	            Id = c.EntityId,
 	            Guid = c.EntityGuid,
-	            Title = c.GetBestValue("EntityTitle").ToString()
+	            Title = (c.GetBestValue("EntityTitle")??"").ToString()
 	        }).ToList();
 
 	        return list;
