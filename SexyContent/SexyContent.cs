d7da7943751bd228bf9f6d43446204a46c21b68f
Raphael Müller <raaphii@gmail.com>
2015-12-01 13:42:38 +0100

    Increased version numbers to 08.00.07

--- SexyContent/SexyContent.cs
+++ SexyContent/SexyContent.cs
@@ -46,7 +46,7 @@ namespace ToSic.SexyContent
     {
         #region Constants
 
-        public const string ModuleVersion = "08.00.06"; // always the newest version
+        public const string ModuleVersion = "08.00.07"; // always the newest version
         public const string TemplateID = "TemplateID";
 		public const string ContentGroupGuidString = "ToSIC_SexyContent_ContentGroupGuid";
         public const string AppIDString = "AppId";
