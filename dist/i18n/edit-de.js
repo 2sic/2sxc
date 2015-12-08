3363c0445f35b4a8cebf10ffe9565d48ee51e3bf
Raphael Müller <raaphii@gmail.com>
2015-12-08 23:23:57 +0100

    Added german dist files

---
+++ dist/i18n/edit-de.js
@@ -0,0 +1,68 @@
+﻿{
+  "Button": {
+    "Save": "Speichern",
+    "Cancel": "Abbrechen",
+    "MoreOptions": "Mehr Optionen",
+    "SaveAndKeepOpen": "Speichern und fortsetzen"
+
+  },
+  "Status": {
+    "Published": "Veröffentlicht",
+    "Unpublished": "Nicht veröffentlicht / Entwurf"
+  },
+  "Message": {
+    "Saving": "Speichern...",
+    "Saved": "Gespeichert",
+    "ErrorWhileSaving":  "Während dem Speichern ist ein Fehler aufgetreten.",
+    "WillPublish": "Wird veröffentlicht beim Speichern",
+    "WontPublish": "Wird nicht veröffentlicht beim Speichern",
+    "PleaseCreateDefLang": "Eintrag bitte zuerst in der Hauptsprache bearbeiten.",
+    "ExitOk": "Wirklich beenden?"
+  },
+  "Errors": {
+    "UnclearError": "Da lief was falsch - vielleicht funktioniert ein Teil, vielleicht auch nicht. Entschuldigung :(",
+    "InnerControlMustOverride": "Inner control must override this function.",
+    "UnsavedChanges": "Du hast nicht gespeicherte Änderungen.",
+    "DefLangNotFound": "Default language value not found, but found multiple values - can't handle editing for"
+  },
+  "General": {
+  },
+  "EditEntity": {
+    "DefaultTitle": "Element bearbeiten",
+    "SlotUsedtrue": "Dieses Element kann bearbeitet werden. Klicke um die Bearbeitung zu sperren - eingegebene Werte werden dann mit den Standardwerten ersetzt.",
+    "SlotUsedfalse": "Dieses Element kann nicht bearbeitet werden und zeigt die Standardwerte an. Klicke um das Element freizuschalten."
+  },
+  "FieldType": {
+    "Entity": {
+      "Choose": "-- Element hinzufügen --",
+      "New": "-- Neues Element erstellen --",
+      "EntityNotFound": "(Element nicht gefunden)",
+      "DragMove": "Ziehen um die Liste neu anzuordnen",
+      "Edit": "Dieses Element bearbeiten",
+      "Remove": "Aus Liste entfernen"
+    }
+  },
+  "LangMenu": {
+    "UseDefault": "auto (Standardsprache)",
+    "In": "in {{languages}}",
+    "EditableIn": "Editierbar in {{languages}}",
+    "AlsoUsedIn": ", auch verwendet in {{more}}",
+    "NotImplemented": "Diese Aktion kann noch nicht verwendet werden.",
+    "CopyNotPossible": "Kopieren nicht möglich, das Feld ist gesperrt.",
+
+    "Unlink": "Übersetzen (abkoppeln)",
+    "LinkDefault": "Mit Standardsprache koppeln",
+    "GoogleTranslate": "Online Übersetzung (Google)",
+    "Copy": "Kopieren von",
+    "Use": "Verwenden von",
+    "Share": "Teilen mit"
+  },
+  "LangWrapper": {
+    "CreateValueInDefFirst": "Bitte das Feld '{{fieldname}}' zuerst in der Standardsprache befüllen vor dem Übersetzen."
+
+  },
+  "Dialog1": {
+  },
+  "dialog2": {
+  }
+}
