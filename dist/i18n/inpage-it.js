0b0f8599884a8269b351dcfd33c0ecf1e67aee27
iJugleboy <daniel.mettler@2sic.com>
2016-01-11 23:39:29 +0100

    i18n - IT (Italian) + TinyMCE Lib i18n

---
+++ dist/i18n/inpage-it.js
@@ -0,0 +1,27 @@
+﻿{
+  "TemplatePicker": {
+    "AppPickerDefault": "&lt;Scegli app&gt;",
+    "ContentTypePickerDefault": "&lt;Scegli tipo contenuto&gt;",
+    "Save": "Salva e chiudi",
+    "Cancel": "Cancella cambiamenti",
+    "Close": "Chiudi"
+  },
+  "Toolbar": {
+    "Edit": "modifica",
+    "New": "nuovo",
+    "AddDemo": "aggiungi demo",
+    "Replace": "sostiutisci",
+    "Published": "pubblicato",
+    "MoveUp": "muovi su",
+    "MoveDown": "muovi giù",
+    "Sort": "ordina / gestisci lista",
+    "Remove": "rimuovi",
+    "MoreActions": "altre azioni"
+  },
+  "EditFormTitle": {
+    "Content": "Contenuti",
+    "Presentation": "Presentation Settings",
+    "ListContent": "List Header Content",
+    "ListPresentation":  "List Presentation Settings"
+  }
+}
