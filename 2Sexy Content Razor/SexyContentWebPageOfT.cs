712cf515980a78aeee6f17aeee1853b6ba324583
iJugleboy <daniel.mettler@2sic.com>
2016-01-19 20:19:55 +0100

    Created generic type for the SexyContentWebPage<T> which the new razor engine expects to fix #582

---
+++ 2Sexy Content Razor/SexyContentWebPageOfT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToSic.SexyContent.Razor
+{
+    /// <summary>
+    /// this is just a generic variation of SexyContentWebPage
+    /// We need it for the Razor Engine - which in V5+ explicitly expects a generic type
+    /// We have no need for the internal type, so there is no code except for the inheritance
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+
+    public abstract class SexyContentWebPage<T>:SexyContentWebPage
+    {
+
+    }
+}
