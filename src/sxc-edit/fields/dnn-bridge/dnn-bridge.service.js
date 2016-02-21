559471ec0e81870e846c09bed3a5f66605bfc76c
unknown <2dm@NBK-DEV-01.2sic.com>
2016-02-21 06:31:54 +0100

    Trying to solve #656 but can't continue yet, because I can't reproduce error

--- src/sxc-edit/fields/dnn-bridge/dnn-bridge.service.js
+++ src/sxc-edit/fields/dnn-bridge/dnn-bridge.service.js
@@ -1,7 +1,7 @@
 // This is the service which allows opening dnn-bridge dialogs and processes the results
 
 angular.module("sxcFieldTemplates")
-    .factory("dnnBridgeSvc", function($modal, $http, eavConfig, sxc) {
+    .factory("dnnBridgeSvc", function ($modal, $http, promiseToastr, eavConfig, sxc) {
         var svc = {};
         svc.open = function open(type, oldValue, params, callback) {
             var template = type === "pagepicker" ? "pagepicker" : "filemanager";
@@ -38,8 +38,13 @@ angular.module("sxcFieldTemplates")
         // convert the url to a Id-code
         svc.convertPathToId = function(path, type) {
             var pathWithoutVersion = path.replace(/\?ver=[0-9\-]*$/gi, "");
-            var promise = $http.get("dnn/Hyperlink/GetFileByPath?relativePath=" + encodeURIComponent(pathWithoutVersion));
-            return promise;
+            // todo: working on https://github.com/2sic/2sxc/issues/656 but can't reproduce error
+            // this is why I tried ignoreErrors and promisetoaster, but atm there is nothing to work on...
+            var promise = $http.get("dnn/Hyperlink/GetFileByPath?relativePath=" + encodeURIComponent(pathWithoutVersion),
+            {
+                //ignoreErrors: true
+            });
+            return promiseToastr(promise, "Edit.Field.Hyperlink.Message.Loading", "Edit.Field.Hyperlink.Message.Ok", "Edit.Field.Hyperlink.Message.Error", 0, 0, 1000);
         };
 
         // handle short-ID links like file:17
