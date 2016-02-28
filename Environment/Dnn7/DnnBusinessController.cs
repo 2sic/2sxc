6de845f1e13677281237bf637a993aa55cb6b384
unknown <2dm@NBK-DEV-01.2sic.com>
2016-02-28 07:56:31 +0100

    Refactored the DNN Business Controller out of the SexyContent object - fix #698

---
+++ Environment/Dnn7/DnnBusinessController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Services.Search.Entities;
+using ToSic.SexyContent.Search;
+
+namespace ToSic.SexyContent.Environment.Dnn7
+{
+    public class DnnBusinessController : ModuleSearchBase, IUpgradeable
+    {
+
+        #region DNN Interface Members - search, upgrade
+
+        /// <summary>
+        /// Constructor overload for DotNetNuke
+        /// (BusinessControllerClass needs parameterless constructor)
+        /// </summary>
+        public DnnBusinessController()
+        {
+        } //: this(0, 0) { }
+
+
+        public override IList<SearchDocument> GetModifiedSearchDocuments(ModuleInfo moduleInfo, DateTime beginDate)
+        {
+            try
+            {
+                return new SearchController().GetModifiedSearchDocuments(moduleInfo, beginDate);
+            }
+            catch (Exception e)
+            {
+                throw new SearchIndexException(moduleInfo, e);
+            }
+        }
+
+        /// <summary>
+        /// This is part of the IUpgradeable of DNN
+        /// </summary>
+        /// <param name="version"></param>
+        /// <returns></returns>
+        public string UpgradeModule(string version)
+        {
+            return SexyContentModuleUpgrade.UpgradeModule(version);
+        }
+
+        #endregion
+    }
+}
--- SexyContent/SexyContent.cs
+++ SexyContent/SexyContent.cs
@@ -20,7 +20,7 @@ namespace ToSic.SexyContent
     /// it would be hard to get anything done .
     /// Note that it also adds the current-user to the state, so that the system can log data-changes to this user
     /// </summary>
-    public class SexyContent : ModuleSearchBase, IUpgradeable
+    public class SexyContent 
     {
         #region Properties
         /// <summary>
@@ -56,13 +56,6 @@ namespace ToSic.SexyContent
 
 
         #region SexyContent Constructor
-
-        /// <summary>
-        /// Constructor overload for DotNetNuke
-        /// (BusinessControllerClass needs parameterless constructor)
-        /// </summary>
-        public SexyContent() : this(0, 0, true) {}
-
 
 
         /// <summary>
@@ -114,33 +107,40 @@ namespace ToSic.SexyContent
 
         #endregion
 
+        #region all dnn interface members moved to DnnBusinessController
+        //     #region DNN Interface Members - search, upgrade
 
-        #region DNN Interface Members - search, upgrade
+        //     /// <summary>
+        //     /// Constructor overload for DotNetNuke
+        //     /// (BusinessControllerClass needs parameterless constructor)
+        //     /// </summary>
+        //     public SexyContent() : this(0, 0) { }
 
-        public override IList<SearchDocument> GetModifiedSearchDocuments(ModuleInfo moduleInfo, DateTime beginDate)
-        {
-            try
-            {
-                return new SearchController().GetModifiedSearchDocuments(moduleInfo, beginDate);
-            }
-            catch (Exception e)
-            {
-                throw new SearchIndexException(moduleInfo, e);
-            }
-        }
 
-        /// <summary>
-        /// This is part of the IUpgradeable of DNN
-        /// </summary>
-        /// <param name="version"></param>
-        /// <returns></returns>
-        public string UpgradeModule(string version)
-        {
-			return SexyContentModuleUpgrade.UpgradeModule(version);
-        }
+        //     public override IList<SearchDocument> GetModifiedSearchDocuments(ModuleInfo moduleInfo, DateTime beginDate)
+        //     {
+        //         try
+        //         {
+        //             return new SearchController().GetModifiedSearchDocuments(moduleInfo, beginDate);
+        //         }
+        //         catch (Exception e)
+        //         {
+        //             throw new SearchIndexException(moduleInfo, e);
+        //         }
+        //     }
 
+        //     /// <summary>
+        //     /// This is part of the IUpgradeable of DNN
+        //     /// </summary>
+        //     /// <param name="version"></param>
+        //     /// <returns></returns>
+        //     public string UpgradeModule(string version)
+        //     {
+        //return SexyContentModuleUpgrade.UpgradeModule(version);
+        //     }
+
+        //     #endregion
         #endregion
-
 
 
         // --------------------------------------------------------------------------------------
