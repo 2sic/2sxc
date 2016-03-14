f259d937c9efa91f6e3194b96233f6e520de98b2
Raphael Müller <raaphii@gmail.com>
2016-03-14 14:26:38 +0100

    Fixed version number in AssemblyInfo, fixed upgrade xml (node Dimensions is required)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("8.3.4.*")]
+[assembly: AssemblyVersion("08.03.05.*")]
 //[assembly: AssemblyFileVersion("8.2.*")]
