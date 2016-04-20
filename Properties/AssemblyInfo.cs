91128447aab6d42fe57e86e92d96bedb1b359b10
unknown <2dm@NBK-DEV-04.2sic.com>
2016-04-21 00:21:47 +0200

    Updated version to 08.04.01

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("08.04.00.*")]
+[assembly: AssemblyVersion("08.04.01.*")]
 //[assembly: AssemblyFileVersion("8.2.*")]
