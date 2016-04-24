18f4aab0c79e5f95e53d0fb68ab1867fbcb7444d
unknown <2dm@NBK-DEV-04.2sic.com>
2016-04-24 15:00:31 +0200

    Ensured that end-users get more detailed error infos like when deleting...

--- WebApi/SxcWebApiExceptionHandling.cs
+++ WebApi/SxcWebApiExceptionHandling.cs
@@ -13,11 +13,24 @@ namespace ToSic.SexyContent.WebApi
     {
         public override void OnException(HttpActionExecutedContext context)
         {
-            var exception = new Exception("An error occurred while executing the request. Please consult the event log for details.", context.Exception);
+            var exception = context.Exception;
+            var exWrapper = new Exception("An error occurred while executing the request. Please consult the event log for details.", context.Exception);
             // Log errors to DNN log
-            DotNetNuke.Services.Exceptions.Exceptions.LogException(exception);
+            DotNetNuke.Services.Exceptions.Exceptions.LogException(exWrapper);
 
-            throw exception;
+            // if(SxcInstance)
+            // throw exception;
+
+            // special manual exception maker, because otherwise IIS at runtime removes all messages
+            // without this, debug-infos like what entity is causing the problem will not be shown to the client
+            context.Response = context.Request.CreateErrorResponse(HttpStatusCode.BadRequest,
+                                                               "Bad Request",
+                                                               context.Exception);
+            var httpError = (HttpError)((ObjectContent<HttpError>)context.Response.Content).Value;
+            if (!httpError.ContainsKey("ExceptionType"))
+                httpError.Add("ExceptionType", exception.GetType().FullName);
+            if (!httpError.ContainsKey("ExceptionMessage"))
+                httpError.Add("ExceptionMessage", exception.Message);
         }
     }
 }
