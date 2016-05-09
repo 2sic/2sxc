5c6ecb74caa9aad8188776cc915374c18b8cb0ea
unknown <2dm@NBK-DEV-04.2sic.com>
2016-05-09 13:10:59 +0200

    Minor bugfix

--- Environment/Dnn7/RenderingHelpers.cs
+++ Environment/Dnn7/RenderingHelpers.cs
@@ -205,7 +205,7 @@ namespace ToSic.SexyContent.Environment.Dnn7
 
         public ClientInfosLanguages(PortalSettings ps, int zoneId)
         {
-            Current = ps.CultureCode.ToLower();
+            Current = System.Threading.Thread.CurrentThread.CurrentCulture.Name.ToLower(); // 2016-05-09 had to ignore the Portalsettings, as that is wrong ps.CultureCode.ToLower();
             Primary = ps.DefaultLanguage.ToLower();
             All = ZoneHelpers.GetCulturesWithActiveState(ps.PortalId, zoneId)
                     .Where(c => c.Active)
