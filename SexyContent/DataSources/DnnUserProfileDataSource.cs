df5c8a9463e63caf243e4cf6caa5e158078ec089
BeniGemperle <Benjamin.Gemperle@2sic.com>
2016-06-28 14:18:02 +0200

    Extended DnnUserProfileDataSource with UserIds-property changed implementation of Properties so all desired property-names must be specified

--- SexyContent/DataSources/DnnUserProfileDataSource.cs
+++ SexyContent/DataSources/DnnUserProfileDataSource.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using DotNetNuke.Entities.Portals;
 using DotNetNuke.Entities.Users;
@@ -14,14 +16,22 @@ namespace ToSic.SexyContent.DataSources
 		private const string PropertiesKey = "Properties";
 		private const string TitleFieldKey = "TitleField";
 		private const string ContentTypeKey = "ContentType";
-		private const string PropertiesDefaultKeyToken = "[Settings:Properties]";
+		private const string UserIdsKey = "UserIds";
+		private const string UserIdsDefaultKeyToken = "[Settings:UserIds||disabled]";
+		private const string PropertiesDefaultKeyToken = "[Settings:Properties||DisplayName,Email,FirstName,LastName,Username]";
 		private const string EntityTitleDefaultKeyToken = "[Settings:TitleFieldName||DisplayName]";
 		private const string ContentTypeDefaultToken = "[Settings:ContentTypeName||DnnUserInfo]";
 
-		public IEnumerable<string> Properties
+		public string UserIds
 		{
-			get { return Configuration[PropertiesKey].Split(',').Select(p => p.Trim()); }
-			set { Configuration[PropertiesKey] = string.Join(",", value); }
+			get { return Configuration[UserIdsKey]; }
+			set { Configuration[UserIdsKey] = value; }
+		}
+
+		public string Properties
+		{
+			get { return Configuration[PropertiesKey]; }
+			set { Configuration[PropertiesKey] = value; }
 		}
 
 		/// <summary>
@@ -47,6 +57,7 @@ namespace ToSic.SexyContent.DataSources
 		public DnnUserProfileDataSource()
 		{
 			Out.Add(Constants.DefaultStreamName, new DataStream(this, Constants.DefaultStreamName, null, GetList));
+			Configuration.Add(UserIdsKey, UserIdsDefaultKeyToken);
 			Configuration.Add(PropertiesKey, PropertiesDefaultKeyToken);
 			Configuration.Add(ContentTypeKey, ContentTypeDefaultToken);
 			Configuration.Add(TitleFieldKey, EntityTitleDefaultKeyToken);
@@ -56,27 +67,55 @@ namespace ToSic.SexyContent.DataSources
 		{
 			EnsureConfigurationIsLoaded();
 
-			// read user Profiles
+			var properties = Properties.Split(',').Select(p => p.Trim()).ToArray();
 			var portalId = PortalSettings.Current.PortalId;
-			var users = UserController.GetUsers(portalId);
+
+			// read all user Profiles
+			ArrayList users;
+			if (UserIds == "disabled")
+				users = UserController.GetUsers(portalId);
+			// read user Profiles of specified UserIds
+			else
+			{
+				var userIds = UserIds.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+				users = new ArrayList();
+				foreach (var user in userIds.Select(userId => UserController.GetUserById(portalId, userId)))
+					users.Add(user);
+			}
 
 			// convert Profiles to Entities
 			var result = new List<IEntity>();
 			foreach (UserInfo user in users)
 			{
-				// add default Profile-Properties
-				var values = new Dictionary<string, object>
+				// add Profile-Properties
+				var values = new Dictionary<string, object>();
+				foreach (var property in properties)
 				{
-					{"DisplayName", user.DisplayName},
-					{"Email", user.Email},
-					{"FirstName", user.FirstName},
-					{"LastName", user.LastName},
-					{"Username", user.Username}
-				};
+					string value;
+					switch (property)
+					{
+						case "DisplayName":
+							value = user.DisplayName;
+							break;
+						case "Email":
+							value = user.Email;
+							break;
+						case "FirstName":
+							value = user.FirstName;
+							break;
+						case "LastName":
+							value = user.LastName;
+							break;
+						case "Username":
+							value = user.Username;
+							break;
+						default:
+							value = user.Profile.GetPropertyValue(property);
+							break;
+					}
 
-				// add custom Profile-Properties
-				foreach (var property in Properties.Where(p => !values.ContainsKey(p)))
-					values.Add(property, user.Profile.GetPropertyValue(property));
+					values.Add(property, value);
+				}
 
 				// create Entity and add to result
 				var entity = new Eav.Data.Entity(user.UserID, ContentType, values, TitleField);
