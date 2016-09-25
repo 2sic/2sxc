25a72a856893a770a7a45c90c52e707af48a0de5
iJungleboy <daniel.mettler@2sic.com>
2016-09-25 08:17:29 +0200

    Upped version to 8.5.5 to uncache more and prepare for release next week

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("08.05.04.*")]
+[assembly: AssemblyVersion("08.05.05.*")]
 //[assembly: AssemblyFileVersion("8.2.*")]
