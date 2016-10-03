f480e97c0e6003bdc115cdea8efc4f32ffea7e98
iJungleboy <daniel.mettler@2sic.com>
2016-10-03 11:26:21 +0200

    Increased version to 08.05.06 for release of hotfix within 48h...

--- Js/2sxc.api.js
+++ Js/2sxc.api.js
@@ -251,7 +251,7 @@
 
     $2sxc._controllers = {};
     $2sxc.metaName = "The 2sxc Controller object";
-    $2sxc.metaVersion = "08.05.05";
+    $2sxc.metaVersion = "08.05.06";
     $2sxc.beta = {};
     $2sxc._data = {};
     
