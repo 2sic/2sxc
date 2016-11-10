c41e70c237cd0198ea7d85dd755e828a902c7ac3
iJungleboy <daniel.mettler@2sic.com>
2016-11-10 23:50:10 +0100

    seems to work - code now in beta...

--- Edit/InPageEditingSystem/IInPageEditingSystem.cs
+++ Edit/InPageEditingSystem/IInPageEditingSystem.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 
 namespace ToSic.SexyContent.Edit.InPageEditingSystem
 {
@@ -13,6 +14,10 @@ namespace ToSic.SexyContent.Edit.InPageEditingSystem
             object toolbar = null,
             object settings = null);
         
-        HtmlString ContextAttributes(DynamicEntity target, string dontRelyOnParameterOrder = Constants.RandomProtectionParameter, string field = null, string contentType = null);
+        HtmlString ContextAttributes(DynamicEntity target, 
+            string dontRelyOnParameterOrder = Constants.RandomProtectionParameter, 
+            string field = null, 
+            string contentType = null,
+            Guid? newGuid = null);
     }
 }
