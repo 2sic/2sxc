0e1be0a90a55df3e15973091086ee9b604ea74b3
iJungleboy <daniel.mettler@2sic.com>
2016-11-13 09:44:52 +0100

    temp...

---
+++ WebApi/Dnn/ModuleServiceControllerForPre741.cs
@@ -0,0 +1,139 @@
+﻿#region Explanation
+/* This is a clone of https://github.com/dnnsoftware/Dnn.Platform/blob/fd225b8de07042837f7473cd49fba13de42a3cc0/DNN%20Platform/DotNetNuke.Web/InternalServices/ModuleServiceController.cs
+ * because delete isn't included in dnn 7.0 - 7.4.0
+ * */
+#endregion
+
+#region Copyright
+
+// DotNetNuke® - http://www.dotnetnuke.com
+// Copyright (c) 2002-2016
+// by DotNetNuke Corporation
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+
+#endregion
+
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Portals;
+//using DotNetNuke.Instrumentation;
+using DotNetNuke.Web.Api;
+using DotNetNuke.Web.Api.Internal;
+
+namespace ToSic.SexyContent.WebApi.Dnn // DotNetNuke.Web.InternalServices
+{
+    [DnnAuthorize]
+    [DnnExceptionFilter]
+    public class ModuleServiceController : DnnApiController
+    {
+        //private static readonly ILog Logger = LoggerSource.Instance.GetLogger(typeof(ModuleServiceController));
+        //public class MoveModuleDTO
+        //{
+        //    public int ModuleId { get; set; }
+        //    public int ModuleOrder { get; set; }
+        //    public string Pane { get; set; }
+        //    public int TabId { get; set; }
+        //}
+
+        public class DeleteModuleDto
+        {
+            public int ModuleId { get; set; }
+            public int TabId { get; set; }
+            public bool SoftDelete { get; set; }
+        }
+
+        //[HttpGet]
+        //[DnnAuthorize(StaticRoles = "Registered Users")]
+        //public HttpResponseMessage GetModuleShareable(int moduleId, int tabId, int portalId = -1)
+        //{
+        //    var requiresWarning = false;
+        //    if (portalId == -1)
+        //    {
+        //        var portalDict = PortalController.GetPortalDictionary();
+        //        portalId = portalDict[tabId];
+        //    }
+
+        //    DesktopModuleInfo desktopModule;
+        //    if (tabId < 0)
+        //    {
+        //        desktopModule = DesktopModuleController.GetDesktopModule(moduleId, portalId);
+        //    }
+        //    else
+        //    {
+        //        var moduleInfo = ModuleController.Instance.GetModule(moduleId, tabId, false);
+
+        //        desktopModule = moduleInfo.DesktopModule;
+
+        //        requiresWarning = moduleInfo.PortalID != PortalSettings.PortalId && desktopModule.Shareable == ModuleSharing.Unknown;
+        //    }
+
+        //    if (desktopModule == null)
+        //    {
+        //        var message = string.Format("Cannot find module ID {0} (tab ID {1}, portal ID {2})", moduleId, tabId, portalId);
+        //        //Logger.Error(message);
+        //        return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, message);
+        //    }
+
+        //    return Request.CreateResponse(HttpStatusCode.OK, new { Shareable = desktopModule.Shareable.ToString(), RequiresWarning = requiresWarning });
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //[DnnPageEditor]
+        //public HttpResponseMessage MoveModule(MoveModuleDTO postData)
+        //{
+        //    var moduleOrder = postData.ModuleOrder;
+        //    if (moduleOrder > 0)
+        //    {
+        //        //DNN-7099: the deleted modules won't show in page, so when the module index calculated from client, it will lost the 
+        //        //index count of deleted modules and will cause order issue.
+        //        var deletedModules = ModuleController.Instance.GetTabModules(postData.TabId).Values.Where(m => m.IsDeleted);
+        //        foreach (var module in deletedModules)
+        //        {
+        //            if (module.ModuleOrder < moduleOrder)
+        //            {
+        //                moduleOrder += 2;
+        //            }
+        //        }
+        //    }
+        //    ModuleController.Instance.UpdateModuleOrder(postData.TabId, postData.ModuleId, moduleOrder, postData.Pane);
+        //    ModuleController.Instance.UpdateTabModuleOrder(postData.TabId);
+
+        //    return Request.CreateResponse(HttpStatusCode.OK);
+        //}
+
+        /// <summary>
+        /// Web method that deletes a tab module.
+        /// </summary>
+        /// <remarks>This has been introduced for integration testing purpuses.</remarks>
+        /// <param name="deleteModuleDto">delete module dto</param>
+        /// <returns>Http response message</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [DnnAuthorize(StaticRoles = "Administrators")]
+        public HttpResponseMessage DeleteModule(DeleteModuleDto deleteModuleDto)
+        {
+            DotNetNuke.Entities.Modules.
+            ModuleController .Instance.DeleteTabModule(deleteModuleDto.TabId, deleteModuleDto.ModuleId, deleteModuleDto.SoftDelete);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
