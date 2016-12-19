980cce3fddfac889d715c4b330d22780e70d0fe0
iJungleboy <daniel.mettler@2sic.com>
2016-12-19 22:44:29 +0100

    improve error on webapi - close https://github.com/2sic/2sxc/issues/991

--- WebApi/AppApiControllerSelector.cs
+++ WebApi/AppApiControllerSelector.cs
@@ -36,39 +36,39 @@ namespace ToSic.SexyContent.WebApi
             var module = request.FindModuleInfo();
 
             // Handle the app-api queries
-            if (routeData.Route.RouteTemplate.Contains("DesktopModules/2sxc/API/app-api/") && module.DesktopModule.ModuleName == "2sxc-app")
+            if (!routeData.Route.RouteTemplate.Contains("DesktopModules/2sxc/API/app-api/") ||
+                module.DesktopModule.ModuleName != "2sxc-app")
+                return PreviousSelector.SelectController(request);
+
+            var controllerTypeName = routeData.Values["controller"] + "Controller";
+
+            try
             {
-                var controllerTypeName = routeData.Values["controller"] + "Controller";
+                var portalSettings = PortalSettings.Current;
+                var sexy = Helpers.GetSxcOfApiRequest(request);// request.GetSxcOfModuleContext();
 
-                try
+                // previously we assumed that there is a sub-folder with a future-app-id, but 2015-05-15 decided it's probably not worth trying, because each request currently needs tokens anyhow
+                // if ((string) routeData.Values["appFolder"] != "auto-detect-app" && (string) routeData.Values["appFolder"] != sexy.App.Folder)
+                //    throw new HttpException("AppFolder was not correct - was " + routeData.Values["appFolder"] + " but should be " + sexy.App.Folder);
+
+                var controllerPath = Path.Combine(AppHelpers.AppBasePath(portalSettings), sexy.App.Folder,
+                    "Api/" + controllerTypeName + ".cs");
+
+                if (File.Exists(HostingEnvironment.MapPath(controllerPath)))
                 {
-                    var portalSettings = PortalSettings.Current;
-                    var sexy = Helpers.GetSxcOfApiRequest(request);// request.GetSxcOfModuleContext();
-
-                    // previously we assumed that there is a sub-folder with a future-app-id, but 2015-05-15 decided it's probably not worth trying, because each request currently needs tokens anyhow
-                    // if ((string) routeData.Values["appFolder"] != "auto-detect-app" && (string) routeData.Values["appFolder"] != sexy.App.Folder)
-                    //    throw new HttpException("AppFolder was not correct - was " + routeData.Values["appFolder"] + " but should be " + sexy.App.Folder);
-
-                    var controllerPath = Path.Combine(AppHelpers.AppBasePath(portalSettings), sexy.App.Folder,
-                        "Api/" + controllerTypeName + ".cs");
-
-                    if (File.Exists(HostingEnvironment.MapPath(controllerPath)))
-                    {
-                        var assembly = BuildManager.GetCompiledAssembly(controllerPath);
-                        var type = assembly.GetType(controllerTypeName);
-                        return new HttpControllerDescriptor(_config, controllerTypeName, type);
-                    }
+                    var assembly = BuildManager.GetCompiledAssembly(controllerPath);
+                    var type = assembly.GetType(controllerTypeName);
+                    return new HttpControllerDescriptor(_config, controllerTypeName, type);
                 }
-                catch (Exception e)
-                {
-                    var exception = new Exception("Error while selecting / compiling a controller for the request. See the inner exception for more details.", e);
-                    DotNetNuke.Services.Exceptions.Exceptions.LogException(exception);
-                }
-
-                throw new HttpResponseException(request.CreateErrorResponse(HttpStatusCode.NotFound, "Controller " + controllerTypeName + " not found in app."));
             }
-            return PreviousSelector.SelectController(request);
+            catch (Exception e)
+            {
+                var exception = new Exception("Error while selecting / compiling a controller for the request. Pls check the code. See the inner exception for more details.", e);
+                DotNetNuke.Services.Exceptions.Exceptions.LogException(exception);
+                throw new HttpResponseException(request.CreateErrorResponse(HttpStatusCode.InternalServerError, exception.Message, e));
+            }
 
+            throw new HttpResponseException(request.CreateErrorResponse(HttpStatusCode.NotFound, "Controller " + controllerTypeName + " not found in app."));
         }
     }
 }
