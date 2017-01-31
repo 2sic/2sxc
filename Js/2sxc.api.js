fcc8553a4dd61cece52ecb6480b360c2a9912710
Raphael Müller <raaphii@gmail.com>
2017-01-31 08:05:44 +0100

    Use incredible high z-index values to defeat DNN9 bar - fixes #1070

--- Js/2sxc.api.js
+++ Js/2sxc.api.js
@@ -262,8 +262,8 @@
     $2sxc.totalPopup = {
         open: function openTotalPopup(url, callback) {
             // count parents to see how high the z-index needs to be
-            var z = 1500, p = window;
-            while (p !== window.top && z < 1600) {
+            var z = 10000000, p = window; // Needs at least 10000000 to be on top of the DNN9 bar
+            while (p !== window.top && z < 10000100) {
                 z++;
                 p = p.parent;
             }
