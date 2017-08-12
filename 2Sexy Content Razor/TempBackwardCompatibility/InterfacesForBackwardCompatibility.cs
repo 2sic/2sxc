9c29b9eb086ebba0ca78b58f2302cf7491e4c552
iJungleboy <daniel.mettler@2sic.com>
2017-08-12 15:56:14 +0200

    preparing to release 09.04 pre 1

--- 2Sexy Content Razor/TempBackwardCompatibility/InterfacesForBackwardCompatibility.cs
+++ 2Sexy Content Razor/TempBackwardCompatibility/InterfacesForBackwardCompatibility.cs
@@ -3,40 +3,40 @@
 // todo
 namespace ToSic.Eav
 {
-    /// <summary>
-    /// old interface name
-    /// </summary>
-    [Obsolete("use the one in ToSic.Eav.Interfaces instead")]
-    public interface IAttribute : Interfaces.IAttribute { }
+    ///// <summary>
+    ///// old interface name
+    ///// </summary>
+    //[Obsolete("use the one in ToSic.Eav.Interfaces instead")]
+    //public interface IAttribute : Interfaces.IAttribute { }
 
-    /// <summary>
-    /// old interface name
-    /// </summary>
-    [Obsolete("use the one in ToSic.Eav.Interfaces instead")]
-    public interface IEntity : Interfaces.IEntity { }
+    ///// <summary>
+    ///// old interface name
+    ///// </summary>
+    //[Obsolete("use the one in ToSic.Eav.Interfaces instead")]
+    //public interface IEntity : Interfaces.IEntity { }
 
-    /// <summary>
-    /// old interface name
-    /// </summary>
-    [Obsolete("use the one in ToSic.Eav.Interfaces instead")]
-    public interface IValue : Interfaces.IValue { }
+    ///// <summary>
+    ///// old interface name
+    ///// </summary>
+    //[Obsolete("use the one in ToSic.Eav.Interfaces instead")]
+    //public interface IValue : Interfaces.IValue { }
 
-    /// <summary>
-    /// old interface name
-    /// </summary>
-    [Obsolete("use the one in ToSic.Eav.Interfaces instead")]
-    public interface IContentType : Interfaces.IContentType { }
+    ///// <summary>
+    ///// old interface name
+    ///// </summary>
+    //[Obsolete("use the one in ToSic.Eav.Interfaces instead")]
+    //public interface IContentType : Interfaces.IContentType { }
 
-    /// <summary>
-    /// old interface name
-    /// </summary>
-    [Obsolete("use the one in ToSic.Eav.Interfaces instead")]
-    public interface IDimension : Interfaces.IDimension { }
+    ///// <summary>
+    ///// old interface name
+    ///// </summary>
+    //[Obsolete("use the one in ToSic.Eav.Interfaces instead")]
+    //public interface IDimension : Interfaces.IDimension { }
 
-    /// <summary>
-    /// old interface name
-    /// </summary>
-    [Obsolete("use the one in ToSic.Eav.Interfaces instead")]
-    public interface ILanguage : Interfaces.ILanguage { }
+    ///// <summary>
+    ///// old interface name
+    ///// </summary>
+    //[Obsolete("use the one in ToSic.Eav.Interfaces instead")]
+    //public interface ILanguage : Interfaces.ILanguage { }
 
 }
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-2sxc - the DNN extension you can't live without
+# 2sxc - the DNN extension you can't live without
 ============
 
-This tool helps web designers and developers prepare great looking, animated and sexy content templates in DNN (DotNetNuke). It's like mixing DNN with Umbraco and Drupal :).
+This tool helps web designers and developers prepare great looking, animated and sexy content templates in DNN (DotNetNuke). 
+It's like mixing DNN with Umbraco and Drupal :).
 
 BTW: it used to be called 2SexyContent.
+
+Visit [2sxc.org](https://2sxc.org/) for more information.
