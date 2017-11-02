729b9c2bfd257c5b6d240f0470548503031af5ee
iJungleboy <daniel.mettler@2sic.com>
2017-11-02 17:14:50 +0100

    minor update to dnn runtime...

--- Environment/DnnRuntime.cs
+++ Environment/DnnRuntime.cs
@@ -1,11 +1,15 @@
 ﻿using System.Web;
+using ToSic.Eav.Enums;
 using ToSic.Eav.ImportExport.Persistence.File;
 
 namespace ToSic.SexyContent.Environment
 {
     public class DnnRuntime: Runtime
     {
-        public DnnRuntime() : base("Dnn.Runtim") { }
+        public DnnRuntime() : base("Dnn.Runtim")
+        {
+            Source = Repositories.SystemFiles;
+        }
 
         // 1 - find the current path to the .data folder
         public override string Path =>
