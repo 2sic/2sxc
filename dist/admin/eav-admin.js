a605f98dd8b438959fc37fea15272a9677dc715b
iJungleboy <daniel.mettler@2sic.com>
2017-11-12 12:03:00 +0100

    latest feature + version increase to 9.7

--- dist/admin/eav-admin.js
+++ dist/admin/eav-admin.js
@@ -1647,6 +1647,30 @@ angular.module("PipelineManagement", [
 (function () {
     var jsPlumb;    // needed, as we'll fill it later with the window value
 
+    var linePaintDefault = {
+        lineWidth: 4,
+        strokeStyle: "#61B7CF",
+        joinstyle: "round",
+        outlineColor: "white",
+        outlineWidth: 2
+    };
+    var lineCount = 0,
+        lineColors = [
+            "#009688", "#00bcd4", "#3f51b5", "#9c27b0", "#e91e63",
+            "#db4437", "#ff9800", "#60a917", "#60a917", "#008a00",
+            "#00aba9", "#1ba1e2", "#0050ef", "#6a00ff", "#aa00ff",
+            "#f472d0", "#d80073", "#a20025", "#e51400", "#fa6800",
+            "#f0a30a", "#e3c800", "#825a2c", "#6d8764", "#647687",
+            "#76608a", "#a0522d"
+        ],
+        maxCols = lineColors.length - 1;
+    function resetLineCount() { lineCount = 0; }
+    function nextLinePaintStyle() {
+        return Object.assign({}, linePaintDefault, { strokeStyle: lineColors[lineCount++ % maxCols] });
+    }
+
+    console.log(nextLinePaintStyle());
+    
     var instanceTemplate = {
         Connector: ["Bezier", { curviness: 70 }],
         HoverPaintStyle: {
@@ -1655,15 +1679,10 @@ angular.module("PipelineManagement", [
             outlineWidth: 2,
             outlineColor: "white"
         },
-        PaintStyle: {
-            lineWidth: 4,
-            strokeStyle: "#61B7CF",
-            joinstyle: "round",
-            outlineColor: "white",
-            outlineWidth: 2
-        },
+        PaintStyle: nextLinePaintStyle(),// linePaintDefault,
         Container: "pipelineContainer"
     };
+
 
 
     angular.module("PipelineDesigner").factory("plumbGui",
@@ -1770,7 +1789,11 @@ angular.module("PipelineManagement", [
                             plumbGui.addEndpoint(jsPlumb.getSelector("#" + targetElementId), wire.In, true);
 
                         try {
-                            plumbGui.instance.connect({ uuids: [fromUuid, toUuid] });
+                            plumbGui.instance.connect({
+                                uuids: [fromUuid, toUuid],
+                                //PaintStyle: nextLinePaintStyle(),// { strokeWidth: 15, stroke: 'rgba(0, 243,230,18)' }
+                                paintStyle: nextLinePaintStyle()
+                            });
                         } catch (e) {
                             $log.error({ message: "Connection failed", from: fromUuid, to: toUuid });
                         }
