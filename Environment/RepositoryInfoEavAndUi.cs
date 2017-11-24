ec5cf62ff3107bd32ee6605be8ad6ceadc3431dc
iJungleboy <daniel.mettler@2sic.com>
2017-11-24 11:50:39 +0100

    separated test-content types from live ones - part of https://github.com/2sic/2sxc/issues/1392

---
+++ Environment/RepositoryInfoEavAndUi.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using ToSic.Eav.Repositories;
+
+namespace ToSic.SexyContent.Environment
+{
+    /// <summary>
+    /// Provides global information about where the folders are which should be loaded in this environment
+    /// - the .data/
+    /// - the dist/edit/.data/
+    /// - the dist/sxc-edit/.data/
+    /// - the dev/.data (this is for testing only, will never be in the distribution)
+    /// </summary>
+    /// <remarks>
+    /// Is used by reflection, so you won't see any direct references to this anywhere
+    /// </remarks>
+    public class RepositoryInfoEavAndUi: RepositoryInfoOfFolder
+    {
+        public RepositoryInfoEavAndUi() : base(true, true, null) { }
+
+        public override List<string> RootPaths => new List<string>
+        {
+            BuildPath(".data"),
+            BuildPath("dist/edit/.data"),
+            BuildPath("dist/sxc-edit/.data"),
+            BuildPath(".databeta")
+        };
+
+        private string BuildPath(string pathEnd) =>
+            HttpContext.Current.Server.MapPath(System.IO.Path.Combine(Eav.ImportExport.Settings.ModuleDirectory,
+                pathEnd));
+    }
+}
