27c2332d79d02c4c766294291f339d4157de1398
iJungleboy <daniel.mettler@2sic.com>
2017-12-04 23:33:09 +0100

    testing setup with queries-dropdown showing queries...doesn't fully work yet...

--- Interfaces/IApp.cs
+++ Interfaces/IApp.cs
@@ -2,24 +2,15 @@
 {
     public interface IApp: Eav.Apps.Interfaces.IApp
     {
-         //string Name { get; }
-         //string Folder { get; }
-         //bool Hidden { get; }
          dynamic Configuration { get;  }
 
         dynamic Settings { get;  }
 
         dynamic Resources { get;  }
 
-         //string AppGuid { get; }
-
         string Path { get; }
 
         string PhysicalPath { get; }
-
-        //IAppData Data { get; }
-
-        //IDictionary<string, IDataSource> Query { get; }
 
         string Thumbnail { get; }
     }
