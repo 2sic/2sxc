4e56ddeaac18ba68358e412310a1d506488777ff
iJungleboy <daniel.mettler@2sic.com>
2017-12-19 23:22:25 +0100

    upped version for test-release of 09.14

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("09.13.00.*")]
+[assembly: AssemblyVersion("09.14.00.*")]
 //[assembly: AssemblyFileVersion("8.2.*")]
 [assembly: InternalsVisibleTo("ToSic.SexyContent.WebApi")]
 [assembly: InternalsVisibleTo("ToSic.Sxc.Dnn")]
