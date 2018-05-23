49b564f6d1ea19e794f2e7f4581c99f3d4635a1a
iJungleboy <daniel.mettler@2sic.com>
2018-05-23 13:39:47 +0200

    js rebuild for missing bits on 9.31

--- dist/admin/eav-admin.js
+++ dist/admin/eav-admin.js
@@ -1332,43 +1332,49 @@ if (!String.prototype.endsWith) {
 (function () {
     /* jshint laxbreak:true*/
 
-angular.module("EavDirectives", [])
-    .directive("icon", function() {
+  angular.module('EavDirectives', []);
+
+
+})();
+
+(function () {
+    /* jshint laxbreak:true*/
+
+angular.module('EavDirectives')
+    .directive('icon', function() {
         return {
-            restrict: "A",
+            restrict: 'A',
             replace: false,
             transclude: false,
             link: function postLink(scope, elem, attrs) {
                 var icn = attrs.icon;
-                elem.addClass("glyphicon glyphicon-" + icn);
+                elem.addClass('glyphicon glyphicon-' + icn);
             }
         };
-    })
-    .directive("stopEvent", function() {
+    });
+
+
+})();
+
+(function () {
+    /* jshint laxbreak:true*/
+
+angular.module('EavDirectives')
+    .directive('showDebugAvailability', ["eavConfig", function (eavConfig) {
         return {
-            restrict: "A",
-            link: function(scope, element, attr) {
-                if (attr && attr.stopEvent)
-                    element.bind(attr.stopEvent, function(e) {
-                        e.stopPropagation();
-                    });
-            }
-        };
-    })
-    .directive("showDebugAvailability", ["eavConfig", function (eavConfig) {
-        return {
-            restrict: "E",
+            restrict: 'E',
             scope: {},
             template: "<span class=\"debug-indicator low-priority\" ng-class='{ \"debug-enabled\": debugState.on }' "
-            + "uib-tooltip=\"{{ 'AdvancedMode.Info.Available' | translate }} \n" + eavConfig.versionInfo + "\" "
+            + "uib-tooltip=\"{{ 'AdvancedMode.Info.Available' | translate }} \n" + eavConfig.versionInfo + '" '
             + "ng-click='askForLogging()'>"
-                + "&pi;"
-            + "</span><br/>",
-            controller: ["$scope", "debugState", "toastr", function ($scope, debugState, toastr) {
+                + '&pi;'
+            + '</span><br/>',
+            controller: ['$scope', 'debugState', 'toastr', function ($scope, debugState, toastr) {
                 $scope.debugState = debugState;
 
                 function askLogging() {
-                    var duration = prompt("enable extended logging? type desired duration in minutes:", 1);
+                    var duration = prompt('enable extended logging? type desired duration in minutes:\n\n' +
+                      'note: try the new insights instead, see 2sxc.org/help?tag=insights', 1);
                     if (duration === null || duration === undefined) return;
                     debugState.enableExtendedLogging(duration).then(function (res) {
                         console.log(res.data);
@@ -1383,8 +1389,26 @@ angular.module("EavDirectives", [])
                 };
             }]
         };
-    }])
+    }]);
 
+
+})();
+
+(function () {
+    /* jshint laxbreak:true*/
+
+angular.module('EavDirectives')
+    .directive('stopEvent', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attr) {
+                if (attr && attr.stopEvent)
+                    element.bind(attr.stopEvent, function(e) {
+                        e.stopPropagation();
+                    });
+            }
+        };
+    })
     ;
 
 
