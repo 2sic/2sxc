90248d906a1e9230931cf5b8a8705b02c6432557
ijungleboy <daniel.mettler@2sic.com>
2019-09-25 08:59:09 +0200

    added public DynamicEntity properties to interface

--- ToSic.Sxc/Interfaces/IDynamicEntity.cs
+++ ToSic.Sxc/Interfaces/IDynamicEntity.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web;
 
+// ReSharper disable once CheckNamespace
 namespace ToSic.SexyContent.Interfaces
 {
     public interface IDynamicEntity
     {
+        /// <summary>
+        /// Deprecated - avoid using. Use Edit.Toolbar(object...) instead
+        /// </summary>
         HtmlString Toolbar { get; }
 
         int EntityId { get; }
@@ -13,10 +18,14 @@ namespace ToSic.SexyContent.Interfaces
 
         object EntityTitle { get; }
 
+        dynamic Get(string name);
+
         dynamic GetDraft();
 
         dynamic GetPublished();
 
         bool IsDemoItem { get; }
+
+        List<IDynamicEntity> Parents(string type = null, string field = null);
     }
 }
