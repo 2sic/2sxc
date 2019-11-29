ddf1f40f668a380a0cbf87b39862f8ff3a91c861
ijungleboy <daniel.mettler@2sic.com>
2019-11-29 15:09:50 +0100

    moves

--- ToSIC_SexyContent/ToSic.Sxc/SexyContent/IEditContentControl.cs
+++ ToSIC_SexyContent/ToSic.Sxc/SexyContent/IEditContentControl.cs
@@ -1,13 +1,13 @@
-﻿namespace ToSic.SexyContent
-{
-    /// <summary>
-    /// Interface used in EditContentGroupItem
-    /// Allows editing of ContentGroupItems and Entities
-    /// </summary>
-    interface IEditContentControl
-    {
-        void Cancel();
-        void Save();
-        bool IsPublished { get; set; }
-    }
-}
+﻿//namespace ToSic.SexyContent
+//{
+//    /// <summary>
+//    /// Interface used in EditContentGroupItem
+//    /// Allows editing of ContentGroupItems and Entities
+//    /// </summary>
+//    interface IEditContentControl
+//    {
+//        void Cancel();
+//        void Save();
+//        bool IsPublished { get; set; }
+//    }
+//}
