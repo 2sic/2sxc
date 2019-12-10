f24f94d9f374fefcb3fde333ca202df7f12998ce
ijungleboy <daniel.mettler@2sic.com>
2019-12-10 13:54:03 +0100

    include ContentTypeItems in Ui.Load()

--- ToSIC_SexyContent/Sxc WebApi/WebApi/Cms/SaveDataValidator.cs
+++ ToSIC_SexyContent/Sxc WebApi/WebApi/Cms/SaveDataValidator.cs
@@ -4,7 +4,7 @@ using System.Web.Http;
 using ToSic.Eav.Apps;
 using ToSic.Eav.Data;
 using ToSic.Eav.Data.Builder;
-using ToSic.Eav.ImportExport.Json.Format;
+using ToSic.Eav.ImportExport.Json.V1;
 using ToSic.Eav.Logging;
 using ToSic.Eav.WebApi.Formats;
 using IEntity = ToSic.Eav.Data.IEntity;
