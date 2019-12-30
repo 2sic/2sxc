60bd97e5eee47f8b37b1b14e9045cab58413f152
ijungleboy <daniel.mettler@2sic.com>
2019-12-30 17:57:49 +0100

    minor

--- ToSIC_SexyContent/ToSic.Sxc/Apps/AppHelpers.cs
+++ ToSIC_SexyContent/ToSic.Sxc/Apps/AppHelpers.cs
@@ -8,21 +8,21 @@ namespace ToSic.Sxc.Apps
         internal static int GetAppIdFromGuidName(int zoneId, string appName, bool alsoCheckFolderName = false)
         {
             // var baseCache = (RootCacheBase) DataSource.GetCache(Eav.Constants.DefaultZoneId, Eav.Constants.MetaDataAppId);
-            var baseCache = /*Factory.GetAppsCache*/Eav.Apps.Apps.Cache;
+            var zones = /*Factory.GetAppsCache*/Eav.Apps.Apps.Zones;
             // ReSharper disable once UnusedVariable
             //var dummy = baseCache.CacheTimestamp;
 
             if (string.IsNullOrEmpty(appName))
                 return 0; 
 
-            var appId = baseCache.Zones[zoneId].Apps
+            var appId = zones[zoneId].Apps
                     .Where(p => p.Value == appName).Select(p => p.Key).FirstOrDefault();
 
             // optionally check folder names
             if (appId == 0 && alsoCheckFolderName)
             {
                 var nameLower = appName.ToLower();
-                foreach (var p in baseCache.Zones[zoneId].Apps)
+                foreach (var p in zones[zoneId].Apps)
                 {
                     var mds = /*Factory.GetAppState*/Eav.Apps.Apps.Get(new AppIdentity(zoneId, p.Key)); // DataSource.GetMetaDataSource(zoneId, p.Key);
                     var appMetaData = mds
