395067e9bf4b928f025b75696e8bd74578e3af40
ijungleboy <daniel.mettler@2sic.com>
2020-02-14 12:56:03 +0100

    version bump

--- ToSIC_SexyContent/ToSic.Sxc/Settings.cs
+++ ToSIC_SexyContent/ToSic.Sxc/Settings.cs
@@ -65,6 +65,7 @@ namespace ToSic.Sxc
                 "10.25.00", // LTS
                 "10.25.02", // LTS
                 "10.25.03",
+                "10.25.04",
             };
 
             // this is the last version which must run server-side change-code
