f5a3107aec1f97dfef376ee398d3323374b13814
ijungleboy <daniel.mettler@2sic.com>
2020-03-07 15:18:47 +0100

    refactored list management

--- ToSIC_SexyContent/ToSic.Sxc/Apps/Parts/BlocksManager_Lists.cs
+++ ToSIC_SexyContent/ToSic.Sxc/Apps/Parts/BlocksManager_Lists.cs
@@ -8,35 +8,22 @@ namespace ToSic.Sxc.Apps
     {
 
         public void AddEmptyItem(BlockConfiguration block, int? sortOrder = null)
-            => AddContentAndPresentationEntity(block, 
-                sortOrder, 
-                null, 
-                null);
+            => AddContentAndPresentationEntity(block, sortOrder, null, null);
 
         public void ChangeOrder(BlockConfiguration block, int sortOrder, int destinationSortOrder)
         {
             var wrapLog = Log.Call($"change order orig:{sortOrder}, dest:{destinationSortOrder}");
-            GetEntityListManager(block).ReorderAndSave(sortOrder, destinationSortOrder);
+            GetEntityListManager(block).Move(sortOrder, destinationSortOrder);
             wrapLog(null);
         }
 
-        public void RemoveFromList(BlockConfiguration block, int sortOrder)
-        {
-            Log.Add($"remove from list order:{sortOrder}");
-            GetEntityListManager(block)
-                .RemoveInPair(sortOrder);
-        }
+        public void RemoveFromList(BlockConfiguration block, int sortOrder) 
+            => GetEntityListManager(block).Remove(sortOrder);
 
 
-        public void UpdateEntityIfChanged(
-            BlockConfiguration block, 
-            int sortOrder, 
-            int? entityId, 
-            bool updatePair,
-            int? presentationId)
-        {
-            GetEntityListManager(block).UpdateEntityIfChanged(sortOrder, entityId, updatePair, presentationId);
-        }
+        public void UpdateEntityIfChanged(BlockConfiguration block, int sortOrder, 
+            int? entityId, bool updatePair, int? presentationId) =>
+            GetEntityListManager(block).Replace(sortOrder, entityId, updatePair, presentationId);
 
         private EntityListManager GetEntityListManager(BlockConfiguration block)
             => new EntityListManager(CmsManager, block.Entity,
@@ -51,23 +38,10 @@ namespace ToSic.Sxc.Apps
         /// <summary>
         /// If SortOrder is not specified, adds at the end
         /// </summary>
-        /// <param name="block"></param>
-        /// <param name="sortOrder"></param>
-        /// <param name="contentId"></param>
-        /// <param name="presentationId"></param>
-        public void AddContentAndPresentationEntity(BlockConfiguration block, /*string type,*/ int? sortOrder, int? contentId, int? presentationId)
-        {
-            GetEntityListManager(block)
-                .AddContentAndPresentationEntity(sortOrder, contentId, presentationId);
-        }
-        
+        public void AddContentAndPresentationEntity(BlockConfiguration block, int? sortOrder, int? contentId, int? presentationId) =>
+            GetEntityListManager(block).Add(sortOrder, contentId, presentationId);
 
-        public bool ReorderAllAndSave(BlockConfiguration block, 
-            int[] newSequence)
-        {
-            return GetEntityListManager(block)
-                .ReorderAllAndSave(newSequence);
-        }
-
+        public void ReorderAllAndSave(BlockConfiguration block, int[] newSequence) =>
+            GetEntityListManager(block).Reorder(newSequence);
     }
 }
