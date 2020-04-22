cdf43bc32bef3334189daaac820474323a53ffa7
ijungleboy <daniel.mettler@2sic.com>
2020-04-22 18:23:16 +0200

    building .code

---
+++ ToSIC_SexyContent/2Sexy Content Razor/Dnn/RazorComponentCode.cs
@@ -0,0 +1,18 @@
+﻿namespace ToSic.Sxc.Dnn
+{
+    /// <summary>
+    /// This is the type used by code-behind classes of razor components
+    /// </summary>
+    public abstract class RazorComponentCode: RazorComponent
+    {
+        public virtual void OnRender() { }
+
+        /// <summary>
+        /// Override this method to also run any code automatically. <br/>
+        /// It's meant for things like setting page headers etc. <br/>
+        /// Note that it's run at the end of the render-cycle.
+        /// </summary>
+        public virtual void OnRenderComplete() { }
+
+    }
+}
