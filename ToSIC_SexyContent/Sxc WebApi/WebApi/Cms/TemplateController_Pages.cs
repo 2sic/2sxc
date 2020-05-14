20c0e8b6f7e2804e2cdce76b9e6cc4473942337e
ijungleboy <daniel.mettler@2sic.com>
2020-05-14 23:22:02 +0200

    Update TemplateController_Pages.cs

--- ToSIC_SexyContent/Sxc WebApi/WebApi/Cms/TemplateController_Pages.cs
+++ ToSIC_SexyContent/Sxc WebApi/WebApi/Cms/TemplateController_Pages.cs
@@ -23,19 +23,6 @@ namespace ToSic.Sxc.WebApi.Cms
             // treat view as a list - in case future code will want to analyze many views together
             var views = new List<IView> {cms.Views.Get(guid)};
 
-            // get all blocks which have a view assigned
-            //var blockEntities = cms.Blocks.Entities()
-            //    .Select(b =>
-            //    {
-            //        var templateGuid = b.Children(ViewParts.ViewFieldInContentBlock)
-            //            .FirstOrDefault()
-            //            ?.EntityGuid;
-            //        return templateGuid != null
-            //            ? new { Block = b, ViewGuid = templateGuid }
-            //            : null;
-            //    })
-            //    .Where(b => b != null)
-            //    .ToList();
             var blocks = cms.Blocks.AllWithView();
 
             Log.Add($"Found {blocks.Count} content blocks");
@@ -44,42 +31,37 @@ namespace ToSic.Sxc.WebApi.Cms
             var allMods = new Pages(Log).AllModulesWithContent(PortalSettings.PortalId);
             Log.Add($"Found {allMods.Count} modules");
 
-            var blocksWithModules = blocks.Select(b => new
-            {
-                //b.ViewGuid,
-                Block = b,
-                Modules = allMods.Where(m => m.ContentGroup == b.Guid)
-            });
-
             var result = views.Select(vwb => new
             {
                 Id = vwb.Entity.EntityId,
                 Guid = vwb.Entity.EntityGuid,
                 vwb.Name,
                 vwb.Path,
-                Blocks = blocksWithModules
-                    .Where(b => b.Block.View.Guid == vwb.Entity.EntityGuid)
+                Blocks = blocks
+                    .Where(b => b.View.Guid == vwb.Entity.EntityGuid)
                     .Select(blWMod => new
                     {
-                        ContentGroupId = blWMod.Block.Id,
-                        ContentGroupGuid = blWMod.Block.Guid,
-                        Modules = blWMod.Modules?.Select(m => new
-                        {
-                            ModuleId = m.Module.ModuleID,
-                            ShowOnAllPages = m.Module.AllTabs,
-                            Title = m.Module.ModuleTitle,
-                            InstanceId = m.Module.TabModuleID,
-                            IsDeleted = m.Module.IsDeleted || m.Page.IsDeleted,
-                            Page = new
+                        blWMod.Id,
+                        blWMod.Guid,
+                        Modules = allMods
+                            .Where(m => m.ContentGroup == blWMod.Guid)
+                            .Select(m => new
                             {
-                                Id = m.Module.TabID,
-                                Url = m.Page.FullUrl,
-                                PageName = m.Page.TabName,
-                                m.Page.CultureCode,
-                                Visible = m.Page.IsVisible,
-                                m.Page.Title
-                            }
-                        })
+                                ModuleId = m.Module.ModuleID,
+                                ShowOnAllPages = m.Module.AllTabs,
+                                Title = m.Module.ModuleTitle,
+                                Id = m.Module.TabModuleID,
+                                IsDeleted = m.Module.IsDeleted || m.Page.IsDeleted,
+                                Page = new
+                                {
+                                    Id = m.Module.TabID,
+                                    Url = m.Page.FullUrl,
+                                    Name = m.Page.TabName,
+                                    m.Page.CultureCode,
+                                    Visible = m.Page.IsVisible,
+                                    m.Page.Title
+                                }
+                            })
                     })
             });
 
