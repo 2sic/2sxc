051460c6359df809cbdfb60d5f05ac30cede58fd
ijungleboy <daniel.mettler@2sic.com>
2020-07-28 15:05:16 +0200

    Quick-dialog: Integrated i18n with that code

---
+++ ToSIC_SexyContent/dist/ng/i18n/en.js
@@ -0,0 +1,27 @@
+﻿{
+	"TemplatePicker": {
+		"AppPickerDefault": "Choose App",
+		"ContentTypePickerDefault": "Choose Content Type",
+		"LayoutElement": "Layout element",
+		"ChangeView": "Change view",
+
+		"Save": "Save and close",
+		"Cancel": "Cancel change",
+		"Close": "Close",
+
+		"Install": "install apps",
+		"Catalog": "browse app catalog",
+
+		"App": "configure app",
+		"Zone": "manage all apps"
+  },
+  "ItemHistory": {
+    "Title": "History of this item",
+    "Version": "Version {{version}}",
+    "NoHistory": "No previous versions of this item found",
+    "Buttons": {
+      "RestoreLive": "Restore",
+      "RestoreDraft": "Restore as draft"
+    }
+  }
+}
