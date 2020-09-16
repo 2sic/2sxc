fd0033bbaef8843a0f8578e34157bf56b675e00e
ijungleboy <daniel.mettler@2sic.com>
2020-09-16 16:26:46 +0200

    move usage stats to shared #2150 #2160

--- Src/Sxc/ToSic.Sxc.WebApi/Context/ViewDto.cs
+++ Src/Sxc/ToSic.Sxc.WebApi/Context/ViewDto.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using ToSic.Sxc.WebApi.SharedDto;
-using ToSic.Sxc.WebApi.Usage.Dto;
 
 namespace ToSic.Sxc.WebApi.Context
 {
---
+++ Src/Sxc/ToSic.Sxc.WebApi/Usage/UsageBackend.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using ToSic.Eav.Apps.Run;
+using ToSic.Eav.Security.Permissions;
+using ToSic.Eav.WebApi.Errors;
+using ToSic.Eav.WebApi.Security;
+using ToSic.Sxc.Apps;
+using ToSic.Sxc.Apps.Blocks;
+using ToSic.Sxc.Blocks;
+using ToSic.Sxc.WebApi.Context;
+
+namespace ToSic.Sxc.WebApi.Usage
+{
+    internal class UsageBackend: WebApiBackendBase<UsageBackend>
+    {
+        public UsageBackend() : base("Bck.Usage") { }
+
+        public IEnumerable<ViewDto> ViewUsage(IInstanceContext context, int appId, Guid guid, 
+            Func<List<IView>, List<BlockConfiguration>, IEnumerable<ViewDto>> finalBuilder)
+        {
+            var wrapLog = Log.Call<IEnumerable<ViewDto>>($"{appId}, {guid}");
+
+            // extra security to only allow zone change if host user
+            var permCheck = new MultiPermissionsApp().Init(context, GetApp(appId, null), Log);
+            if (!permCheck.EnsureAll(GrantSets.ReadSomething, out var error))
+                throw HttpException.PermissionDenied(error);
+
+            var cms = new CmsRuntime(appId, Log, true);
+            // treat view as a list - in case future code will want to analyze many views together
+            var views = new List<IView> { cms.Views.Get(guid) };
+
+            var blocks = cms.Blocks.AllWithView();
+
+            Log.Add($"Found {blocks.Count} content blocks");
+
+            // create array with all 2sxc modules in this portal
+            //var allMods = new Pages(Log).AllModulesWithContent(PortalSettings.PortalId);
+            //Log.Add($"Found {allMods.Count} modules");
+
+            var result = finalBuilder(views, blocks); // views.Select(vwb => new ViewDto().Init(vwb, blocks, allMods));
+
+            return wrapLog("ok", result);
+        }
+
+    }
+}
