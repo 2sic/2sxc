e7357f63087f3a22a500ff7de6e580988ef8020f
ijungleboy <daniel.mettler@2sic.com>
2020-09-17 11:27:32 +0200

    Move ModuleController features for list editing to shared #2150

--- Src/Dnn/ToSic.Sxc.Dnn.WebApi/WebApi/Cms/ModuleController_FieldLists.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.WebApi/WebApi/Cms/ModuleController_FieldLists.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Linq;
 using System.Web.Http;
 using DotNetNuke.Security;
 using DotNetNuke.Web.Api;
 using ToSic.Eav;
-using ToSic.Eav.Apps;
-using ToSic.Eav.Apps.Environment;
-using ToSic.Eav.Data;
-using ToSic.Eav.Security.Permissions;
-using ToSic.Eav.WebApi.Errors;
-using ToSic.Eav.WebApi.Security;
-using ToSic.Sxc.Blocks;
+using ToSic.Sxc.WebApi.FieldList;
 
 namespace ToSic.Sxc.WebApi.Cms
 {
@@ -20,25 +13,27 @@ namespace ToSic.Sxc.WebApi.Cms
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
         public void AddItem([FromUri] int? index = null)
         {
-            Log.Add($"add order:{index}");
-            var versioning = Factory.Resolve<IPagePublishing>().Init(Log);
+            Factory.Resolve<FieldListBackend>().Init(GetContext(), GetBlock(), Log).AddItem(index);
+            //Log.Add($"add order:{index}");
+            //var versioning = Factory.Resolve<IPagePublishing>().Init(Log);
 
-            void InternalSave(VersioningActionInfo args) =>
-                CmsManager.Blocks.AddEmptyItem(GetBlock().Configuration, index);
+            //void InternalSave(VersioningActionInfo args) =>
+            //    CmsManager.Blocks.AddEmptyItem(GetBlock().Configuration, index);
 
-            // use dnn versioning - this is always part of page
-            versioning.DoInsidePublishing(GetContext(), InternalSave);
+            //// use dnn versioning - this is always part of page
+            //versioning.DoInsidePublishing(GetContext(), InternalSave);
         }
 
         [HttpGet]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
         public void ChangeOrder(Guid? parent, string fields, int index, int toIndex)
         {
-            var wrapLog = Log.Call($"change order sort:{index}, dest:{toIndex}");
-            var entMan = CmsManager.Entities;
-            FindTargetAndModifyList(parent, fields, index,
-                (entity, fieldList, idx, versioning) => entMan.FieldListMove(entity, fieldList, idx, toIndex, versioning));
-            wrapLog(null);
+            Factory.Resolve<FieldListBackend>().Init(GetContext(), GetBlock(), Log).ChangeOrder(parent, fields, index, toIndex);
+            //var wrapLog = Log.Call($"change order sort:{index}, dest:{toIndex}");
+            //var entMan = CmsManager.Entities;
+            //FindTargetAndModifyList(parent, fields, index,
+            //    (entity, fieldList, idx, versioning) => entMan.FieldListMove(entity, fieldList, idx, toIndex, versioning));
+            //wrapLog(null);
         }
 
 
@@ -47,43 +42,47 @@ namespace ToSic.Sxc.WebApi.Cms
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
         public bool Publish(string part, int index)
         {
-            Log.Add($"try to publish #{index} on '{part}'");
-            if (!new MultiPermissionsApp().Init(GetContext(), App, Log)
-                .EnsureAll(GrantSets.WritePublished, out var error))
-                throw HttpException.PermissionDenied(error);
-            return GetEditor().Publish(part, index);
+            return Factory.Resolve<FieldListBackend>().Init(GetContext(), GetBlock(), Log).Publish(part, index);
+
+            //Log.Add($"try to publish #{index} on '{part}'");
+            //if (!new MultiPermissionsApp().Init(GetContext(), App, Log)
+            //    .EnsureAll(GrantSets.WritePublished, out var error))
+            //    throw HttpException.PermissionDenied(error);
+            //return GetEditor().Publish(part, index);
         }
 
         [HttpGet]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
         public void RemoveFromList(Guid? parent, string fields, int index)
         {
-            var wrapLog = Log.Call($"remove from index:{index}");
-            var entMan = CmsManager.Entities;
-            FindTargetAndModifyList(parent, fields, index,
-                (entity, fieldList, idx, versioning) => entMan.FieldListRemove(entity, fieldList, idx, versioning));
-            wrapLog(null);
+            Factory.Resolve<FieldListBackend>().Init(GetContext(), GetBlock(), Log)
+                .RemoveFromList(parent, fields, index);
+            //var wrapLog = Log.Call($"remove from index:{index}");
+            //var entMan = CmsManager.Entities;
+            //FindTargetAndModifyList(parent, fields, index,
+            //    (entity, fieldList, idx, versioning) => entMan.FieldListRemove(entity, fieldList, idx, versioning));
+            //wrapLog(null);
         }
 
-        private void FindTargetAndModifyList(Guid? parent, string fields, int index, Action<IEntity, string[], int, bool> action)
-        {
+        //private void FindTargetAndModifyList(Guid? parent, string fields, int index, Action<IEntity, string[], int, bool> action)
+        //{
 
-            void InternalSave(VersioningActionInfo args)
-            {
-                var block = GetBlock();
-                // find target
-                var target = parent == null ? block.Configuration.Entity : App.Data.List.One(parent.Value);
-                if (target == null) throw new Exception($"Can't find parent {parent}");
-                // determine versioning
-                var useVersioning = block.Configuration.VersioningEnabled;
-                // check field list (default to content-block fields)
-                var fieldList = fields?.Split(',').Select(f => f.Trim()).ToArray() ?? ViewParts.ContentPair;
-                action.Invoke(target, fieldList, index, useVersioning);
-            }
+        //    void InternalSave(VersioningActionInfo args)
+        //    {
+        //        var block = GetBlock();
+        //        // find target
+        //        var target = parent == null ? block.Configuration.Entity : App.Data.List.One(parent.Value);
+        //        if (target == null) throw new Exception($"Can't find parent {parent}");
+        //        // determine versioning
+        //        var useVersioning = block.Configuration.VersioningEnabled;
+        //        // check field list (default to content-block fields)
+        //        var fieldList = fields?.Split(',').Select(f => f.Trim()).ToArray() ?? ViewParts.ContentPair;
+        //        action.Invoke(target, fieldList, index, useVersioning);
+        //    }
 
-            // use dnn versioning - items here are always part of list
-            var publishing = Factory.Resolve<IPagePublishing>().Init(Log);
-            publishing.DoInsidePublishing(GetContext(), InternalSave);
-        }
+        //    // use dnn versioning - items here are always part of list
+        //    var publishing = Factory.Resolve<IPagePublishing>().Init(Log);
+        //    publishing.DoInsidePublishing(GetContext(), InternalSave);
+        //}
     }
 }
---
+++ Src/Sxc/ToSic.Sxc.WebApi/FieldList/FieldListBackend.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using ToSic.Eav;
+using ToSic.Eav.Apps;
+using ToSic.Eav.Apps.Environment;
+using ToSic.Eav.Apps.Run;
+using ToSic.Eav.Data;
+using ToSic.Eav.Logging;
+using ToSic.Eav.Security.Permissions;
+using ToSic.Eav.WebApi.Errors;
+using ToSic.Eav.WebApi.Security;
+using ToSic.Sxc.Apps;
+using ToSic.Sxc.Blocks;
+using ToSic.Sxc.Blocks.Edit;
+
+namespace ToSic.Sxc.WebApi.FieldList
+{
+    internal class FieldListBackend: HasLog
+    {
+        private readonly IPagePublishing _publishing;
+
+        #region constructor / DI
+
+        public FieldListBackend(IPagePublishing publishing) : base("Bck.FldLst")
+        {
+            _publishing = publishing;
+        }
+
+        public FieldListBackend Init(IInstanceContext context, IBlock block, ILog parentLog)
+        {
+            Log.LinkTo(parentLog);
+            _block = block;
+            _context = context;
+            _cmsManager = new CmsManager(_block.App, Log);
+            return this;
+        }
+        private IInstanceContext _context;
+        private IBlock _block;
+        private CmsManager _cmsManager;
+        #endregion
+
+        public void AddItem( int? index = null)
+        {
+            Log.Add($"add order:{index}");
+            // use dnn versioning - this is always part of page
+            _publishing.DoInsidePublishing(_context, args => _cmsManager.Blocks.AddEmptyItem(_block.Configuration, index));
+        }
+
+        public void ChangeOrder(Guid? parent, string fields, int index, int toIndex)
+        {
+            var wrapLog = Log.Call($"change order sort:{index}, dest:{toIndex}");
+            var entMan = _cmsManager.Entities;
+            FindTargetAndModifyList(parent, fields, index,
+                (entity, fieldList, idx, versioning) => entMan.FieldListMove(entity, fieldList, idx, toIndex, versioning));
+            wrapLog(null);
+        }
+
+        public bool Publish(string part, int index)
+        {
+            Log.Add($"try to publish #{index} on '{part}'");
+            if (!new MultiPermissionsApp().Init(_context, _block.App, Log)
+                .EnsureAll(GrantSets.WritePublished, out var error))
+                throw HttpException.PermissionDenied(error);
+            return BlockEditorBase.GetEditor(_block).Publish(part, index);
+        }
+
+
+        public void RemoveFromList(Guid? parent, string fields, int index)
+        {
+            var wrapLog = Log.Call($"remove from index:{index}");
+            var entMan = _cmsManager.Entities;
+            FindTargetAndModifyList(parent, fields, index,
+                (entity, fieldList, idx, versioning) => entMan.FieldListRemove(entity, fieldList, idx, versioning));
+            wrapLog(null);
+        }
+
+
+
+
+        private void FindTargetAndModifyList(Guid? parent, string fields, int index, Action<IEntity, string[], int, bool> action)
+        {
+            // use dnn versioning - items here are always part of list
+            _publishing.DoInsidePublishing(_context, args =>
+            {
+                // find target
+                var target = parent == null ? _block.Configuration.Entity : _block.App.Data.List.One(parent.Value);
+                if (target == null) throw new Exception($"Can't find parent {parent}");
+                // determine versioning
+                var useVersioning = _block.Configuration.VersioningEnabled;
+                // check field list (default to content-block fields)
+                var fieldList = fields?.Split(',').Select(f => f.Trim()).ToArray() ?? ViewParts.ContentPair;
+                action.Invoke(target, fieldList, index, useVersioning);
+            });
+        }
+
+    }
+}
