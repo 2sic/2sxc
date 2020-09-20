8582876cb8e9cdfe81ffa603151cf41bc9ae5343
ijungleboy <daniel.mettler@2sic.com>
2020-09-20 16:51:25 +0200

    app export import for #2173

--- Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/AppController.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/AppController.cs
@@ -4,93 +4,82 @@ using System.Web.Http;
 using DotNetNuke.Security;
 using DotNetNuke.Services.Exceptions;
 using DotNetNuke.Web.Api;
-using ToSic.Eav;
 using ToSic.Eav.WebApi.Dto;
 using ToSic.Eav.WebApi.PublicApi;
 using ToSic.Sxc.Dnn.Run;
-using ToSic.Sxc.Dnn.WebApi;
 using ToSic.Sxc.WebApi.ImportExport;
 
-namespace ToSic.Sxc.WebApi.Cms
+namespace ToSic.Sxc.Dnn.WebApi.Admin
 {
     // [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)] can't be used, because it forces the security
     // token, which fails in the cases where the url is called using get, which should result in a download
     // [ValidateAntiForgeryToken] because the exports are called by the browser directly (new tab) 
     // we can't set this globally (only needed for imports)
-    public class ImportExportController : DnnApiControllerWithFixes, IImportExportController
+    public class AppController : DnnApiControllerWithFixes, IAppController
     {
-        protected override string HistoryLogName => "Api.2sIExC";
+        protected override string HistoryLogName => "Api.App";
 
+        /// <summary>
+        /// Used to be GET ImportExport/GetAppInfo
+        /// </summary>
+        /// <param name="appId"></param>
+        /// <param name="zoneId"></param>
+        /// <returns></returns>
         [HttpGet]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
         [ValidateAntiForgeryToken]
-        public AppExportInfoDto GetAppInfo(int appId, int zoneId)
-            => Factory.Resolve<ExportApp>().Init(PortalSettings.PortalId, new DnnUser(UserInfo), Log)
+        public AppExportInfoDto Statistics(int appId, int zoneId)
+            => Eav.Factory.Resolve<ExportApp>().Init(PortalSettings.PortalId, new DnnUser(UserInfo), Log)
                 .GetAppInfo(appId, zoneId);
 
+        /// <summary>
+        /// Used to be GET ImportExport/ExportApp
+        /// </summary>
+        /// <param name="appId"></param>
+        /// <param name="zoneId"></param>
+        /// <param name="includeContentGroups"></param>
+        /// <param name="resetAppGuid"></param>
+        /// <returns></returns>
         [HttpGet]
-        [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
-        [ValidateAntiForgeryToken]
-        public ExportPartsOverviewDto GetContentInfo(int appId, int zoneId, string scope)
-            => Factory.Resolve<ExportContent>().Init(PortalSettings.PortalId, new DnnUser(UserInfo), Log)
-                .PreExportSummary(appId, zoneId, scope);
-
-
-        [HttpGet]
-        public HttpResponseMessage ExportApp(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid)
-            => Factory.Resolve<ExportApp>().Init(PortalSettings.PortalId, new DnnUser(UserInfo), Log)
+        public HttpResponseMessage Export(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid)
+            => Eav.Factory.Resolve<ExportApp>().Init(PortalSettings.PortalId, new DnnUser(UserInfo), Log)
                 .Export(appId, zoneId, includeContentGroups, resetAppGuid);
 
+        /// <summary>
+        /// Used to be GET ImportExport/ExportForVersionControl
+        /// </summary>
+        /// <param name="appId"></param>
+        /// <param name="zoneId"></param>
+        /// <param name="includeContentGroups"></param>
+        /// <param name="resetAppGuid"></param>
+        /// <returns></returns>
         [HttpGet]
         [ValidateAntiForgeryToken]
-        public bool ExportForVersionControl(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid)
-            => Factory.Resolve<ExportApp>().Init(PortalSettings.PortalId, new DnnUser(UserInfo), Log)
+        public bool SaveData(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid)
+            => Eav.Factory.Resolve<ExportApp>().Init(PortalSettings.PortalId, new DnnUser(UserInfo), Log)
                 .SaveDataForVersionControl(appId, zoneId, includeContentGroups, resetAppGuid);
 
-        [HttpGet]
-        public HttpResponseMessage ExportContent(int appId, int zoneId, string contentTypeIdsString,
-            string entityIdsString, string templateIdsString)
-            => Factory.Resolve<ExportContent>().Init(PortalSettings.PortalId, new DnnUser(UserInfo), Log)
-                .Export(appId, zoneId, contentTypeIdsString, entityIdsString, templateIdsString);
 
-
+        /// <summary>
+        /// Used to be POST ImportExport/ImportApp
+        /// </summary>
+        /// <returns></returns>
         [HttpPost]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
         [ValidateAntiForgeryToken]
-        public ImportResultDto ImportApp()
+        public ImportResultDto Import(int zoneId)
         {
             Log.Add("import app start");
 
             var request = HttpContext.Current.Request;
             PreventServerTimeout300();
 
-            var zoneId  = int.Parse(request["ZoneId"]);
             if (request.Files.Count <= 0) return new ImportResultDto();
 
-            return Factory.Resolve<ImportApp>().Init(new DnnUser(UserInfo), Log)
+            return Eav.Factory.Resolve<ImportApp>().Init(new DnnUser(UserInfo), Log)
                 .Import(zoneId, request["Name"], request.Files[0].InputStream, Exceptions.LogException);
         }
 
-        [HttpPost]
-        [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
-        [ValidateAntiForgeryToken]
-        public ImportResultDto ImportContent()
-        {
-            var wrapLog = Log.Call<ImportResultDto>();
-
-            var request = HttpContext.Current.Request;
-            var appId = int.Parse(request["AppId"]);
-            var zoneId = int.Parse(request["ZoneId"]);
-
-            if (request.Files.Count <= 0) return new ImportResultDto();
-
-            PreventServerTimeout300();
-
-            var result = Factory.Resolve<ImportContent>().Init(new DnnUser(UserInfo), Log).Import(zoneId, appId, request.Files[0].FileName,
-                request.Files[0].InputStream, PortalSettings.DefaultLanguage, Exceptions.LogException);
-
-            return wrapLog("ok", result);
-        }
 
     }
 }
