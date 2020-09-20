c2d7fe08357bb4a067ab33c718103df67cb0f00d
ijungleboy <daniel.mettler@2sic.com>
2020-09-20 16:01:50 +0200

    Entity controller now in /admin/entity #2173

--- Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/EntityController.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/EntityController.cs
@@ -5,51 +5,53 @@ using DotNetNuke.Web.Api;
 using ToSic.Eav.Security.Permissions;
 using ToSic.Eav.WebApi;
 using ToSic.Eav.WebApi.PublicApi;
+using ToSic.Sxc.WebApi;
+using ToSic.Sxc.WebApi.Cms;
 using Guid = System.Guid;
 
-namespace ToSic.Sxc.WebApi.Cms
+namespace ToSic.Sxc.Dnn.WebApi.Admin
 {
 	/// <summary>
 	/// Proxy Class to the EAV EntitiesController (Web API Controller)
 	/// </summary>
 	[SupportedModules("2sxc,2sxc-app")]
     [ValidateAntiForgeryToken]
-	public partial class EntitiesController : SxcApiControllerBase, IEntitiesController
+	public partial class EntityController : SxcApiControllerBase, IEntitiesController
 	{
         protected override string HistoryLogName => "Api.EntCnt";
 
         /// <summary>
-        /// Get all Entities of specified Type
+        /// Used to be Entities/GetOllOfTypeForAdmin
+        /// Used to be Entities/GetEntities
         /// </summary>
-        /// <remarks>
-        /// Is currently only used in Queries / Pipelines to list the items
-        /// TODO: should be merged with the GetAllOfTypeForAdmin
-        /// </remarks>
-        [HttpGet]
-	    [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
-	    public IEnumerable<Dictionary<string, object>> GetEntities(string contentType, int appId)
-	        => new EntityApi(appId, true, Log).GetEntities(contentType);
-
+        /// <param name="appId"></param>
+        /// <param name="contentType"></param>
+        /// <returns></returns>
         [HttpGet]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
-        public IEnumerable<Dictionary<string, object>> GetAllOfTypeForAdmin(int appId, string contentType) 
+        public IEnumerable<Dictionary<string, object>> List(int appId, string contentType) 
             => EntityApi.GetOrThrowBasedOnGrants(GetContext(), GetApp(appId), contentType, GrantSets.ReadSomething, Log)
                 .GetEntitiesForAdmin(contentType);
 
 
         [HttpDelete]
-        [HttpGet]
+        // todo: unsure why only Edit - is this used anywhere else than admin?
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
         public void Delete(string contentType, int id, int appId, bool force = false) 
             => EntityApi.GetOrThrowBasedOnGrants(GetContext(), GetApp(appId), contentType, GrantSets.DeleteSomething, Log)
                 .Delete(contentType, id, force);
 
         [HttpDelete]
-        [HttpGet]
+        // todo: unsure why only Edit - is this used anywhere else than admin?
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
         public void Delete(string contentType, Guid guid, int appId, bool force = false)
             => EntityApi.GetOrThrowBasedOnGrants(GetContext(), GetApp(appId), contentType, GrantSets.DeleteSomething, Log)
                 .Delete(contentType, guid, force);
+
+
+        // New feature in 11.03 - Usage Statistics
+        // not final yet, so no [HttpGet]
+        public dynamic Usage(int appId, Guid guid) => new EntityBackend().Init(Log).Usage(GetContext(), GetApp(appId), guid);
 
     }
 }
