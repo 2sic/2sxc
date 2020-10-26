de07d22f329cb0709f60cf5c21adb6ad594c4991
Tonći Vatavuk <tonci@sistemi.hr>
2020-10-26 12:00:58 +0100

    ToSic Interop inherits Oqtane.UI.Interop

--- Src/Oqtane/ToSic.Sxc.Oqt.Client/Interop.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Client/Interop.cs
@@ -3,86 +3,13 @@ using System.Threading.Tasks;
 
 namespace ToSic.Sxc.Oqt.Client
 {
-    public class Interop
+    public class Interop : Oqtane.UI.Interop
     {
         private readonly IJSRuntime _jsRuntime;
 
-        public Interop(IJSRuntime jsRuntime)
+        public Interop(IJSRuntime jsRuntime) : base(jsRuntime)
         {
             _jsRuntime = jsRuntime;
-        }
-
-        public ValueTask<string> GetElementByName(string name)
-        {
-            try
-            {
-                return _jsRuntime.InvokeAsync<string>(
-                    "Oqtane.Interop.getElementByName",
-                    name);
-            }
-            catch
-            {
-                return new ValueTask<string>(Task.FromResult(string.Empty));
-            }
-        }
-
-        public Task IncludeMeta(string id, string attribute, string name, string content, string key)
-        {
-            try
-            {
-                _jsRuntime.InvokeVoidAsync(
-                    "Oqtane.Interop.includeMeta",
-                    id, attribute, name, content, key);
-                return Task.CompletedTask;
-            }
-            catch
-            {
-                return Task.CompletedTask;
-            }
-        }
-
-        public Task IncludeScript(string id, string src, string integrity, string crossorigin, string content, string location, string key)
-        {
-            try
-            {
-                _jsRuntime.InvokeVoidAsync(
-                    "Oqtane.Interop.includeScript",
-                    id, src, integrity, crossorigin, content, location, key);
-                return Task.CompletedTask;
-            }
-            catch
-            {
-                return Task.CompletedTask;
-            }
-        }
-
-        public async Task IncludeScripts(object[] scripts)
-        {
-            try
-            {
-                await _jsRuntime.InvokeVoidAsync(
-                    "Oqtane.Interop.includeScripts",
-                    (object)scripts);
-            }
-            catch
-            {
-                // ignore exception
-            }
-        }
-
-        public Task UpdateTitle(string title)
-        {
-            try
-            {
-                _jsRuntime.InvokeVoidAsync(
-                    "Oqtane.Interop.updateTitle",
-                    title);
-                return Task.CompletedTask;
-            }
-            catch
-            {
-                return Task.CompletedTask;
-            }
         }
     }
 }
