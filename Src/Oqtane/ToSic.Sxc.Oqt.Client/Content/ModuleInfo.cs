218ded277d49794680de3cb5debba307b695f10e
Tonći Vatavuk <tonci@sistemi.hr>
2020-10-27 01:07:52 +0100

    2sxc App and 2sxc Content module definition

---
+++ Src/Oqtane/ToSic.Sxc.Oqt.Client/Content/ModuleInfo.cs
@@ -0,0 +1,20 @@
+using Oqtane.Models;
+using Oqtane.Modules;
+
+namespace ToSic.Sxc.Oqt.Content
+{
+    public class ModuleInfo : IModule
+    {
+        public ModuleDefinition ModuleDefinition => new ModuleDefinition
+        {
+            Name = "Content",
+            Description = "2sxc helps create designed, cross-platform content",
+            Version = "0.0.55",
+            ServerManagerType = "ToSic.Sxc.Oqt.Server.Manager.SxcManager, ToSic.Sxc.Oqtane.Server",
+            ReleaseVersions = "0.0.55",
+            Dependencies = "ToSic.Sxc.Oqtane.Shared",
+            Categories = "Common",
+            Runtimes = "Server",
+        };
+    }
+}
