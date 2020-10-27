345c3bf0dbbf81fce6aac1769a01945461d8a94e
ijungleboy <daniel.mettler@2sic.com>
2020-10-27 14:41:11 +0100

    clean up razor engine part 4 (renames)

--- Src/Razor/ToSic.Sxc.Razor.Engine/RazorEngineFinder.cs
+++ Src/Razor/ToSic.Sxc.Razor.Engine/RazorEngineFinder.cs
@@ -3,7 +3,6 @@ using ToSic.Sxc.Engines;
 
 namespace ToSic.Sxc.Razor.Engine
 {
-
     public class RazorEngineFinder : IEngineFinder
     {
         public Type RazorEngineType() => typeof(RazorEngine);
