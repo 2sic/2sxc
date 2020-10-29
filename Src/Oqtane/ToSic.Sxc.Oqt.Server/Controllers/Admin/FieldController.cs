12051894fbe20f641c2a4a9b4753088bad62f6f3
Tonći Vatavuk <tonci@sistemi.hr>
2020-10-29 18:52:31 +0100

    implement FieldController

---
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/Controllers/Admin/FieldController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToSic.Eav.Apps;
+using ToSic.Eav.Apps.Parts;
+using ToSic.Eav.WebApi;
+using ToSic.Eav.WebApi.Dto;
+using ToSic.Eav.WebApi.PublicApi;
+using ToSic.Sxc.Oqt.Shared;
+
+namespace ToSic.Sxc.Oqt.Server.Controllers.Admin
+{
+    [ValidateAntiForgeryToken]
+    [Authorize(Roles = Oqtane.Shared.Constants.AdminRole)]
+    [Route(WebApiConstants.WebApiStateRoot + "/admin/field/[action]")]
+    public class FieldController : SxcStatefulControllerBase, IFieldController
+    {
+        protected override string HistoryLogName => "Api.Fields";
+        private ContentTypeApi Backend => new ContentTypeApi(Log);
+        public FieldController(StatefulControllerDependencies dependencies) : base(dependencies)
+        { }
+
+        #region Fields - Get, Reorder, Data-Types (for dropdown), etc.
+
+        /// <summary>
+        /// Returns the configuration for a content type
+        /// </summary>
+        [HttpGet]
+        public IEnumerable<ContentTypeFieldDto> All(int appId, string staticName) => Backend.GetFields(appId, staticName);
+
+        /// <summary>
+        /// Used to be GET ContentType/DataTypes
+        /// </summary>
+        [HttpGet]
+        public string[] DataTypes(int appId) => Backend.DataTypes(appId);
+
+        /// <summary>
+        /// Used to be GET ContentType/InputTypes
+        /// </summary>
+	    [HttpGet]
+        public List<InputTypeInfo> InputTypes(int appId) => new AppRuntime(appId, true, Log).ContentTypes.GetInputTypes();
+
+        /// <summary>
+        /// Used to be GET ContentType/AddField
+        /// </summary>
+        [HttpPost]
+        public int Add(int appId, int contentTypeId, string staticName, string type, string inputType, int index)
+            => Backend.AddField(appId, contentTypeId, staticName, type, inputType, index);
+
+        /// <summary>
+        /// Used to be GET ContentType/DeleteField
+        /// </summary>
+        [HttpDelete]
+        public bool Delete(int appId, int contentTypeId, int attributeId)
+            => Backend.DeleteField(appId, contentTypeId, attributeId);
+
+        /// <summary>
+        /// Used to be GET ContentType/Reorder
+        /// </summary>
+	    [HttpPost]
+        public bool Sort(int appId, int contentTypeId, string order) => Backend.Reorder(appId, contentTypeId, order);
+
+
+        /// <summary>
+        /// Used to be GET ContentType/UpdateInputType
+        /// </summary>
+        [HttpPost]
+        public bool InputType(int appId, int attributeId, string inputType) => Backend.SetInputType(appId, attributeId, inputType);
+
+        #endregion
+
+        /// <summary>
+        /// Used to be GET ContentType/Rename
+        /// </summary>
+        [HttpPost]
+        public void Rename(int appId, int contentTypeId, int attributeId, string newName) => Backend.Rename(appId, contentTypeId, attributeId, newName);
+    }
+}
