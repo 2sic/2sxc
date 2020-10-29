57ba5955983cc46840092778d5fe79197fe8aa7c
ijungleboy <daniel.mettler@2sic.com>
2020-10-29 16:11:40 +0100

    Prepared Oqtane value ccoverter - not yet tested

---
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/Adam/PathFixer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToSic.Sxc.Oqt.Server.Adam
+{
+    internal static class PathFixer
+    {
+        public static string Backslash(this string original)
+            => original.Replace("/", "\\").Replace("\\\\", "\\");
+
+        public static string Forwardslash(this string original)
+            => original.Replace("\\", "/").Replace("//", "/").Replace("//", "/");
+
+    }
+}
