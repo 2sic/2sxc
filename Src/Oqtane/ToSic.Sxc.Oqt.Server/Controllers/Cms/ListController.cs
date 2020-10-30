e7cbed6d67738a5eb9cdb6aaca78850129d641d2
Tonći Vatavuk <tonci@sistemi.hr>
2020-10-30 09:34:09 +0100

    implement ListController

---
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/Controllers/Cms/ListController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using ToSic.Sxc.Oqt.Shared;
+using ToSic.Sxc.WebApi.FieldList;
+
+namespace ToSic.Sxc.Oqt.Server.Controllers
+{
+    [Route(WebApiConstants.WebApiStateRoot + "/cms/[controller]/[action]")]
+    [ValidateAntiForgeryToken]
+    //[Authorize(Policy = "EditModule")] // TODO: disabled
+    public class ListController : SxcStatefulControllerBase
+    {
+        protected override string HistoryLogName => "Api.List";
+
+        private FieldListBackend FieldBacked => Eav.Factory.Resolve<FieldListBackend>().Init(GetContext(), GetBlock(), Log);
+
+        public ListController(StatefulControllerDependencies dependencies) : base(dependencies)
+        {
+        }
+
+        /// <summary>
+        /// used to be GET Module/ChangeOrder
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="fields"></param>
+        /// <param name="index"></param>
+        /// <param name="toIndex"></param>
+        [HttpPost]
+        public void Move(Guid? parent, string fields, int index, int toIndex)
+            => FieldBacked.ChangeOrder(parent, fields, index, toIndex);
+
+        /// <summary>
+        /// Used to be Get Module/RemoveFromList
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="fields"></param>
+        /// <param name="index"></param>
+        [HttpDelete]
+        public void Delete(Guid? parent, string fields, int index)
+            => FieldBacked.Remove(parent, fields, index);
+    }
+}
