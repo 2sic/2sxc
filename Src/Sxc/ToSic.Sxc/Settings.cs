03e331b34f48294af963e21208c363f457c8ff80
ijungleboy <daniel.mettler@2sic.com>
2020-11-17 11:10:32 +0100

    releasing 11.10

--- Src/Sxc/ToSic.Sxc/Settings.cs
+++ Src/Sxc/ToSic.Sxc/Settings.cs
@@ -84,7 +84,7 @@ namespace ToSic.Sxc
                 "11.07.01", // LTS
                 "11.07.02", // LTS
                 "11.07.03", // LTS
-                "11.08.00",
+                "11.10.00",
             };
 
             // this is the last version which must run server-side change-code
