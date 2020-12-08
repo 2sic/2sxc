bb3cde27019ecedb43af1228493c0dcd24897fe4
ijungleboy <daniel.mettler@2sic.com>
2020-12-08 09:01:30 +0100

    ToSic.Eav.Core\Run\IIsUnknown.cs

--- Src/Sxc/ToSic.Sxc.WebApi/Context/UiContextBuilderUnknown.cs
+++ Src/Sxc/ToSic.Sxc.WebApi/Context/UiContextBuilderUnknown.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ToSic.Eav.Run;
 
 namespace ToSic.Sxc.WebApi.Context
 {
-    public sealed class UiContextBuilderUnknown: UiContextBuilderBase
+    public sealed class UiContextBuilderUnknown: UiContextBuilderBase, IIsUnknown
     {
         public UiContextBuilderUnknown(Dependencies dependencies) : base(dependencies)
         {
