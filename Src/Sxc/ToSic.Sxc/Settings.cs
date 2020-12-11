e4e480a6ee0f7e70742c55bdf6695ea1777389c3
ijungleboy <daniel.mettler@2sic.com>
2020-12-11 13:57:36 +0100

    Version bump 11.11.02 for future changes

--- Src/Sxc/ToSic.Sxc/Settings.cs
+++ Src/Sxc/ToSic.Sxc/Settings.cs
@@ -60,7 +60,7 @@ namespace ToSic.Sxc
                 "11.07.01", // LTS
                 "11.07.02", // LTS
                 "11.07.03", // LTS
-                "11.10.00", "11.10.01", "11.11.00", "11.11.01",
+                "11.10.00", "11.10.01", "11.11.00", "11.11.01", "11.11.02",
             };
 
             // this is the last version which must run server-side change-code
