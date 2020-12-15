a9d342e47a47e1ada0453a1a25891f43b204aa89
ijungleboy <daniel.mettler@2sic.com>
2020-12-15 16:39:26 +0100

    minor - clean up old code which will only run in .net451

--- Src/Sxc/ToSic.Sxc/Data/DynamicEntity_Obsolete.cs
+++ Src/Sxc/ToSic.Sxc/Data/DynamicEntity_Obsolete.cs
@@ -1,52 +1,46 @@
 ﻿using System;
 using ToSic.Eav.Documentation;
-using ToSic.Sxc.Edit.Toolbar;
-#if NET451
-using HtmlString = System.Web.HtmlString;
-using IHtmlString = System.Web.IHtmlString;
-#else
-using HtmlString = Microsoft.AspNetCore.Html.HtmlString;
-using IHtmlString = Microsoft.AspNetCore.Html.IHtmlContent;
-#endif
 
 namespace ToSic.Sxc.Data
 {
     public partial class DynamicEntity
     {
+#if NETFRAMEWORK
         /// <inheritdoc />
         [Obsolete("use Edit.Toolbar instead")]
         [PrivateApi]
-        public HtmlString Toolbar
+        public System.Web.IHtmlString Toolbar
         {
             get
             {
                 // if it's neither in a running context nor in a running portal, no toolbar
                 if (Block == null)
-                    return new HtmlString("");
+                    return new System.Web.HtmlString("");
 
                 // If we're not in a running context, of which we know the permissions, no toolbar
                 var userMayEdit = Block?.Context.UserMayEdit ?? false;
 
                 if (!userMayEdit)
-                    return new HtmlString("");
+                    return new System.Web.HtmlString("");
 
                 if (CompatibilityLevel == 10)
                     throw new Exception("content.Toolbar is deprecated in the new RazorComponent. Use @Edit.TagToolbar(content) or @Edit.Toolbar(content) instead. See https://r.2sxc.org/EditToolbar");
 
-                var toolbar = new ItemToolbar(Entity).Toolbar;
-                return new HtmlString(toolbar);
+                var toolbar = new ToSic.Sxc.Edit.Toolbar.ItemToolbar(Entity).Toolbar;
+                return new System.Web.HtmlString(toolbar);
             }
         }
 
         [Obsolete]
         [PrivateApi("probably we won't continue recommending to use this, but first we must provide an alternative")]
-        public IHtmlString Render()
+        public System.Web.IHtmlString Render()
         {
             if (CompatibilityLevel == 10)
                 throw new Exception("content.Render() is deprecated in the new RazorComponent. Use ToSic.Sxc.Blocks.Render.One(content) instead. See https://r.2sxc.org/EditToolbar");
 
             return Blocks.Render.One(this);
         }
+#endif
 
         [PrivateApi("shouldn't be used, but it may be published by accident, so shouldn't be removed. ")]
         [Obsolete("please use Get instead")]
