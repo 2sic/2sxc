ef927374f26a659f53ca1bd9668c2772a1000bfd
ijungleboy <daniel.mettler@2sic.com>
2020-12-19 10:04:24 +0100

    minor docs

--- Src/Sxc/ToSic.Sxc.WebApi/PublicApi/IAppQueryController.cs
+++ Src/Sxc/ToSic.Sxc.WebApi/PublicApi/IAppQueryController.cs
@@ -2,6 +2,13 @@
 
 namespace ToSic.Sxc.WebApi.PublicApi
 {
+    /// <summary>
+    /// In charge of delivering Pipeline-Queries on the fly
+    /// They will only be delivered if the security is confirmed - it must be publicly available
+    /// </summary>
+    /// <remarks>
+    /// The route for this is usually [some-base]/app/[app-name-or-auto]/query/[query-name]
+    /// </remarks>
     public interface IAppQueryController
     {
         // will check security internally, so assume no requirements
