af9c95baeb991e916228dbf436ea9c190ade3bc6
Tonći Vatavuk <tonci@sistemi.hr>
2021-02-10 14:05:44 +0100

    add 2sxc assets to 2sxc oqtane module nupkg

--- Src/Oqtane/ToSic.Sxc.Oqt.Package/release.cmd
+++ Src/Oqtane/ToSic.Sxc.Oqt.Package/release.cmd
@@ -1,2 +1,18 @@
+@set BuildTarget=..\ToSic.Sxc.Oqt.Server\wwwroot\Modules\ToSic.Sxc
+
+@REM Copy the data folders
+robocopy /mir "..\..\Data\.data\ " "%BuildTarget%\.data\ "
+robocopy /mir "..\..\Data\.databeta\ " "%BuildTarget%\.databeta\ "
+robocopy /mir "..\..\Data\.data-custom\ " "%BuildTarget%\.data-custom\ "
+
+@REM ... find better source
+@REM don't use this, the path is already in the env-variables @set Dev2sxcAssets=C:\Projects\2sxc\2sxc\Src\Mvc\Website\wwwroot
+
+@REM Copy 2sxc JS stuff
+robocopy /mir "%Dev2sxcAssets%\js\ " "%BuildTarget%\js\ "
+robocopy /mir "%Dev2sxcAssets%\dist\ " "%BuildTarget%\dist\ "
+robocopy /mir "%Dev2sxcAssets%\system\ " "%BuildTarget%\system\ "
+
+
 "c:\Projects\2sxc\oqtane\oqtane.framework\oqtane.package\nuget.exe" pack ToSic.Sxc.nuspec
 XCOPY "*.nupkg" "c:\Projects\2sxc\2sxc\InstallPackages\OqtaneModule\" /Y
