c3b0ecee12c3bad8541f2e8f278c1b13ce26f1da
Tonći Vatavuk <tonci@sistemi.hr>
2021-02-18 22:28:35 +0100

    web.config is not needed for oqtane

--- Src/Sxc/ToSic.Sxc/Engines/TemplateHelpers.cs
+++ Src/Sxc/ToSic.Sxc/Engines/TemplateHelpers.cs
@@ -24,7 +24,7 @@ namespace ToSic.Sxc.Engines
 
         #region Constructor / DI
 
-        
+
         private IServerPaths ServerPaths { get; }
         public IApp App;
         public TemplateHelpers(IServerPaths serverPaths, ILinkPaths linkPaths, IGlobalConfiguration globalConfiguration): base("Viw.Help")
@@ -63,10 +63,13 @@ namespace ToSic.Sxc.Engines
             // Create 2sxc folder if it does not exists
             sexyFolder.Create();
 
-            // Create web.config (copy from DesktopModules folder)
-            if (!sexyFolder.GetFiles(Settings.WebConfigFileName).Any())
-                File.Copy(Path.Combine(_globalConfiguration.GlobalFolder, Settings.WebConfigTemplateFile),
+            // Create web.config (copy from DesktopModules folder, but only if is there, and for Oqtane is not)
+            var webConfigTempletFilePath = Path.Combine(_globalConfiguration.GlobalFolder, Settings.WebConfigTemplateFile);
+            if (File.Exists(webConfigTempletFilePath) && !sexyFolder.GetFiles(Settings.WebConfigFileName).Any())
+            {
+                File.Copy(webConfigTempletFilePath,
                     Path.Combine(sexyFolder.FullName, Settings.WebConfigFileName));
+            }
 
             // Create a Content folder (or App Folder)
             if (string.IsNullOrEmpty(App.Folder))
