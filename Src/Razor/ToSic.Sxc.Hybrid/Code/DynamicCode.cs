2fe570a6b44cc2a613221a560cfc2ebd5542f549
Tonći Vatavuk <tonci@sistemi.hr>
2021-02-26 14:07:32 +0100

    dynamic code implementation for hybrid

---
+++ Src/Razor/ToSic.Sxc.Hybrid/Code/DynamicCode.cs
@@ -0,0 +1,20 @@
+﻿using ToSic.Eav.Documentation;
+
+namespace ToSic.Sxc.Hybrid.Code
+{
+    // ReSharper disable once UnusedMember.Global
+    /// <summary>
+    /// This is a base class for custom code files with context. <br/>
+    /// If you create a class file for dynamic use and inherit from this, then the compiler will automatically add objects like Link, Dnn, etc.
+    /// The class then also has AsDynamic(...) and AsList(...) commands like a normal razor page.
+    /// </summary>
+    [PublicApi_Stable_ForUseInYourCode]
+    public abstract class DynamicCode : Sxc.Code.DynamicCode, IHybridDynamicCode, IHasHybridDynamicCodeContext
+    {
+        /// <inheritdoc />
+        /// TODO: WIP
+        //public IOqtContext Oqt => DynCode?.Oqt;
+
+        [PrivateApi] public HybridDynamicCode DynCode => (UnwrappedContents as IHasHybridDynamicCodeContext)?.DynCode;
+    }
+}
---
+++ Src/Razor/ToSic.Sxc.Hybrid/Code/IHasHybridDynamicCodeContext.cs
@@ -0,0 +1,14 @@
+﻿using ToSic.Eav.Documentation;
+
+namespace ToSic.Sxc.Hybrid.Code
+{
+    /// <summary>
+    /// Marks objects which have DynCode - the connection to the Oqt and App objects
+    /// </summary>
+    [PrivateApi]
+    public interface IHasHybridDynamicCodeContext
+    {
+        [PrivateApi("internal, for passing around context!")]
+        HybridDynamicCode DynCode { get; }
+    }
+}
