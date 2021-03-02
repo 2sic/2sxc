cafe4258bbc9b0592f99304488db9ac233653100
Tonći Vatavuk <tonci@sistemi.hr>
2021-03-02 06:27:03 +0100

    comment inspect middleware

--- Src/Oqtane/ToSic.Sxc.Oqt.Server/InspectMiddleware/ConditionalMiddlewareBuilder.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/InspectMiddleware/ConditionalMiddlewareBuilder.cs
@@ -1,47 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
-using ToSic.Sxc.Oqt.Server.Controllers.WebApiRouting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.AspNetCore.Mvc.Routing;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿//using System;
+//using System.Collections.Generic;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Http.Features;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Mvc.Infrastructure;
+//using Microsoft.AspNetCore.Mvc.Routing;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
 
-namespace ToSic.Sxc.Oqt.Server.InspectMiddleware
-{
-    // based on https://github.com/aspnet/AspNetCore/blob/master/src/Middleware/MiddlewareAnalysis/src/AnalysisBuilder.cs
-    internal class ConditionalMiddlewareBuilder : IApplicationBuilder
-    {
-        private readonly string _runBefore;
-        public ConditionalMiddlewareBuilder(IApplicationBuilder inner, string runBefore)
-        {
-            _runBefore = runBefore;
-            InnerBuilder = inner;
-        }
+//namespace ToSic.Sxc.Oqt.Server.InspectMiddleware
+//{
+//    // based on https://github.com/aspnet/AspNetCore/blob/master/src/Middleware/MiddlewareAnalysis/src/AnalysisBuilder.cs
+//    internal class ConditionalMiddlewareBuilder : IApplicationBuilder
+//    {
+//        private readonly string _runBefore;
+//        public ConditionalMiddlewareBuilder(IApplicationBuilder inner, string runBefore)
+//        {
+//            _runBefore = runBefore;
+//            InnerBuilder = inner;
+//        }
 
-        private IApplicationBuilder InnerBuilder { get; }
+//        private IApplicationBuilder InnerBuilder { get; }
 
-        public IServiceProvider ApplicationServices
-        {
-            get => InnerBuilder.ApplicationServices;
-            set => InnerBuilder.ApplicationServices = value;
-        }
+//        public IServiceProvider ApplicationServices
+//        {
+//            get => InnerBuilder.ApplicationServices;
+//            set => InnerBuilder.ApplicationServices = value;
+//        }
 
-        public IDictionary<string, object> Properties => InnerBuilder.Properties;
-        public IFeatureCollection ServerFeatures => InnerBuilder.ServerFeatures;
-        public RequestDelegate Build() => InnerBuilder.Build();
-        public IApplicationBuilder New() => throw new NotImplementedException();
+//        public IDictionary<string, object> Properties => InnerBuilder.Properties;
+//        public IFeatureCollection ServerFeatures => InnerBuilder.ServerFeatures;
+//        public RequestDelegate Build() => InnerBuilder.Build();
+//        public IApplicationBuilder New() => throw new NotImplementedException();
 
-        public IApplicationBuilder Use(Func<RequestDelegate, RequestDelegate> middleware)
-        {
-            return InnerBuilder
-                .UseMiddleware<ConditionalMiddleware>(_runBefore)
-                .Use(middleware);
-        }
-    }
-}
+//        public IApplicationBuilder Use(Func<RequestDelegate, RequestDelegate> middleware)
+//        {
+//            return InnerBuilder
+//                .UseMiddleware<ConditionalMiddleware>(_runBefore)
+//                .Use(middleware);
+//        }
+//    }
+//}
--- Src/Oqtane/ToSic.Sxc.Oqt.Server/InspectMiddleware/ConditionalMiddlewareStartupFilter.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/InspectMiddleware/ConditionalMiddlewareStartupFilter.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+﻿//using System;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
 
-namespace ToSic.Sxc.Oqt.Server.InspectMiddleware
-{
-    // https://andrewlock.net/inserting-middleware-between-userouting-and-useendpoints-as-a-library-author-part-2/
-    public class ConditionalMiddlewareStartupFilter : IStartupFilter
-    {
-        private readonly string _runBefore;
-        public ConditionalMiddlewareStartupFilter(string runBefore)
-        {
-            _runBefore = runBefore;
-        }
+//namespace ToSic.Sxc.Oqt.Server.InspectMiddleware
+//{
+//    // https://andrewlock.net/inserting-middleware-between-userouting-and-useendpoints-as-a-library-author-part-2/
+//    public class ConditionalMiddlewareStartupFilter : IStartupFilter
+//    {
+//        private readonly string _runBefore;
+//        public ConditionalMiddlewareStartupFilter(string runBefore)
+//        {
+//            _runBefore = runBefore;
+//        }
 
-        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
-        {
-            return builder =>
-            {
-                var wrappedBuilder = new ConditionalMiddlewareBuilder(builder, _runBefore);
-                next(wrappedBuilder);
-            };
-        }
-    }
-}
+//        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+//        {
+//            return builder =>
+//            {
+//                var wrappedBuilder = new ConditionalMiddlewareBuilder(builder, _runBefore);
+//                next(wrappedBuilder);
+//            };
+//        }
+//    }
+//}
