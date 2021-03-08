95c63b265cce9e8e008a44426234ca93ef223c3b
Tonći Vatavuk <tonci@sistemi.hr>
2021-03-08 13:29:23 +0100

    re-factor duplicate code to OqtState

--- Src/Oqtane/ToSic.Sxc.Oqt.Server/Controllers/OqtStatefulControllerBase.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/Controllers/OqtStatefulControllerBase.cs
@@ -21,12 +21,14 @@ namespace ToSic.Sxc.Oqt.Server.Controllers
     public abstract class OqtStatefulControllerBase: OqtStatelessControllerBase, IHasOqtaneDynamicCodeContext
     {
         protected readonly IServiceProvider ServiceProvider;
+        private readonly OqtState _oqtState;
         private readonly IModuleRepository _moduleRepository;
         private readonly OqtTempInstanceContext _oqtTempInstanceContext;
 
         protected OqtStatefulControllerBase(StatefulControllerDependencies dependencies) : base()
         {
             ServiceProvider = dependencies.ServiceProvider;
+            _oqtState = new OqtState(HttpContext, Log);
             _moduleRepository = dependencies.ModuleRepository;
             _oqtTempInstanceContext = dependencies.OqtTempInstanceContext;
 
@@ -34,76 +36,17 @@ namespace ToSic.Sxc.Oqt.Server.Controllers
             dependencies.CtxResolver.AttachBlockContext(GetContext);
         }
 
+        protected IContextOfSite GetSiteContext() => _oqtState.GetSiteContext();
 
-        protected IContextOfSite GetSiteContext()
-        {
-            return ServiceProvider.Build<IContextOfSite>();
-        }
+        protected IContextOfApp GetAppContext(int appId) => _oqtState.GetAppContext(appId);
+
+        protected IContextOfBlock GetContext() => _oqtState.GetContext();
+
+        protected IBlock GetBlock(bool allowNoContextFound = true) => _oqtState.GetBlock(allowNoContextFound);
+
+        protected IApp GetApp(int appId) => _oqtState.GetApp(appId);
 
 
-        protected IContextOfApp GetAppContext(int appId)
-        {
-            // First get a normal basic context which is initialized with site, etc.
-            var appContext = ServiceProvider.Build<IContextOfApp>();
-            appContext.Init(Log);
-            appContext.ResetApp(appId);
-            return appContext;
-        }
-
-        protected IContextOfBlock GetContext() => GetBlock()?.Context ?? ServiceProvider.Build<IContextOfBlock>().Init(Log) as IContextOfBlock;
-
-        protected IBlock GetBlock(bool allowNoContextFound = true) => _block ??= InitializeBlock(allowNoContextFound);
-        private IBlock _block;
-
-        private IBlock InitializeBlock(bool allowNoContextFound)
-        {
-            var wrapLog = Log.Call<IBlock>($"request:..., {nameof(allowNoContextFound)}: {allowNoContextFound}");
-
-            var moduleId = GetTypedHeader(WebApi.WebApiConstants.HeaderInstanceId, -1);
-            var contentBlockId =
-                GetTypedHeader(WebApi.WebApiConstants.HeaderContentBlockId, 0); // this can be negative, so use 0
-            var pageId = GetTypedHeader(WebApi.WebApiConstants.HeaderPageId, -1);
-
-            if (moduleId == -1 || pageId == -1)
-            {
-                if (allowNoContextFound) return wrapLog("not found", null);
-                throw new Exception("No context found, cannot continue");
-            }
-
-            var module = _moduleRepository.GetModule(moduleId);
-            var ctx = _oqtTempInstanceContext.CreateContext(pageId, module, Log);
-            IBlock block = ServiceProvider.Build<BlockFromModule>().Init(ctx, Log);
-
-            // only if it's negative, do we load the inner block
-            if (contentBlockId > 0) return wrapLog("found", block);
-
-            Log.Add($"Inner Content: {contentBlockId}");
-            block = ServiceProvider.Build<BlockFromEntity>().Init(block, contentBlockId, Log);
-            return wrapLog("found", block);
-        }
-
-        private T GetTypedHeader<T>(string headerName, T fallback)
-        {
-            var valueString = Request.Headers[headerName];
-            if (valueString == StringValues.Empty) return fallback;
-
-            try
-            {
-                return (T)Convert.ChangeType(valueString.ToString(), typeof(T));
-            }
-            catch
-            {
-                return fallback;
-            }
-
-        }
-
-        /// <summary>
-        /// used for API calls to get the current app
-        /// </summary>
-        /// <param name="appId"></param>
-        /// <returns></returns>
-        internal IApp GetApp(int appId) => ServiceProvider.Build<App>().Init(ServiceProvider, appId, Log, GetBlock());
 
         public string CreateInstancePath { get; set; }
 
@@ -117,6 +60,8 @@ namespace ToSic.Sxc.Oqt.Server.Controllers
             bool throwOnError = true) =>
             DynCode.CreateInstance(virtualPath, dontRelyOnParameterOrder, name,
                 CreateInstancePath, throwOnError);
+
+
 
         #region Adam - Shared Code Across the APIs (prevent duplicate code)
 
@@ -141,7 +86,7 @@ namespace ToSic.Sxc.Oqt.Server.Controllers
             if (!Eav.Configuration.Features.EnabledOrException(feats, "can't save in ADAM", out var exp))
                 throw exp;
 
-            var appId = DynCode?.Block?.AppId ?? DynCode?.App?.AppId ?? throw new Exception("Error, SaveInAdam needs an App-Context to work, but the App is not known.");
+            var appId = DynCode?.Block?.AppId ?? DynCode?.App?.AppId ?? throw new Exception("Error, SaveInAdam needs an App-OqtState to work, but the App is not known.");
             return ServiceProvider.Build<AdamTransUpload<int, int>>(typeof(AdamTransUpload<int, int>))
                 .Init(appId, contentType, guid.Value, field, false, Log)
                 .UploadOne(stream, fileName, subFolder, true);
