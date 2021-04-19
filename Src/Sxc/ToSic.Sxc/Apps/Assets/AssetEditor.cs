feb71b08f533fe6a0868f6ebe47afb8d37d7672c
Tonći Vatavuk <tonci@sistemi.hr>
2021-04-19 13:30:27 +0200

    new api cs generated from template that is using ToSic.Custom.Api12

--- Src/Sxc/ToSic.Sxc/Apps/Assets/AssetEditor.cs
+++ Src/Sxc/ToSic.Sxc/Apps/Assets/AssetEditor.cs
@@ -98,15 +98,16 @@ using DotNetNuke.Web.Api;	// this is to verify the AntiForgeryToken
 
 [AllowAnonymous]			// define that all commands can be accessed without a login
 [ValidateAntiForgeryToken]	// protects the API from users not on your site (CSRF protection)
-// Inherit from ToSic...ApiController to get features like App, Data or Dnn - see https://r.2sxc.org/CustomWebApi
-public class " + CsApiTemplateControllerName + @" : ToSic.Sxc.Dnn.ApiController
+// Inherit from ToSic.Custom.Api12 to get features like App, Data...
+// see https://docs.2sxc.org/api/dot-net/ToSic.Custom.Api12.html and https://docs.2sxc.org/web-api/custom/index.html
+public class " + CsApiTemplateControllerName + @" : ToSic.Custom.Api12
 {
 
-	[HttpGet]				// [HttpGet] says we're listening to GET requests
-	public string Hello()
-	{
-		return ""Hello from the controller with ValidateAntiForgeryToken in /api"";
-	}
+    [HttpGet]				// [HttpGet] says we're listening to GET requests
+    public string Hello()
+    {
+        return ""Hello from the controller with ValidateAntiForgeryToken in /api"";
+    }
 
 }
 ";
