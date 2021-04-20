83cd7e09405cfd54911f3e4c78f1f3598bd4d5dc
Tonći Vatavuk <tonci@sistemi.hr>
2021-04-21 00:25:39 +0200

    parent razor page provide _DynCodeRoot to child razer page with ViewData

--- Src/Razor/ToSic.Sxc.Hybrid/Razor/Razor12_T_Block.cs
+++ Src/Razor/ToSic.Sxc.Hybrid/Razor/Razor12_T_Block.cs
@@ -1,31 +1,46 @@
-﻿using ToSic.Sxc.Code;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Razor.Internal;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using ToSic.Sxc.Code;
+
 
 namespace ToSic.Custom
 {
     public partial class Razor12<TModel>: IRazor12
     {
+        const string DynCode = "_dynCode";
+
         #region DynCode
 
         public IDynamicCodeRoot _DynCodeRoot
         {
-            get => _dynCode;// 2dm I don't think this inner call would ever work, as it needs DynCode to work ??= GetService<DynamicCodeRoot>().Init(Block, Log);
+            get
+            {
+                // Child razor page will have _dynCode == null, so it is provided via ViewData from parent razor page.
+                if (_dynCode == null && ViewData != null && ViewData[DynCode] != null) _dynCode = ViewData[DynCode] as IDynamicCodeRoot;
+
+                return _dynCode;
+            }
+
             set => _dynCode = value;
         }
 
         private IDynamicCodeRoot _dynCode;
         #endregion
 
-        //public virtual IBlock Block 
-        //{
-        //    get
-        //    {
-        //        if (_block != null) return _block;
-        //        if(_dynCode == null) throw new Exception($"{nameof(BlockBuilder)} is empty, and DynCode isn't created - can't continue. Requires DynCode to be attached");
-        //        return _block = _dynCode._Block;
-        //    }
-        //    protected set => _block = value;
-        //}
-        //protected IBlock _block;
-
+        /// <summary>
+        /// Gets or sets the dictionary for view data.
+        /// </summary>
+        [RazorInject]
+        public new ViewDataDictionary<TModel> ViewData
+        {
+            get => base.ViewData;
+            set
+            {
+                base.ViewData = value;
+                // Store _dynCode in ViewData, for child razor page.
+                if (_dynCode != null && base.ViewData != null && base.ViewData[DynCode] == null) base.ViewData[DynCode] = _dynCode;
+            }
+        }
     }
 }
