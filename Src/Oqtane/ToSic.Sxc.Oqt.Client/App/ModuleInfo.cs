070f798f2660a06c7546329f18cf71dd8e568f26
Tonći Vatavuk <tonci@sistemi.hr>
2021-04-24 09:56:26 +0200

    Oqtane db fix for https://github.com/oqtane/oqtane.framework/issues/1269

--- Src/Oqtane/ToSic.Sxc.Oqt.Client/App/ModuleInfo.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Client/App/ModuleInfo.cs
@@ -11,10 +11,14 @@ namespace ToSic.Sxc.Oqt.App
             Description = "2sxc helps create designed, cross-platform content",
             Version = "11.90.0",
             ServerManagerType = "ToSic.Sxc.Oqt.Server.Manager.SxcManager, ToSic.Sxc.Oqtane.Server",
-            ReleaseVersions = "11.90.0",
+            ReleaseVersions = "0.0.1,11.90.0",
             Dependencies = "ToSic.Sxc.Oqtane.Shared",
             Categories = "Common",
             Runtimes = "Server",
         };
     }
+    /*
+     * 0.0.1 - Oqtane db fix for https://github.com/oqtane/oqtane.framework/issues/1269 (probably we will remove latter).
+     * 11.90.0 - 2sxc oqtane module pre-release.
+     */
 }
