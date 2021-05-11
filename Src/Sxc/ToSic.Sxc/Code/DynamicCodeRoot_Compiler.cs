089407da2058056af342595f7dc1bc190fa1258e
Tonći Vatavuk <tonci@sistemi.hr>
2021-05-11 17:24:38 +0200

    fix compiler cache to cache oqt assembly only

--- Src/Sxc/ToSic.Sxc/Code/DynamicCodeRoot_Compiler.cs
+++ Src/Sxc/ToSic.Sxc/Code/DynamicCodeRoot_Compiler.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.Caching;
-using ToSic.Eav.Run;
-
-namespace ToSic.Sxc.Code
+﻿namespace ToSic.Sxc.Code
 {
     public partial class DynamicCodeRoot
     {
@@ -20,46 +15,23 @@ namespace ToSic.Sxc.Code
             Eav.Constants.ProtectAgainstMissingParameterNames(dontRelyOnParameterOrder, "CreateInstance",
                 $"{nameof(name)},{nameof(throwOnError)}");
 
-            var cache = MemoryCache.Default;
-            var instance = cache[virtualPath.ToLowerInvariant()];
-            if (instance == null)
-            {
-                // Compile
-                var compiler = Deps.CodeCompilerLazy.IsValueCreated
-                    ? Deps.CodeCompilerLazy.Value
-                    : Deps.CodeCompilerLazy.Value.Init(Log);
-                instance = compiler
-                    //new CodeCompiler(_serviceProvider)
-                    //.Init(Log)
-                    .InstantiateClass(virtualPath, name, relativePath, throwOnError);
+            // Compile
+            var compiler = Deps.CodeCompilerLazy.IsValueCreated
+                ? Deps.CodeCompilerLazy.Value
+                : Deps.CodeCompilerLazy.Value.Init(Log);
+            var instance = compiler
+                //new CodeCompiler(_serviceProvider)
+                //.Init(Log)
+                .InstantiateClass(virtualPath, name, relativePath, throwOnError);
 
-                // if it supports all our known context properties, attach them
-                if (instance is ICoupledDynamicCode isShared) isShared.DynamicCodeCoupling(this);
-
-                if (instance != null)
-                    cache.Set(virtualPath.ToLowerInvariant(), instance, GetCacheItemPolicy(virtualPath));
-            }
+            // if it supports all our known context properties, attach them
+            if (instance is ICoupledDynamicCode isShared) isShared.DynamicCodeCoupling(this);
 
             return wrap((instance != null).ToString(), instance);
         }
 
         /// <inheritdoc />
         public string CreateInstancePath { get; set; }
-
-        private CacheItemPolicy GetCacheItemPolicy(string virtualPath)
-        {
-            var serverPaths = GetService<IServerPaths>();
-            var fullFilePath = serverPaths.FullContentPath(virtualPath);
-            var filePaths = new List<string> { fullFilePath };
-
-            var cacheItemPolicy = new CacheItemPolicy();
-            // expire cache item if not used in 30 mins
-            cacheItemPolicy.SlidingExpiration = TimeSpan.FromMinutes(30);
-            // expire cache item on CS file change
-            cacheItemPolicy.ChangeMonitors.Add(new
-                HostFileChangeMonitor(filePaths));
-            return cacheItemPolicy;
-        }
 
         #endregion
     }
