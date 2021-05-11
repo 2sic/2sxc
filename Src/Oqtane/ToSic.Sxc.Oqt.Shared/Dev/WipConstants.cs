ed6e357572808f4164c4f76989e2004d6fe58df7
ijungleboy <daniel.mettler@2sic.com>
2021-05-11 15:20:36 +0200

    minor clean-up

--- Src/Oqtane/ToSic.Sxc.Oqt.Shared/Dev/WipConstants.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Shared/Dev/WipConstants.cs
@@ -1,27 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using Oqtane.Models;
-using ToSic.Eav.Run;
-
-namespace ToSic.Sxc.Oqt.Shared.Dev
+﻿namespace ToSic.Sxc.Oqt.Shared.Dev
 {
     /// <summary>
     /// Temporary constants which should be removed soon
     /// </summary>
     public class WipConstants
     {
-        public static string DefaultLanguage => "en-us";
-        public static Dictionary<string,string> EmptyLanguages = new Dictionary<string, string>();
-        public static List<TempTempCulture> EmptyCultureList = new List<TempTempCulture>();
-
-        //public static string ContentRoot = "/content-wip/";
-
         // ADAM
         public const int MaxUploadSize = 25000;
         public const int ParentFolderNotFound = 0;
         public static void AdamNotImplementedYet() { }
-       
-
+        
         public static void DontDoAnythingImplementLater() { }
 
         // Settings
