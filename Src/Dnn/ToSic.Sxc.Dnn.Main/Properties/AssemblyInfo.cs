367f31ba4509776e7dc13d59a437b77efb86393c
ijungleboy <daniel.mettler@2sic.com>
2021-05-11 19:47:38 +0200

    Version bump fixing CmsBlock REST issue https://github.com/2sic/2sxc/issues/2413

--- Src/Dnn/ToSic.Sxc.Dnn.Main/Properties/AssemblyInfo.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.Main/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("11.22.00.*")]
+[assembly: AssemblyVersion("11.22.01.*")]
 
 
 [assembly: InternalsVisibleTo("ToSic.SexyContent.WebApi")]
