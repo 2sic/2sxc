2e793a6729e34333d32efd730718355989061929
Tonći Vatavuk <tonci@sistemi.hr>
2021-05-25 17:50:00 +0200

    add missing latest release to ReleaseVersions

--- Src/Oqtane/ToSic.Sxc.Oqt.Client/Content/ModuleInfo.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Client/Content/ModuleInfo.cs
@@ -11,7 +11,7 @@ namespace ToSic.Sxc.Oqt.Content
             Description = "2sxc helps create designed, cross-platform content",
             Version = "12.01.00",
             ServerManagerType = "ToSic.Sxc.Oqt.Server.Manager.SxcManager, ToSic.Sxc.Oqtane.Server",
-            ReleaseVersions = "0.0.1,12.00.00", // list versions with sql scripts in \ToSic.Sxc.Oqt.Server\Scripts\
+            ReleaseVersions = "0.0.1,12.00.00,12.01.00", // list versions with sql scripts in \ToSic.Sxc.Oqt.Server\Scripts\
             Dependencies = "ToSic.Sxc.Oqtane.Shared",
             Categories = "Common",
             //Runtimes = "Server",
