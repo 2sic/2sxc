7cf7f5eec68fb427db99078164e1e92ee5c3c808
ijungleboy <daniel.mettler@2sic.com>
2021-06-07 19:06:37 +0200

    Experimental Resources on Razor using DynamicStack

---
+++ Src/Dnn/ToSic.Sxc.Razor/Custom/Hybrid/Razor12_WIP.cs
@@ -0,0 +1,17 @@
+﻿using ToSic.Sxc.Context;
+using ToSic.Sxc.Data;
+
+// ReSharper disable once CheckNamespace
+namespace Custom.Hybrid
+{
+    public partial class Razor12
+    {
+        protected dynamic Resources => _resources ?? (_resources = new DynamicStack(
+            _DynCodeRoot.Block, 
+            _DynCodeRoot.DataSourceFactory.ServiceProvider, 
+            CmsContext.SafeLanguagePriorityCodes(), 
+            _DynCodeRoot.Block?.View?.Resources, 
+            _DynCodeRoot.App?.Resources?.Entity));
+        private dynamic _resources;
+    }
+}
---
+++ Src/Sxc/ToSic.Sxc/Data/DynamicStack/DynamicStack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Dynamic;
+using ToSic.Eav.Data;
+using ToSic.Eav.Documentation;
+using ToSic.Sxc.Blocks;
+
+namespace ToSic.Sxc.Data
+{
+    [PrivateApi("WIP")]
+    public class DynamicStack: DynamicEntityBase, IWrapper<IEntityStack>
+    {
+        public DynamicStack(IBlock block, IServiceProvider serviceProvider, string[] dimensions, params IEntity[] entities) : base(block, serviceProvider, dimensions)
+        {
+            var stack = new EntityStack();
+            stack.Init(dimensions, entities);
+            UnwrappedContents = stack;
+        }
+        
+
+        public IEntityStack UnwrappedContents { get; }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = UnwrappedContents.Value(binder.Name);
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+            => throw new NotImplementedException($"Setting a value on {nameof(DynamicStack)} is not supported");
+
+    }
+}
