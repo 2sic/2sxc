8e79ea079bd497cf4466867062715ec3ebb71533
Tonći Vatavuk <tonci@sistemi.hr>
2021-06-09 16:40:09 +0200

    update for Oqt 2.0.2

--- Src/Oqtane/ToSic.Sxc.Oqt.Server/Scripts/ToSic.Sxc.0.0.1.sql
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/Scripts/ToSic.Sxc.0.0.1.sql
@@ -1,11 +1,10 @@
--- Commented because it is fixed from Oqt 2.1.0
 /* Oqtane db fix for https://github.com/oqtane/oqtane.framework/issues/1269 */
---BEGIN TRANSACTION
---GO
---IF COLUMNPROPERTY(OBJECT_ID('Folder', 'U'), 'Path', 'charmaxlen')=50
---ALTER TABLE dbo.Folder ALTER COLUMN [Path] nvarchar(1000) NOT NULL
---GO
---IF COLUMNPROPERTY(OBJECT_ID('Folder', 'U'), 'Name', 'charmaxlen')=50
---ALTER TABLE dbo.Folder ALTER COLUMN [Name] nvarchar(250) NOT NULL
---GO
---COMMIT
+BEGIN TRANSACTION
+GO
+IF COLUMNPROPERTY(OBJECT_ID('Folder', 'U'), 'Path', 'charmaxlen')=50
+ALTER TABLE dbo.Folder ALTER COLUMN [Path] nvarchar(512) NOT NULL
+GO
+IF COLUMNPROPERTY(OBJECT_ID('Folder', 'U'), 'Name', 'charmaxlen')=50
+ALTER TABLE dbo.Folder ALTER COLUMN [Name] nvarchar(256) NOT NULL
+GO
+COMMIT
