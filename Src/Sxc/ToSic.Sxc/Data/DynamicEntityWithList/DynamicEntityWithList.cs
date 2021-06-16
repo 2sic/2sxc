e440d8b2439c978a416c0f6b00d80f3c81d0a2ce
ijungleboy <daniel.mettler@2sic.com>
2021-06-16 14:32:17 +0200

    Update DynamicEntityWithList.cs
    minor lazy-init

--- Src/Sxc/ToSic.Sxc/Data/DynamicEntityWithList/DynamicEntityWithList.cs
+++ Src/Sxc/ToSic.Sxc/Data/DynamicEntityWithList/DynamicEntityWithList.cs
@@ -13,36 +13,58 @@ namespace ToSic.Sxc.Data
     /// So if it has any elements you can enumerate it (foreach). <br/>
     /// You can also do things like `.EntityId` or `.SomeProperty` just like a DynamicEntity.
     /// </summary>
-    /// <remarks>Added in 2sxc 10.27</remarks>
-    [PublicApi_Stable_ForUseInYourCode]
+    /// <remarks>
+    /// * Added in 2sxc 10.27
+    /// * Hidden from docs in 2sxc 12.03 because we'll probably merge the functionality with IDynamicEntity
+    /// - it was never actually documented / mentioned, and since the use is usually dynamic, people won't have any code with the type-name
+    /// </remarks>
+    [PrivateApi]
     public partial class DynamicEntityWithList: DynamicEntity, IReadOnlyList<IDynamicEntity>
     {
         [PrivateApi]
-        protected List<IDynamicEntity> DynEntities;
+        protected List<IDynamicEntity> DynEntities {
+            get
+            {
+                if (_list != null) return _list;
+                var index = 0;
+                _list = _listEntities != null // note: ATM as of 2021-06-16 this can never happen, but it will in future for single-entity use
+                    ? _listEntities
+                        .Select(e =>
+                        {
+                            // we create an Entity with some metadata-decoration, so that toolbars know it's part of a list
+                            var blockEntity = new EntityInBlock(e, _parent.EntityGuid, _parentField, index++);
+                            return SubDynEntity(blockEntity);
+                        })
+                        .ToList()
+                    : new List<IDynamicEntity> {this};
+                return _list;
+            }
+        }
+
+        private List<IDynamicEntity> _list;
+        private readonly IEntity[] _listEntities;
 
         // ReSharper disable once NotAccessedField.Local
-        private string _debugFieldName;
+        private readonly IEntity _parent;
+        private readonly string _parentField;
 
         [PrivateApi]
-        internal DynamicEntityWithList(
-            IEntity parent, 
-            string field, 
-            IEnumerable<IEntity> entities, 
-            DynamicEntityDependencies dependencies
-            ) 
+        internal DynamicEntityWithList(IEntity parent, string field, IEnumerable<IEntity> entities, DynamicEntityDependencies dependencies) 
             : base(null, dependencies)
         {
-            _debugFieldName = field; // remember name in case we do debugging and need to know what list was accessed
-            var index = 0;
-            DynEntities = entities
-                .Select(e =>
-                {
-                    // we create an Entity with some metadata-decoration, so that toolbars know it's part of a list
-                    var blockEntity = new EntityInBlock(e, parent.EntityGuid, field, index++);
-                    return SubDynEntity(blockEntity);
-                })
-                .ToList();
-            SetEntity(DynEntities.FirstOrDefault()?.Entity
+            _parent = parent;
+            _parentField = field;
+            _listEntities = entities.ToArray();
+            //var index = 0;
+            //DynEntities = _listEntities
+            //    .Select(e =>
+            //    {
+            //        // we create an Entity with some metadata-decoration, so that toolbars know it's part of a list
+            //        var blockEntity = new EntityInBlock(e, parent.EntityGuid, _parentField, index++);
+            //        return SubDynEntity(blockEntity);
+            //    })
+            //    .ToList();
+            SetEntity(_listEntities.FirstOrDefault() //?.Entity
                      // check empty list - create a dummy Entity so toolbars will know what to do
                      ?? PlaceHolder(parent, field));
         }
@@ -51,7 +73,7 @@ namespace ToSic.Sxc.Data
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-        public int Count => DynEntities.Count;
+        public int Count => _listEntities.Length;
 
         public IDynamicEntity this[int index]
         {
@@ -60,7 +82,7 @@ namespace ToSic.Sxc.Data
             set => throw new NotImplementedException();
         }
 
-        public EntityInBlock PlaceHolder(IEntity parent, string field)
+        private EntityInBlock PlaceHolder(IEntity parent, string field)
         {
             var sp = _Dependencies.ServiceProviderOrNull ?? Eav.Factory.GetServiceProvider();
             var builder = sp.Build<IDataBuilder>();
