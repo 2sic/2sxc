2e4fe0533b7d8a9509cfaf0e6f208bf9a10e0a44
ijungleboy <daniel.mettler@2sic.com>
2021-08-26 13:24:28 +0200

    finalize

--- Src/Sxc/ToSic.Sxc/Web/PageService/PageService_Features.cs
+++ Src/Sxc/ToSic.Sxc/Web/PageService/PageService_Features.cs
@@ -1,66 +1,44 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using ToSic.Eav.Context;
 using ToSic.Sxc.Data;
 using ToSic.Sxc.Web.PageFeatures;
+using static ToSic.Eav.Configuration.ConfigurationConstants;
 
 namespace ToSic.Sxc.Web.PageService
 {
     public partial class PageService
     {
-
         /// <inheritdoc />
         public void Activate(params string[] keys)
         {
+            // 1. Try to add manual resources from WebResources
+            // This must happen in the IPageService which is per-module
+            // The PageServiceShared cannot do this, because it doesn't have the WebResources which vary by module
             var webRes = WebResources;
-
-            // TODO: CONTINUE HERE - NOW wEBrESOURCES IS A dYNAMIC AND WE SHOULD EASILY WORK WITH IT
             if (webRes != null)
             {
-                //var siteContext = CtxResolver.Site();
-                //var languages = siteContext.Site.SafeLanguagePriorityCodes();
                 var keysToRemove = new List<string>();
                 foreach (var key in keys)
                 {
                     var resConfig = webRes.Get(key) as DynamicEntity;
                     if(resConfig == null) continue;
 
-                    var enabled = resConfig.Get("Enabled") as bool?;
-
-
-                    //var resSettingsProperty = webRes.FindPropertyInternal(key, languages, null);
-                    //if (!resSettingsProperty.IsFinal || !(resSettingsProperty.Result is IEnumerable<IEntity> listSettings)) continue;
-                    
-                    //var resSettings = listSettings.FirstOrDefault();
-                    //if (resSettings == null) continue;
-
-                    //var enabledProp = resSettings.FindPropertyInternal("Enabled", languages, null);
-                    //if (!enabledProp.IsFinal || enabledProp.FieldType != DataTypes.Boolean) continue;
-
-                    //var enabled = enabledProp.Result as bool?;
+                    var enabled = resConfig.Get(WebResourceEnabledField) as bool?;
                     if (enabled == false) continue;
 
-                    //var htmlProp = resSettings.FindPropertyInternal("Html", languages, null);
-                    //if (!htmlProp.IsFinal || enabledProp.FieldType != DataTypes.String) continue;
-                    //var html = htmlProp.Result as string;
-
-                    var html = resConfig.Get("Html") as string;
-
+                    var html = resConfig.Get(WebResourceHtmlField) as string;
                     if (html == null) continue;
 
                     // all ok so far
                     keysToRemove.Add(key);
-                    PageServiceShared.Features.ManualFeatureAdd(new PageFeature(
-                        key, 
-                        "manual",
-                        "manual-description",
-                        html: html));
+                    PageServiceShared.Features.ManualFeatureAdd(new PageFeature(key, "", "", html: html));
                 }
 
                 // drop keys which were already taken care of
                 keys = keys.Where(k => !keysToRemove.Contains(k)).ToArray();
             }
 
+            // 2. If any keys are left, they are probably preconfigured keys, so add them now
             if (keys.Any())
                 PageServiceShared.Activate(keys);
         }
@@ -70,39 +48,12 @@ namespace ToSic.Sxc.Web.PageService
             get
             {
                 if (_alreadyTriedToFindWebResources) return _webResources;
-                var settings = SettingsStack;
-                if (settings != null)
-                {
-                    var resources = settings.Get("WebResources"); // .FindPropertyInternal("WebResources", new string[] { null }, null);
-                    if(resources != null)
-                        _webResources = resources as DynamicEntity;
-                    //if (resources.FieldType == DataTypes.Entity && resources.Result is IEnumerable<IEntity> webResEntities)
-                    //    _webResources = webResEntities.FirstOrDefault();
-                }
+                _webResources = (CodeRoot?.Settings as DynamicStack)?.Get(WebResourcesNode) as DynamicEntity;
                 _alreadyTriedToFindWebResources = true;
                 return _webResources;
             }
         }
         private DynamicEntity _webResources;
         private bool _alreadyTriedToFindWebResources;
-
-        private DynamicStack SettingsStack => _settingsStack ?? (_settingsStack = LoadSettings());
-        private DynamicStack _settingsStack;
-
-        private DynamicStack LoadSettings()
-        {
-            return CodeRoot?.Settings as DynamicStack;
-
-            // Since the IPageService is used in templates, this should always return something real
-            // But just to be sure, we'll go for the null-check
-            //var maybeBlock = CtxResolver.BlockOrNull();
-            //var appState = maybeBlock?.AppState;
-            //var sources = appState?.SettingsInApp.GetStack(true, null);
-            //if (sources == null) return null;
-            //var settings = new PropertyStack();
-            //settings.Init(AppConstants.RootNameSettings, sources.ToArray());
-
-            //return settings;
-        }
     }
 }
