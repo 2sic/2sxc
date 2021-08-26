48762f9fe3c59a9ef33b35a02fa52ce71a6b270f
ijungleboy <daniel.mettler@2sic.com>
2021-08-26 14:11:55 +0200

    logging & bugfix

--- Src/Sxc/ToSic.Sxc/Web/PageService/PageService.cs
+++ Src/Sxc/ToSic.Sxc/Web/PageService/PageService.cs
@@ -1,17 +1,16 @@
 ﻿using ToSic.Eav.Documentation;
 using ToSic.Eav.Logging;
-using ToSic.Sxc.Blocks;
 using ToSic.Sxc.Code;
 using ToSic.Sxc.Context;
 
 namespace ToSic.Sxc.Web.PageService
 {
-    public partial class PageService: IPageService, INeedsCodeRoot
+    public partial class PageService: HasLog, IPageService, INeedsCodeRoot
     {
         public PageServiceShared PageServiceShared { get; }
         public IContextResolver CtxResolver { get; }
 
-        public PageService(PageServiceShared pageServiceShared, IContextResolver ctxResolver)
+        public PageService(PageServiceShared pageServiceShared, IContextResolver ctxResolver): base("2sxc.PgeSrv")
         {
             PageServiceShared = pageServiceShared;
             CtxResolver = ctxResolver;
@@ -20,6 +19,8 @@ namespace ToSic.Sxc.Web.PageService
         public void AddBlockContext(IDynamicCodeRoot codeRoot)
         {
             CodeRoot = codeRoot;
+            Log.LinkTo(codeRoot?.Log);
+            Log.Call(message: $"Linked {nameof(PageService)}")(null);
         }
 
         public IDynamicCodeRoot CodeRoot;
--- Src/Sxc/ToSic.Sxc/Web/PageService/PageService_Features.cs
+++ Src/Sxc/ToSic.Sxc/Web/PageService/PageService_Features.cs
@@ -11,36 +11,46 @@ namespace ToSic.Sxc.Web.PageService
         /// <inheritdoc />
         public void Activate(params string[] keys)
         {
+            var wrapLog = Log.Call();
+
             // 1. Try to add manual resources from WebResources
             // This must happen in the IPageService which is per-module
             // The PageServiceShared cannot do this, because it doesn't have the WebResources which vary by module
-            var webRes = WebResources;
-            if (webRes != null)
-            {
-                var keysToRemove = new List<string>();
-                foreach (var key in keys)
-                {
-                    var resConfig = webRes.Get(key) as DynamicEntity;
-                    if(resConfig == null) continue;
-
-                    var enabled = resConfig.Get(WebResourceEnabledField) as bool?;
-                    if (enabled == false) continue;
-
-                    var html = resConfig.Get(WebResourceHtmlField) as string;
-                    if (html == null) continue;
-
-                    // all ok so far
-                    keysToRemove.Add(key);
-                    PageServiceShared.Features.ManualFeatureAdd(new PageFeature(key, "", "", html: html));
-                }
-
-                // drop keys which were already taken care of
-                keys = keys.Where(k => !keysToRemove.Contains(k)).ToArray();
-            }
+            if (WebResources != null) 
+                keys = AddManualResources(keys);
 
             // 2. If any keys are left, they are probably preconfigured keys, so add them now
             if (keys.Any())
                 PageServiceShared.Activate(keys);
+            
+            wrapLog(null);
+        }
+
+        private string[] AddManualResources(string[] keys)
+        {
+            var wrapLog = Log.Call<string[]>();
+            var keysToRemove = new List<string>();
+            foreach (var key in keys)
+            {
+                Log.Add($"Key: {key}");
+                var resConfig = WebResources.Get(key) as DynamicEntity;
+                if (resConfig is null) continue; // special problem: DynamicEntity null-compare isn't quite right, don't! use ==
+
+                var enabled = resConfig.Get(WebResourceEnabledField) as bool?;
+                if (enabled == false) continue;
+
+                var html = resConfig.Get(WebResourceHtmlField) as string;
+                if (html == null) continue;
+
+                Log.Add("Found html and everything, will register");
+                // all ok so far
+                keysToRemove.Add(key);
+                PageServiceShared.Features.ManualFeatureAdd(new PageFeature(key, "", "", html: html));
+            }
+
+            // drop keys which were already taken care of
+            keys = keys.Where(k => !keysToRemove.Contains(k)).ToArray();
+            return wrapLog(null, keys);
         }
 
         private DynamicEntity WebResources
