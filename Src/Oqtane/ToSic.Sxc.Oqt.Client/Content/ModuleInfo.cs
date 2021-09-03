0ff4df56280657be77d035357fb8de81ea7c136b
ijungleboy <daniel.mettler@2sic.com>
2021-09-03 10:50:51 +0200

    version bump 12.04.01

--- Src/Oqtane/ToSic.Sxc.Oqt.Client/Content/ModuleInfo.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Client/Content/ModuleInfo.cs
@@ -18,12 +18,12 @@ namespace ToSic.Sxc.Oqt.Content
         {
             Name = "Content",
             Description = "2sxc helps create designed, cross-platform content",
-            Version = "12.04.00",
+            Version = "12.04.01",
             ServerManagerType = "ToSic.Sxc.Oqt.Server.Manager.SxcManager, ToSic.Sxc.Oqtane.Server",
             // This must contain all versions with a SQL script and current/latest version
             // list versions with sql scripts in \ToSic.Sxc.Oqt.Server\Scripts\
             // Always keep this in sync wth the App and Content ModuleInfo.cs
-            ReleaseVersions = "0.0.1,12.00.00,12.02.00,12.02.01,12.04.00",
+            ReleaseVersions = "0.0.1,12.00.00,12.02.00,12.02.01,12.04.01,12.04.01",
             Dependencies = "ToSic.Sxc.Oqtane.Shared",
             Categories = "Common",
             //Runtimes = "Server",
