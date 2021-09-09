af2bf6087b2670119e8c94c5a5577d89b14092cc
ijungleboy <daniel.mettler@2sic.com>
2021-09-09 17:36:47 +0200

    implement IConvertJson service #2513

---
+++ Src/Sxc/ToSic.Sxc/Data/Convert/ConvertJson.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using ToSic.Eav.Documentation;
+
+namespace ToSic.Sxc.Data
+{
+    [PrivateApi("Hide implementation")]
+    public class ConvertJson: IConvertJson
+    {
+        /// <inheritdoc />
+        public T Deserialize<T>(string json) => JsonConvert.DeserializeObject<T>(json);
+
+        /// <inheritdoc />
+        public object Deserialize(string json) => JsonConvert.DeserializeObject(json);
+
+        /// <inheritdoc />
+        public string Serialize(object dynamicEntity) => JsonConvert.SerializeObject(dynamicEntity);
+    }
+}
