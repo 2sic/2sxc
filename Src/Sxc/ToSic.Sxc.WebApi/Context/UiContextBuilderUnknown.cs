eecbcdbc893fe0147db0d19a0b0dcdde85c9b301
ijungleboy <daniel.mettler@2sic.com>
2021-09-23 13:48:56 +0200

    add insights warnings to unknown services #2529

--- Src/Sxc/ToSic.Sxc.WebApi/Context/UiContextBuilderUnknown.cs
+++ Src/Sxc/ToSic.Sxc.WebApi/Context/UiContextBuilderUnknown.cs
@@ -1,10 +1,11 @@
 ﻿using ToSic.Eav.Run;
+using ToSic.Eav.Run.Unknown;
 
 namespace ToSic.Sxc.WebApi.Context
 {
     public sealed class UiContextBuilderUnknown: UiContextBuilderBase, IIsUnknown
     {
-        public UiContextBuilderUnknown(Dependencies dependencies) : base(dependencies)
+        public UiContextBuilderUnknown(Dependencies dependencies, WarnUseOfUnknown<UiContextBuilderBase> warn) : base(dependencies)
         {
         }
     }
