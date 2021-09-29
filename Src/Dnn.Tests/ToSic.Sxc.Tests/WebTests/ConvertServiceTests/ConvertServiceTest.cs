de7519fca3d46b283f96eec0d1e3917ebb2ad2f6
ijungleboy <daniel.mettler@2sic.com>
2021-09-29 09:34:52 +0200

    fix number to go to string with . notation

--- Src/Dnn.Tests/ToSic.Sxc.Tests/WebTests/ConvertServiceTests/ConvertServiceTest.cs
+++ Src/Dnn.Tests/ToSic.Sxc.Tests/WebTests/ConvertServiceTests/ConvertServiceTest.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Globalization;
 using ToSic.Sxc.Web;
 using ToSic.Testing.Shared;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace ToSic.Sxc.Tests.WebTests.ConvertServiceTests
 {
@@ -12,16 +14,31 @@ namespace ToSic.Sxc.Tests.WebTests.ConvertServiceTests
         public void ForCodeDate()
         {
             var conv = ConvertService();
-            Assert.AreEqual("2021-09-29T08:45:59.000z", conv.ForCode(new DateTime(2021, 09, 29, 08, 45, 59)));
+            AreEqual("2021-09-29T08:45:59.000z", conv.ForCode(new DateTime(2021, 09, 29, 08, 45, 59)));
         }
 
         [TestMethod]
         public void ForCodeBool()
         {
             var conv = ConvertService();
-            Assert.AreEqual("true", conv.ForCode(true));
-            Assert.AreNotEqual(true.ToString(), conv.ForCode(true));
-            Assert.AreEqual("false", conv.ForCode(false));
+            AreEqual("true", conv.ForCode(true));
+            AreNotEqual(true.ToString(), conv.ForCode(true));
+            AreEqual("false", conv.ForCode(false));
+        }
+
+        [TestMethod]
+        public void ForCodeNumberBadCulture()
+        {
+            // Now change threading culture to a comma-culture and verify that change happened
+            System.Threading.Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("de-DE");
+            AreEqual("1,11", 1.11.ToString());
+
+            // Now run tests expecting things to "just-work"
+            var conv = ConvertService();
+            AreEqual("0", conv.ForCode("0"));
+            AreEqual("1.11", conv.ForCode(1.11f));
+            AreEqual("27.42", conv.ForCode(27.42));
+            AreEqual("-27.42", conv.ForCode(-27.42));
         }
 
         /// <summary>
