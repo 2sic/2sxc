f2f245fc1ebb819a515a64f1220c693d2a6685ab
ijungleboy <daniel.mettler@2sic.com>
2021-10-04 15:24:42 +0200

    just unit tests

---
+++ Src/Dnn.Tests/ToSic.Sxc.Tests/DynamicData/DynamicReadRealClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ToSic.Sxc.Tests.DynamicData
+{
+    [TestClass]
+    public class DynamicReadRealClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var dynAnon = TestAccessors.DynReadObjT(new SimpleObject {NameAsProperty = "PropName", NameAsValue = "ValName"}, false, false) as dynamic;
+            Assert.AreEqual("PropName", dynAnon.NameAsProperty);
+
+            // Simple values shouldn't work, only properties
+            Assert.AreEqual(null, dynAnon.NameAsValue);
+            Assert.AreNotEqual("ValName", dynAnon.NameAsValue);
+
+        }
+    }
+}
---
+++ Src/Dnn.Tests/ToSic.Sxc.Tests/DynamicData/SimpleObject.cs
@@ -0,0 +1,9 @@
+﻿namespace ToSic.Sxc.Tests.DynamicData
+{
+    public class SimpleObject
+    {
+        public string NameAsValue;
+
+        public string NameAsProperty { get; set; }
+    }
+}
