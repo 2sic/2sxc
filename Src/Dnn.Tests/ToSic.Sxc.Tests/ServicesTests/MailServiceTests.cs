0755ff07ee54f9eb53ff1dadec6894cf071fe4d9
Tonći Vatavuk <tonci@sistemi.hr>
2021-10-13 13:03:59 +0200

    update MailService API ipmlementation

---
+++ Src/Dnn.Tests/ToSic.Sxc.Tests/ServicesTests/MailServiceTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToSic.Sxc.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace ToSic.Sxc.Tests.ServicesTests
+{
+    [TestClass]
+    public class MailServiceTests
+    {
+        [TestMethod]
+        public void MailAddressTests()
+        {
+            MailAddressAreEqual("user@domain", "user@domain", "");
+            MailAddressAreEqual("<user@domain>", "user@domain", "");
+            MailAddressAreEqual("first last user@domain", "user@domain", "first last");
+            MailAddressAreEqual("   first last    user@domain   ", "user@domain", "first last");
+            MailAddressAreEqual("first last  user@domain", "user@domain", "first last");
+            MailAddressAreEqual("first last <user@domain>", "user@domain", "first last");
+            MailAddressAreEqual("first last   <user@domain>  ", "user@domain", "first last");
+            MailAddressAreEqual(@"""first last"" user@domain", "user@domain", "first last");
+            MailAddressAreEqual(@"  ""first last""  user@domain", "user@domain", "first last");
+            MailAddressAreEqual(@"""first last"" <user@domain>", "user@domain", "first last");
+            // MailAddressAreEqual(@"""first""<> last"" <user@domain>", "user@domain", @"first""<> last");
+            MailAddressAreEqual("first@last user@domain", "user@domain", "first@last");
+            // MailAddressAreEqual("<first@last> <user@domain>", "user@domain", "<first@last>");
+            MailAddressAreEqual("first last<user@domain>", "user@domain", "first last");
+            MailAddressAreEqual(@"""first last""user@domain", "user@domain", "first last");
+        }
+
+        //[TestMethod]
+        //[Ignore]
+        //public void MailAddressTestsThatAreStrange()
+        //{
+        //    MailAddressAreEqual(@"first last user@domain>", "user@domain", @"first last");
+        //    MailAddressAreEqual(@"first last <user@domain", "user@domain", @"first last");
+        //    MailAddressAreEqual(@"""first last user@domain", "user@domain", @"first last");
+        //    MailAddressAreEqual(@"first last"" user@domain", "user@domain", @"first last");
+        //    MailAddressAreEqual(@" "" "" first last "" "" user@domain", "user@domain", @"first last");
+        //    MailAddressAreEqual(@"first"" last user@domain", "user@domain", @"first"" last");
+        //    MailAddressAreEqual(@"<first@last> user@domain", "user@domain", @"<first@last>");
+        //}
+
+        [TestMethod]
+        public void MailAddressesCommon1()
+        {
+            const string addresses = @"""John, Doe"" <user@host>, ""Bob, Smith"" <user2@host>";
+            var mailAddressCollection = new MailAddressCollection { addresses };
+
+            AreEqual(mailAddressCollection[0].Address, "user@host");
+            AreEqual(mailAddressCollection[0].DisplayName, "John, Doe");
+            AreEqual(mailAddressCollection[1].Address, "user2@host");
+            AreEqual(mailAddressCollection[1].DisplayName, "Bob, Smith");
+        }
+
+        [TestMethod]
+        public void MailAddressesCommon2()
+        {
+            const string addresses = @"info@a.com, info@b.com";
+            var mailAddressCollection = new MailAddressCollection { addresses };
+
+            AreEqual(mailAddressCollection[0].Address, "info@a.com");
+            AreEqual(mailAddressCollection[0].DisplayName, "");
+            AreEqual(mailAddressCollection[1].Address, "info@b.com");
+            AreEqual(mailAddressCollection[1].DisplayName, "");
+        }
+
+        private void MailAddressAreEqual(string input, string address, string displayName)
+        {
+            var mailAddress = new MailAddress(input);
+            AreEqual(mailAddress.Address, address);
+            AreEqual(mailAddress.DisplayName, displayName);
+        }
+
+        // TODO: STV - unit tests for
+        // - MailAddressCollection
+        // - Attachments
+    }
+}
--- Src/Sxc/ToSic.Sxc/Services/MailService/MailServiceBase.cs
+++ Src/Sxc/ToSic.Sxc/Services/MailService/MailServiceBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Mail;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -7,12 +8,15 @@ using ToSic.Eav.Documentation;
 using ToSic.Eav.Logging;
 using ToSic.Sxc.Apps;
 using ToSic.Sxc.Code;
+using MailMessage = System.Net.Mail.MailMessage;
 
 // ReSharper disable once CheckNamespace
 namespace ToSic.Sxc.Services
 {
     public abstract class MailServiceBase : HasLog, IMailService
     {
+        private static readonly Regex HtmlDetectionRegex = new Regex("<(.*\\s*)>", RegexOptions.Compiled);
+
         [PrivateApi] protected IApp App;
 
         protected MailServiceBase() : base($"{Constants.SxcLogName}.MailSrv")
@@ -60,60 +64,48 @@ namespace ToSic.Sxc.Services
             Eav.Parameters.ProtectAgainstMissingParameterNames(noParamOrder, $"{nameof(Send)}", $"{nameof(to)},{nameof(body)}");
 
             var mailMessage = new MailMessage();
+            
+            if (from != null) mailMessage.From = MailAddress(from);
+            AddMailAddresses(mailMessage.To, to);
+            AddMailAddresses(mailMessage.CC, cc);
+            AddMailAddresses(mailMessage.Bcc, bcc);
+            AddMailAddresses(mailMessage.ReplyToList, replyTo);
 
-            if (from is MailAddress mailAddress)
-                mailMessage.From = mailAddress;
-            else if (from is string fromString)
-                mailMessage.From = new MailAddress(fromString);
+            mailMessage.Subject = subject;
+            mailMessage.SubjectEncoding = encoding ?? Encoding.UTF8;
+            mailMessage.IsBodyHtml = isHtml ?? AutoDetectHtml(body);
+            mailMessage.BodyEncoding = encoding ?? Encoding.UTF8;
+            mailMessage.Body = body;
 
-
-
-
-
-            //if (!string.IsNullOrEmpty(from)) mailMessage.From = new MailAddress(from);
-            //AddMailAddresses(mailMessage.To, to);
-            //AddMailAddresses(mailMessage.CC, cc);
-            //AddMailAddresses(mailMessage.Bcc, bcc);
-            //if (!string.IsNullOrEmpty(replyTo)) mailMessage.ReplyTo = new MailAddress(replyTo);
-            //mailMessage.Subject = subject;
-            //mailMessage.SubjectEncoding = encoding ?? Encoding.UTF8;
-            //mailMessage.IsBodyHtml = isHtml ?? AutoDetectHtml(body);
-            //mailMessage.BodyEncoding = encoding ?? Encoding.UTF8;
-            //mailMessage.Body = body;
-
-            //foreach (var attachment in attachments)
-            //{
-            //    mailMessage.Attachments.Add(attachment);
-            //}
+            AddAttachments(mailMessage.Attachments, attachments);
 
             return mailMessage;
         }
 
-        // TODO: STV
-        private bool AutoDetectHtml(string body)
+        private static bool AutoDetectHtml(string body)
         {
-            throw new NotImplementedException();
+            return !string.IsNullOrEmpty(body) && HtmlDetectionRegex.IsMatch(body);
         }
 
         public void Send(
             string noParamOrder =
                 "Rule: all params must be named (https://r.2sxc.org/named-params), Example: \'enable: true, version: 10\'",
-            string @from = null,
-            string to = null,
-            string cc = null,
-            string bcc = null,
-            string replyTo = null,
+            object from = null,
+            object to = null,
+            object cc = null,
+            object bcc = null,
+            object replyTo = null,
             string subject = null,
             bool? isHtml = null,
             Encoding encoding = null,
             string body = null,
-            IEnumerable<Attachment> attachments = null)
+            object attachments = null)
         {
             // prevent incorrect use without named parameters
             Eav.Parameters.ProtectAgainstMissingParameterNames(noParamOrder, $"{nameof(Send)}", $"{nameof(to)},{nameof(body)}");
 
             var mailMessage = Create(
-                @from: @from,
+                from: from,
                 to: to,
                 cc: cc,
                 bcc: bcc,
@@ -124,45 +116,103 @@ namespace ToSic.Sxc.Services
                 body: body,
                 attachments: attachments);
 
-            //var mailMessage = new MailMessage();
-            //if (!string.IsNullOrEmpty(mailFrom)) mailMessage.From = new MailAddress(mailFrom);
-            //AddMailAddresses(mailMessage.To, mailTo);
-            //AddMailAddresses(mailMessage.CC, cc);
-            //AddMailAddresses(mailMessage.Bcc, bcc);
-            //if (!string.IsNullOrEmpty(replyTo)) mailMessage.ReplyTo = new MailAddress(replyTo);
-            //mailMessage.Priority = priority;
-            //mailMessage.Subject = subject;
-            //mailMessage.IsBodyHtml = isBodyHtml;
-            //mailMessage.BodyEncoding = bodyEncoding;
-            //mailMessage.Body = body;
-
-            //foreach (var attachment in attachments)
-            //{
-            //    mailMessage.Attachments.Add(attachment);
-            //}
-
             Send(mailMessage);
         }
 
-        private void AddMailAddresses(MailAddressCollection mails, string emails)
+        public static MailAddress MailAddress(object input)
         {
-            if (string.IsNullOrEmpty(emails)) return;
-            foreach (var email in emails.Split(",;".ToCharArray()))
+            switch (input)
             {
-                mails.Add(new MailAddress(email));
+                case MailAddress mailAddress:
+                    return mailAddress;
+                case string fromString:
+                    return new MailAddress(fromString);
+                default:
+                    throw new ArgumentException("Unknown type for MailAddress");
             }
         }
 
-        public MailAddress GetMailAddress(string input)
+        public static void AddMailAddresses(MailAddressCollection mails, object input)
         {
-            const string pattern = @"^(""?(.+)[,;\s]""?)?<?(([\w\.\-]+)@([\w\.\-]+))>?$";
-            var match = Regex.Match(input.Trim(), pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
-            
-            if (!match.Success) return null;
+            switch (input)
+            {
+                case MailAddressCollection inputMailAddressCollection:
+                    foreach (var mailAddress in inputMailAddressCollection)
+                    {
+                        mails.Add(mailAddress);
+                    }
+                    break;
 
-            var displayName = match.Groups[2].Value.Trim(" ,;".ToCharArray());
-            var address = match.Groups[3].Value.Trim(" <>".ToCharArray());
-            return new MailAddress(address, displayName);
+                case IEnumerable<MailAddress> inputMailAddressesArray:
+                    foreach (var mailAddress in inputMailAddressesArray)
+                    {
+                        mails.Add(mailAddress);
+                    }
+                    break;
+
+                case IEnumerable<string> inputStringArray:
+                    foreach (var emailAddress in inputStringArray)
+                    {
+                        if (!string.IsNullOrEmpty(emailAddress)) 
+                            mails.Add(NormalizeEmailSeparators(emailAddress));
+                    }
+                    break;
+
+                case string inputString: 
+                    if (!string.IsNullOrEmpty(inputString)) 
+                        mails.Add(NormalizeEmailSeparators(inputString));
+                    break;
+                    
+                default:
+                    throw new ArgumentException("Unknown type for MailAddressCollection");
+            }
+        }
+
+        public static string NormalizeEmailSeparators(string input)
+        {
+            return string.IsNullOrEmpty(input) ? null : input.Replace(";", ",");
+        }
+
+        public static void AddAttachments(AttachmentCollection attachments, object input)
+        {
+            switch (input)
+            {
+                case Attachment inputAttachment:
+                    attachments.Add(inputAttachment);
+                    break;
+
+                case AttachmentCollection attachmentCollection:
+                    foreach (var attachment in attachmentCollection)
+                    {
+                        attachments.Add(attachment);
+                    }
+                    break;
+
+                case IEnumerable<Attachment> inputAttachmentsArray:
+                    foreach (var attachment in inputAttachmentsArray)
+                    {
+                        attachments.Add(attachment);
+                    }
+                    break;
+
+                case Adam.IFile inputFile:
+                    attachments.Add(new Attachment(
+                        new FileStream(inputFile.PhysicalPath, FileMode.Open, FileAccess.Read, FileShare.Read),
+                        inputFile.FullName));
+                    break;
+
+                case IEnumerable<Adam.IFile> inputFiles:
+                    foreach (var file in inputFiles)
+                    {
+                        attachments.Add(new Attachment(
+                            new FileStream(file.PhysicalPath, FileMode.Open, FileAccess.Read, FileShare.Read),
+                            file.FullName));
+                    }
+                    break;
+
+                default:
+                    throw new ArgumentException("Unknown type for AttachmentCollection");
+            }
         }
     }
 }
