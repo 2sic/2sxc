39e356dfa12bf170903f060d44e4cf04fea40a4b
ijungleboy <daniel.mettler@2sic.com>
2021-10-26 11:17:57 +0200

    disable jQuery in edit mode #2492

--- Src/Dnn/ToSic.Sxc.Dnn/Dnn/Web/DnnClientResources.cs
+++ Src/Dnn/ToSic.Sxc.Dnn/Dnn/Web/DnnClientResources.cs
@@ -143,11 +143,13 @@ namespace ToSic.Sxc.Dnn.Web
                 // note: the inpage only works if it's not in the head, so we're adding it below
                 RegisterJs(page, ver, root + InpageCms.EditJs, false, priority + 1);
                 // request full $services and jQuery etc.
-                JavaScript.RequestRegistration(CommonJs.jQuery);
-                ServicesFramework.Instance.RequestAjaxAntiForgerySupport();
+
+                // #2492 2021-10-26 v12-07 we believe we don't need this any more
+                //JavaScript.RequestRegistration(CommonJs.jQuery);
+                //ServicesFramework.Instance.RequestAjaxAntiForgerySupport();
             }
 
-            if(features.Contains(BuiltInFeatures.JQuery))
+            if (features.Contains(BuiltInFeatures.JQuery))
                 JavaScript.RequestRegistration(CommonJs.jQuery);
             
             if (features.Contains(BuiltInFeatures.TurnOn))
