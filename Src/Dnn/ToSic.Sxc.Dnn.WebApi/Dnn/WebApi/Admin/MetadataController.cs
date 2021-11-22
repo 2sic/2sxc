5577130f02c71e7982f264ae54eb03fcfde26500
ijungleboy <daniel.mettler@2sic.com>
2021-11-22 23:51:53 +0100

    Prepare for better UI to manage metadata
    https://github.com/2sic/2sxc/issues/2600

--- Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/MetadataController.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/MetadataController.cs
@@ -3,6 +3,7 @@ using System.Web.Http;
 using DotNetNuke.Security;
 using DotNetNuke.Web.Api;
 using ToSic.Eav.WebApi;
+using ToSic.Eav.WebApi.Dto;
 using ToSic.Eav.WebApi.PublicApi;
 using ToSic.Sxc.WebApi;
 
@@ -16,6 +17,10 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
     {
         [HttpGet]
         public IEnumerable<IDictionary<string, object>> Get(int appId, int targetType, string keyType, string key, string contentType = null)
+            => GetService<MetadataBackend>().Get(appId, targetType, keyType, key, contentType).Items;
+
+        [HttpGet]
+        public MetadataListDto GetV13(int appId, int targetType, string keyType, string key, string contentType = null)
             => GetService<MetadataBackend>().Get(appId, targetType, keyType, key, contentType);
     }
 }
