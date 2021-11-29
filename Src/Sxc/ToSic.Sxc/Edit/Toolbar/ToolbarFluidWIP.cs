522c5ffd32644cf13d2e7d4a176acfffd79a8499
ijungleboy <daniel.mettler@2sic.com>
2021-11-29 15:08:45 +0100

    minor / experiments with toolbar for #2597

---
+++ Src/Sxc/ToSic.Sxc/Edit/Toolbar/ToolbarFluidWIP.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using ToSic.Sxc.Web;
+
+namespace ToSic.Sxc.Edit.Toolbar
+{
+    public class ToolbarFluidWIP: HybridHtmlString
+    {
+        public ToolbarFluidWIP() : base(string.Empty)
+        {
+        }
+
+        public ToolbarFluidWIP(ToolbarFluidWIP original): this()
+        {
+            Rules = original.Rules.Select(r => r).ToList();
+        }
+
+        public List<ToolbarRuleBase> Rules { get; private set; } = new List<ToolbarRuleBase>();
+
+        public override string ToString()
+        {
+            var rules = Rules.Select(r => r.Rule).ToArray();
+            var rulesJson = JsonConvert.SerializeObject(rules);
+            return '{' + rulesJson.TrimStart('[').TrimEnd(']') + "}";
+        }
+    }
+}
--- Src/Sxc/ToSic.Sxc/Web/HybridHtmlString/HybridHtmlString.cs
+++ Src/Sxc/ToSic.Sxc/Web/HybridHtmlString/HybridHtmlString.cs
@@ -1,31 +1,32 @@
 ﻿using ToSic.Eav.Documentation;
+using ToSic.Eav.Plumbing;
 
 namespace ToSic.Sxc.Web
 {
     /// <inheritdoc />
     [InternalApi_DoNotUse_MayChangeWithoutNotice("Helper to ensure that code providing an IHtmlString will work on .net Framework and .net Standard")]
-    public class HybridHtmlString: IHybridHtmlString
+    public class HybridHtmlString: IHybridHtmlString, IString
     {
         public HybridHtmlString(string value)
         {
-            Value = value;
+            _value = value;
         }
 
-        protected string Value { get; }
+        private string _value { get; }
 
         /// <summary>
         /// Standard ToString overload - used when concatenating strings.
         /// </summary>
         /// <returns></returns>
-        public override string ToString() => Value;
+        public override string ToString() => _value;
 
 #if NETSTANDARD
         public void WriteTo(System.IO.TextWriter writer, System.Text.Encodings.Web.HtmlEncoder encoder) 
-            => new Microsoft.AspNetCore.Html.HtmlString(Value).WriteTo(writer, encoder);
+            => new Microsoft.AspNetCore.Html.HtmlString(ToString()).WriteTo(writer, encoder);
 #endif
 
 #if NETFRAMEWORK
-        public string ToHtmlString() => Value;
+        public string ToHtmlString() => ToString();
 #endif
     }
 }
