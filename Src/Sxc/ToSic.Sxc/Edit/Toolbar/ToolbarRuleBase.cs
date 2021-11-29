dc5edfa61f21e6a4850b89bdea50516a2da02f0e
ijungleboy <daniel.mettler@2sic.com>
2021-11-29 15:08:45 +0100

    minor / experiments with toolbar for #2597

---
+++ Src/Sxc/ToSic.Sxc/Edit/Toolbar/ToolbarRuleBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ToSic.Eav.Data;
+using ToSic.Eav.Plumbing;
+
+namespace ToSic.Sxc.Edit.Toolbar
+{
+    public class ToolbarRuleBase: IString
+    {
+        protected ToolbarRuleBase() {}
+
+        protected ToolbarRuleBase(string rule)
+        {
+            Rule = rule;
+        }
+
+        public IEntity Target { get; set; }
+        public bool TargetRequired { get; set; }
+
+        public bool Prepared { get; set; }
+
+        public string Command { get; set; }
+
+        public string CommandParams { get; set; }
+
+        public string UiParams { get; set; }
+
+        public string Rule
+        {
+            get
+            {
+                if (Prepared) return _rule;
+                Prepared = true;
+                _rule = PrepareRule();
+                return _rule;
+            }
+            set
+            {
+                _rule = value;
+                Prepared = true;
+            }
+        }
+
+        private string PrepareRule()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string _rule;
+
+    }
+}
