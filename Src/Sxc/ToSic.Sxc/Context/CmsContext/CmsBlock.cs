7c2365c431b9f51867193f61162eace032430742
ijungleboy <daniel.mettler@2sic.com>
2021-11-29 23:22:37 +0100

    Extend CmsContext with Block and Metadata #2608 #2609

---
+++ Src/Sxc/ToSic.Sxc/Context/CmsContext/CmsBlock.cs
@@ -0,0 +1,24 @@
+﻿using ToSic.Eav.Data;
+using ToSic.Eav.Documentation;
+using ToSic.Eav.Metadata;
+using ToSic.Sxc.Blocks;
+
+// ReSharper disable ConvertToNullCoalescingCompoundAssignment
+
+namespace ToSic.Sxc.Context
+{
+    [PrivateApi("Hide implementation")]
+    public class CmsBlock: Wrapper<IBlock>, ICmsBlock
+    {
+        public CmsBlock(IBlock block): base(block) { }
+
+        /// <inheritdoc />
+        public int Id => _contents?.Configuration.Id ?? 0;
+
+        /// <inheritdoc />
+        public bool IsRoot => _contents != null && _contents.BlockBuilder.RootBuilder == _contents.BlockBuilder;
+
+        /// <inheritdoc />
+        public IMetadataOf Metadata => _contents.Configuration.Metadata;
+    }
+}
---
+++ Src/Sxc/ToSic.Sxc/Context/CmsContext/ICmsBlock.cs
@@ -0,0 +1,20 @@
+﻿using ToSic.Eav.Documentation;
+using ToSic.Eav.Metadata;
+
+namespace ToSic.Sxc.Context
+{
+    [PrivateApi("WIP v13")]
+    public interface ICmsBlock: IHasMetadata
+    {
+        /// <summary>
+        /// The ID of this Block
+        /// </summary>
+        int Id { get; }
+
+        /// <summary>
+        /// Determines if this is the root block.
+        /// Will be true in most cases, but false on inner-content
+        /// </summary>
+        bool IsRoot { get; }
+    }
+}
