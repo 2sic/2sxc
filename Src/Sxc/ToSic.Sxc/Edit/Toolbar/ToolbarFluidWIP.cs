382efacbea7b04fb53289536644e088026a21096
ijungleboy <daniel.mettler@2sic.com>
2021-11-29 16:21:13 +0100

    WIP better metadata toolbar #2597

--- Src/Sxc/ToSic.Sxc/Edit/Toolbar/ToolbarFluidWIP.cs
+++ Src/Sxc/ToSic.Sxc/Edit/Toolbar/ToolbarFluidWIP.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 using ToSic.Sxc.Web;
 
 namespace ToSic.Sxc.Edit.Toolbar
 {
-    public class ToolbarFluidWIP: HybridHtmlString
+    public class ToolbarFluidWIP: HybridHtmlString, IEnumerable<string>
     {
+        #region Constructors
         public ToolbarFluidWIP() : base(string.Empty)
         {
         }
@@ -15,14 +17,42 @@ namespace ToSic.Sxc.Edit.Toolbar
         {
             Rules = original.Rules.Select(r => r).ToList();
         }
+        #endregion
 
-        public List<ToolbarRuleBase> Rules { get; private set; } = new List<ToolbarRuleBase>();
+        public ToolbarFluidWIP Add(params string[] rules)
+        {
+            var clone = new ToolbarFluidWIP(this);
+            if (!rules.Any()) return clone;
+            foreach (var rule in rules)
+                clone.Rules.Add(new ToolbarRuleGeneric(rule));
+            return clone;
+        }
+
+        public ToolbarFluidWIP Add(params ToolbarRuleBase[] rules)
+        {
+            var clone = new ToolbarFluidWIP(this);
+            if (!rules.Any()) return clone;
+            foreach (var rule in rules)
+                clone.Rules.Add(rule);
+            return clone;
+        }
+
+
+        public List<ToolbarRuleBase> Rules { get; } = new List<ToolbarRuleBase>();
+
+        public IEnumerator<string> GetEnumerator() => Rules.Select(r => r.Rule).GetEnumerator();
 
         public override string ToString()
         {
             var rules = Rules.Select(r => r.Rule).ToArray();
-            var rulesJson = JsonConvert.SerializeObject(rules);
-            return '{' + rulesJson.TrimStart('[').TrimEnd(']') + "}";
+            return JsonConvert.SerializeObject(rules);
         }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+
+
+
+        public ToolbarFluidWIP AddMeta(object target, string contentType) => Add(new ToolbarRuleMetadata(target, contentType));
     }
 }
