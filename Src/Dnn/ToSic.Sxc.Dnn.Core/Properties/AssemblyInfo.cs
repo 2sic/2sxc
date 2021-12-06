2f935bcae266cf39e22629a8505b066a8c25d766
Tonći Vatavuk <tonci@sistemi.hr>
2021-12-06 16:14:38 +0100

    rename 2sxc dnn folders to follow project names

--- Src/Dnn/ToSic.Sxc.Dnn.Core/Properties/AssemblyInfo.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.Core/Properties/AssemblyInfo.cs
@@ -5,20 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ToSic.Sxc.Dnn")]
-[assembly: AssemblyDescription("2sxc - the content module for DotNetNuke")]
+[assembly: AssemblyTitle("2sxc Dnn")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ToSic")]
-[assembly: AssemblyProduct("2SexyContent")]
-[assembly: AssemblyCopyright("Copyright © 2sic 2021")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2sxc Dnn")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
-[assembly: Guid("bd90b92e-79ec-4846-8998-b56277d5b2b4")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a94de6d7-d1e6-4be2-b5b5-dc4a6a61cd80")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -27,10 +29,10 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("13.00.00")]
-
-
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("ToSic.Sxc.Dnn.WebApi")]
-[assembly: InternalsVisibleTo("ToSic.Sxc.Dnn.Core")]
+[assembly: InternalsVisibleTo("ToSic.Sxc.Dnn.Razor")]
