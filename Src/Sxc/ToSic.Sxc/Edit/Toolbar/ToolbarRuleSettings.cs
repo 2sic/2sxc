f7866067e6981d06c9ac8463bc106b04806a3077
ijungleboy <daniel.mettler@2sic.com>
2021-12-22 11:28:27 +0100

    Update ToolbarRuleSettings.cs

--- Src/Sxc/ToSic.Sxc/Edit/Toolbar/ToolbarRuleSettings.cs
+++ Src/Sxc/ToSic.Sxc/Edit/Toolbar/ToolbarRuleSettings.cs
@@ -2,40 +2,27 @@
 {
     public class ToolbarRuleSettings: ToolbarRule
     {
-
         public ToolbarRuleSettings(
-            string noParamOrder = Eav.Parameters.Protector, 
-            string show = null, 
-            string hover = null, 
-            string follow = null, 
+            string noParamOrder = Eav.Parameters.Protector,
+            string show = null,
+            string hover = null,
+            string follow = null,
             string classes = null,
             string autoAddMore = null,
-            string ui = "", 
-            string parameters = "") : base("settings", ui: ui, parameters: parameters)
-        {
-            _show = show;
-            _hover = hover;
-            _follow = follow;
-            _classes = classes;
-            _autoAddMore = autoAddMore;
-        }
-        private readonly string _show;
-        private readonly string _hover;
-        private readonly string _follow;
-        private readonly string _classes;
-        private readonly string _autoAddMore;
-
-        public override string GeneratedUiParams()
-        {
-            return BuildValidParameterList(new[]
+            string ui = "",
+            string parameters = "")
+            : base("settings", ui: ui, parameters: parameters)
+            => _uiParams = new[]
             {
-                ("show", _show),
-                ("hover", _hover),
-                ("follow", _follow),
-                ("autoAddMore", _autoAddMore),
-                ("classes", _classes),
-            });
-        }
-        
+                ("show", show),
+                ("hover", hover),
+                ("follow", follow),
+                ("autoAddMore", autoAddMore),
+                ("classes", classes),
+            };
+
+        private readonly (string, string)[] _uiParams;
+
+        public override string GeneratedUiParams() => BuildValidParameterList(_uiParams);
     }
 }
