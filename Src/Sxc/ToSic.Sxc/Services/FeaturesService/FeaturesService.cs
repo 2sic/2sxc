7909613638934f33cac4aeb0794df563ad2e5d45
ijungleboy <daniel.mettler@2sic.com>
2022-01-05 08:22:26 +0100

    minor

--- Src/Sxc/ToSic.Sxc/Services/FeaturesService/FeaturesService.cs
+++ Src/Sxc/ToSic.Sxc/Services/FeaturesService/FeaturesService.cs
@@ -8,13 +8,7 @@ namespace ToSic.Sxc.Services
     {
         public FeaturesService(ToSic.Eav.Configuration.IFeaturesService contents) : base(contents) { }
 
-        public bool Enabled(Guid guid) => _contents.Enabled(guid);
-
-        public bool Enabled(string nameId) => _contents.Enabled(nameId);
-
-        public bool Enabled(IEnumerable<Guid> guids) => _contents.Enabled(guids);
-
-        public bool Enabled(IEnumerable<string> nameIds) => _contents.Enabled(nameIds);
+        public bool Enabled(params string[] nameIds) => _contents.Enabled(nameIds);
 
         public bool Valid => _contents.Valid;
 
--- Src/Sxc/ToSic.Sxc/Services/FeaturesService/IFeaturesService.cs
+++ Src/Sxc/ToSic.Sxc/Services/FeaturesService/IFeaturesService.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using ToSic.Eav.Documentation;
+﻿using ToSic.Eav.Documentation;
 
 // Important
 // This is just the public API for this
-// It basically matches the Eav.Services implementation
-// So make sure to keep these in sync for the docs
+// Many other APIs simply shouldn't be public, so the Eav-version isn't fully surfaced here
+
 namespace ToSic.Sxc.Services
 {
     /// <summary>
@@ -13,51 +11,23 @@ namespace ToSic.Sxc.Services
     /// It's important to detect if the admin must activate certain features to let your code do it's work.
     /// </summary>
     /// <remarks>
-    /// This replaces the older static Features accessor - please only use this from now on
+    /// This replaces the older static Features accessor - please only use this from now on.
+    ///
+    /// History:
+    /// - Added this implementation in 13.01
     /// </remarks>
     [PublicApi]
     public interface IFeaturesService
     {
         /// <summary>
-        /// Checks if a feature is enabled
-        /// </summary>
-        /// <param name="guid">The feature Guid</param>
-        /// <returns>true if the feature is enabled</returns>
-        bool Enabled(Guid guid);
-
-        /// <summary>
-        /// Checks if a feature is enabled
-        /// </summary>
-        /// <param name="nameId">The feature name ID</param>
-        /// <returns>true if the feature is enabled</returns>
-        /// <remarks>
-        /// Added in v13.01
-        /// </remarks>
-        bool Enabled(string nameId);
-
-        /// <summary>
         /// Checks if a list of features are enabled, in case you need many features to be activated.
         /// </summary>
-        /// <param name="guids">list/array of Guids</param>
-        /// <returns>true if all features are enabled, false if any one of them is not</returns>
-        bool Enabled(IEnumerable<Guid> guids);
-
-        /// <summary>
-        /// Checks if a list of features are enabled, in case you need many features to be activated.
-        /// </summary>
-        /// <param name="nameIds">list/array of name IDs</param>
+        /// <param name="nameIds">one or many name IDs</param>
         /// <returns>true if all features are enabled, false if any one of them is not</returns>
         /// <remarks>
         /// Added in v13.01
         /// </remarks>
-        bool Enabled(IEnumerable<string> nameIds);
-
-        /// <summary>
-        /// Informs you if the enabled features are valid or not - meaning if they have been countersigned by the 2sxc features system.
-        /// As of now, it's not enforced, but in future it will be. 
-        /// </summary>
-        /// <returns>true if the features were signed correctly</returns>
-        bool Valid { get; }
+        bool Enabled(params string[] nameIds);
 
     }
 }
