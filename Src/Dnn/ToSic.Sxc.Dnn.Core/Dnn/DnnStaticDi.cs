30073bd3c62d1c5b65391b45110041de95ca1b3c
ijungleboy <daniel.mettler@2sic.com>
2022-02-07 13:49:58 +0100

    minor/simplify

--- Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn/DnnStaticDi.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn/DnnStaticDi.cs
@@ -40,13 +40,10 @@ namespace ToSic.Sxc.Dnn
 
         private static IServiceProvider _sp;
 
-        // Might need this depending on research...
-        //public static IServiceProvider GetGlobalServiceProviderForStartUp() => _sp;
-
         [PrivateApi("This is just a temporary solution - shouldn't be used long term")]
-        public static IServiceProvider GetPageScopedServiceProvider() => GetPageScope().ServiceProvider;
+        public static IServiceProvider GetPageScopedServiceProvider() => GetPageServiceProvider();
 
-        private static IServiceScope GetPageScope()
+        private static IServiceProvider GetPageServiceProvider()
         {
             // Because 2sxc runs inside DNN as a webforms project and not asp.net core mvc, we have
             // to make sure the service-provider object is disposed correctly. If we don't do this,
@@ -55,7 +52,7 @@ namespace ToSic.Sxc.Dnn
             // Work-around for issue https://github.com/2sic/2sxc/issues/1200
             // Scope service-provider based on request
             var httpCtx = HttpContext.Current;
-            if (httpCtx == null) return _sp.CreateScope();
+            if (httpCtx == null) return _sp.CreateScope().ServiceProvider;
 
             // This only runs in Dnn 7.4.2 - Dnn 9.3, because Dnn 9.4 provides this in the http context
             if (!httpCtx.Items.Contains(ServiceScopeKey))
@@ -66,22 +63,22 @@ namespace ToSic.Sxc.Dnn
                 httpCtx.AddOnRequestCompleted(context => ((IDisposable)context.Items[ServiceScopeKey])?.Dispose());
             }
 
-            return httpCtx.Items[ServiceScopeKey] is IServiceScope scope ? scope : null;
+            return httpCtx.Items[ServiceScopeKey] is IServiceScope scope ? scope.ServiceProvider : null;
         }
 
         [PrivateApi]
-        public static IServiceProvider CreateModuleScopedServiceProvider() => CreateModuleScope().ServiceProvider;
+        public static IServiceProvider CreateModuleScopedServiceProvider() => CreateModuleServiceProvider();
 
-        private static IServiceScope CreateModuleScope()
+        private static IServiceProvider CreateModuleServiceProvider()
         {
-            var pageScope = GetPageScope();
-            var moduleScope = pageScope.ServiceProvider.CreateScope();
+            var pageSp = GetPageServiceProvider();
+            var moduleSp = pageSp.CreateScope().ServiceProvider;
 
             // In the module scope, we initialize the scoped PageScope Accessor and give it the parent scope
             // This is necessary for it to be able to give page-scoped objects
-            moduleScope.ServiceProvider.Build<PageScopeAccessor>()
-                .InitPageOfModule(pageScope.ServiceProvider);
-            return moduleScope;
+            moduleSp.Build<PageScopeAccessor>()
+                .InitPageOfModule(pageSp);
+            return moduleSp;
         }
     }
 }
