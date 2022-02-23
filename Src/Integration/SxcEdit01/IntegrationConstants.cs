23a59c3d87561250b9e9fbefe87e91c0e4abc589
ijungleboy <daniel.mettler@2sic.com>
2022-02-23 17:53:23 +0100

    minor clean-up

--- Src/Integration/SxcEdit01/IntegrationConstants.cs
+++ Src/Integration/SxcEdit01/IntegrationConstants.cs
@@ -18,8 +18,8 @@ namespace IntegrationSamples.SxcEdit01
         public const int AppId = 1369;
         public static IAppIdentity AppIdentity = new AppIdentity(ZoneId, AppId);
 
-        public const int PageId = 4427;         // probably not needed TODO: VERIFY
-        public const int ModuleId = 9771;       // probably not needed TODO: VERIFY
+        //public const int PageId = 4427;         // probably not needed TODO: VERIFY
+        //public const int ModuleId = 9771;       // probably not needed TODO: VERIFY
 
         // An entity in the test-db - yours will be different
         public const int ItemWithImagesId = 128280;
