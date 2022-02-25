f69a783c34939a4af99dc90593122dccd2452d92
Tonći Vatavuk <tonci@sistemi.hr>
2022-02-25 20:27:26 +0100

    implement TypeControllerReal

--- Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/TypeController.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/TypeController.cs
@@ -1,22 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using DotNetNuke.Security;
+using DotNetNuke.Web.Api;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
-using DotNetNuke.Security;
-using DotNetNuke.Web.Api;
-using ToSic.Eav.Persistence.Logging;
-using ToSic.Eav.WebApi;
-using ToSic.Eav.WebApi.Assets;
+using ToSic.Eav.WebApi.Admin;
 using ToSic.Eav.WebApi.Dto;
-using ToSic.Eav.WebApi.ImportExport;
-using ToSic.Eav.WebApi.PublicApi;
-using ToSic.Sxc.Dnn.Context;
-using ToSic.Sxc.Dnn.Run;
 using ToSic.Sxc.Dnn.WebApi.Logging;
 using ToSic.Sxc.WebApi;
-using ToSic.Sxc.WebApi.ImportExport;
-using ContentTypeApi = ToSic.Eav.WebApi.ContentTypeApi;
+using ToSic.Sxc.WebApi.Adam;
+using ToSic.Sxc.WebApi.Admin;
 
 namespace ToSic.Sxc.Dnn.WebApi.Admin
 {
@@ -33,11 +26,10 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
     /// Security checking is possible, because the cookie still contains user information
     /// </remarks>
     [DnnLogExceptions]
-    public class TypeController : SxcApiControllerBase<DummyControllerReal>, ITypeController
+    public class TypeController : SxcApiControllerBase<TypeControllerReal>, ITypeController
     {
-        public TypeController() : base("Types") { }
+        public TypeController() : base(TypeControllerReal.LogSuffix) { }
 
-        private ContentTypeApi Backend => GetService<ContentTypeApi>();
 
         /// <summary>
         /// Get a list of all content-types.
@@ -50,8 +42,8 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
         [HttpGet]
         [ValidateAntiForgeryToken]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
-        public IEnumerable<ContentTypeDto> List(int appId, string scope = null, bool withStatistics = false) 
-            => Backend.Init(appId, Log).Get(scope, withStatistics);
+        public IEnumerable<ContentTypeDto> List(int appId, string scope = null, bool withStatistics = false) => Real.List(appId, scope, withStatistics);
+
 
         /// <summary>
         /// Used to be GET Scopes.
@@ -60,8 +52,8 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
         [HttpGet]
         [ValidateAntiForgeryToken]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
-        public IDictionary<string, string> Scopes(int appId)
-            => Backend.Init(appId, Log).Scopes();
+        public IDictionary<string, string> Scopes(int appId) => Real.Scopes(appId);
+
 
         /// <summary>
         /// Used to be GET ContentTypes.
@@ -70,7 +62,8 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
         [HttpGet]
         [ValidateAntiForgeryToken]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
-        public ContentTypeDto Get(int appId, string contentTypeId, string scope = null) => Backend.Init(appId, Log).GetSingle(contentTypeId, scope);
+        public ContentTypeDto Get(int appId, string contentTypeId, string scope = null) => Real.Get(appId, contentTypeId, scope);
+
 
         /// <summary>
         /// Delete a Content-Type
@@ -85,7 +78,8 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
         [HttpDelete]
         [ValidateAntiForgeryToken]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
-        public bool Delete(int appId, string staticName) => Backend.Init(appId, Log).Delete(staticName);
+        public bool Delete(int appId, string staticName) => Real.Delete(appId, staticName);
+
 
         /// <summary>
         /// Save a Content-Type.
@@ -100,11 +94,8 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
         // 2019-11-15 2dm special change: item to be Dictionary<string, object> because in DNN 9.4
         // it causes problems when a content-type has metadata, where a value then is a deeper object
         // in future, the JS front-end should send something clearer and not the whole object
-        public bool Save(int appId, Dictionary<string, object> item)
-        {
-            var cleanList = item.ToDictionary(i => i.Key, i => i.Value?.ToString());
-            return Backend.Init(appId, Log).Save(cleanList);
-        }
+        public bool Save(int appId, Dictionary<string, object> item) => Real.Save(appId, item);
+
 
         /// <summary>
         /// Used to add a Ghost content-type.
@@ -116,7 +107,7 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
         [HttpPost]
         [ValidateAntiForgeryToken]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Host)]
-        public bool AddGhost(int appId, string sourceStaticName) => Backend.Init(appId, Log).CreateGhost(sourceStaticName);
+        public bool AddGhost(int appId, string sourceStaticName) => Real.AddGhost(appId, sourceStaticName);
 
 
         /// <summary>
@@ -128,8 +119,7 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
         [HttpPost]
         [ValidateAntiForgeryToken]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
-        public void SetTitle(int appId, int contentTypeId, int attributeId)
-            => Backend.Init(appId, Log).SetTitle(contentTypeId, attributeId);
+        public void SetTitle(int appId, int contentTypeId, int attributeId) => Real.SetTitle(appId, contentTypeId, attributeId);
 
 
         /// <summary>
@@ -140,9 +130,7 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
         /// </remarks>
         [HttpGet]
         [AllowAnonymous] // will do security check internally
-        public HttpResponseMessage Json(int appId, string name)
-            => GetService<ContentExportApi>().Init(appId, Log).DownloadTypeAsJson(new DnnUser(), name);
-
+        public HttpResponseMessage Json(int appId, string name) => Real.Json(appId, name);
 
 
         /// <summary>
@@ -155,22 +143,6 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
         [HttpPost]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
         [ValidateAntiForgeryToken]
-        public ImportResultDto Import(int zoneId, int appId)
-        {
-            var wrapLog = Log.Call<ImportResultDto>();
-
-            PreventServerTimeout300();
-            if (HttpContext.Current.Request.Files.Count <= 0)
-                return new ImportResultDto(false, "no file uploaded", Message.MessageTypes.Error);
-
-            var files = HttpContext.Current.Request.Files;
-            var streams = new List<FileUploadDto>();
-            for(var i = 0; i < files.Count; i++)
-                streams.Add(new FileUploadDto { Name = files[i].FileName, Stream = files[i].InputStream});
-            var result = GetService<ImportContent>().Init(new DnnUser(), Log)
-                .ImportContentType(zoneId, appId, streams, PortalSettings.DefaultLanguage);
-
-            return wrapLog("ok", result);
-        }
+        public ImportResultDto Import(int zoneId, int appId) => Real.Set(PreventServerTimeout300).Import(new HttpUploadedFile(Request, HttpContext.Current.Request), zoneId, appId);
     }
 }
