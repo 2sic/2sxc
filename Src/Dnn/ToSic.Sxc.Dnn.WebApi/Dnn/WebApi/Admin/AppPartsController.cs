389e789ce1fea5f1e26ef8b729ed4fb4c321d162
Tonći Vatavuk <tonci@sistemi.hr>
2022-02-28 10:44:57 +0100

    imlement AppPartsControllerReal

--- Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/AppPartsController.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/AppPartsController.cs
@@ -1,13 +1,12 @@
-﻿using System.Net.Http;
+﻿using DotNetNuke.Security;
+using DotNetNuke.Web.Api;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
-using DotNetNuke.Security;
-using DotNetNuke.Web.Api;
-using ToSic.Eav.WebApi;
+using ToSic.Eav.WebApi.Admin;
 using ToSic.Eav.WebApi.Dto;
-using ToSic.Eav.WebApi.PublicApi;
-using ToSic.Sxc.Dnn.Context;
-using ToSic.Sxc.WebApi.ImportExport;
+using ToSic.Sxc.WebApi.Adam;
+using ToSic.Sxc.WebApi.Admin;
 
 namespace ToSic.Sxc.Dnn.WebApi.Admin
 {
@@ -15,62 +14,36 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
     // token, which fails in the cases where the url is called using get, which should result in a download
     // [ValidateAntiForgeryToken] because the exports are called by the browser directly (new tab) 
     // we can't set this globally (only needed for imports)
-    public class AppPartsController : DnnApiControllerWithFixes<DummyControllerReal>, IAppPartsController
+    public class AppPartsController : DnnApiControllerWithFixes<AppPartsControllerReal>, IAppPartsController
     {
-        public AppPartsController() : base("Parts") { }
+        public AppPartsController() : base(AppPartsControllerReal.LogSuffix) { }
 
         #region Parts Export/Import
 
-        /// <summary>
-        /// Used to be GET ImportExport/GetContentInfo
-        /// </summary>
-        /// <param name="zoneId"></param>
-        /// <param name="appId"></param>
-        /// <param name="scope"></param>
-        /// <returns></returns>
+        /// <inheritdoc />
         [HttpGet]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
         [ValidateAntiForgeryToken]
-        public ExportPartsOverviewDto Get(int zoneId, int appId, string scope)
-            => GetService<ExportContent>().Init(Log).PreExportSummary(appId, zoneId, scope);
+        public ExportPartsOverviewDto Get(int zoneId, int appId, string scope) => Real.Get(appId, zoneId, scope);
 
-        /// <summary>
-        /// Used to be GET ImportExport/ExportContent
-        /// </summary>
-        /// <param name="zoneId"></param>
-        /// <param name="appId"></param>
-        /// <param name="contentTypeIdsString"></param>
-        /// <param name="entityIdsString"></param>
-        /// <param name="templateIdsString"></param>
-        /// <returns></returns>
+
+        /// <inheritdoc />
         [HttpGet]
-        public HttpResponseMessage Export(int zoneId, int appId, string contentTypeIdsString,
-            string entityIdsString, string templateIdsString)
-            => GetService<ExportContent>().Init(Log)
-                .Export(appId, zoneId, contentTypeIdsString, entityIdsString, templateIdsString);
+        public HttpResponseMessage Export(int zoneId, int appId, string contentTypeIdsString, string entityIdsString, string templateIdsString)
+            => Real.Export(appId, zoneId, contentTypeIdsString, entityIdsString, templateIdsString);
 
 
-        /// <summary>
-        /// Used to be POST ImportExport/ImportContent
-        /// </summary>
-        /// <returns></returns>
+        /// <inheritdoc />
         [HttpPost]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
         [ValidateAntiForgeryToken]
-        public ImportResultDto Import(int zoneId, int appId)
-        {
-            var wrapLog = Log.Call<ImportResultDto>();
+        public ImportResultDto Import(int zoneId, int appId) 
+            => Real.Set(PreventServerTimeout300).Import(new HttpUploadedFile(Request, HttpContext.Current.Request), zoneId, appId);
 
-            PreventServerTimeout300();
-            if (HttpContext.Current.Request.Files.Count <= 0) return new ImportResultDto(false, "no files uploaded");
-            var file = HttpContext.Current.Request.Files[0];
-            var result = GetService<ImportContent>().Init(new DnnUser(), Log).Import(zoneId, appId, file.FileName,
-                file.InputStream, PortalSettings.DefaultLanguage);
-
-            return wrapLog("ok", result);
-        }
 
         #endregion
+
+
 
 
     }
--- Src/Oqtane/ToSic.Sxc.Oqt.Server/WebApi/Admin/AppPartsController.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/WebApi/Admin/AppPartsController.cs
@@ -1,16 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Oqtane.Shared;
-using System;
 using System.Net.Http;
-using ToSic.Eav.Context;
-using ToSic.Eav.WebApi;
+using ToSic.Eav.WebApi.Admin;
 using ToSic.Eav.WebApi.Dto;
-using ToSic.Eav.WebApi.PublicApi;
 using ToSic.Eav.WebApi.Routing;
 using ToSic.Sxc.Oqt.Server.Controllers;
-using ToSic.Sxc.Oqt.Shared;
-using ToSic.Sxc.WebApi.ImportExport;
+using ToSic.Sxc.WebApi.Adam;
+using ToSic.Sxc.WebApi.Admin;
 
 namespace ToSic.Sxc.Oqt.Server.WebApi.Admin
 {
@@ -24,70 +21,32 @@ namespace ToSic.Sxc.Oqt.Server.WebApi.Admin
     [Route(WebApiConstants.ApiRootPathOrLang + $"/{AreaRoutes.Admin}")]
     [Route(WebApiConstants.ApiRootPathNdLang + $"/{AreaRoutes.Admin}")]
 
-    // Beta routes - TODO: @STV - why is this beta?
-    [Route(WebApiConstants.WebApiStateRoot + $"/{AreaRoutes.Admin}")]
-    public class AppPartsController : OqtStatefulControllerBase<DummyControllerReal>, IAppPartsController
+    public class AppPartsController : OqtStatefulControllerBase<AppPartsControllerReal>, IAppPartsController
     {
-        public AppPartsController(Lazy<ExportContent> exportContentLazy, Lazy<ImportContent> importContentLazy, Lazy<IUser> userLazy): base("AParts")
-        {
-            _exportContentLazy = exportContentLazy;
-            _importContentLazy = importContentLazy;
-            _userLazy = userLazy;
-        }
-        private readonly Lazy<ExportContent> _exportContentLazy;
-        private readonly Lazy<ImportContent> _importContentLazy;
-        private readonly Lazy<IUser> _userLazy;
+        public AppPartsController(): base(AppPartsControllerReal.LogSuffix) { }
 
 
         #region Parts Export/Import
 
-        /// <summary>
-        /// Used to be GET ImportExport/GetContentInfo
-        /// </summary>
-        /// <param name="zoneId"></param>
-        /// <param name="appId"></param>
-        /// <param name="scope"></param>
-        /// <returns></returns>
+        /// <inheritdoc />
         [HttpGet]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = RoleNames.Admin)]
-        public ExportPartsOverviewDto Get(int zoneId, int appId, string scope)
-            => _exportContentLazy.Value.Init(Log).PreExportSummary(appId, zoneId, scope);
+        public ExportPartsOverviewDto Get(int zoneId, int appId, string scope) => Real.Get(appId, zoneId, scope);
 
-        /// <summary>
-        /// Used to be GET ImportExport/ExportContent
-        /// </summary>
-        /// <param name="zoneId"></param>
-        /// <param name="appId"></param>
-        /// <param name="contentTypeIdsString"></param>
-        /// <param name="entityIdsString"></param>
-        /// <param name="templateIdsString"></param>
-        /// <returns></returns>
+
+        /// <inheritdoc />
         [HttpGet]
-        public HttpResponseMessage Export(int zoneId, int appId, string contentTypeIdsString,
-            string entityIdsString, string templateIdsString)
-            => _exportContentLazy.Value.Init(Log)
-                .Export(appId, zoneId, contentTypeIdsString, entityIdsString, templateIdsString);
+        public HttpResponseMessage Export(int zoneId, int appId, string contentTypeIdsString, string entityIdsString, string templateIdsString)
+            => Real.Export(appId, zoneId, contentTypeIdsString, entityIdsString, templateIdsString);
 
-        /// <summary>
-        /// Used to be POST ImportExport/ImportContent
-        /// </summary>
-        /// <returns></returns>
+
+        /// <inheritdoc />
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = RoleNames.Admin)]
-        public ImportResultDto Import(int zoneId, int appId)
-        {
-            var wrapLog = Log.Call<ImportResultDto>();
-
-            PreventServerTimeout300();
-            if (HttpContext.Request.Form.Files.Count <= 0) return new ImportResultDto(false, "no files uploaded");
-            var file = HttpContext.Request.Form.Files[0];
-            var result = _importContentLazy.Value.Init(_userLazy.Value, Log).Import(zoneId, appId, file.FileName,
-                file.OpenReadStream(), GetContext().Site.DefaultCultureCode);
-
-            return wrapLog("ok", result);
-        }
+        public ImportResultDto Import(int zoneId, int appId) 
+            => Real.Set(PreventServerTimeout300).Import(new HttpUploadedFile(Request), zoneId, appId);
 
         #endregion
     }
