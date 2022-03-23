be20a2bdef7aa97cb835e7397e3e89925480eb52
ijungleboy <daniel.mettler@2sic.com>
2022-03-23 09:05:51 +0100

    reactivate Data.Cache.GetContentType and mark obsolete

--- Src/Sxc/ToSic.Sxc/Compatibility/Obsolete.cs
+++ Src/Sxc/ToSic.Sxc/Compatibility/Obsolete.cs
@@ -9,6 +9,9 @@ namespace ToSic.Sxc.Compatibility
     {
         public static ILog Warning13To14(string obsoleteId, string specificId, string link, Action<ILog> addMore = null) 
             => new LogObsolete(obsoleteId, specificId, "v13", "v14 ca. middle of 2022", link, addMore).Log;
+        public static ILog Warning13To15(string obsoleteId, string specificId, string link, Action<ILog> addMore = null) 
+            => new LogObsolete(obsoleteId, specificId, "v13", "v15 ca. end of 2022", link, addMore).Log;
+
         public static ILog Killed13(string obsoleteId, string specificId, string link, Action<ILog> addMore = null) 
             => new LogObsolete(obsoleteId, specificId, "v13 EOY 2021", null, link, addMore).Log;
 
