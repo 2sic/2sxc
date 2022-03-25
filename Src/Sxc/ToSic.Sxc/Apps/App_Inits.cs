d59ff25494259a8d1a5399e39f508aece4a06574
Tonći Vatavuk <tonci@sistemi.hr>
2022-03-25 12:38:13 +0100

    improve Apps.App to get ServiceProvider from constructor

---
+++ Src/Sxc/ToSic.Sxc/Apps/App_Inits.cs
@@ -0,0 +1,22 @@
+﻿using ToSic.Eav.Apps;
+using ToSic.Eav.Logging;
+using ToSic.Eav.Plumbing;
+using ToSic.Sxc.Blocks;
+using ToSic.Sxc.LookUp;
+
+namespace ToSic.Sxc.Apps
+{
+    public partial class App
+    {
+        public IApp Init(int appId, ILog log, IBlock optionalBlock = null, bool showDrafts = false)
+        {
+            var appStates = _serviceProvider.Build<IAppStates>();
+            var appIdentity = appStates.IdentityOfApp(appId);
+            var confProvider = _serviceProvider.Build<AppConfigDelegate>().Init(log);
+            var buildConfig = (optionalBlock == null)
+                ? confProvider.Build(showDrafts)
+                : confProvider.Build(optionalBlock);
+            return Init(appIdentity, buildConfig, log);
+        }
+    }
+}
