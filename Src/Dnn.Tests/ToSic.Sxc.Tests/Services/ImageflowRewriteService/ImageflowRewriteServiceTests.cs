1c8b20ae262d32e34fc4d63c09594bea0344787d
Tonći Vatavuk <tonci@sistemi.hr>
2022-04-01 12:38:08 +0200

    always add quality param it if it's not there yet

---
+++ Src/Dnn.Tests/ToSic.Sxc.Tests/Services/ImageflowRewriteService/ImageflowRewriteServiceTests.cs
@@ -0,0 +1,497 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace ToSic.Sxc.Services.Tests
+{
+    [TestClass()]
+    public class ImageflowRewriteServiceTests
+    {
+        public static readonly string JpgQualityDefault = ImageflowRewriteService.JpgQualityDefault;
+        public static readonly string PngQualityDefault = ImageflowRewriteService.PngQualityDefault;
+        public static readonly string WebpQualityDefault = ImageflowRewriteService.WebpQualityDefault;
+        public static readonly string Quality = ImageflowRewriteService.Quality;
+        public static readonly string JpgQuality = ImageflowRewriteService.JpgQuality;
+        public static readonly string PngQuality = ImageflowRewriteService.PngQuality;
+        public static readonly string WebpQuality = ImageflowRewriteService.WebpQuality;
+        public static readonly string Format = ImageflowRewriteService.Format;
+
+        private static NameValueCollection AddKeyWhenMissing(NameValueCollection queryString, string key, string value) =>
+            ImageflowRewriteService.AddKeyWhenMissing(queryString, key, value);
+
+        private static NameValueCollection AddQualityIfMissing(NameValueCollection queryString) =>
+            ImageflowRewriteService.AddQualityIfMissing(queryString);
+
+        private static NameValueCollection RewriteQuality(NameValueCollection queryString) =>
+            ImageflowRewriteService.RewriteQuality(queryString);
+
+        private NameValueCollection QueryStringRewrite(NameValueCollection queryString) => 
+            new ImageflowRewriteService().QueryStringRewrite(queryString);
+
+        
+        [TestMethod()]
+        public void AddKeyWhenMissingInQueryStringEmpty()
+        {
+            var actual = AddKeyWhenMissing(new NameValueCollection(), "key", "value");
+            var expected = new NameValueCollection { { "key", "value" } };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddKeyWhenNotMissingInQueryStringOneElement()
+        {
+            var actual = AddKeyWhenMissing(
+                new NameValueCollection { { "key", "value" } },
+                "key", "value");
+            var expected = new NameValueCollection { { "key", "value" } };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddKeyWhenMissingInQueryStringOneElement()
+        {
+            var actual = AddKeyWhenMissing(
+                new NameValueCollection { { "1", "1" } },
+                "key", "value");
+            var expected = new NameValueCollection { { "1", "1" }, { "key", "value" } };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddKeyWhenNotMissingInQueryStringManyElement()
+        {
+            var actual = AddKeyWhenMissing(
+                new NameValueCollection { { "1", "1" }, { "key", "value" } },
+                "key", "value");
+            var expected = new NameValueCollection { { "1", "1" }, { "key", "value" } };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddKeyWhenMissingInQueryStringManyElement()
+        {
+            var actual = AddKeyWhenMissing(
+                new NameValueCollection { { "1", "1" }, { "2", "2" } },
+                "key", "value");
+            var expected = new NameValueCollection { { "1", "1" }, { "2", "2" } , { "key", "value" } };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddQualityWhenMissingInQueryStringEmpty()
+        {
+            var actual = AddQualityIfMissing(new NameValueCollection());
+            var expected = new NameValueCollection { { Quality, JpgQualityDefault } };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddQualityWhenNotMissingInQueryStringExactItem()
+        {
+            var actual = AddQualityIfMissing(new NameValueCollection{{ Quality, "99" }});
+            var expected = new NameValueCollection { { Quality, "99" } };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddQualityWhenMissingInQueryStringManyItems()
+        {
+            var actual = AddQualityIfMissing(new NameValueCollection
+            {
+                { JpgQuality, "79" },
+                { PngQuality, "69" },
+                { WebpQuality, "59" }
+            });
+            var expected = new NameValueCollection
+            {
+                { JpgQuality, "79" },
+                { PngQuality, "69" },
+                { WebpQuality, "59" },
+                { Quality, JpgQualityDefault }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddQualityWhenNotMissingInQueryStringManyItems()
+        {
+            var actual = AddQualityIfMissing(new NameValueCollection
+            {
+                { JpgQuality, "79" },
+                { PngQuality, "69" },
+                { WebpQuality, "59" },
+                { Quality, "89" }
+            });
+            var expected = new NameValueCollection
+            {
+                { JpgQuality, "79" },
+                { PngQuality, "69" },
+                { WebpQuality, "59" },
+                { Quality, "89" }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddWebpQualityWhenMissingInQueryStringEmpty()
+        {
+            var actual = AddQualityIfMissing(new NameValueCollection{{ Format, "webp"}});
+            var expected = new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, WebpQualityDefault }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddWebpQualityWhenNotMissingInQueryStringExactItem()
+        {
+            var actual = AddQualityIfMissing(new NameValueCollection
+            {
+                { Format, "webp" },
+                { Quality, "99" }
+            });
+            var expected = new NameValueCollection
+            {
+                { Format, "webp" },
+                { Quality, "99" }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddWebpQualityWhenMissingInQueryStringManyItems()
+        {
+            var actual = AddQualityIfMissing(new NameValueCollection
+            {
+                { Format, "webp" },
+            });
+            var expected = new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, WebpQualityDefault },
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void AddWebpQualityWhenNotMissingInQueryStringManyItems()
+        {
+            var actual = AddQualityIfMissing(new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, "59" }
+            });
+            var expected = new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, "59" }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void RewriteQualityWhenQueryStringEmpty()
+        {
+            var actual = RewriteQuality(new NameValueCollection());
+            var expected = new NameValueCollection ();
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void RewriteQualityWhenNotMissingInQueryStringExactItem()
+        {
+            var actual = RewriteQuality(new NameValueCollection { { Quality, "99" } });
+            var expected = new NameValueCollection { { Quality, "99" } };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+
+        [TestMethod()]
+        public void RewriteWebpQualityWhenNotMissingInQueryStringExactItem()
+        {
+            var actual = RewriteQuality(new NameValueCollection
+            {
+                { Format, "webp" },
+                { Quality, "99" }
+            });
+            var expected = new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, "99" }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void RewriteWebpQualityWhenMissingInQueryStringManyItems()
+        {
+            var actual = RewriteQuality(new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, "99" }
+            });
+            var expected = new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, "99" },
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void RewriteWebpQualityWhenNotMissingInQueryStringManyItems()
+        {
+            var actual = RewriteQuality(new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, "59" },
+                { Quality, "99" }
+            });
+            var expected = new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, "59" },
+                { Quality, "99" }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void QueryStringRewriteWhenQueryStringNull()
+        {
+            var actual = QueryStringRewrite(null);
+            var expected = new NameValueCollection { { Quality, JpgQualityDefault } };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+
+        [TestMethod()]
+        public void QueryStringRewriteWhenQueryStringEmpty()
+        {
+            var actual = QueryStringRewrite(new NameValueCollection());
+            var expected = new NameValueCollection { { Quality, JpgQualityDefault } };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void QueryStringRewriteWhenNotMissingInQueryStringOneElement()
+        {
+            var actual = QueryStringRewrite(new NameValueCollection { { Quality, "value" } });
+            var expected = new NameValueCollection { { Quality, "value" } };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void QueryStringRewriteWhenMissingInQueryStringOneElement()
+        {
+            var actual = QueryStringRewrite(new NameValueCollection { { "1", "1" } });
+            var expected = new NameValueCollection
+            {
+                { "1", "1" }, 
+                { Quality, JpgQualityDefault }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void QueryStringRewriteWhenNotMissingInQueryStringManyElement()
+        {
+            var actual = QueryStringRewrite(
+                new NameValueCollection
+                {
+                    { "1", "1" }, 
+                    { Quality, JpgQualityDefault }
+                });
+            var expected = new NameValueCollection
+            {
+                { "1", "1" }, 
+                { Quality, JpgQualityDefault }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void QueryStringRewriteWhenMissingInQueryStringManyElement()
+        {
+            var actual = QueryStringRewrite(
+                new NameValueCollection
+                {
+                    { "1", "1" }, 
+                    { "2", "2" }
+                });
+            var expected = new NameValueCollection
+            {
+                { "1", "1" }, 
+                { "2", "2" },
+                { Quality, JpgQualityDefault }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void QueryStringRewriteWhenMissingInQueryStringManyItems()
+        {
+            var actual = QueryStringRewrite(new NameValueCollection
+            {
+                { JpgQuality, "79" },
+                { PngQuality, "69" },
+                { WebpQuality, "59" }
+            });
+            var expected = new NameValueCollection
+            {
+                { JpgQuality, "79" },
+                { PngQuality, "69" },
+                { WebpQuality, "59" },
+                { Quality, JpgQualityDefault }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void QueryStringRewriteWhenNotMissingInQueryStringManyItems()
+        {
+            var actual = QueryStringRewrite(new NameValueCollection
+            {
+                { JpgQuality, "79" },
+                { PngQuality, "69" },
+                { WebpQuality, "59" },
+                { Quality, "89" }
+            });
+            var expected = new NameValueCollection
+            {
+                { JpgQuality, "79" },
+                { PngQuality, "69" },
+                { WebpQuality, "59" },
+                { Quality, "89" }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void QueryStringRewriteWebpWhenMissingInQueryStringEmpty()
+        {
+            var actual = QueryStringRewrite(new NameValueCollection { { Format, "webp" } });
+            var expected = new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, WebpQualityDefault }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void QueryStringRewriteWebpQuality()
+        {
+            var actual = QueryStringRewrite(new NameValueCollection
+            {
+                { Format, "webp" },
+                { Quality, "99" }
+            });
+            var expected = new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, "99" }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void QueryStringRewriteWebpQualityWhenNotMissingInQueryString()
+        {
+            var actual = QueryStringRewrite(new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, "99" }
+            });
+            var expected = new NameValueCollection
+            {
+                { Format, "webp" },
+                { WebpQuality, "99" }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+        [TestMethod()]
+        public void QueryStringRewriteWebpQualityWhenNotMissingInQueryString2()
+        {
+            var actual = QueryStringRewrite(new NameValueCollection
+            {
+                { Format, "webp" },
+                { Quality, "99" },
+                { WebpQuality, "59" }
+            });
+            var expected = new NameValueCollection
+            {
+                { Format, "webp" },
+                { Quality, "99" },
+                { WebpQuality, "59" }
+            };
+            CollectionAssert.AreEquivalent(
+                expected.AllKeys.ToDictionary(k => k, k => expected[k]),
+                actual.AllKeys.ToDictionary(k => k, k => actual[k]));
+        }
+
+
+
+    }
+}
--- Src/Sxc/ToSic.Sxc/Services/ImageflowRewriteService/ImageflowRewriteService.cs
+++ Src/Sxc/ToSic.Sxc/Services/ImageflowRewriteService/ImageflowRewriteService.cs
@@ -1,11 +1,65 @@
 ﻿using System;
 using System.Collections.Specialized;
+using System.Linq;
 
 namespace ToSic.Sxc.Services
 {
     public class ImageflowRewriteService : IImageflowRewriteService
     {
+        public static readonly string JpgQualityDefault = "70";
+        public static readonly string PngQualityDefault = "60";
+        public static readonly string WebpQualityDefault = "50";
+        public static readonly string Quality = "quality";
+        public static readonly string JpgQuality = "jpg.quality";
+        public static readonly string PngQuality = "png.quality";
+        public static readonly string WebpQuality = "webp.quality";
+        public static readonly string Format = "format";
+
         public NameValueCollection QueryStringRewrite(NameValueCollection queryString)
+        {
+            if (queryString == null) queryString = new NameValueCollection();
+
+            // TODO: eventual add "format" parameter if is missing
+
+            queryString = AddQualityIfMissing(queryString);
+
+            queryString = RewriteQuality(queryString);
+            
+            return queryString;
+        }
+
+        public static NameValueCollection AddKeyWhenMissing(NameValueCollection queryString, string key, string value)
+        {
+            if (string.IsNullOrEmpty(queryString[key]))
+                queryString.Add(key, value);
+
+            return queryString;
+        }
+
+        public static NameValueCollection AddQualityIfMissing(NameValueCollection queryString)
+        {
+            if (string.IsNullOrEmpty(queryString[Quality]))
+            {
+                switch (queryString[Format]?.ToLower())
+                {
+                    case "jpg":
+                        AddKeyWhenMissing(queryString, JpgQuality, JpgQualityDefault);
+                        break;
+                    case "png":
+                        AddKeyWhenMissing(queryString, PngQuality, PngQualityDefault);
+                        break;
+                    case "webp":
+                        AddKeyWhenMissing(queryString, WebpQuality, WebpQualityDefault);
+                        break;
+                    default:
+                        AddKeyWhenMissing(queryString, Quality, JpgQualityDefault);
+                        break;
+                }
+            }
+            return queryString;
+        }
+
+        public static NameValueCollection RewriteQuality(NameValueCollection queryString)
         {
             var qsNew = new NameValueCollection();
 
@@ -13,31 +67,36 @@ namespace ToSic.Sxc.Services
             {
                 var value = queryString[key];
                 var newKey = key;
-                
+
                 // rewrite query string
-                if (key.Equals("quality", StringComparison.OrdinalIgnoreCase))
+                if (key.Equals(Quality, StringComparison.OrdinalIgnoreCase))
                 {
                     // use the quality parameter of format,
                     // rather than reusing the jpeg quality command.
-                    switch (queryString["format"].ToLower())
+                    switch (queryString[Format]?.ToLower())
                     {
                         case "jpg":
-                            newKey = "jpeg.quality";
+                            newKey = JpgQuality;
                             break;
                         case "png":
-                            newKey = "png.quality";
+                            newKey = PngQuality;
                             break;
                         case "webp":
-                            newKey = "webp.quality";
+                            newKey = WebpQuality;
                             break;
                         default:
                             newKey = key;
                             break;
                     }
+                    if (!queryString.AllKeys.Contains(newKey))
+                        qsNew[newKey] = value; // rewrite
+                    else
+                        qsNew[Quality] = value; // do not rewrite, because we already have quality parameters specified for format
                 }
-                
-                qsNew.Add(newKey, value);
+                else
+                    qsNew[newKey] = value; // just copy
             }
+
             return qsNew;
         }
     }
