d6db606018cc8d06f7fb5fbdd5f78887192cda1c
ijungleboy <daniel.mettler@2sic.com>
2022-04-04 07:48:33 +0200

    minor - add Sxc.Services to all templates

--- Src/Sxc/ToSic.Sxc/Apps/Assets/AssetTemplates.cs
+++ Src/Sxc/ToSic.Sxc/Apps/Assets/AssetTemplates.cs
@@ -71,51 +71,7 @@ namespace ToSic.Sxc.Apps.Assets
             .FirstOrDefault(t => t.Key.Equals(key, StringComparison.InvariantCultureIgnoreCase));
 
 
-
-
         public const string CsApiTemplateControllerName = "PleaseRenameController";
 
-        // copied from the razor tutorial
-
-        //internal static string DefaultWebApiBody =
-//            @"#if NETCOREAPP // Oqtane
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
-//#else // DNN
-//using System.Web.Http;
-//using DotNetNuke.Web.Api;
-//#endif
-
-//[AllowAnonymous]      // define that all commands can be accessed without a login
-//// Inherit from Custom.Hybrid.Api12 to get features like App, CmsContext, Data etc.
-//// see https://docs.2sxc.org/web-api/custom/index.html
-//public class " + CsApiTemplateControllerName + @" : Custom.Hybrid.Api12
-//{
-//    [HttpGet]        // [HttpGet] says we're listening to GET requests
-//    public string Hello()
-//    {
-//        return ""Hello from the controller with ValidateAntiForgeryToken in /api"";
-//    }
-
-//    [HttpPost]        // [HttpPost] says we're listening to POST requests
-//    [ValidateAntiForgeryToken] // protects from the users not on your site (CSRF protection)
-//    public int Sum([FromBody] dynamic bodyJson) // post body { ""a"": 2, ""b"": 3 }
-//    {
-//        int a = bodyJson.a;
-//        int b = bodyJson.b;
-//        return a + b;
-//    }
-//}
-//";
-
-
-
-//        internal static string Readme = @"# Readme
-
-//A standard README file.
-//";
-
-//        internal static string Txt = @"Simple text file.
-//";
     }
 }
