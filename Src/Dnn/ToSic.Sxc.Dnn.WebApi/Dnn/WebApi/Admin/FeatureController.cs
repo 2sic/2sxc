b75d73396d36746d7f935190cad58d4af5a79568
Tonći Vatavuk <tonci@sistemi.hr>
2022-04-07 16:50:48 +0200

    changing features should change the json, not create it new

--- Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/FeatureController.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Admin/FeatureController.cs
@@ -19,51 +19,6 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
     {
         public FeatureController(): base(FeatureControllerReal.LogSuffix) { }
 
-        // TODO: PROBABLY REMOVE, PROBABLY NOT USED ANY MORE
-        ///// <summary>
-        ///// Used to be GET a list of Features
-        ///// </summary>
-        ///// <remarks>
-        ///// Added in 2sxc 10
-        ///// </remarks>
-        //[HttpGet]
-        //[DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
-        //public IEnumerable<FeatureState> List(bool reload = false) => Real.List(reload);
-
-        // v13.02 not used any more
-        ///// <summary>
-        ///// Used to be GET the URL which will open the UI to enable/disable features.
-        ///// Note that the implementation for Oqtane will have to be different. 
-        ///// </summary>
-        ///// <remarks>
-        ///// Added in 2sxc 10
-        ///// </remarks>
-        //[HttpGet]
-        //[DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Host)]
-        //public string RemoteManageUrl()
-        //{
-        //    var site = GetService<ISite>();
-        //    var module = Request.FindModuleInfo();
-        //    var link = GetService<RemoteRouterLink>().LinkToRemoteRouter(RemoteDestinations.Features,
-        //        site,
-        //        module.ModuleID,
-        //        app: null,
-        //        module.DesktopModule.ModuleName == "2sxc");
-        //    return link;
-        //}
-
-
-        // TODO: PROBABLY REMOVE, PROBABLY NOT USED ANY MORE
-        ///// <summary>
-        ///// Used to be POST updated features JSON configuration.
-        ///// These must always be signed by the 2sxc.org private key.
-        ///// </summary>
-        ///// <remarks>
-        ///// Added in 2sxc 10
-        ///// </remarks>
-        //[HttpPost]
-        //[DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Host)]
-        //public bool Save([FromBody] FeaturesDto featuresManagementResponse) => Real.Save(featuresManagementResponse);
 
         /// <summary>
         /// POST updated features JSON configuration.
@@ -73,6 +28,6 @@ namespace ToSic.Sxc.Dnn.WebApi.Admin
         /// </remarks>
         [HttpPost]
         [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Host)]
-        public bool SaveNew([FromBody] List<FeatureNewDto> featuresManagementResponse) => Real.SaveNew(featuresManagementResponse);
+        public bool SaveNew([FromBody] List<FeatureManagementChange> changes) => Real.SaveNew(changes);
     }
 }
