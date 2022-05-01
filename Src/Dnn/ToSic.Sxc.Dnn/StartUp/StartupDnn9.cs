c4384977bbe7d8b86295942ef57181abfb6f8c1b
Tonći Vatavuk <tonci@sistemi.hr>
2022-05-01 19:01:24 +0200

    Revert "wip get dnn 9 root service provider"
    This reverts commit f7cab314b7b9a0555465adba3c388384c825fa1a.

--- Src/Dnn/ToSic.Sxc.Dnn/StartUp/StartupDnn9.cs
+++ Src/Dnn/ToSic.Sxc.Dnn/StartUp/StartupDnn9.cs
@@ -1,6 +1,4 @@
-﻿using System.Web;
-using DotNetNuke.Common;
-using DotNetNuke.DependencyInjection;
+﻿using DotNetNuke.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ToSic.Sxc.Dnn.StartUp
@@ -23,39 +21,7 @@ namespace ToSic.Sxc.Dnn.StartUp
             // TODO: @STV
             // Give it the Dnn 9 Global Service Provider
             // https://github.com/dnnsoftware/Dnn.Platform/blob/9f83285a15d23203cbaad72d62add864ab5b8c7f/DNN%20Platform/DotNetNuke.Web/Common/LazyServiceProvider.cs#L28
-            DnnDi.GetPreparedServiceProvider = () => HttpContext.Current.GetScopeDnn9().ServiceProvider; 
-        }
-    }
-
-    /// <summary>
-    /// Dependency injection extensions for HttpContext.
-    /// https://github.com/dnnsoftware/Dnn.Platform/blob/9f83285a15d23203cbaad72d62add864ab5b8c7f/DNN%20Platform/Library/Common/Extensions/HttpContextDependencyInjectionExtensions.cs
-    /// </summary>
-    public static class PartOfDnn9HttpContextDependencyInjectionExtensions
-    {
-        /// <summary>
-        /// Gets the http context service scope.
-        /// </summary>
-        /// <param name="httpContext">The http context from which to get the scope from.</param>
-        /// <returns>A service scope.</returns>
-        public static IServiceScope GetScopeDnn9(this HttpContext httpContext)
-        {
-            return GetScope(httpContext.Items);
-        }
-
-        /// <summary>
-        /// Gets the service scope from a collection of context items.
-        /// </summary>
-        /// <param name="contextItems">A dictionary of context items.</param>
-        /// <returns>The found service scope.</returns>
-        internal static IServiceScope GetScope(System.Collections.IDictionary contextItems)
-        {
-            if (!contextItems.Contains(typeof(IServiceScope)))
-            {
-                return null;
-            }
-
-            return contextItems[typeof(IServiceScope)] is IServiceScope scope ? scope : null;
+            // DnnDi.GetPreparedServiceProvider = () => Globals.ServiceProvider; //.GetService(typeof(IServiceProvider));
         }
     }
 }
