368b48f3f2144863380138e16ea2c7f41752bb08
Tonći Vatavuk <tonci@sistemi.hr>
2022-05-02 08:57:32 +0200

    get Dnn 9 global ServiceProvider from dnn9 internal static property Globals.DependencyProvider

--- Src/Dnn/ToSic.Sxc.Dnn/StartUp/StartupDnn9.cs
+++ Src/Dnn/ToSic.Sxc.Dnn/StartUp/StartupDnn9.cs
@@ -21,7 +21,7 @@ namespace ToSic.Sxc.Dnn.StartUp
 
             // Give it the Dnn 9 Global Service Provider
             // https://github.com/dnnsoftware/Dnn.Platform/blob/9f83285a15d23203cbaad72d62add864ab5b8c7f/DNN%20Platform/DotNetNuke.Web/Common/LazyServiceProvider.cs#L28
-            DnnDi.GetPreparedServiceProvider = () => typeof(DotNetNuke.Common.Globals).GetField("dependencyProvider", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic)?.GetValue(null) as IServiceProvider;
+            DnnDi.GetPreparedServiceProvider = () => typeof(DotNetNuke.Common.Globals).GetProperty("DependencyProvider", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic)?.GetValue(null) as IServiceProvider;
         }
     }
 }
