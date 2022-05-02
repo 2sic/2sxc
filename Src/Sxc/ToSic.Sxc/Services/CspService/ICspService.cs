c6038a805e9e5d98d3aa38d855636f4146907c57
ijungleboy <daniel.mettler@2sic.com>
2022-05-02 12:01:57 +0200

    Update ICspService.cs
    disable unnecessary APIs for now

--- Src/Sxc/ToSic.Sxc/Services/CspService/ICspService.cs
+++ Src/Sxc/ToSic.Sxc/Services/CspService/ICspService.cs
@@ -2,26 +2,19 @@
 {
     public interface ICspService
     {
-        ///// <summary>
-        ///// Name of the CSP header to be added, based on the report-only aspect
-        ///// </summary>
-        //string Name { get; }
-
         /// <summary>
-        /// You can determine if CSP should report only. 
+        /// You can determine if CSP should report only. You cannot change it in code.
+        ///
+        /// To enable, do this in the settings (Global or Site)
         /// </summary>
         bool IsEnforced { get; }
 
         /// <summary>
-        /// Tells you if CSP is enabled or not. You cannot change it.
+        /// Tells you if CSP is enabled or not. You cannot change it in code.
+        ///
+        /// To enable, do this in the settings (Global or Site)
         /// </summary>
         bool IsEnabled { get; }
-
-        //void Activate();
-        //void AddPolicy(string policy);
-        //void AddPolicy(CspParameters policy);
-        //void AddReport(string policy);
-        //void AddReport(CspParameters policy);
 
         /// <summary>
         /// Add a CSP rule where you also specify the name.
@@ -31,32 +24,35 @@
         /// <param name="values"></param>
         void Add(string name, params string[] values);
 
+        #region Specific Commands, probably not needed - will disable for now while it's not yet a public API
 
-        void DefaultSrc(params string[] values);
-        void ChildSrc(params string[] values);
-        void ConnectSrc(params string[] values);
-        void FontSrc(params string[] values);
-        void FrameSrc(params string[] values);
-        void ImgSrc(params string[] values);
-        void ManifestSrc(params string[] values);
-        void MediaSrc(params string[] values);
-        void ObjectSrc(params string[] values);
-        void PrefetchSrc(params string[] values);
-        void ScriptSrc(params string[] values);
-        void ScriptSrcElem(params string[] values);
-        void ScriptSrcAttr(params string[] values);
-        void StyleSrc(params string[] values);
-        void StyleSrcElem(params string[] values);
-        void StyleSrcAttr(params string[] values);
-        void WorkerSrc(params string[] values);
-        void BaseUri(params string[] values);
-        void FormAction(params string[] values);
-        void FrameAncestors(params string[] values);
-        void NavigateTo(params string[] values);
-        void ReportTo(params string[] values);
-        void RequireTrustedTypesFor(params string[] values);
-        void Sandbox(params string[] values);
-        void TrustedTypes(params string[] values);
-        void UpgradeInsecureRequests();
+        //void DefaultSrc(params string[] values);
+        //void ChildSrc(params string[] values);
+        //void ConnectSrc(params string[] values);
+        //void FontSrc(params string[] values);
+        //void FrameSrc(params string[] values);
+        //void ImgSrc(params string[] values);
+        //void ManifestSrc(params string[] values);
+        //void MediaSrc(params string[] values);
+        //void ObjectSrc(params string[] values);
+        //void PrefetchSrc(params string[] values);
+        //void ScriptSrc(params string[] values);
+        //void ScriptSrcElem(params string[] values);
+        //void ScriptSrcAttr(params string[] values);
+        //void StyleSrc(params string[] values);
+        //void StyleSrcElem(params string[] values);
+        //void StyleSrcAttr(params string[] values);
+        //void WorkerSrc(params string[] values);
+        //void BaseUri(params string[] values);
+        //void FormAction(params string[] values);
+        //void FrameAncestors(params string[] values);
+        //void NavigateTo(params string[] values);
+        //void ReportTo(params string[] values);
+        //void RequireTrustedTypesFor(params string[] values);
+        //void Sandbox(params string[] values);
+        //void TrustedTypes(params string[] values);
+        //void UpgradeInsecureRequests();
+
+        #endregion
     }
 }
