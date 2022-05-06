060bd8d7a375bb1b6c1ab002a8592f57eb554251
Tonći Vatavuk <tonci@sistemi.hr>
2022-05-06 15:25:13 +0200

    bump Disable Page Publishing version to 13.10

--- Src/Patches/ToSic.Sxc.Patch.DisablePagePublishing/Constants.cs
+++ Src/Patches/ToSic.Sxc.Patch.DisablePagePublishing/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ToSic.Sxc.Patch.DisablePagePublishing
+﻿namespace ToSic.Sxc.Patch.DisablePagePublishing
 {
     internal class Constants
     {
