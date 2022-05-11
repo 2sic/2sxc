1b9429e30e06721fe11f8a90c6fe643cdab23864
Tonći Vatavuk <tonci@sistemi.hr>
2022-05-11 13:15:09 +0200

    CSP: Create dev.aspx for DNN to provide index.html

---
+++ Src/Dnn/ToSic.Sxc.Dnn/dist/ng-edit/dev.aspx.cs
@@ -0,0 +1,30 @@
+﻿using DotNetNuke.Framework;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using ToSic.Sxc.Services;
+
+namespace ToSic.Sxc.Dnn.dist.ng_edit
+{
+    public partial class Dev : CDefault
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Response.Clear();
+            Response.AppendHeader("test-dev", "2sxc");
+
+            //var sp = DnnStaticDi.GetPageScopedServiceProvider();
+            //var pageService = sp.GetRequiredService<IPageService>();
+            //pageService.AddCsp("test-csp", "2sxc");
+
+            ResponseIndex();
+        }
+
+        private void ResponseIndex()
+        {
+            var path = Server.MapPath("~/DesktopModules/ToSIC_SexyContent/dist/ng-edit/index.html");
+            if (!System.IO.File.Exists(path)) throw new Exception("File not found: " + path);
+            var html = System.IO.File.ReadAllText(path);
+            Response.Write(html);
+        }
+    }
+}
