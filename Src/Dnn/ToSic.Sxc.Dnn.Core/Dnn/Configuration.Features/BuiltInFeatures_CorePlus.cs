d345f5b58075a0ab9c5eea6eb58763c082ba0ff0
ijungleboy <daniel.mettler@2sic.com>
2022-05-17 20:52:20 +0200

    Update BuiltInFeatures_CorePlus.cs
    #2749

--- Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn/Configuration.Features/BuiltInFeatures_CorePlus.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn/Configuration.Features/BuiltInFeatures_CorePlus.cs
@@ -8,11 +8,13 @@ namespace ToSic.Sxc.Dnn.Configuration.Features
         public static readonly FeatureDefinition DnnPageWorkflow = new FeatureDefinition(
             "DnnPageWorkflow",
             new Guid("da68d954-5220-4f9c-a485-86f16b98629a"),
-            "Disable Dnn / Evoq Page Workflow",
+            "Support for Dnn / Evoq Page Workflow",
             false,
             false,
             "Enables support for Page Workflow in Evoq; on by default. It seems that in Evoq ca. v9.4+ it has become buggy. So you can disable it now. ",
             FeaturesCatalogRules.Security0Neutral,
+
+            // Todo: SHOULD MOVE TO CorePlus, but must wait till PinWheel adjusted their setup - ask 2dm
             Eav.Configuration.BuiltInFeatures.ForAllEnabled
         );
 
