e64f5a00d4e8c4e4e15ae51edd962d997c1f0c1a
ijungleboy <daniel.mettler@2sic.com>
2022-05-17 11:58:18 +0200

    prep for shared assembly info

---
+++ Src/Sxc/ToSic.Sxc/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace ToSic.Sxc
+{
+    /// <summary>
+    /// Contains information for all assemblies to use
+    /// </summary>
+    public static class SharedAssemblyInfo
+    {
+        public const string AssemblyVersion = ToSic.Eav.SharedAssemblyInfo.AssemblyVersion;
+        public const string Company = ToSic.Eav.SharedAssemblyInfo.Company;
+        public const string SxcProduct = "2sxc CMS- and Meta-Module for Dnn and Oqtane";
+        public const string SxcCopyright = "Copyright MIT © 2sic 2022";
+    }
+
+}
