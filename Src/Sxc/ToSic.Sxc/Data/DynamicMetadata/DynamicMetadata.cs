e65fdba09aecc8b33fab09b6cf9c02018adc1356
ijungleboy <daniel.mettler@2sic.com>
2022-05-30 16:33:53 +0200

    fix DynamicMetadata to lookup values from multiple sources

--- Src/Sxc/ToSic.Sxc/Data/DynamicMetadata/DynamicMetadata.cs
+++ Src/Sxc/ToSic.Sxc/Data/DynamicMetadata/DynamicMetadata.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using ToSic.Eav.Data;
+using ToSic.Eav.Data.PropertyLookup;
 using ToSic.Eav.Documentation;
+using ToSic.Eav.Logging;
 using ToSic.Eav.Metadata;
 
 namespace ToSic.Sxc.Data
@@ -22,5 +25,24 @@ namespace ToSic.Sxc.Data
         public bool HasType(string typeName) => _metadata.HasType(typeName);
 
         public IEnumerable<IEntity> OfType(string typeName) => _metadata.OfType(typeName);
+
+        public override PropertyRequest FindPropertyInternal(string field, string[] dimensions, ILog parentLogOrNull, PropertyLookupPath path)
+        {
+            var logOrNull = parentLogOrNull.SubLogOrNull("Sxc.DynEnt", Debug);
+            var safeWrap = logOrNull.Fn<PropertyRequest>($"{nameof(field)}: {field}", "DynEntity");
+            // check Entity is null (in cases where null-objects are asked for properties)
+            if (Entity == null) return safeWrap.ReturnNull("no entity");
+            path = path.KeepOrNew().Add("DynEnt", field);
+
+            // Note: most of the following lines are copied from Metadata
+            var list = (this as IHasMetadata).Metadata; // ? MetadataWithoutPermissions : OfType(typeName);
+            var found = list.FirstOrDefault(md => md.Attributes.ContainsKey(field));
+            if (found == null) return safeWrap.ReturnNull("no entity had attribute");
+            var propRequest = found.FindPropertyInternal(field, dimensions, logOrNull, path);
+            if (propRequest != null && propRequest.Result != null)
+                return safeWrap.Return(propRequest, "found");
+
+            return safeWrap.Return(base.FindPropertyInternal(field, dimensions, parentLogOrNull, path), "base...");
+        }
     }
 }
