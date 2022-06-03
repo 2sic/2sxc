05f3c4752f07ce4cace8fd7097714fca639400a2
ijungleboy <daniel.mettler@2sic.com>
2022-06-03 12:38:16 +0200

    Update DocsAttribute.cs

--- Src/Sxc/ToSic.Sxc/Code/Documentation/DocsAttribute.cs
+++ Src/Sxc/ToSic.Sxc/Code/Documentation/DocsAttribute.cs
@@ -1,15 +1,30 @@
 ﻿using System;
+using System.Linq;
 
 namespace ToSic.Sxc.Code.Documentation
 {
     public class DocsAttribute: Attribute
     {
+        public string[] Messages
+        {
+            set => _messages = value;
+        }
 
-        public string[] Messages { get; set; }
+        private string[] GetMessages(string fullName)
+        {
+            if (!AutoLink) return _messages;
+
+            var newMessages = _messages.ToList();
+            var helpLink = $"[documentation](https://docs.2sxc.org/api/dot-net/{fullName}.html";
+            newMessages.Add(helpLink);
+            return newMessages.ToArray();
+        }
+
 
         public bool AutoLink = true;
 
         public bool AllProperties = true;
+        private string[] _messages;
 
         public string HelpLink { get; set; }
     }
