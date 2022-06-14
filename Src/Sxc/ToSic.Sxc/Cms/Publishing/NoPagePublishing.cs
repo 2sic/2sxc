3fb490a8a2ed05a17bf4eb59d38806e5ed63fe6e
ijungleboy <daniel.mettler@2sic.com>
2022-06-14 16:11:23 +0200

    Pre-Rename for #2785

--- Src/Sxc/ToSic.Sxc/Cms/Publishing/NoPagePublishing.cs
+++ Src/Sxc/ToSic.Sxc/Cms/Publishing/NoPagePublishing.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using ToSic.Eav.Apps.Environment;
-using ToSic.Eav.Apps.Run;
 using ToSic.Eav.Context;
 using ToSic.Eav.Logging;
-using ToSic.Sxc.Context;
 
 
 namespace ToSic.Sxc.Cms.Publishing
