e5af510186d0115230db0f839b113587c0b048bc
ijungleboy <daniel.mettler@2sic.com>
2022-06-16 14:00:05 +0200

    Implement Razor14 and WebApi14 in Oqtane

---
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/Custom/Hybrid.Api14_TT.cs
@@ -0,0 +1,25 @@
+﻿using ToSic.Eav.Documentation;
+using ToSic.Eav.Plumbing;
+using ToSic.Sxc.Code;
+using ToSic.Sxc.Services;
+
+// ReSharper disable once CheckNamespace
+namespace Custom.Oqtane
+{
+    /// <summary>
+    /// Oqtane specific Api base class.
+    ///
+    /// As of 2sxc v12 it's identical to [](xref:Custom.Hybrid.Api12) but this may be enhanced in future. 
+    /// </summary>
+    [PublicApi]
+    public abstract class Api14<TModel, TServiceKit> : Hybrid.Api12, IDynamicCode<TModel, TServiceKit>
+        where TModel : class
+        where TServiceKit : ServiceKit
+    {
+        public TModel Model => _DynCodeRoot is not IDynamicCode<TModel, TServiceKit> root ? default : root.Model;
+
+        public TServiceKit Kit => _kit.Get(() => _DynCodeRoot.GetKit<TServiceKit>());
+        private readonly ValueGetOnce<TServiceKit> _kit = new();
+
+    }
+}
