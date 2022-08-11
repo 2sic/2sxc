4589ca84afe3cd587393b56c4d8a90683699d749
ijungleboy <daniel.mettler@2sic.com>
2022-08-11 14:13:05 +0200

    Correct getting DNN global DI / deduplicate code

--- Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn940/WebApiHttpControllerActivator.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn940/WebApiHttpControllerActivator.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Net.Http;
-using System.Reflection;
 using System.Web.Http.Controllers;
 using System.Web.Http.Dispatcher;
-using DotNetNuke.Common;
 using Microsoft.Extensions.DependencyInjection;
+using ToSic.Sxc.Dnn;
 
 // This is a special workaround for DNN 9.4
 // Review the readme.md to understand how and why
@@ -14,45 +13,22 @@ namespace ToSic.Sxc.Dnn940
 {
     public class WebApiHttpControllerActivator : IHttpControllerActivator
     {
-        public const string Dnn9DepProviderProperty = "DependencyProvider";
         public IHttpControllerActivator PreviousActivator { get; set; }
 
         public IHttpController Create(HttpRequestMessage request, HttpControllerDescriptor controllerDescriptor, Type controllerType)
         {
             // first try to just get it from the DI - if it's there
             // note that the PreviousActivator doesn't exist
-            return PreviousActivator?.Create(request, controllerDescriptor, controllerType) ??
-                   // If it's not found (null), then it's probably a dynamically compiled type from a .cs file or similar
-                   // Such types are never registered in the DI catalog, as they may change on-the-fly.
-                   // In this case we must use ActivatorUtilities, which will create the object and if it expects 
-                   // any DI parameters, they will come from the DependencyInjection as should be best practice
-                   (IHttpController)ActivatorUtilities.CreateInstance(Dnn9DependencyProvider, controllerType);
+            var resultFromDnnActivator = PreviousActivator?.Create(request, controllerDescriptor, controllerType);
+            if (resultFromDnnActivator != null) return resultFromDnnActivator;
+
+            // If it's not found (null), then it's probably a dynamically compiled type from a .cs file or similar
+            // Such types are never registered in the DI catalog, as they may change on-the-fly.
+            // In this case we must use ActivatorUtilities, which will create the object and if it expects 
+            // any DI parameters, they will come from the DependencyInjection as should be best practice
+            var dnnGlobalDi = DnnStaticDi.GetGlobalServiceProvider(); // 2022-08-11 2dm cleaned up, shouldn't use duplicate code to get dnn internal object
+            var resultFromUtilities = (IHttpController)ActivatorUtilities.CreateInstance(dnnGlobalDi, controllerType);
+            return resultFromUtilities;
         }
-
-        private IServiceProvider Dnn9DependencyProvider
-        {
-            get
-            {
-                // Only attempt this once
-                if (!FirstTry) return _dnn9DependencyProvider;
-                FirstTry = false;
-
-                try
-                {
-                    _dnn9DependencyProvider =
-                        typeof(Globals).GetProperty(Dnn9DepProviderProperty,
-                                BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
-                            ?.GetValue(null, null) as IServiceProvider;
-                }
-                catch
-                {
-                    /* ignore */
-                }
-
-                return _dnn9DependencyProvider;
-            }
-        }
-        private IServiceProvider _dnn9DependencyProvider;
-        public bool FirstTry = true;
     }
 }
--- Src/Dnn/ToSic.Sxc.Dnn/StartUp/StartupDnn9.cs
+++ Src/Dnn/ToSic.Sxc.Dnn/StartUp/StartupDnn9.cs
@@ -12,9 +12,6 @@ namespace ToSic.Sxc.Dnn.StartUp
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            // Tell the static factory (used in obsolete code) about the services we have
-            //Eav.Factory.UseExistingServices(services);
-
             // Do standard registration of all services
             DnnDi.RegisterServices(services);
 
@@ -23,10 +20,12 @@ namespace ToSic.Sxc.Dnn.StartUp
             // When we do start-up and use singletons.
             // Otherwise singletons won't be properly registered. 
             // https://github.com/dnnsoftware/Dnn.Platform/blob/9f83285a15d23203cbaad72d62add864ab5b8c7f/DNN%20Platform/DotNetNuke.Web/Common/LazyServiceProvider.cs#L28
-            DnnDi.GetPreparedServiceProvider = () => typeof(DotNetNuke.Common.Globals).GetProperty("DependencyProvider", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic)?.GetValue(null) as IServiceProvider;
+            IServiceProvider GetPreparedServiceProvider() => typeof(DotNetNuke.Common.Globals)
+                .GetProperty("DependencyProvider", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic)
+                ?.GetValue(null) as IServiceProvider;
 
             // Now activate the Service Provider, because some Dnn code still needs the static implementation
-            DnnStaticDi.StaticDiReady(DnnDi.GetPreparedServiceProvider?.Invoke());
+            DnnStaticDi.StaticDiReady(GetPreparedServiceProvider);
         }
     }
 }
