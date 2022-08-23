6b7a1cabdb76f140db0480d958bc96fbdcc7dae5
ijungleboy <daniel.mettler@2sic.com>
2022-08-23 23:32:39 +0200

    add internal APIs to improve tutorials/demos

---
+++ Src/Sxc/ToSic.Sxc/Demo/ToolbarServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using ToSic.Sxc.Services;
+
+namespace ToSic.Sxc.Demo
+{
+    /// <summary>
+    /// Demo extensions to help in tutorials with the ToolbarService.
+    ///
+    /// Not meant for production, could change at any time. 
+    /// </summary>
+    public static class ToolbarServiceExtensions
+    {
+        /// <summary>
+        /// Created for 14.07.05, used in the tutorial starting 2022-08-23
+        /// </summary>
+        /// <param name="toolbarService"></param>
+        /// <param name="noParamOrder"></param>
+        /// <param name="ui"></param>
+        public static void ActivateDemoMode(this IToolbarService toolbarService, 
+            string noParamOrder = Eav.Parameters.Protector,
+            string ui = null
+            )
+        {
+            if (!(toolbarService is ToolbarService typed)) return;
+            typed._setDemoDefaults(ui);
+        }
+    }
+}
