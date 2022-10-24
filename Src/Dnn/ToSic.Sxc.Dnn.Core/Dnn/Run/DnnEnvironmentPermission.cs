444f73f4cc672a5c8ff355816b0ad3ed6c7ea740
ijungleboy <daniel.mettler@2sic.com>
2022-10-24 15:26:09 +0200

    fix issue with content-admins not seeing all data #2904

--- Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn/Run/DnnEnvironmentPermission.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn/Run/DnnEnvironmentPermission.cs
@@ -2,10 +2,9 @@
 using DotNetNuke.Security;
 using DotNetNuke.Security.Permissions;
 using System;
-using System.Collections.Generic;
 using ToSic.Eav.Apps.Security;
 using ToSic.Eav.Logging;
-using ToSic.Eav.Security;
+using ToSic.Eav.Plumbing;
 using ToSic.Sxc.Context;
 
 namespace ToSic.Sxc.Dnn.Run
@@ -14,7 +13,6 @@ namespace ToSic.Sxc.Dnn.Run
     {
         public DnnEnvironmentPermission() : base(DnnConstants.LogName) { }
 
-        private readonly string _salPrefix = "SecurityAccessLevel.".ToLowerInvariant();
         public string CustomPermissionKey = ""; // "CONTENT";
 
         /// <summary>
@@ -23,30 +21,18 @@ namespace ToSic.Sxc.Dnn.Run
         /// <remarks>
         /// In some cases the container is a ContainerNull object without ModuleInfo, so we must really do null-checks
         /// </remarks>
-        protected ModuleInfo Module =>
-            _module ?? (_module = ((Context as IContextOfBlock)?.Module as Module<ModuleInfo>)?.GetContents());
-        private ModuleInfo _module;
-
-        public override bool EnvironmentAllows(List<Grants> grants)
-        {
-            var logWrap = Log.Fn<bool>(() => $"[{string.Join(",", grants)}]");
-            if (UserIsAnonymous()) logWrap.Return(false, "user anonymous");
-            var ok = UserIsSystemAdmin(); // superusers are always ok
-            if (!ok && CurrentZoneMatchesSiteZone())
-                ok = UserIsSiteAdmin()
-                     || UserIsModuleAdmin()
-                     || UserIsModuleEditor();
-            if (ok) GrantedBecause = Conditions.EnvironmentGlobal;
-            return logWrap.Return(ok, $"{ok} because:{GrantedBecause}");
-        }
+        protected ModuleInfo Module => _module.Get(
+            () => ((Context as IContextOfBlock)?.Module as Module<ModuleInfo>)?.GetContents());
+        private GetOnce<ModuleInfo> _module = new GetOnce<ModuleInfo>();
 
         public override bool VerifyConditionOfEnvironment(string condition)
         {
             var wrapLog = Log.Fn<bool>(condition);
-            if (!condition.ToLowerInvariant().StartsWith(_salPrefix)) 
+            var fullPrefix = (SalPrefix + ".").ToLowerInvariant();
+            if (!condition.StartsWith(fullPrefix, StringComparison.InvariantCultureIgnoreCase)) 
                 return wrapLog.ReturnFalse("unknown condition: false");
 
-            var salWord = condition.Substring(_salPrefix.Length);
+            var salWord = condition.Substring(fullPrefix.Length);
             var sal = (SecurityAccessLevel)Enum.Parse(typeof(SecurityAccessLevel), salWord);
             // check anonymous - this is always valid, even if not in a module context
             if (sal == SecurityAccessLevel.Anonymous)
@@ -61,13 +47,14 @@ namespace ToSic.Sxc.Dnn.Run
                 return wrapLog.Return(result, $"module: {result}");
             }
 
-            Log.A("trying to check permission " + _salPrefix + ", but don't have module in context");
+            Log.A("trying to check permission " + fullPrefix + ", but don't have module in context");
             return wrapLog.ReturnFalse("can't verify: false");
         }
 
-        private bool UserIsModuleAdmin() => Log.Return(() => Module != null && ModulePermissionController.CanAdminModule(Module));
+        protected override bool UserIsModuleAdmin() 
+            => Log.Return(() => Module != null && ModulePermissionController.CanAdminModule(Module));
 
-        private bool UserIsModuleEditor()
+        protected override bool UserIsModuleEditor()
             => Log.Return(() =>
             {
                 if (Module == null) return false;
