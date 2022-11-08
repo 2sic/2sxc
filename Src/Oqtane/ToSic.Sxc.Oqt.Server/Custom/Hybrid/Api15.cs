4c5af03c3fc2308b4d7a31d30b15b57515fd04f8
ijungleboy <daniel.mettler@2sic.com>
2022-11-08 13:15:53 +0100

    Prepare Api15 base class to test with System.Text #2917

---
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/Custom/Hybrid/Api15.cs
@@ -0,0 +1,18 @@
+﻿using Custom.Hybrid.Advanced;
+using ToSic.Eav.Documentation;
+using ToSic.Sxc.Services;
+
+// ReSharper disable once CheckNamespace
+namespace Custom.Hybrid
+{
+    /// <summary>
+    /// Oqtane specific Api base class.
+    ///
+    /// It's identical to [](xref:Custom.Hybrid.Api14) but this may be enhanced in future. 
+    /// </summary>
+    [PrivateApi("This will already be documented through the Dnn DLL so shouldn't appear again in the docs")]
+    public abstract class Api15: Api14<dynamic, ServiceKit14>
+    {
+
+    }
+}
