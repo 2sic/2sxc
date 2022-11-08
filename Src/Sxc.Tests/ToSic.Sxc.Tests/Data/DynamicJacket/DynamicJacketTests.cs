4362462115a14154c4edd89b5bac6a12a21644d1
Tonći Vatavuk <tonci@sistemi.hr>
2022-11-08 19:21:17 +0100

    fix https://github.com/2sic/2sxc/issues/2918

---
+++ Src/Sxc.Tests/ToSic.Sxc.Tests/Data/DynamicJacket/DynamicJacketTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// ReSharper disable once CheckNamespace
+namespace ToSic.Sxc.Data.Tests
+{
+    [TestClass]
+    public class DynamicJacketTests
+    {
+        public dynamic AsDynamic(string jsonString) => DynamicJacket.AsDynamicJacket(jsonString);
+
+        [TestMethod]
+        public void ObjectWithStringProperty()
+        {
+            var jsonString = "{ \"FirstName\": \"test\", }";
+            Assert.AreEqual<string>("test", AsDynamic(jsonString).FirstName);
+        }
+
+        [TestMethod]
+        public void ArrayOfObjectsWithStringProperty()
+        {
+            var jsonString = "[ { \"FirstName\": \"test\" }, { \"FirstName\": \"fn2\" }, ]";
+            Assert.AreEqual<string>("test", AsDynamic(jsonString)[0].FirstName);
+        }
+
+        [TestMethod]
+        public void ObjectWithArrayPropertyOfObjectsWithStringProperty()
+        {
+            var jsonString = "{ \"a\": [ { \"FirstName\": \"test\" }, { \"FirstName\": \"fn2\" }, ] }";
+            Assert.AreEqual<string>("test", AsDynamic(jsonString).a[0].FirstName);
+        }
+
+        [TestMethod]
+        public void ArrayWithStringArrays()
+        {
+            var jsonString = "[ [\"test\", \"a2\" ], [\"b1\", \"b2\" ], ]";
+            Assert.AreEqual<string>("test", AsDynamic(jsonString)[0][0]);
+        }
+
+        [TestMethod]
+        public void ObjectWithArrayPropertyWithObjectWithStringArrayProperty()
+        {
+            var jsonString = "{ \"a\": [ { \"p1\": \"fn1b\", \"p2\": [\"test\", \"a2\" ] }, { \"p1\": \"fn2\", \"p2\": [\"b1\", \"b2\" ]}, ] }";
+            Assert.AreEqual<string>("test", AsDynamic(jsonString).a[0].p2[0]);
+        }
+    }
+}
--- Src/Sxc/ToSic.Sxc/Data/DynamicJacket/DynamicJacket.Statics.cs
+++ Src/Sxc/ToSic.Sxc/Data/DynamicJacket/DynamicJacket.Statics.cs
@@ -17,39 +17,38 @@ namespace ToSic.Sxc.Data
         private const string JsonErrorCode = "error";
 
         [PrivateApi]
-        public static object AsDynamicJacket(string json, string fallback = EmptyJson) 
-            => JsonSerializer.Deserialize<dynamic>(json, JsonOptions.UnsafeJsonWithoutEncodingHtml);
+        public static object AsDynamicJacket(string json, string fallback = EmptyJson) => WrapIfJObjectUnwrapIfJValue(AsJsonNode(json, fallback));
 
-        //[PrivateApi]
-        //private static JsonNode AsJsonNode(string json, string fallback = EmptyJson)
-        //{
-        //    if (!string.IsNullOrWhiteSpace(json))
-        //        try
-        //        {
-        //            // find first possible opening character
-        //            var firstCharPos = json.IndexOfAny(new[] { JObjStart, JArrayStart });
-        //            if (firstCharPos > -1)
-        //            {
-        //                var firstChar = json[firstCharPos];
-        //                switch (firstChar)
-        //                {
-        //                    case JObjStart:
-        //                        return JsonNode.Parse(json, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions)?.AsObject();
-        //                    case JArrayStart:
-        //                        return JsonNode.Parse(json, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions)?.AsArray();
-        //                }
-        //            }
-        //        }
-        //        catch
-        //        {
-        //            if (fallback == JsonErrorCode) throw;
-        //        }
+        [PrivateApi]
+        private static JsonNode AsJsonNode(string json, string fallback = EmptyJson)
+        {
+            if (!string.IsNullOrWhiteSpace(json))
+                try
+                {
+                    // find first possible opening character
+                    var firstCharPos = json.IndexOfAny(new[] { JObjStart, JArrayStart });
+                    if (firstCharPos > -1)
+                    {
+                        var firstChar = json[firstCharPos];
+                        switch (firstChar)
+                        {
+                            case JObjStart:
+                                return JsonNode.Parse(json, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions)?.AsObject();
+                            case JArrayStart:
+                                return JsonNode.Parse(json, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions)?.AsArray();
+                        }
+                    }
+                }
+                catch
+                {
+                    if (fallback == JsonErrorCode) throw;
+                }
 
-        //    // fallback
-        //    return fallback == null
-        //        ? null
-        //        : JsonNode.Parse(fallback, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions);
-        //}
+            // fallback
+            return fallback == null
+                ? null
+                : JsonNode.Parse(fallback, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions);
+        }
 
         /// <summary>
         /// Takes a result of a object query and ensures it will be treated as a DynamicJacket as well.
@@ -60,7 +59,6 @@ namespace ToSic.Sxc.Data
         [PrivateApi]
         internal static object WrapIfJObjectUnwrapIfJValue(object original)
         {
-
             if (!(original is JsonNode jsonNode)) return original;
 
             switch (jsonNode)
@@ -70,9 +68,35 @@ namespace ToSic.Sxc.Data
                 case JsonObject jResult: // it's another complex object, so return another wrapped reader
                     return new DynamicJacket(jResult);
                 case JsonValue jValue: // it's a simple value - so we want to return the underlying real value
-                    return jValue.AsValue();
-                case null:
-                    return null;
+                {
+                    var je = jValue.GetValue<JsonElement>();
+                    switch (je.ValueKind)
+                    {
+                        case JsonValueKind.True:
+                            return true;
+                        case JsonValueKind.False:
+                            return false;
+                        case JsonValueKind.Number when je.TryGetInt32(out var intValue):
+                            return intValue;
+                        case JsonValueKind.Number when je.TryGetInt64(out var longValue):
+                            return longValue;
+                        case JsonValueKind.Number:
+                            return je.GetDouble();
+                        case JsonValueKind.String when je.TryGetDateTime(out var dateTime):
+                            return dateTime;
+                        case JsonValueKind.String:
+                            return je.GetString();
+                        case JsonValueKind.Null:
+                        case JsonValueKind.Undefined:
+                                return null;
+                        case JsonValueKind.Object:
+                            return new DynamicJacket(JsonObject.Create(je));
+                        case JsonValueKind.Array:
+                            return new DynamicJacketList(JsonArray.Create(je));
+                        default:
+                            return jValue.AsValue();
+                    }
+                }
                 default: // it's something else, let's just return that
                     return original;
             }
