3961453e13eeca0ef2165ca34e51791d70138573
Tonći Vatavuk <tonci@sistemi.hr>
2022-11-08 16:03:22 +0100

    https://github.com/2sic/2sxc/issues/2918

--- Src/Sxc/ToSic.Sxc/Data/DynamicJacket/DynamicJacket.Statics.cs
+++ Src/Sxc/ToSic.Sxc/Data/DynamicJacket/DynamicJacket.Statics.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Nodes;
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
 using ToSic.Eav.Documentation;
 using ToSic.Eav.Serialization;
 
@@ -16,38 +17,39 @@ namespace ToSic.Sxc.Data
         private const string JsonErrorCode = "error";
 
         [PrivateApi]
-        public static object AsDynamicJacket(string json, string fallback = EmptyJson) => WrapIfJObjectUnwrapIfJValue(AsJsonNode(json, fallback));
+        public static object AsDynamicJacket(string json, string fallback = EmptyJson) 
+            => JsonSerializer.Deserialize<dynamic>(json, JsonOptions.UnsafeJsonWithoutEncodingHtml);
 
-        [PrivateApi]
-        private static JsonNode AsJsonNode(string json, string fallback = EmptyJson)
-        {
-            if (!string.IsNullOrWhiteSpace(json))
-                try
-                {
-                    // find first possible opening character
-                    var firstCharPos = json.IndexOfAny(new[] { JObjStart, JArrayStart });
-                    if (firstCharPos > -1)
-                    {
-                        var firstChar = json[firstCharPos];
-                        switch (firstChar)
-                        {
-                            case JObjStart:
-                                return JsonNode.Parse(json, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions)?.AsObject();
-                            case JArrayStart:
-                                return JsonNode.Parse(json, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions)?.AsArray();
-                        }
-                    }
-                }
-                catch
-                {
-                    if (fallback == JsonErrorCode) throw;
-                }
+        //[PrivateApi]
+        //private static JsonNode AsJsonNode(string json, string fallback = EmptyJson)
+        //{
+        //    if (!string.IsNullOrWhiteSpace(json))
+        //        try
+        //        {
+        //            // find first possible opening character
+        //            var firstCharPos = json.IndexOfAny(new[] { JObjStart, JArrayStart });
+        //            if (firstCharPos > -1)
+        //            {
+        //                var firstChar = json[firstCharPos];
+        //                switch (firstChar)
+        //                {
+        //                    case JObjStart:
+        //                        return JsonNode.Parse(json, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions)?.AsObject();
+        //                    case JArrayStart:
+        //                        return JsonNode.Parse(json, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions)?.AsArray();
+        //                }
+        //            }
+        //        }
+        //        catch
+        //        {
+        //            if (fallback == JsonErrorCode) throw;
+        //        }
 
-            // fallback
-            return fallback == null
-                ? null
-                : JsonNode.Parse(fallback, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions);
-        }
+        //    // fallback
+        //    return fallback == null
+        //        ? null
+        //        : JsonNode.Parse(fallback, JsonOptions.JsonNodeDefaultOptions, JsonOptions.JsonDocumentDefaultOptions);
+        //}
 
         /// <summary>
         /// Takes a result of a object query and ensures it will be treated as a DynamicJacket as well.
@@ -58,7 +60,7 @@ namespace ToSic.Sxc.Data
         [PrivateApi]
         internal static object WrapIfJObjectUnwrapIfJValue(object original)
         {
-            
+
             if (!(original is JsonNode jsonNode)) return original;
 
             switch (jsonNode)
