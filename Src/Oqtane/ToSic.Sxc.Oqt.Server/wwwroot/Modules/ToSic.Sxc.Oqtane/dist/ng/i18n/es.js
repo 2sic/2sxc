45c34bc1fdbe3c8af69856247faf553228791000
Tonći Vatavuk <tonci@sistemi.hr>
2022-11-28 23:46:33 +0100

    clean git repo unneeded js/css files from ToSic.Sxc.Oqt.Server/wwwroot/Modules/ToSic.Sxc.Oqtane/

--- Src/Oqtane/ToSic.Sxc.Oqt.Server/wwwroot/Modules/ToSic.Sxc.Oqtane/dist/ng/i18n/es.js
+++
@@ -1,9 +0,0 @@
-{
-	"TemplatePicker": {
-		"AppPickerDefault": "Elegir App",
-		"ContentTypePickerDefault": "Elegir tipo de contenido",
-		"Save": "Guardar y cerrar",
-		"Cancel": "Cancelar cambios",
-		"Close": "Cerrar"
-	}
-}
--- Src/Oqtane/ToSic.Sxc.Oqt.Server/wwwroot/Modules/ToSic.Sxc.Oqtane/dist/quickDialog/i18n/uk.js
+++
@@ -1,9 +0,0 @@
-﻿{
-  "TemplatePicker": {
-    "AppPickerDefault": "Вибрати App",
-    "ContentTypePickerDefault": "Вибрати Тип Контенту",
-    "Save": "Зберегти та закрити",
-    "Cancel": "Відмінити зміни",
-    "Close": "Закрити"
-  }
-}
