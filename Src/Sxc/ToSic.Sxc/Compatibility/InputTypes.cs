7e61faf601d0df9235ab377ce02c38b131475d36
ijungleboy <daniel.mettler@2sic.com>
2022-12-09 10:13:52 +0100

    Prep draft ICmsService for #2959

--- Src/Sxc/ToSic.Sxc/Compatibility/InputTypes.cs
+++ Src/Sxc/ToSic.Sxc/Compatibility/InputTypes.cs
@@ -15,15 +15,15 @@ namespace ToSic.Sxc.Compatibility
         {
             // This one would be used once multiple wysiwyg implementations would need
             // to explicitly say TinyMCE. As of now, the default is the same
-            {"string-wysiwyg-tinymce", "string-wysiwyg" },
+            { "string-wysiwyg-tinymce", Eav.Data.InputTypes.InputTypeWysiwyg },
 
             // This one used to say "use the wysiwyg of DNN" 
             // this is currently not supported in 2sxc 10
-            {"string-wysiwyg-dnn", "string-wysiwyg" },
+            { "string-wysiwyg-dnn", Eav.Data.InputTypes.InputTypeWysiwyg },
 
             // This one was thought to be a configurable wysiwyg
             // ATM this feature doesn't exist yet, so just fall back to default
-            {"string-wysiwyg-adv","string-wysiwyg" },
+            { "string-wysiwyg-adv", Eav.Data.InputTypes.InputTypeWysiwyg },
         };
 
         public static string MapInputTypeV10(string original) =>
--- Src/Sxc/ToSic.Sxc/Services/CmsService/CmsService.cs
+++ Src/Sxc/ToSic.Sxc/Services/CmsService/CmsService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using ToSic.Eav.Data;
 using ToSic.Eav.Documentation;
 using ToSic.Eav.Plumbing;
 using ToSic.Lib.Logging;
 using ToSic.Razor.Blade;
+using ToSic.Sxc.Data;
 
 namespace ToSic.Sxc.Services.CmsService
 {
@@ -13,33 +15,78 @@ namespace ToSic.Sxc.Services.CmsService
         {
         }
 
-        public IHtmlTag Show(object thing, string noParamOrder = Eav.Parameters.Protector,
-            object container = null)
+        public IHtmlTag Show(object thing, string noParamOrder = Eav.Parameters.Protector, object container = null)
         {
+            var l = Log.Fn<IHtmlTag>();
+
             // Prepare the real container
             var realContainer = GetContainer(container);
 
-            // Todo: Check if the thing is a Field, if not, just return the thing wrapped inside a raw tag
+            // If it's not a field, we cannot find out more about the object
+            // In that case, just wrap the result in the container and return it
+            if (!(thing is IDynamicField field))
+                return l.Return(realContainer.Wrap(thing), "No field, will just treat as value");
 
-            // todo: check if the field is of type string-wysiwyg, otherwise just return the thing inside a tag
-            return realContainer;
+            // Get Content type and field information
+            var contentType = field.Parent.Entity.Type;
+            if (contentType == null)
+                return l.Return(realContainer.Wrap(thing), "can't find content-type, treat as value");
+
+            var attribute = contentType[field.Name];
+            if (attribute == null) 
+                return l.Return(realContainer.Wrap(thing), "no attribute info, treat as value");
+
+            // Now we handle all kinds of known special treatments
+            // Start with strings...
+            if (attribute.ControlledType == ValueTypes.String)
+            {
+                // ...wysiwyg
+                if (attribute.InputType() == InputTypes.InputTypeWysiwyg)
+                {
+                    // todo: special treatment now @STV
+                    // 1. check if we have an img tags with data-cmsid="file:..." attributes
+                    // ...if not, return wrapped
+
+                    // 2. If yes, take them apart using regex
+                    // ...best re-use code that you already have to extract script tags
+                    // - also specially look at the classes
+                    // - and the data-cmsid property
+
+                    // 3. Based on the classes, check if you can find something like
+                    // ..."wysiwyg-width#of#" - this is for resize ratios
+                    // Convert to a format like "#/#" - as a string
+
+
+                    // 4. Then use the IImageService to create Picture tags for it
+                    // - use the "#/#" as the `factor` parameter
+
+                    // Remember to re-attach an alt-attribute, class etc. from the original if it had it
+                    // Then reconstruct the original html
+                    // ...and return wrapped in the realContainer
+                }
+            }
+
+
+            // Fallback...
+            return l.Return(realContainer.Wrap(thing), "nothing else hit, will treat as value");
         }
 
         private IHtmlTag GetContainer(object container)
         {
+            var l = Log.Fn<IHtmlTag>();
             // Already an ITag
-            if (container is IHtmlTag iTagContainer) return iTagContainer;
+            if (container is IHtmlTag iTagContainer) return l.Return(iTagContainer, "container is Blade tag");
 
             if (container is string tagName)
             {
                 if (!tagName.IsEmpty() && !tagName.Contains(" "))
-                    return Tag.Custom(tagName);
+                    return l.Return(Tag.Custom(tagName), "was a tag name, created tag");
                 throw new ArgumentException("Must be a tag name like 'div' or a RazorBlade Html Tag object",
                     nameof(container));
             }
 
             // Nothing to do, just return an empty tag which can be filled...
-            return Tag.RawHtml();
+            return l.Return(Tag.RawHtml(), "no container, return empty tag");
         }
 
     }
