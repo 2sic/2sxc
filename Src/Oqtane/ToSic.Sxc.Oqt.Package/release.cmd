2817e02b1ee830e56f41cd31c6c3782a8556f9d7
Tonći Vatavuk <tonci@sistemi.hr>
2022-12-16 21:43:39 +0100

    clean tmp App_Data from git

--- Src/Oqtane/ToSic.Sxc.Oqt.Package/release.cmd
+++ Src/Oqtane/ToSic.Sxc.Oqt.Package/release.cmd
@@ -3,21 +3,12 @@
 @set BuildTarget=%OqtaneRoot%\wwwroot\Modules\%PackageName%
 
 @REM Copy the data folders
-robocopy /mir "..\..\Data\App_Data\new-app\ " "%OqtaneRoot%\Content\2sxc\system\App_Data\new-app\ "
-robocopy /mir "..\..\Data\App_Data\system\ " "%OqtaneRoot%\Content\2sxc\system\App_Data\system\ "
-rmdir /Q /S "%BuildTarget%\.databeta"
-rmdir /Q /S "%BuildTarget%\.data-custom"
-rmdir /Q /S "%BuildTarget%\App_Data\system-beta"
-rmdir /Q /S "%BuildTarget%\App_Data\system-custom"
 robocopy /mir "..\..\Data\assets\ " "%BuildTarget%\assets\ "
 
 @REM Copy 2sxc JS stuff
 robocopy /mir "%Dev2sxcAssets%\js\ " "%BuildTarget%\js\ "
 robocopy /mir "%Dev2sxcAssets%\dist\ " "%BuildTarget%\dist\ "
 robocopy /mir "%Dev2sxcAssets%\system\ " "%BuildTarget%\system\ "
-
-@REM Copy ImportExpor assets
-robocopy /mir "..\..\Dnn\ToSic.Sxc.Dnn\ImportExport\ " "%OqtaneRoot%\Content\2sxc\system\ImportExport\ "
 
 
 .nuget\nuget.exe pack %PackageName%.nuspec
