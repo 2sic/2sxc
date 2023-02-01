15451165d79d2c6ed6077699949869ea1fd703dd
Tonći Vatavuk <tonci@sistemi.hr>
2023-02-01 05:53:10 +0100

    add JsonFormatterAttribute to make significant json serialization options user customizable on v15 apis

---
+++ Src/Sxc/ToSic.Sxc.WebApi/Attributes/JsonFormatterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ToSic.Sxc.WebApi
+{
+    [AttributeUsage(AttributeTargets.Class /*| AttributeTargets.Method*/, AllowMultiple = false)]
+    public class JsonFormatterAttribute : Attribute
+    {
+        public Casing Casing { get; set; } = Casing.CamelCase;
+        public bool AutoConvertEntity { get; set; } = true;
+        public JsonFormatterAttribute()
+        { }
+    }
+
+    public enum Casing
+    {
+        CamelCase, // camelCase
+        PascalCase
+    }
+}
