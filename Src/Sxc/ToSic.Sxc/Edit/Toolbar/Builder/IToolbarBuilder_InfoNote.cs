8eb7d196133e6bd0a704e832e751ccc130c2f66b
ijungleboy <daniel.mettler@2sic.com>
2023-03-07 00:45:53 +0100

    Update IToolbarBuilder_InfoNote.cs

--- Src/Sxc/ToSic.Sxc/Edit/Toolbar/Builder/IToolbarBuilder_InfoNote.cs
+++ Src/Sxc/ToSic.Sxc/Edit/Toolbar/Builder/IToolbarBuilder_InfoNote.cs
@@ -6,14 +6,14 @@ namespace ToSic.Sxc.Edit.Toolbar
     public partial interface IToolbarBuilder
     {
         /// <summary>
-        /// 
+        /// Create an info, warning, help or link-button to assist the user.
         /// </summary>
-        /// <param name="noParamOrder"></param>
-        /// <param name="note"></param>
+        /// <param name="noParamOrder">see [](xref:NetCode.Conventions.NamedParameters)</param>
+        /// <param name="note">A string containing the note, or a object with the note and more specs.</param>
         /// <param name="link">If provided, will make the button open the link in a new window.</param>
-        /// <param name="ui"></param>
-        /// <param name="parameters"></param>
-        /// <param name="operation"></param>
+        /// <param name="ui">_optional_ configuration how to show, see [ui guide](xref:ToSic.Sxc.Services.ToolbarBuilder.Ui)</param>
+        /// <param name="parameters">_optional_ parameters for the command, see [parameters guide](xref:ToSic.Sxc.Services.ToolbarBuilder.Parameters)</param>
+        /// <param name="operation">_optional_ change [what should happen](xref:ToSic.Sxc.Services.ToolbarBuilder.Operation)</param>
         /// <returns></returns>
         [PrivateApi("WIP v15.04")]
         IToolbarBuilder Info(
