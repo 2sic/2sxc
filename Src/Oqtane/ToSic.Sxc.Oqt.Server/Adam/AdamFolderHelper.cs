6f4d8a34f4d511408ebb0378cd59ca15b2aff6ba
Tonći Vatavuk <tonci@sistemi.hr>
2023-03-16 10:46:22 +0100

    Oqtane v3.4.0 comaptibility, and preserving compatibility with older versions

--- Src/Oqtane/ToSic.Sxc.Oqt.Server/Adam/AdamFolderHelper.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/Adam/AdamFolderHelper.cs
@@ -2,15 +2,19 @@
 using Oqtane.Models;
 using Oqtane.Shared;
 using System.Collections.Generic;
+using System.Reflection;
 using ToSic.Sxc.Oqt.Server.Integration;
 
 namespace ToSic.Sxc.Oqt.Server.Adam
 {
     public class AdamFolderHelper
     {
+        private static List<Permission> _permissionList;
+        private static bool? _hasPermissionList;
+
         public static Folder NewVirtualFolder(int siteId, int? parentId, string path, string folder)
         {
-            return new()
+            var newVirtualFolder = new Folder()
             {
                 SiteId = siteId,
                 ParentId = parentId,
@@ -18,16 +22,36 @@ namespace ToSic.Sxc.Oqt.Server.Adam
                 Path = path.EnsureOqtaneFolderFormat(),
                 Order = 1,
                 IsSystem = false,
-                Type = FolderTypes.Private,
-                Permissions = new List<Permission>
-                {
-                    new(PermissionNames.Browse, RoleNames.Everyone, true),
-                    new(PermissionNames.View, RoleNames.Everyone, true),
-                    new(PermissionNames.Browse, RoleNames.Admin, true),
-                    new(PermissionNames.View, RoleNames.Admin, true),
-                    new(PermissionNames.Edit, RoleNames.Admin, true),
-                }.EncodePermissions()
+                Type = FolderTypes.Private
             };
+
+            // default permission list
+            _permissionList ??= new List<Permission>
+            {
+                new(PermissionNames.Browse, RoleNames.Everyone, true),
+                new(PermissionNames.View, RoleNames.Everyone, true),
+                new(PermissionNames.Browse, RoleNames.Admin, true),
+                new(PermissionNames.View, RoleNames.Admin, true),
+                new(PermissionNames.Edit, RoleNames.Admin, true),
+            };
+
+            // check if the class has a "PermissionList" property ( Oqtane >= 3.4.0)
+            _hasPermissionList ??= typeof(Folder).GetProperty(nameof(Folder.PermissionList)) != null;
+
+            PropertyInfo propInfo;
+            if (_hasPermissionList == true)
+            {
+                propInfo = newVirtualFolder.GetType().GetProperty(nameof(Folder.PermissionList));
+                propInfo?.SetValue(newVirtualFolder, _permissionList);
+            }
+            else
+            {
+                // for Oqtane < 3.4.0
+                propInfo = newVirtualFolder.GetType().GetProperty(nameof(Folder.Permissions));
+                propInfo?.SetValue(newVirtualFolder, _permissionList.EncodePermissions());
+            }
+
+            return newVirtualFolder;
         }
     }
 }
