4c2b87ec79afe968b204762bd740bb58ad4af040
ijungleboy <daniel.mettler@2sic.com>
2023-03-17 17:26:28 +0100

    minor cdn settigns

--- Src/Sxc/ToSic.Sxc/Web/WebResources/WebResourceProcessor.cs
+++ Src/Sxc/ToSic.Sxc/Web/WebResources/WebResourceProcessor.cs
@@ -13,16 +13,17 @@ namespace ToSic.Sxc.Web.WebResources
     {
         #region Constants
 
-        internal const string Cdn2SxcRoot = "https://cdn.2sxc.org/packages";
-        internal const string CdnLocalRoot = "/cdn/packages";
         // Note: this should not change often
         // As usually new versions of assets will often run side-by side with older versions
         // But do keep in sync w/2sxc versions (possibly just not upgrade as only small things change) for clarity
         internal const string VersionSuffix = "/v15";
 
+        //internal const string Cdn2SxcRoot = "https://cdn.2sxc.org/packages";
+        //internal const string CdnLocalRoot = "/cdn/packages";
+
         internal const string CdnDefault = "cdn";
-        internal const string Cdn2Sxc = "cdn.2sxc.org";
-        internal const string CdnLocal = "local";
+        //internal const string Cdn2Sxc = "cdn.2sxc.org";
+        //internal const string CdnLocal = "local";
 
         private const string WebResHtmlField = "Html";
 
@@ -60,7 +61,7 @@ namespace ToSic.Sxc.Web.WebResources
                 return l.Return(new PageFeatureFromSettings(key, html: html, autoOptimize: autoOptimize), "ok, using built-in cdn-path");
 
             // override temp dev
-            var newRoot = PickBestNewRoot();
+            var newRoot = CdnSource + VersionSuffix; // PickBestNewRoot();
 
             // Replacements will be delayed until preparing to generate the final HTML
             // to be sure we only replace things in the url.
@@ -92,15 +93,15 @@ namespace ToSic.Sxc.Web.WebResources
                 $"ok; root now {newRoot}");
         }
 
-        private string PickBestNewRoot()
-        {
-            switch (CdnSource)
-            {
-                case Cdn2Sxc: return Cdn2SxcRoot + VersionSuffix;
-                case CdnLocal: return CdnLocalRoot + VersionSuffix;
-                default: return CdnSource + VersionSuffix;
-            }
-        }
+        //private string PickBestNewRoot()
+        //{
+        //    switch (CdnSource)
+        //    {
+        //        case Cdn2Sxc: return Cdn2SxcRoot + VersionSuffix;
+        //        case CdnLocal: return CdnLocalRoot + VersionSuffix;
+        //        default: return CdnSource + VersionSuffix;
+        //    }
+        //}
 
     }
 }
