f9f74db937f011133e5aaabb61ec44aad7bf7912
ijungleboy <daniel.mettler@2sic.com>
2023-04-05 17:06:23 +0200

    minor

--- Src/Sxc/ToSic.Sxc.WebApi/Attributes/JsonFormatterAttribute.cs
+++ Src/Sxc/ToSic.Sxc.WebApi/Attributes/JsonFormatterAttribute.cs
@@ -1,19 +1,70 @@
 ﻿using System;
+using ToSic.Eav.Data;
+using ToSic.Eav.DataFormats.EavLight;
+using ToSic.Lib.Documentation;
 
 namespace ToSic.Sxc.WebApi
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    /// <summary>
+    /// Mark a WebApi to use the modern Json Formatter based on System.Text.Json.
+    /// Without this, older WebApi Controllers use the Newtonsoft JSON Formatter.
+    /// Also provides additional configuration to make certain work easier. 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class JsonFormatterAttribute : Attribute
     {
-        public Casing Casing { get; set; } = Casing.CamelCase;
+        /// <summary>
+        /// Automatically convert IEntity objects in the result to the preferred object structure. 
+        /// </summary>
+        // @STV TODO: - switch to EntityFormat
         public bool AutoConvertEntity { get; set; } = true;
-        public JsonFormatterAttribute()
-        { }
+
+        public EntityFormats EntityFormat { get; set; } = EntityFormats.Light;
+
+
+        public Casing Casing { get; set; } = Casing.CamelCase;
+
+        [PrivateApi("Hide constructor, not important for docs")]
+        public JsonFormatterAttribute() { }
     }
 
+    /// <summary>
+    /// Determines what casing to use when converting data to JSON
+    /// </summary>
     public enum Casing
     {
-        CamelCase, // camelCase
+        /// <summary>
+        /// Leave casing as is.
+        /// Currently not supported, will behave as CamelCase for now.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Set casing to use CamelCase for Attributes
+        /// </summary>
+        CamelCase,
+
+        /// <summary>
+        /// Set casing to use pascalCase for Attributes.
+        /// </summary>
         PascalCase
+    }
+
+    /// <summary>
+    /// Formats to use for automatic Entity to JSON conversion.
+    /// </summary>
+    public enum EntityFormats
+    {
+        /// <summary>
+        /// Do not auto-convert into any specific format.
+        /// If <see cref="IEntity"/> objects are in the result, will throw an error.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Format <see cref="IEntity"/> objects as <see cref="EavLightEntity"/>.
+        /// This results in single-language objects with name/value pairs like a JavaScript object.
+        /// </summary>
+        Light,
     }
 }
