c4641a8ee3d21feecbf2976050de84672b6e2ce3
ijungleboy <daniel.mettler@2sic.com>
2023-05-01 18:42:26 +0200

    minor logging

--- Src/Sxc/ToSic.Sxc/DataSources/AppDataSourcesLoader.cs
+++ Src/Sxc/ToSic.Sxc/DataSources/AppDataSourcesLoader.cs
@@ -11,7 +11,6 @@ using ToSic.Eav.Context;
 using ToSic.Eav.DataSource;
 using ToSic.Eav.DataSource.Catalog;
 using ToSic.Eav.DataSource.VisualQuery;
-using ToSic.Eav.DataSources;
 using ToSic.Eav.Plumbing;
 using ToSic.Lib.DI;
 using ToSic.Lib.Logging;
@@ -60,7 +59,7 @@ namespace ToSic.Sxc.DataSources
 
                 policy.ChangeMonitors.Add(new FolderChangeMonitor(new List<string> { physicalPath }));
 
-                var data = CreateDataSourceInfos(appId, physicalPath, relativePath);
+                var data = CreateDataSourceInfos(appId, LoadAppDataSources(appId, physicalPath, relativePath));
 
                 return l.ReturnAsOk((data, policy));
             }
@@ -70,36 +69,44 @@ namespace ToSic.Sxc.DataSources
             }
         }
 
-        private List<DataSourceInfo> CreateDataSourceInfos(int appId, string physicalPath, string relativePath)
+        private List<DataSourceInfo> CreateDataSourceInfos(int appId, IEnumerable<TempDsInfo> types)
         {
+            var l = Log.Fn<List<DataSourceInfo>>($"{nameof(appId)}: {appId}");
             // App state for automatic lookup of configuration content-types
             var appState = _appStates.Get(appId);
-
-            var types = LoadAppDataSources(appId, physicalPath, relativePath);
             var data = types
                 .Select(pair =>
                 {
+                    var l2 = l.Fn<DataSourceInfo>(pair.ClassName);
 
                     // 0. If error then type is null, in this case, return a specially crafted DSI
                     if (pair.Type == null)
-                        return DataSourceInfo.CreateError(pair.ClassName, false, DataSourceType.App, pair.Error);
+                        return l2.Return(DataSourceInfo.CreateError(pair.ClassName, false, DataSourceType.App, pair.Error), "error: type==null");
 
                     // 1. Make sure we only keep DataSources and not other classes in the same folder
                     // but assume all null-types are errors, which we should preserve
-                    if (!typeof(IDataSource).IsAssignableFrom(pair.Type)) return null;
+                    if (!typeof(IDataSource).IsAssignableFrom(pair.Type))
+                        return l2.ReturnNull($"error: not a {nameof(IDataSource)}");
 
                     // 2. Get VisualQuery Attribute if available, or create new, since it's optional in DynamicCode
                     var vq = pair.Type.GetDirectlyAttachedAttribute<VisualQueryAttribute>()
                              ?? new VisualQueryAttribute();
 
                     var typeName = pair.Type.Name;
-                  
 
                     // 3. Update various properties which are needed for further functionality
                     // The global name is always necessary
                     vq.NameId = vq.NameId.NullIfNoValue() ?? typeName;
                     // The configuration type is automatically picked as *Configuration (if the type exists)
-                    vq.ConfigurationType = vq.ConfigurationType.NullIfNoValue() ?? appState.GetContentType($"{typeName}Configuration")?.NameId;
+                    vq.ConfigurationType = vq.ConfigurationType.NullIfNoValue();
+                    if (vq.ConfigurationType == null)
+                    {
+                        var autoConfigTypeName = $"{typeName}Configuration";
+                        var autoConfigType = appState.GetContentType(autoConfigTypeName);
+                        vq.ConfigurationType = autoConfigType?.NameId;
+                        l2.A($"Type name '{typeName}' had no config in definition, checked '{autoConfigTypeName}', found: {autoConfigType != null}");
+                    }
+
                     // Force the type of all local DataSources to be App
                     vq.Type = DataSourceType.App;
                     // Optionally set the star-icon if none is set
@@ -110,12 +117,12 @@ namespace ToSic.Sxc.DataSources
                     if (!vq._DynamicInWasSet) vq.DynamicIn = true;
 
                     // 4. Build DataSourceInfo with the manually built Visual Query Attribute
-                    return new DataSourceInfo(pair.Type, false, overrideVisualQuery: vq);
+                    return l2.ReturnAsOk(new DataSourceInfo(pair.Type, false, overrideVisualQuery: vq));
                 })
                 .Where(dsi => dsi != null)
                 .ToList();
 
-            return data;
+            return l.Return(data, $"{data.Count}");
         }
 
         private (string physicalPath, string relativePath) GetAppDataSourceFolderPaths(int appId)
@@ -127,14 +134,14 @@ namespace ToSic.Sxc.DataSources
             return (physicalPath, relativePath);
         }
 
-        private IEnumerable<(string ClassName, Type Type, DataSourceInfoError Error)> LoadAppDataSources(
-            int appId,
-            string physicalPath, 
-            string relativePath
-        ) => Log.Func($"a:{appId}", l =>
+        private IEnumerable<TempDsInfo> LoadAppDataSources(int appId, string physicalPath, string relativePath
+        )
         {
+            var l = Log.Fn<IEnumerable<TempDsInfo>>(
+                $"{nameof(appId)}: {appId}; {nameof(physicalPath)}: '{physicalPath}'; {nameof(relativePath)}: '{relativePath}'");
+
             if (!Directory.Exists(physicalPath))
-                return (null, $"no folder {physicalPath}");
+                return l.ReturnNull($"no folder {physicalPath}");
 
             var compiler = _codeCompilerLazy.Value;
 
@@ -150,22 +157,32 @@ namespace ToSic.Sxc.DataSources
                             className: className,
                             throwOnError: false);
 
-                        if (!errorMessages.HasValue()) 
-                            return (className, type, null);
+                        if (!errorMessages.HasValue())
+                            return new TempDsInfo { ClassName = className, Type = type };
                         l.E(errorMessages);
-                        return(className, type, new DataSourceInfoError("Error Compiling", errorMessages));
+                        return new TempDsInfo
+                        {
+                            ClassName = className, Type = type,
+                            Error = new DataSourceInfoError("Error Compiling", errorMessages)
+                        };
                     }
                     catch (Exception ex)
                     {
                         l.Ex(ex);
-                        return (className, null, new DataSourceInfoError("Unknown Exception", ex.Message));
-                    }
+                        return new TempDsInfo { ClassName = className, Error = new DataSourceInfoError("Unknown Exception", ex.Message) };                    }
                 })
                 .ToList();
 
-            return types.Any() 
-                ? (types2: types, $"OK, DataSources:{types.Count} ({string.Join(";", types.Select(t => t.className))}), path:{relativePath}")
-                : (types2: types, $"OK, no working DataSources found, path:{relativePath}") ;
-        });
+            return types.Any()
+                ? l.Return(types, $"OK, DataSources:{types.Count} ({string.Join(";", types.Select(t => t.ClassName))}), path:{relativePath}")
+                : l.Return(types, $"OK, no working DataSources found, path:{relativePath}");
+        }
+
+        private class TempDsInfo
+        {
+            public string ClassName;
+            public Type Type;
+            public DataSourceInfoError Error;
+        }
     }
 }
