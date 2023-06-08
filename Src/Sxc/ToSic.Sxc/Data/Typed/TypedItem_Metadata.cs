4b131b750a79f9e626ff103a835866b37d443d26
ijungleboy <daniel.mettler@2sic.com>
2023-06-08 08:37:37 +0200

    For now, remove TypedItem.cs as not used

--- Src/Sxc/ToSic.Sxc/Data/Typed/TypedItem_Metadata.cs
+++ Src/Sxc/ToSic.Sxc/Data/Typed/TypedItem_Metadata.cs
@@ -1,14 +1,14 @@
-﻿using ToSic.Eav.Metadata;
-using ToSic.Lib.Helpers;
+﻿//using ToSic.Eav.Metadata;
+//using ToSic.Lib.Helpers;
 
-namespace ToSic.Sxc.Data
-{
-    public partial class TypedItem: IHasMetadata
-    {
-        public IMetadata Metadata => _typedMd.Get(() => DynEntity.Metadata);
-        private readonly GetOnce<IMetadata> _typedMd = new GetOnce<IMetadata>();
+//namespace ToSic.Sxc.Data
+//{
+//    public partial class TypedItem: IHasMetadata
+//    {
+//        public IMetadata Metadata => _typedMd.Get(() => DynEntity.Metadata);
+//        private readonly GetOnce<IMetadata> _typedMd = new GetOnce<IMetadata>();
 
-        IMetadataOf IHasMetadata.Metadata => Entity?.Metadata;
+//        IMetadataOf IHasMetadata.Metadata => Entity?.Metadata;
 
-    }
-}
+//    }
+//}
--- Src/Sxc/ToSic.Sxc/Data/Typed/TypedItem_Relationships.cs
+++ Src/Sxc/ToSic.Sxc/Data/Typed/TypedItem_Relationships.cs
@@ -1,25 +1,25 @@
-﻿using System.Collections.Generic;
-using ToSic.Eav;
+﻿//using System.Collections.Generic;
+//using ToSic.Eav;
 
-namespace ToSic.Sxc.Data
-{
-    public partial class TypedItem
-    {
-        /// <inheritdoc />
-        public IEnumerable<ITypedItem> Parents(
-            string type = null,
-            string noParamOrder = Parameters.Protector,
-            string field = null) =>
-            (DynEntity as ITypedItem).Parents(type, noParamOrder, field);
+//namespace ToSic.Sxc.Data
+//{
+//    public partial class TypedItem
+//    {
+//        /// <inheritdoc />
+//        public IEnumerable<ITypedItem> Parents(
+//            string type = null,
+//            string noParamOrder = Parameters.Protector,
+//            string field = null) =>
+//            (DynEntity as ITypedItem).Parents(type, noParamOrder, field);
 
-        /// <inheritdoc />
-        public IEnumerable<ITypedItem> Children(
-            string field = null,
-            string noParamOrder = Parameters.Protector,
-            string type = null) =>
-            (DynEntity as ITypedItem).Children(field, noParamOrder, type);
+//        /// <inheritdoc />
+//        public IEnumerable<ITypedItem> Children(
+//            string field = null,
+//            string noParamOrder = Parameters.Protector,
+//            string type = null) =>
+//            (DynEntity as ITypedItem).Children(field, noParamOrder, type);
 
-        /// <inheritdoc />
-        public ITypedItem Child(string field) => (DynEntity as ITypedItem).Child(field);
-    }
-}
+//        /// <inheritdoc />
+//        public ITypedItem Child(string field) => (DynEntity as ITypedItem).Child(field);
+//    }
+//}
