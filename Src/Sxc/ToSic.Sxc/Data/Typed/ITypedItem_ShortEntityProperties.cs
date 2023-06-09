b02eb225c3f1d5feade42c99de5f9f9599d503af
ijungleboy <daniel.mettler@2sic.com>
2023-06-09 12:05:58 +0200

    minor ITypedItem Id/Guid/Title/Type

--- Src/Sxc/ToSic.Sxc/Data/Typed/ITypedItem_ShortEntityProperties.cs
+++ Src/Sxc/ToSic.Sxc/Data/Typed/ITypedItem_ShortEntityProperties.cs
@@ -1,27 +1,22 @@
 ﻿using System;
-using ToSic.Lib.Documentation;
+using ToSic.Eav.Data;
 
 namespace ToSic.Sxc.Data
 {
     public partial interface ITypedItem
     {
-        // !! Important - these are the same as IDynamicEntity
-        // but we don't want to show them in the documentation for ITypedItem
-
         /// <summary>
         /// The ID of the underlying entity.
         /// Use it for edit-functionality or just to have a unique number for this item.
         /// </summary>
         /// <remarks>If the entity doesn't exist, it will return 0</remarks>
-        [PrivateApi]
-        new int EntityId { get; }
+        int Id { get; }
 
         /// <summary>
         /// The guid of the underlying entity.
         /// </summary>
         /// <remarks>If the entity doesn't exist, it will return an empty guid</remarks>
-        [PrivateApi]
-        new Guid EntityGuid { get; }
+        Guid Guid { get; }
 
         /// <summary>
         /// The title of this item. This is always available no matter what the underlying field for the title is. 
@@ -32,13 +27,11 @@ namespace ToSic.Sxc.Data
         /// It can also be null if there is no underlying entity. 
         /// </returns>
         /// <remarks>This returns a string which is usually what's expected. In previous versions (before v15) 2sxc it returned an object.</remarks>
-        [PrivateApi]
-        new string EntityTitle { get; }
+        string Title { get; }
 
         /// <summary>
-        /// The type name of the current entity. This provides the nice name like "Person" and not the technical internal StaticName
+        /// The Content-Type of the current entity.
         /// </summary>
-        [PrivateApi]
-        new string EntityType { get; }
+        IContentType Type { get; }
     }
 }
