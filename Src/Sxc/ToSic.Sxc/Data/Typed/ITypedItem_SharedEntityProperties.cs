d5874b7b4834f28e7b7cbfcfadec8d4a1275b2f4
Tonći Vatavuk <tonci@sistemi.hr>
2023-06-12 17:42:49 +0200

    rebase

--- Src/Sxc/ToSic.Sxc/Data/Typed/ITypedItem_SharedEntityProperties.cs
+++ Src/Sxc/ToSic.Sxc/Data/Typed/ITypedItem_SharedEntityProperties.cs
@@ -14,31 +14,31 @@ namespace ToSic.Sxc.Data
         /// </summary>
         /// <remarks>If the entity doesn't exist, it will return 0</remarks>
         [PrivateApi]
-        new int EntityId { get; }
+        int EntityId { get; }
 
         /// <summary>
         /// The guid of the underlying entity.
         /// </summary>
         /// <remarks>If the entity doesn't exist, it will return an empty guid</remarks>
         [PrivateApi]
-        new Guid EntityGuid { get; }
+        Guid EntityGuid { get; }
 
         /// <summary>
-        /// The title of this item. This is always available no matter what the underlying field for the title is. 
+        /// The title of this item. This is always available no matter what the underlying field for the title is.
         /// </summary>
         /// <returns>
         /// The title of the underlying entity.
         /// In rare cases where no title-field is known, it can be null.
-        /// It can also be null if there is no underlying entity. 
+        /// It can also be null if there is no underlying entity.
         /// </returns>
         /// <remarks>This returns a string which is usually what's expected. In previous versions (before v15) 2sxc it returned an object.</remarks>
         [PrivateApi]
-        new string EntityTitle { get; }
+        string EntityTitle { get; }
 
         /// <summary>
         /// The type name of the current entity. This provides the nice name like "Person" and not the technical internal StaticName
         /// </summary>
         [PrivateApi]
-        new string EntityType { get; }
+        string EntityType { get; }
     }
 }
