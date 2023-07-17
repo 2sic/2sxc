d77881c127d9ef7ea3e3d2cdf4f187748fbcf9ae
ijungleboy <daniel.mettler@2sic.com>
2023-07-17 08:09:28 +0200

    Create FakeDataService.cs
    not in use yet

---
+++ Src/Sxc/ToSic.Sxc/Services/FakeData/FakeDataService.cs
@@ -0,0 +1,19 @@
+﻿using ToSic.Eav.Data;
+using ToSic.Lib.Helpers;
+using ToSic.Sxc.Data;
+
+namespace ToSic.Sxc.Services.FakeData
+{
+    internal class FakeDataService: ServiceForDynamicCode
+    {
+        public FakeDataService() : base($"{Constants.SxcLogName}.FkData")
+        {
+        }
+
+        public ITypedItem EmptyItem => _emptyItem.Get(() => _DynCodeRoot.AsC.AsItem(EmptyEntity, Eav.Parameters.Protector));
+        private readonly GetOnce<ITypedItem> _emptyItem = new GetOnce<ITypedItem>();
+
+        public IEntity EmptyEntity => _emptyEntity.Get(() => _DynCodeRoot.AsC.FakeEntity(_DynCodeRoot.App?.AppId));
+        private readonly GetOnce<IEntity> _emptyEntity = new GetOnce<IEntity>();
+    }
+}
