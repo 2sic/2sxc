6d40840af2a84ab680a2be569eb56e0fed40b377
Tonći Vatavuk <tonci@sistemi.hr>
2023-08-23 15:04:45 +0200

    fix webassembly service implementations in client

--- Src/Oqtane/ToSic.Sxc.Oqt.Client/ClientStartup.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Client/ClientStartup.cs
@@ -2,18 +2,23 @@
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Oqtane.Services;
 using ToSic.Sxc.Oqt.Client.Services;
+using ToSic.Sxc.Oqt.Client.Services.NoOp;
 using ToSic.Sxc.Oqt.Shared.Interfaces;
 
 namespace ToSic.Sxc.Oqt.Client
 {
-  public class ClientStartup : IClientStartup
-  {
-    public void ConfigureServices(IServiceCollection services)
+    public class ClientStartup : IClientStartup
     {
-      // Must register services here, because can not use IService for registration when service has DI interface in another assembly (eg. ToSic.Sxc.Oqtane.Shared.dll)
-      services.TryAddScoped<OqtPageChangeService>();
-      services.TryAddScoped<IOqtDebugStateService, OqtDebugStateService>();
-      services.TryAddScoped<OqtSxcRenderService>();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Must register services here, because can not use IService for registration when service has DI interface in another assembly (eg. ToSic.Sxc.Oqtane.Shared.dll)
+            services.TryAddScoped<OqtPageChangeService>();
+            services.TryAddScoped<OqtSxcRenderService>();
+            services.TryAddScoped<IOqtDebugStateService, OqtDebugStateService>();
+
+            // No Operation Service
+            services.AddScoped<IOqtPageChangesOnServerService, OqtPageChangesOnServerNoOpService>();
+            services.TryAddScoped<IOqtPrerenderService, OqtPrerenderNoOpService>();
+        }
     }
-  }
 }
