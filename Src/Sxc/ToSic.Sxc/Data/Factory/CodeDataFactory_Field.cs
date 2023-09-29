94801cf54bfe6a1e700276c936e25c00af42a22d
iJungleboy <daniel.mettler@2sic.com>
2023-09-29 18:03:04 +0200

    Update CodeDataFactory_Field.cs
    implement #3189

--- Src/Sxc/ToSic.Sxc/Data/Factory/CodeDataFactory_Field.cs
+++ Src/Sxc/ToSic.Sxc/Data/Factory/CodeDataFactory_Field.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ToSic.Eav.Data;
+using ToSic.Razor.Blade;
 using static ToSic.Eav.Parameters;
 using static ToSic.Sxc.Data.Typed.TypedHelpers;
 
@@ -11,8 +12,19 @@ namespace ToSic.Sxc.Data
         {
             Protect(noParamOrder, nameof(required));
             // TODO: make sure that if we use a path, the field is from the correct parent
-            if (name.Contains(PropertyStack.PathSeparator.ToString()))
-                throw new NotImplementedException("Path support on this method is not yet supported. Ask iJungleboy");
+            var dot = PropertyStack.PathSeparator.ToString();
+            if (name.Contains(dot))
+            {
+                var parentPath = name.BeforeLast(dot);
+                var field = name.AfterLast(dot);
+                var newParent = parent.Child(parentPath);
+                if (newParent == null)
+                    throw new NullReferenceException(
+                        $"Tried to get the child object the path '{name}' (would be '{parentPath}') but got null. Can't return the field '{field}' of this null object.");
+                return newParent.Field(field, required: propsRequired);
+
+                // throw new NotImplementedException("Path support on this method is not yet supported. Ask iJungleboy");
+            }
 
             return IsErrStrict(parent, name, required, propsRequired)
                 ? throw ErrStrictForTyped(parent, name)
