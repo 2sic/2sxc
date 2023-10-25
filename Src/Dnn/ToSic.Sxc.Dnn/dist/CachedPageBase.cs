0de79272724dee3e0a19138206f344f1018c5b89
Tonći Vatavuk <tonci@sistemi.hr>
2023-10-25 12:09:55 +0200

    more fault-tolerant code, case insensitive with multiple fallbacks #3188

--- Src/Dnn/ToSic.Sxc.Dnn/dist/CachedPageBase.cs
+++ Src/Dnn/ToSic.Sxc.Dnn/dist/CachedPageBase.cs
@@ -9,8 +9,12 @@ using DotNetNuke.Entities.Portals;
 using DotNetNuke.Entities.Urls;
 using DotNetNuke.Framework;
 using Microsoft.Extensions.DependencyInjection;
+using ToSic.Eav.Apps.Environment;
 using ToSic.Eav.Helpers;
 using ToSic.Eav.Plumbing;
+using ToSic.Lib.DI;
+using ToSic.Lib.Helpers;
+using ToSic.Lib.Logging;
 using ToSic.Sxc.Dnn.Web;
 using ToSic.Sxc.Services;
 using ToSic.Sxc.Web;
@@ -22,6 +26,26 @@ namespace ToSic.Sxc.Dnn.dist
     {
         private const int UnknownSiteId = -1;
         private const int UnknownPageId = -1;
+
+        #region GetService and Service Provider
+
+        /// <summary>
+        /// Get the service provider only once - ideally in Dnn9.4 we will get it from Dnn
+        /// If we would get it multiple times, there are edge cases where it could be different each time! #2614
+        /// </summary>
+        private IServiceProvider ServiceProvider => _serviceProvider.Get(Log, DnnStaticDi.CreateModuleScopedServiceProvider);
+        private readonly GetOnce<IServiceProvider> _serviceProvider = new GetOnce<IServiceProvider>();
+        private TService GetService<TService>() => ServiceProvider.Build<TService>(Log);
+
+        #endregion
+
+        #region Logging
+
+        private ILog Log { get; } = new Log("Sxc.Dnn.CachedPageBase");
+        private IEnvironmentLogger EnvLogger => _envLogger.Get(Log, GetService<IEnvironmentLogger>);
+        private readonly GetOnce<IEnvironmentLogger> _envLogger = new GetOnce<IEnvironmentLogger>();
+        #endregion
+
         protected string PageOutputCached(string virtualPath, EditUiResourceSettings settings)
         {
             var key = CacheKey(virtualPath);
@@ -70,20 +94,30 @@ namespace ToSic.Sxc.Dnn.dist
         /// <param name="pageId"></param>
         /// <param name="portalId"></param>
         /// <returns></returns>
-        private static string GetSiteRoot(int pageId, int portalId)
+        private string GetSiteRoot(int pageId, int portalId)
         {
-            // this is fallback
-            if (pageId == UnknownPageId) return ServicesFramework.GetServiceFrameworkRoot();
-            if (portalId == UnknownSiteId) portalId = PortalController.GetPortalDictionary()[pageId];
+            try
+            {
+                // this is fallback
+                if (pageId == UnknownPageId) return ServicesFramework.GetServiceFrameworkRoot();
+                if (portalId == UnknownSiteId) portalId = PortalController.GetPortalDictionary()[pageId];
 
-            //var cultureCode = LocaleController.Instance.GetCurrentLocale(portalId).Code;
-            var cultureCode = System.Threading.Thread.CurrentThread.CurrentCulture.ToString();
-            var primaryPortalAlias = PortalAliasController.Instance.GetPortalAliasesByPortalId(portalId)
-                .Where(a => HttpContext.Current.Request.Url.ToString().Contains(a.HTTPAlias))
-                .GetAliasByPortalIdAndSettings(portalId, result: null, cultureCode, settings: new FriendlyUrlSettings(portalId));
-            var siteRoot = primaryPortalAlias != null ? CleanLeadingPartSiteRoot(primaryPortalAlias.HTTPAlias) : ServicesFramework.GetServiceFrameworkRoot();
-            if (string.IsNullOrEmpty(siteRoot)) siteRoot = "/";
-            return siteRoot;
+                //var cultureCode = LocaleController.Instance.GetCurrentLocale(portalId).Code;
+                var cultureCode = System.Threading.Thread.CurrentThread.CurrentCulture.ToString();
+                var primaryPortalAlias = PortalAliasController.Instance.GetPortalAliasesByPortalId(portalId)
+                                             .Where(a => HttpContext.Current.Request.Url.ToString().IndexOf(a.HTTPAlias, StringComparison.OrdinalIgnoreCase) >= 0) // case insensitive
+                                             .GetAliasByPortalIdAndSettings(portalId, result: null, cultureCode, settings: new FriendlyUrlSettings(portalId)) ?? 
+                                         PortalAliasController.Instance.GetPortalAliasesByPortalId(portalId).FirstOrDefault(); // fallback to first alias
+                var siteRoot = primaryPortalAlias != null ? CleanLeadingPartSiteRoot(primaryPortalAlias.HTTPAlias) : ServicesFramework.GetServiceFrameworkRoot();
+                if (string.IsNullOrEmpty(siteRoot)) siteRoot = "/";
+                return siteRoot;
+            }
+            catch (Exception ex)
+            {
+                EnvLogger.LogException(ex);
+                // if all breaks, falling back to a default value
+                return ServicesFramework.GetServiceFrameworkRoot();
+            }
         }
 
         private static string CleanLeadingPartSiteRoot(string path)
