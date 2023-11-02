def7ed1d56db6016f6e9ac013065cd065d3005ab
iJungleboy <daniel.mettler@2sic.com>
2023-11-02 10:00:38 +0100

    Update HistoryControllerReal.cs
    https://github.com/2sic/2sxc/issues/3199

--- Src/Sxc/ToSic.Sxc.WebApi/Cms/HistoryControllerReal.cs
+++ Src/Sxc/ToSic.Sxc.WebApi/Cms/HistoryControllerReal.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.Generic;
-using ToSic.Eav.Apps;
+using ToSic.Eav.Apps.AppSys;
+using ToSic.Eav.Apps.Parts;
+using ToSic.Eav.Apps.Work;
 using ToSic.Eav.Persistence.Versions;
 using ToSic.Eav.WebApi.Cms;
 using ToSic.Eav.WebApi.Formats;
-using ToSic.Lib.DI;
 using ToSic.Lib.Services;
 
 namespace ToSic.Sxc.WebApi.Cms
@@ -13,23 +14,23 @@ namespace ToSic.Sxc.WebApi.Cms
         public const string LogSuffix = "Hist";
 
         // #UnusedFeatureHistoryOfGroup 2022-07-05 2dm removed - probably clean up ca. Q4 2022
-        public HistoryControllerReal(LazySvc<AppManager> appManagerLazy) : base("Api.CmsHistoryRl")
+        public HistoryControllerReal(AppWorkUnit<WorkEntityVersioning, IAppWorkCtxWithDb> versioner) : base("Api.CmsHistoryRl")
         {
             ConnectServices(
-                _appManagerLazy = appManagerLazy
+                _versioner = versioner
             );
         }
 
-        private readonly LazySvc<AppManager> _appManagerLazy;
+        private readonly AppWorkUnit<WorkEntityVersioning, IAppWorkCtxWithDb> _versioner;
 
 
         public List<ItemHistory> Get(int appId, ItemIdentifier item)
-            => _appManagerLazy.Value.Init(appId).Entities.VersionHistory(item.EntityId);
+            => _versioner.New(appId: appId).VersionHistory(item.EntityId);
 
 
         public bool Restore(int appId, int changeId, ItemIdentifier item)
         {
-            _appManagerLazy.Value.Init(appId).Entities.VersionRestore(item.EntityId, changeId);
+            _versioner.New(appId: appId).VersionRestore(item.EntityId, changeId);
             return true;
         }
     }
