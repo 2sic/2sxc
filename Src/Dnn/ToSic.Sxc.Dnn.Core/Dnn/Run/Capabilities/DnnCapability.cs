f9dd7213654829c52d11e7098d4ac43052cb9e6d
iJungleboy <daniel.mettler@2sic.com>
2023-11-08 17:07:45 +0100

    Create DnnCapability.cs
    #3203

---
+++ Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn/Run/Capabilities/DnnCapability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ToSic.Eav.Run.Capabilities;
+
+namespace ToSic.Sxc.Dnn.Run.Capabilities
+{
+    public class DnnCapability: SystemCapabilityBase
+    {
+
+        private static readonly SystemCapabilityDefinition DefStatic = new SystemCapabilityDefinition(
+                "Dnn",
+                new Guid("00cb8d98-bfac-4a18-b7de-b1237498f183"),
+                "Dnn"
+            );
+
+        public DnnCapability() : base(DefStatic, true) { }
+
+    }
+}
