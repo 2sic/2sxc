eb211dca648e66ba6ad2334b023efd646e4b8bb3
iJungleboy <daniel.mettler@2sic.com>
2023-11-17 11:57:50 +0100

    minor

--- Src/Sxc/ToSic.Sxc/Configuration/Features/BuiltInFeatures_Beta.cs
+++ Src/Sxc/ToSic.Sxc/Configuration/Features/BuiltInFeatures_Beta.cs
@@ -9,10 +9,10 @@ namespace ToSic.Sxc.Configuration.Features
         public static readonly Feature RazorThrowPartial = new Feature(
             "RazorThrowPartial",
             new Guid("d5a327c5-db0f-472b-93b2-94e66b15e16b"),
-            "Will render most of the page and only error on a partial-render, instead of breaking the entire module. ",
+            "Razor: Handle Errors in sub-components for all users",
             false,
             false,
-            "If enabled, then Html.Render or similar activities which throw an error won't stop the entire module, but just that part. ",
+            "Will render most of the page and only error on a partial-render, instead of breaking the entire module. If enabled, then Html.Render or similar activities which throw an error won't stop the entire module, but just that part. ",
             FeaturesCatalogRules.Security0Neutral,
             Eav.Internal.Features.BuiltInFeatures.ForCorePlusEnabled
         );
@@ -20,10 +20,10 @@ namespace ToSic.Sxc.Configuration.Features
         public static readonly Feature RenderThrowPartialSystemAdmin = new Feature(
             "RenderThrowPartialSystemAdmin",
             new Guid("5b0c9379-2fef-4f6e-9022-4d3c50e894e5"),
-            "Will render most of the page and only error on a partial-render, instead of breaking the entire module. But only when the sys-admin is viewing the page.",
+            "Razor: Handle Errors in sub-components for system admins only (host users)",
             false,
             false,
-            "If enabled, then Html.Render or similar activities which throw an error won't stop the entire module, but just that part. ",
+            "Will render most of the page and only error on a partial-render, instead of breaking the entire module. But only when the sys-admin is viewing the page. If enabled, then Html.Render or similar activities which throw an error won't stop the entire module, but just that part. ",
             FeaturesCatalogRules.Security0Neutral,
             Eav.Internal.Features.BuiltInFeatures.ForCorePlusEnabled
         );
