e4ae63049fa7cf3ec44596c29ec0c44ea221cd73
iJungleboy <daniel.mettler@2sic.com>
2023-11-17 11:38:50 +0100

    Move old MetadataFor to Dnn-Compatibility

---
+++ Src/Dnn/ToSic.Sxc.Dnn.Core/Compatibility/Eav/ToSic.Eav.Data.MetadataFor.cs
@@ -0,0 +1,10 @@
+﻿// 2019-11-13 2dm - introduced this, because we moved metadatafor to Metadata (backwards compatibility)
+// it's used by others, like on https://stackoverflow.com/questions/55814403/2sxc-web-api-create-content-item-metadata-for-adam-asset
+
+// ReSharper disable once CheckNamespace
+namespace ToSic.Eav.Data
+{
+    public class MetadataFor: Metadata.Target
+    {
+    }
+}
