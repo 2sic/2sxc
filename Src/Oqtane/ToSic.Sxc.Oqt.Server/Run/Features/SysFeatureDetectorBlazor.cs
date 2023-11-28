c40b6e8dee65e6eb5d1e15eafeb525654baebf24
iJungleboy <daniel.mettler@2sic.com>
2023-11-28 16:02:29 +0100

    minor Oqtane features / description

--- Src/Oqtane/ToSic.Sxc.Oqt.Server/Run/Features/SysFeatureDetectorBlazor.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/Run/Features/SysFeatureDetectorBlazor.cs
@@ -1,7 +1,7 @@
 ﻿using ToSic.Eav.Internal.Features;
 using static ToSic.Eav.Internal.Features.SysFeatureSuggestions;
 
-namespace ToSic.Sxc.Oqt.Server.Run.Capabilities;
+namespace ToSic.Sxc.Oqt.Server.Run.Features;
 
 internal class SysFeatureDetectorBlazor: SysFeatureDetector
 {
--- Src/Oqtane/ToSic.Sxc.Oqt.Server/Run/Features/SysFeatureDetectorCSharpInOqt.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/Run/Features/SysFeatureDetectorCSharpInOqt.cs
@@ -1,7 +1,7 @@
 ﻿using ToSic.Eav.Internal.Features;
 using static ToSic.Eav.Internal.Features.SysFeatureSuggestions;
 
-namespace ToSic.Sxc.Oqt.Server.Run.Capabilities;
+namespace ToSic.Sxc.Oqt.Server.Run.Features;
 
 public class SysFeatureDetectorCSharp6: SysFeatureDetector
 {
