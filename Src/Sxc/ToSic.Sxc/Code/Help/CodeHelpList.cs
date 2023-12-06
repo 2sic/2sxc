b42905a16e57eaa2199979b52f7895bc30546d07
iJungleboy <daniel.mettler@2sic.com>
2023-12-06 13:53:15 +0100

    add help on noParamOrder coding issue #3226

--- Src/Sxc/ToSic.Sxc/Code/Help/CodeHelpList.cs
+++ Src/Sxc/ToSic.Sxc/Code/Help/CodeHelpList.cs
@@ -6,25 +6,31 @@ namespace ToSic.Sxc.Code.Help;
 [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
 internal class CodeHelpList
 {
-    public static CodeHelp IEntityOnEavNamespace = new(name: "ToSic.Eav.IEntity",
+    private static readonly CodeHelp IEntityOnEavNamespace = new(name: "ToSic.Eav.IEntity",
         detect: "error CS0234: The type or namespace name 'IEntity' does not exist in the namespace 'ToSic.Eav",
         linkCode: "ErrIEntity",
-        uiMessage: "IEntity is used on the wrong namespace.");
+        uiMessage: "IEntity is used on the wrong namespace, correct would be ToSic.Eav.Data.IEntity.");
 
-    public static CodeHelp DynamicList = new(name: "Can't use Lambda",
+    private static readonly CodeHelp DynamicList = new(name: "Can't use Lambda",
         detect: "error CS1977: Cannot use a lambda expression as an argument to a dynamically dispatched operation without first casting it to a delegate or expression tree type",
         linkCode: "ErrLambda",
         uiMessage: "Lambdas have difficulties with dynamic objects.");
 
-    public static CodeHelp DynamicEntity = new(name: "DynamicEntity not found",
+    private static readonly CodeHelp DynamicEntity = new(name: "DynamicEntity not found",
         detect: "error CS0246: The type or namespace name 'DynamicEntity' could not be found",
         linkCode: "ErrDynamicEntity", 
         uiMessage: "The type DynamicEntity shouldn't be used.");
 
-    public static CodeHelp MethodOnObjectNotFound = new(name: "Method on Object not found",
+    private static readonly CodeHelp MethodOnObjectNotFound = new(name: "Method on Object not found",
         detect: "Cannot perform runtime binding on a null reference",
         linkCode: "err-binding-on-null-reference",
         uiMessage: "Method not found - typically on a dynamic object.");
+
+    private static readonly CodeHelp NoParamOrderUsed = new(name: "Advanced APIs should use Parameter-Names and not Param-Order",
+        // real error is ca. :error CS1503: Argument 2: cannot convert from 'bool' to 'ToSic.Lib.Coding.NoParamOrder' at System.Web.Compilation.AssemblyBuilder.Compile()
+        detect: "to 'ToSic.Lib.Coding.NoParamOrder'",
+        linkCode: "named-params",
+        uiMessage: "Many methods have optional parameters - these must be named, otherwise you see this error.");
 
     public static List<CodeHelp> ListInvalidCast = new()
     {
@@ -35,6 +41,7 @@ internal class CodeHelpList
     {
         DynamicList,
         DynamicEntity,
+        NoParamOrderUsed,
     };
 
     public static List<CodeHelp> ListRuntime = new(ListInvalidCast)
