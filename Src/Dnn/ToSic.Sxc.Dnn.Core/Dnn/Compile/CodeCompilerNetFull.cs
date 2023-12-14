934d9145fdb55dcc87ee4aaa0da0356bd82b0416
Tonći Vatavuk <tonci@sistemi.hr>
2023-12-14 10:10:35 +0100

    clean logging

--- Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn/Compile/CodeCompilerNetFull.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn/Compile/CodeCompilerNetFull.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Web.Compilation;
 using ToSic.Lib.Documentation;
-using ToSic.Lib.Logging;
 using ToSic.Sxc.Code;
 
 namespace ToSic.Sxc.Dnn.Compile;
@@ -12,30 +11,25 @@ internal class CodeCompilerNetFull(IServiceProvider serviceProvider) : CodeCompi
 {
     protected internal override AssemblyResult GetAssembly(string relativePath, string className, int appId = 0)
     {
-        var l = Log.Fn<AssemblyResult>(
-            $"{nameof(relativePath)}: '{relativePath}'; {nameof(className)}: '{className}'; {nameof(appId)}: {appId}");
-
         try
         {
             var assembly = BuildManager.GetCompiledAssembly(relativePath);
-            return l.ReturnAsOk(new AssemblyResult(assembly));
+            return new AssemblyResult(assembly);
         }
         catch (Exception ex)
         {
-            l.Ex(ex);
             var errorMessage =
                 $"Error: Can't compile '{className}' in {Path.GetFileName(relativePath)}. Details are logged into insights. " +
                 ex.Message;
-            return l.ReturnAsError(new AssemblyResult(errorMessages: errorMessage), "error");
+            return new AssemblyResult(errorMessages: errorMessage);
         }
     }
 
     protected override (Type Type, string ErrorMessage) GetCsHtmlType(string relativePath)
     {
-        var l = Log.Fn<(Type Type, string ErrorMessage)>($"{nameof(relativePath)}: '{relativePath}'", timer: true);
         var compiledType = BuildManager.GetCompiledType(relativePath);
         var errMsg = compiledType == null
             ? $"Couldn't create instance of {relativePath}. Compiled type == null" : null;
-        return l.Return((compiledType, errMsg), errMsg ?? "Ok");
+        return (compiledType, errMsg);
     }
 }
