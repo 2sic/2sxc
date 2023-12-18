afeb2cfdc2275e37cc807caba7f127180fc9e660
iJungleboy <daniel.mettler@2sic.com>
2023-12-18 13:55:24 +0100

    Add minor help to new App.Code / App.Data namespaces

---
+++ Src/Sxc/ToSic.Sxc/App/Data/_Help.cs
@@ -0,0 +1,21 @@
+﻿using ToSic.Lib.Documentation;
+
+// ReSharper disable once CheckNamespace
+namespace MyApp.Data
+{
+    // this class is just placeholder to enable compilation of razor code with MyApp.Code namespace that is used in some razor templates
+
+    /// <summary>
+    /// This class is a placeholder to provide help in the docs.
+    ///
+    /// The namespace <see cref="MyApp.Data"/> is reserved for custom code in your app.
+    /// As such, it doesn't have any code in the public docs, but will contain classes and code from your app.
+    ///
+    /// To use, see TODO: Add link to docs
+    /// </summary>
+    [PrivateApi]
+     // ReSharper disable once UnusedType.Global
+    // ReSharper disable once InconsistentNaming
+   public class _Help
+    { }
+}
