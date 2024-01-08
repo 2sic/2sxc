ee4a39fd4b81baefb9e80214672f1241bd31831f
iJungleboy <daniel.mettler@2sic.com>
2024-01-08 16:30:01 +0100

    Create TypedItem.cs
    experimental

---
+++ Src/Sxc/ToSic.Sxc/Data/Experimental/TypedItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using ToSic.Lib.Coding;
+
+namespace ToSic.Sxc.Data.Experimental;
+
+public abstract class TypedItem(ITypedItem item)
+{
+    public ITypedItem Item => item;
+
+#pragma warning disable IDE0060
+    protected TValue GetThis<TValue>(NoParamOrder protector = default, TValue fallback = default, [CallerMemberName] string name = default)
+#pragma warning restore IDE0060
+    {
+        return item.Get(name, fallback: fallback);
+    }
+}
