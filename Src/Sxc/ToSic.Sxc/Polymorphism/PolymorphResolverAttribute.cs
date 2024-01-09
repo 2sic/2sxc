b56cd4eb4b3afd6e9d456a646ee2ce52aabd6b65
iJungleboy <daniel.mettler@2sic.com>
2024-01-09 21:44:47 +0100

    internalize #3198

--- Src/Sxc/ToSic.Sxc/Polymorphism/PolymorphResolverAttribute.cs
+++ Src/Sxc/ToSic.Sxc/Polymorphism/PolymorphResolverAttribute.cs
@@ -4,9 +4,7 @@ namespace ToSic.Sxc.Polymorphism;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
 [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
-public class PolymorphResolverAttribute : Attribute
+public class PolymorphResolverAttribute(string name) : Attribute
 {
-    public string Name { get; }
-
-    public PolymorphResolverAttribute(string name) => Name = name;
+    public string Name { get; } = name;
 }
