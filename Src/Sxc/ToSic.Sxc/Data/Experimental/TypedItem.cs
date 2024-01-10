07b6ed347fba46e98c5a4623bbc8748cce96e7be
iJungleboy <daniel.mettler@2sic.com>
2024-01-10 17:04:01 +0100

    minor https://github.com/2sic/2sxc/issues/3244

--- Src/Sxc/ToSic.Sxc/Data/Experimental/TypedItem.cs
+++ Src/Sxc/ToSic.Sxc/Data/Experimental/TypedItem.cs
@@ -1,10 +1,12 @@
 ﻿using System.Runtime.CompilerServices;
+using ToSic.Eav.Data;
 using ToSic.Lib.Coding;
 
 namespace ToSic.Sxc.Data.Experimental;
 
-public abstract class TypedItem(ITypedItem item)
+public abstract class TypedItem(ITypedItem item): ICanBeEntity
 {
+
     public ITypedItem Item => item;
 
 #pragma warning disable IDE0060
@@ -13,4 +15,10 @@ public abstract class TypedItem(ITypedItem item)
     {
         return item.Get(name, fallback: fallback);
     }
+
+    /// <summary>
+    /// This is necessary so the object can be used in places where an IEntity is expected,
+    /// like toolbars.
+    /// </summary>
+    IEntity ICanBeEntity.Entity => Item.Entity;
 }
