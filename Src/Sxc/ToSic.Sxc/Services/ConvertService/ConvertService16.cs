ef6783e804c398db519e134ac7412c12c7fe3da9
iJungleboy <daniel.mettler@2sic.com>
2024-01-22 18:15:30 +0100

    slim down ConvertService16, remove optional signatures

--- Src/Sxc/ToSic.Sxc/Services/ConvertService/ConvertService16.cs
+++ Src/Sxc/ToSic.Sxc/Services/ConvertService/ConvertService16.cs
@@ -1,10 +1,11 @@
-﻿using ToSic.Eav.Plumbing;
-using ToSic.Lib.DI;
+﻿using ToSic.Lib.DI;
 using ToSic.Lib.Services;
 using ToSic.Sxc.Services.Internal;
 
-
-// ReSharper disable MethodOverloadWithOptionalParameter
+// 2024-01-22 2dm
+// Remove all convert methods which are just missing the optional parameters, to make the API smaller.
+// Assume it has no side effects, must watch.
+// Remove this note 2024-Q3 (ca. July)
 
 namespace ToSic.Sxc.Services;
 
@@ -24,49 +25,50 @@ internal class ConvertService16: ServiceBase, IConvertService16
         );
     }
 
-    public bool OptimizeNumbers => true;
+    //public bool OptimizeNumbers => true;
 
-    public bool OptimizeBoolean => true;
+    //public bool OptimizeBoolean => true;
 
-    public T To<T>(object value) => value.ConvertOrDefault<T>(numeric: OptimizeNumbers, truthy: OptimizeBoolean);
+    //public T To<T>(object value) => value.ConvertOrDefault<T>(numeric: OptimizeNumbers, truthy: OptimizeBoolean);
 
     public T To<T>(object value, NoParamOrder noParamOrder = default, T fallback = default) => _cnvSvc.To(value, noParamOrder, fallback);
 
-    public int ToInt(object value) => _cnvSvc.To<int>(value);
+    //public int ToInt(object value) => _cnvSvc.To<int>(value);
     public int ToInt(object value, NoParamOrder noParamOrder = default, int fallback = 0) => _cnvSvc.To(value, fallback: fallback);
 
-    public Guid ToGuid(object value) => _cnvSvc.To<Guid>(value);
+    //public Guid ToGuid(object value) => _cnvSvc.To<Guid>(value);
     public Guid ToGuid(object value, NoParamOrder noParamOrder = default, Guid fallback = default) => _cnvSvc.To(value, fallback: fallback);
 
-    public float ToFloat(object value) => _cnvSvc.To<float>(value);
+    //public float ToFloat(object value) => _cnvSvc.To<float>(value);
     public float ToFloat(object value, NoParamOrder noParamOrder = default, float fallback = default) => _cnvSvc.To(value, fallback: fallback);
 
-    public decimal ToDecimal(object value) => _cnvSvc.To<decimal>(value);
+    //public decimal ToDecimal(object value) => _cnvSvc.To<decimal>(value);
     public decimal ToDecimal(object value, NoParamOrder noParamOrder = default, decimal fallback = default) => _cnvSvc.To(value, fallback: fallback);
 
-    public double ToDouble(object value) => _cnvSvc.To<double>(value);
+    //public double ToDouble(object value) => _cnvSvc.To<double>(value);
     public double ToDouble(object value, NoParamOrder noParamOrder = default, double fallback = default) => _cnvSvc.To(value, fallback: fallback);
 
-    public bool ToBool(object value) => _cnvSvc.To<bool>(value);
+    //public bool ToBool(object value) => _cnvSvc.To<bool>(value);
     public bool ToBool(object value, NoParamOrder noParamOrder = default, bool fallback = false) => _cnvSvc.To(value, fallback: fallback);
         
-    public string ToString(object value) => _cnvSvc.To<string>(value);
+    //public string ToString(object value) => _cnvSvc.To<string>(value);
 
     public string ToString(object value, NoParamOrder noParamOrder = default, string fallback = default, bool fallbackOnNull = true) 
         => _cnvSvc.ToString(value, noParamOrder, fallback, fallbackOnNull);
 
-    public string ForCode(object value) => _code.Value.ForCode(value);
+    //public string ForCode(object value) => _code.Value.ForCode(value);
     public string ForCode(object value, NoParamOrder noParamOrder = default, string fallback = default) => _code.Value.ForCode(value, noParamOrder, fallback);
         
 
     public IJsonService Json => _jsonLazy.Value;
     private readonly LazySvc<IJsonService> _jsonLazy;
 
-    #region Invisible Converts for backward compatibility
 
-    public int ToInt32(object value) => ToInt(value);
+    #region Invisible Converts for backward compatibility - 2dm removed 2024-01-22 since it's not in the interface and can't be in use - del 2024-Q3
 
-    public float ToSingle(object value) => ToFloat(value);
+    //public int ToInt32(object value) => ToInt(value);
+
+    //public float ToSingle(object value) => ToFloat(value);
 
     #endregion
 }
