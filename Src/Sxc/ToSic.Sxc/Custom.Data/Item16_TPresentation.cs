f4a6f4fd9c5dc166b48e2e6c3d368961549a6cd3
iJungleboy <daniel.mettler@2sic.com>
2024-02-27 17:10:22 +0100

    experiments, non-production code

---
+++ Src/Sxc/ToSic.Sxc/Custom.Data/Item16_TPresentation.cs
@@ -0,0 +1,23 @@
+﻿// 2024-02-27 2dm - was an experiment, will probably drop it...
+
+//using ToSic.Eav.Plumbing;
+//using ToSic.Sxc.Data;
+//using ToSic.Sxc.Data.Internal.Typed;
+
+//// ReSharper disable once CheckNamespace
+//namespace Custom.Data;
+
+//public abstract class Item16<TPresentation> : Item16
+//    where TPresentation : class, ITypedItem //, ITypedItemWrapper16, new()
+//{
+//    public new TPresentation Presentation => _presentation
+//        ??= _myItem.Presentation.NullOrGetWith(p =>
+//        {
+//            if (p is TPresentation pTyped) return pTyped;
+//            if (_myItem is not TypedItemOfEntity typed) return null;
+
+//            return typed.Cdf.AsCustom(typeof(TPresentation), p) as TPresentation;
+//        });
+
+//    private TPresentation _presentation;
+//}
--- Src/Sxc/ToSic.Sxc/Data/Internal/Factory/CodeDataFactory_AsCustom.cs
+++ Src/Sxc/ToSic.Sxc/Data/Internal/Factory/CodeDataFactory_AsCustom.cs
@@ -5,7 +5,7 @@ partial class CodeDataFactory
     /// <summary>
     /// EXPERIMENTAL
     /// </summary>
-    public T AsCustom<T>(ICanBeEntity source, NoParamOrder protector, bool mock)
+    public T AsCustom<T>(ICanBeEntity source, NoParamOrder protector = default, bool mock = false)
         where T : class, ITypedItemWrapper16, ITypedItem, new()
     {
         if (!mock && source == null) return null;
@@ -13,6 +13,29 @@ partial class CodeDataFactory
 
         var item = source as ITypedItem ?? AsItem(source);
         var wrapper = new T();
+        wrapper.Setup(item);
+        return wrapper;
+    }
+
+    /// <summary>
+    /// WIP / experimental, would be for types which are not as T, but as a type-object.
+    /// Not in use, so not fully tested.
+    ///
+    /// Inspired by https://stackoverflow.com/questions/3702916/is-there-a-typeof-inverse-operation
+    /// </summary>
+    /// <param name="t"></param>
+    /// <param name="source"></param>
+    /// <param name="protector"></param>
+    /// <param name="mock"></param>
+    /// <returns></returns>
+    public object AsCustom(Type t, ICanBeEntity source, NoParamOrder protector = default, bool mock = false)
+    {
+        if (!mock && source == null) return null;
+        if (source.GetType() == t) return source;
+
+        var item = source as ITypedItem ?? AsItem(source);
+        var wrapperObj = Activator.CreateInstance(t);
+        if (wrapperObj is not ITypedItemWrapper16 wrapper) return null;
         wrapper.Setup(item);
         return wrapper;
     }
