782f56d3a03d8839425a586e60f01aa9063e127e
iJungleboy <daniel.mettler@2sic.com>
2024-02-29 21:37:55 +0100

    improve generate models

--- Src/Sxc/ToSic.Sxc/Code/Internal/Generate/DataClassProperties/GeneratePropertyBase.cs
+++ Src/Sxc/ToSic.Sxc/Code/Internal/Generate/DataClassProperties/GeneratePropertyBase.cs
@@ -1,5 +1,5 @@
-﻿using Custom.Data;
-using ToSic.Eav.Plumbing;
+﻿using ToSic.Eav.Plumbing;
+using ToSic.Sxc.Data;
 
 namespace ToSic.Sxc.Code.Internal.Generate;
 
@@ -28,6 +28,20 @@ internal abstract class GeneratePropertyBase
         );
     }
 
+    private static readonly string[] OverridePropertyNames =
+    [
+        nameof(ITypedItem.Id),
+        nameof(ITypedItem.Guid),
+        nameof(ITypedItem.Title),
+        nameof(ITypedItem.Type),
+        nameof(ITypedItem.Metadata),
+        nameof(ITypedItem.Presentation),
+        nameof(ITypedItem.IsPublished),
+        nameof(ITypedItem.Publishing),
+        // nameof(ITypedItem.Dyn), - this one is explicitly implemented, so it's not available
+        nameof(ITypedItem.IsDemoItem)
+    ];
+
     private string GenProp(int tabs, string returnType, string name, string method, string parameters, bool cache = false)
     {
         if (parameters.HasValue())
@@ -37,7 +51,10 @@ internal abstract class GeneratePropertyBase
 
         var cacheVarName = $"_{char.ToLower(name[0])}{name.Substring(1)}";
         var cacheResult = cache ? $"{cacheVarName} ??= " : "";
-        var mainCode = $"{indent}public {returnType} {name} => {cacheResult}{method}(\"{name}\"{parameters});";
+
+        var newPrefix = OverridePropertyNames.Contains(name) ? "new " : "";
+
+        var mainCode = $"{indent}public {newPrefix}{returnType} {name} => {cacheResult}{method}(\"{name}\"{parameters});";
 
         var cacheCode = cache
             ? $"\n{indent}private {returnType} {cacheVarName};"
--- Src/Sxc/ToSic.Sxc/Code/Internal/Generate/DataClassProperties/GeneratePropertyString.cs
+++ Src/Sxc/ToSic.Sxc/Code/Internal/Generate/DataClassProperties/GeneratePropertyString.cs
@@ -7,6 +7,11 @@ internal class GeneratePropertyString: GeneratePropertyBase
     public override List<CodeFragment> Generate(CodeGenSpecs specs, IContentTypeAttribute attribute, int tabs)
     {
         var name = attribute.Name;
+
+        // If we're generating a "Title" for a property that is already the title, don't generate it again
+        // Don't do this. Reason is that it will confuse people why it's missing, and serialization might also miss it.
+        // if (name == Attributes.TitleNiceName && attribute.IsTitle) return [];
+
         return
         [
             GenPropSnip(tabs: tabs, returnType: "string", name: name, method: $"{specs.ItemAccessor}.String",
