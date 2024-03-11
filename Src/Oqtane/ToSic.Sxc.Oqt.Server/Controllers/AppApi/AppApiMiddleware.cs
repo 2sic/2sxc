3d84887e07332ac71d9c6c00c5633687ec0936a0
Tonći Vatavuk <tonci@sistemi.hr>
2024-03-11 14:48:33 +0100

    minor doc

--- Src/Oqtane/ToSic.Sxc.Oqt.Server/Controllers/AppApi/AppApiMiddleware.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/Controllers/AppApi/AppApiMiddleware.cs
@@ -7,34 +7,45 @@ using ToSic.Eav.WebApi.Errors;
 
 namespace ToSic.Sxc.Oqt.Server.Controllers.AppApi;
 
-// TODO: @STV - PLS EXPLAIN what this does / what it's for
+
+/// <summary>
+/// Middleware for handling requests to the custom App API.
+/// - extract route values like appFolder, controller, action, apiFile, dllName, etc.
+/// - compile controller if not already compiled
+/// - ensure security, 
+/// - provide action context and invoke controller action
+/// </summary>
 internal static class AppApiMiddleware
 {
-    [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
-    internal static async Task InvokeAsync(HttpContext context)
+  /// <summary>
+  /// Invokes the middleware asynchronously.
+  /// </summary>
+  /// <param name="context">The HTTP context.</param>
+  [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+  internal static async Task InvokeAsync(HttpContext context)
+  {
+    // Transform route values.
+    var appApiDynamicRouteValueTransformer = context.RequestServices.GetService<AppApiDynamicRouteValueTransformer>();
+    var values = await appApiDynamicRouteValueTransformer.TransformAsync(context, context.Request.RouteValues);
+
+    // Compile and register dyncode app api controller.
+    var appApiControllerManager = context.RequestServices.GetService<AppApiControllerManager>();
+    if (!await appApiControllerManager.PrepareController(values))
+      throw new HttpExceptionAbstraction(HttpStatusCode.NotFound, "Error, can't compile controller.", "Not Found");
+
+    // Provide ActionContext.
+    var appApiActionContext = context.RequestServices.GetService<AppApiActionContext>();
+    var actionContext = appApiActionContext.Provide(context, values);
+
+    async Task InvokeActionAfterAuthorization(HttpContext context)
     {
-        // Transform route values.
-        var appApiDynamicRouteValueTransformer = context.RequestServices.GetService<AppApiDynamicRouteValueTransformer>();
-        var values = await appApiDynamicRouteValueTransformer.TransformAsync(context, context.Request.RouteValues);
-
-        // Compile and register dyncode app api controller.
-        var appApiControllerManager = context.RequestServices.GetService<AppApiControllerManager>();
-        if (!await appApiControllerManager.PrepareController(values))
-            throw new HttpExceptionAbstraction(HttpStatusCode.NotFound, "Error, can't compile controller.", "Not Found");
-
-        // Provide ActionContext.
-        var appApiActionContext = context.RequestServices.GetService<AppApiActionContext>();
-        var actionContext = appApiActionContext.Provide(context, values);
-
-        async Task InvokeActionAfterAuthorization(HttpContext context)
-        {
-            // Invoke controller action.
-            var appApiActionInvoker = context.RequestServices.GetService<AppApiActionInvoker>();
-            await appApiActionInvoker.Invoke(actionContext);
-        }
-
-        // Check security.
-        var appApiAuthorization = context.RequestServices.GetService<AppApiAuthorization>().Init(InvokeActionAfterAuthorization);
-        await appApiAuthorization.Invoke(actionContext);
+      // Invoke controller action.
+      var appApiActionInvoker = context.RequestServices.GetService<AppApiActionInvoker>();
+      await appApiActionInvoker.Invoke(actionContext);
     }
+
+    // Check security.
+    var appApiAuthorization = context.RequestServices.GetService<AppApiAuthorization>().Init(InvokeActionAfterAuthorization);
+    await appApiAuthorization.Invoke(actionContext);
+  }
 }
