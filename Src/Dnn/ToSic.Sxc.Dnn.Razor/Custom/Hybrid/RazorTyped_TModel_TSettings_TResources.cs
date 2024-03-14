6a68f63b356e9ae052542a8f239ed87bd751d555
iJungleboy <daniel.mettler@2sic.com>
2024-03-14 18:23:30 +0100

    minor

--- Src/Dnn/ToSic.Sxc.Dnn.Razor/Custom/Hybrid/RazorTyped_TModel_TSettings_TResources.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.Razor/Custom/Hybrid/RazorTyped_TModel_TSettings_TResources.cs
@@ -1,15 +1,19 @@
-﻿using ToSic.Sxc.Data;
-using ToSic.Sxc.Apps;
+﻿// 2024-03-14 2dm - experimental, don't use yet
+// will probably never make it, as we prefer to use Customize on each AppRazor
+// since we would also need parameters for View<TSettings, TResources> etc.
 
-// ReSharper disable once CheckNamespace
-namespace Custom.Hybrid;
+//using ToSic.Sxc.Data;
+//using ToSic.Sxc.Apps;
 
-[PrivateApi]
-[System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
-public abstract class RazorTyped<TModel, TSettings, TResources> : RazorTyped<TModel>
-    where TSettings : class, ITypedItem, ITypedItemWrapper16, new()
-    where TResources : class, ITypedItem, ITypedItemWrapper16, new()
-{
-    public new IAppTyped<TSettings, TResources> App => _app ??= Customize.App<TSettings, TResources>(); // GetService<IAppTyped<TSettings, TResources>>(reuse: true);
-    private IAppTyped<TSettings, TResources> _app;
-}
+//// ReSharper disable once CheckNamespace
+//namespace Custom.Hybrid;
+
+//[PrivateApi]
+//[System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+//public abstract class RazorTyped<TModel, TSettings, TResources> : RazorTyped<TModel>
+//    where TSettings : class, ITypedItem, ITypedItemWrapper16, new()
+//    where TResources : class, ITypedItem, ITypedItemWrapper16, new()
+//{
+//    public new IAppTyped<TSettings, TResources> App => _app ??= Customize.App<TSettings, TResources>(); // GetService<IAppTyped<TSettings, TResources>>(reuse: true);
+//    private IAppTyped<TSettings, TResources> _app;
+//}
