50993326449b01a233e822487a01e9724b789fd4
Tonći Vatavuk <tonci@sistemi.hr>
2024-05-03 10:52:58 +0200

    minor logging

--- Src/Sxc/ToSic.Sxc/Blocks/Internal/View/BlockViewLoader.cs
+++ Src/Sxc/ToSic.Sxc/Blocks/Internal/View/BlockViewLoader.cs
@@ -26,17 +26,25 @@ internal class BlockViewLoader(ILog parentLog) : HelperBase(parentLog, "Blk.View
         var l = Log.Fn<IView>("template override - check");
         if (context.Page.Parameters == null) return l.ReturnNull("no params");
 
+        var urlParameterString = string.Join(", ", context.Page.Parameters.Select(pair => $"'{pair.Key}'='{pair.Value}'"));
+        l.A($"url params: {context.Page.Parameters.Count} - {urlParameterString}");
+
         var urlParameterDict = context.Page.Parameters.ToDictionary(pair => pair.Key?.ToLowerInvariant() ?? "", pair =>
             $"{pair.Key}/{pair.Value}".ToLowerInvariant());
 
         var allTemplates = views.GetAll();
+        l.A($"all templates: {allTemplates.Count}");
 
         foreach (var template in allTemplates.Where(t => !string.IsNullOrEmpty(t.UrlIdentifier)))
         {
             var desiredFullViewName = template.UrlIdentifier.ToLowerInvariant();
+            l.A($"checking: {desiredFullViewName}");
+
             if (desiredFullViewName.EndsWith("/.*"))   // match details/.* --> e.g. details/12
             {
                 var keyName = desiredFullViewName.Substring(0, desiredFullViewName.Length - 3);
+                l.A($"checking: {keyName}");
+
                 if (urlParameterDict.ContainsKey(keyName))
                     return l.Return(template, "template override - found:" + template.Name);
             }
