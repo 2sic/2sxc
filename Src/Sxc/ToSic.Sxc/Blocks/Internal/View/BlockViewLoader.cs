a2137ab6f1a261c6bcd3596d9bd08280a72a91ef
iJungleboy <daniel.mettler@2sic.com>
2024-05-06 11:40:09 +0200

    improve template-view-switch and logging

--- Src/Sxc/ToSic.Sxc/Blocks/Internal/View/BlockViewLoader.cs
+++ Src/Sxc/ToSic.Sxc/Blocks/Internal/View/BlockViewLoader.cs
@@ -21,6 +21,7 @@ internal class BlockViewLoader(ILog parentLog) : HelperBase(parentLog, "Blk.View
         return viewFromUrlParam ?? configView;
     }
 
+
     private IView TryGetViewBasedOnUrlParams(IContextOfBlock context, WorkViews views)
     {
         var l = Log.Fn<IView>("template override - check");
@@ -32,27 +33,22 @@ internal class BlockViewLoader(ILog parentLog) : HelperBase(parentLog, "Blk.View
         var urlParameterDict = context.Page.Parameters.ToDictionary(pair => pair.Key?.ToLowerInvariant() ?? "", pair =>
             $"{pair.Key}/{pair.Value}".ToLowerInvariant());
 
-        var allTemplates = views.GetAll();
-        l.A($"all templates: {allTemplates.Count}");
+        // Get all views which can switch, and exit early if there are none
+        var templatesWithUrlIdentifier = views.GetForViewSwitch();
+        if (templatesWithUrlIdentifier.Count == 0)
+            return l.ReturnNull("no templates with view-switch");
 
-        foreach (var template in allTemplates.Where(t => !string.IsNullOrEmpty(t.UrlIdentifier)))
+        // check all views if they apply here
+        foreach (var set in templatesWithUrlIdentifier)
         {
-            var desiredFullViewName = template.UrlIdentifier.ToLowerInvariant();
-            l.A($"checking: {desiredFullViewName}");
-
-            if (desiredFullViewName.EndsWith("/.*"))   // match details/.* --> e.g. details/12
-            {
-                var keyName = desiredFullViewName.Substring(0, desiredFullViewName.Length - 3);
-                l.A($"checking: {keyName}");
-
-                if (urlParameterDict.ContainsKey(keyName))
-                    return l.Return(template, "template override - found:" + template.Name);
-            }
-            else if (urlParameterDict.ContainsValue(desiredFullViewName)) // match view/details
-                return l.Return(template, "template override - found:" + template.Name);
+            l.A($"checking: '{set.UrlIdentifier}'; IsRegex: {set.IsRegex}; MainKey: '{set.MainKey}'");
+            var foundMatch = set.IsRegex
+                ? urlParameterDict.ContainsKey(set.MainKey)         // match details/.*
+                : urlParameterDict.ContainsValue(set.MainKey);      // match view/details
+            if (foundMatch)
+                return l.Return(set.View, "template override: " + set.Name);
         }
 
-        return l.ReturnNull("template override - none");
+        return l.ReturnNull("template override: none");
     }
-
 }
