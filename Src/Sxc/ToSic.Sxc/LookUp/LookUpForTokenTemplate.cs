45ae216ce92c8f199ab70788b54ed79ab72d692c
iJungleboy <daniel.mettler@2sic.com>
2024-05-14 13:10:48 +0200

    minor experimental new ICanBeLookup #3373

--- Src/Sxc/ToSic.Sxc/LookUp/LookUpForTokenTemplate.cs
+++ Src/Sxc/ToSic.Sxc/LookUp/LookUpForTokenTemplate.cs
@@ -31,6 +31,7 @@ internal partial class LookUpForTokenTemplate(
     int repeaterTotal = -1)
     : ILookUp
 {
+    private ILookUp _lookUp;
     public string Name { get; } = name;
 
     public string Description => "LookUp for creating token based templates. In addition to retrieving values, it also resolves special tokens like repeater:index, repeater:isfirst, etc.";
@@ -99,4 +100,6 @@ internal partial class LookUpForTokenTemplate(
 
     /// <inheritdoc/>
     public virtual string Get(string key) => Get(key, "");
+
+    ILookUp ICanBeLookUp.LookUp => this;
 }
