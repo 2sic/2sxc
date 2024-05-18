e461f960faf01aced46ad4da266a3033c53ce3cf
iJungleboy <daniel.mettler@2sic.com>
2024-05-18 14:47:09 +0200

    Create cache key specs and test #3390

---
+++ Src/Sxc.Tests/ToSic.Sxc.Tests/ServicesTests/CacheTests/CacheKeyTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ToSic.Sxc.Services.Cache;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using static ToSic.Sxc.Services.Cache.CacheServiceConstants;
+
+namespace ToSic.Sxc.Tests.ServicesTests.CacheTests;
+
+[TestClass]
+public class CacheKeyTests
+{
+    private const string FullDefaultPrefix = DefaultPrefix + Sep + SegmentPrefix + DefaultSegment + Sep;
+
+    [DataRow(null)]
+    [DataRow("")]
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void MainKeyBad(string main)
+        => AreEqual(FullDefaultPrefix + main, new CacheKeySpecs(main).Key);
+
+    [DataRow("TestKey")]
+    [DataRow("A")]
+    [DataRow("A\nB")]
+    [TestMethod]
+    public void MainKeyOnly(string main)
+        => AreEqual( FullDefaultPrefix + main, new CacheKeySpecs(main).Key);
+
+    private const string FullSegmentPrefix = DefaultPrefix + Sep + SegmentPrefix;
+
+    [DataRow($"{DefaultSegment}{Sep}Main", "Main", null)]
+    [DataRow($"{DefaultSegment}{Sep}Main", "Main", "")]
+    [DataRow($"MySegment{Sep}Main", "Main", "MySegment")]
+    [TestMethod]
+    public void MainAndSegment(string expected, string main, string segment)
+        => AreEqual( FullSegmentPrefix + expected, new CacheKeySpecs(main, segment).Key);
+
+}
---
+++ Src/Sxc/ToSic.Sxc/Services/Cache/CacheKeySpecs.cs
@@ -0,0 +1,26 @@
+﻿using ToSic.Eav.Plumbing;
+using static ToSic.Sxc.Services.Cache.CacheServiceConstants;
+
+namespace ToSic.Sxc.Services.Cache;
+
+internal record CacheKeySpecs(string Main, string Segment = default, string VaryBy = default)
+{
+    public string Key => _key ??= GetKey(this);
+    private string _key;
+
+    private static string GetKey(CacheKeySpecs keySpecs)
+    {
+        if (string.IsNullOrWhiteSpace(keySpecs.Main))
+            throw new ArgumentException("Key must not be empty", nameof(keySpecs.Main));
+
+        // Prevent accidental adding of the prefix/segment multiple times
+        var mainKey = keySpecs.Main.StartsWith(DefaultPrefix)
+            ? keySpecs.Main
+            : $"{DefaultPrefix}{Sep}{SegmentPrefix}{keySpecs.Segment.NullIfNoValue() ?? DefaultSegment}{Sep}{keySpecs.Main}";
+
+        // Prevent accidental adding of the varyBy multiple times
+        return !string.IsNullOrWhiteSpace(keySpecs.VaryBy) && !mainKey.EndsWith(keySpecs.VaryBy)
+            ? $"{mainKey}{keySpecs.VaryBy}"
+            : mainKey;
+    }
+}
