c57486bf1a2cc4a66a0a3a3c8edba12489850e1b
iJungleboy <daniel.mettler@2sic.com>
2024-05-21 15:18:11 +0200

    docs

--- Src/Sxc/ToSic.Sxc/Services/Cache/ICacheSpecs.cs
+++ Src/Sxc/ToSic.Sxc/Services/Cache/ICacheSpecs.cs
@@ -48,7 +48,13 @@ public interface ICacheSpecs
     /// <returns></returns>
     ICacheSpecs WatchAppData(NoParamOrder protector = default);
 
-    ICacheSpecs WatchAppFolder(NoParamOrder protector = default, bool? withSubfolders = true);
+    /// <summary>
+    /// Depend on the app folder, so if any file in the app folder changes, the cache will be invalidated. WIP!
+    /// </summary>
+    /// <param name="protector">see [](xref:NetCode.Conventions.NamedParameters)</param>
+    /// <param name="withSubfolders">should it also watch subfolders? default is `true`</param>
+    /// <returns></returns>
+    ICacheSpecs WatchAppFolder(NoParamOrder protector = default, bool? withSubfolders = default);
 
     ///// <summary>
     ///// Add files
@@ -59,18 +65,6 @@ public interface ICacheSpecs
 
     #region VaryBy
 
-    // Disabled for now, not sure if this single-value case makes sense
-    ///// <summary>
-    ///// Vary the cache by a specific value.
-    ///// All cache items where this value is the same, will be considered the same.
-    ///// For example, this could be a category name or something where the data for this category is always the same.
-    ///// </summary>
-    ///// <param name="value"></param>
-    ///// <param name="protector"></param>
-    ///// <param name="caseSensitive"></param>
-    ///// <returns></returns>
-    //ICacheSpecs VaryBy(string value, NoParamOrder protector = default, bool caseSensitive = false);
-
     /// <summary>
     /// Vary the cache by a specific name and value.
     /// All cache items where this value is the same, will be considered the same.
@@ -78,26 +72,69 @@ public interface ICacheSpecs
     /// </summary>
     /// <param name="name"></param>
     /// <param name="value"></param>
-    /// <param name="protector"></param>
+    /// <param name="protector">see [](xref:NetCode.Conventions.NamedParameters)</param>
     /// <param name="caseSensitive"></param>
     /// <returns></returns>
     ICacheSpecs VaryBy(string name, string value, NoParamOrder protector = default, bool caseSensitive = false);
 
-    //ICacheSpecs VaryByModule(int id);
-
+    /// <summary>
+    /// Vary the cache by page, so that each page has its own cache.
+    /// By default, it will take the current page, but you can optionally specify a custom page or ID.
+    /// </summary>
+    /// <param name="protector">see [](xref:NetCode.Conventions.NamedParameters)</param>
+    /// <param name="id">optional page id to use instead of the default</param>
+    /// <param name="page">optional page object to use instead of the default</param>
+    /// <returns></returns>
     ICacheSpecs VaryByPage(NoParamOrder protector = default, ICmsPage page = default, int? id = default);
 
+    /// <summary>
+    /// Vary the cache by module, so that each module has its own cache.
+    /// By default, it will take the current module, but you can optionally specify a custom module or ID.
+    /// </summary>
+    /// <param name="protector">see [](xref:NetCode.Conventions.NamedParameters)</param>
+    /// <param name="id">optional page id to use instead of the default</param>
+    /// <param name="module">optional module object to use instead of the default</param>
     ICacheSpecs VaryByModule(NoParamOrder protector = default, ICmsModule module = default, int? id = default);
 
+    /// <summary>
+    /// Vary the cache by user, so that each user has its own cache.
+    /// By default, it will take the current user, but you can specify a custom user or ID.
+    /// </summary>
+    /// <param name="protector">see [](xref:NetCode.Conventions.NamedParameters)</param>
+    /// <param name="id">optional page id to use instead of the default</param>
+    /// <param name="user">optional module object to use instead of the default</param>
+    /// <returns></returns>
     ICacheSpecs VaryByUser(NoParamOrder protector = default, ICmsUser user = default, int? id = default);
 
-    //ICacheSpecs VaryByPage(int id);
-    //ICacheSpecs VaryByPage(ICmsPage page = default);
+    /// <summary>
+    /// Vary the cache by a specific page parameter, eg `?category=1`.
+    /// Using this method will only vary the cache by this specific parameter and ignore the rest.
+    /// </summary>
+    /// <param name="name">Name of a single parameter</param>
+    /// <param name="protector">see [](xref:NetCode.Conventions.NamedParameters)</param>
+    /// <param name="caseSensitive">Determines if the value should be treated case-sensitive, default is `false`</param>
+    /// <returns></returns>
     ICacheSpecs VaryByPageParameter(string name, NoParamOrder protector = default, bool caseSensitive = false);
 
+    /// <summary>
+    /// Vary the cache by one or more specific page parameter, eg `?category=1&sort=asc`.
+    /// Using this method will only vary the cache by the mentioned parameters and ignore the rest.
+    /// </summary>
+    /// <param name="names">Names of one or more parameters, comma-separated</param>
+    /// <param name="protector">see [](xref:NetCode.Conventions.NamedParameters)</param>
+    /// <param name="caseSensitive">Determines if the value should be treated case-sensitive, default is `false`</param>
+    /// <returns></returns>
+    ICacheSpecs VaryByPageParameters(string names = default, NoParamOrder protector = default, bool caseSensitive = false);
+
+    /// <summary>
+    /// Vary the cache by parameters which may not come from the page. 
+    /// </summary>
+    /// <param name="parameters">parameters object</param>
+    /// <param name="protector">see [](xref:NetCode.Conventions.NamedParameters)</param>
+    /// <param name="names">Names of one or more parameters, comma-separated</param>
+    /// <param name="caseSensitive">Determines if the value should be treated case-sensitive, default is `false`</param>
+    /// <returns></returns>
     ICacheSpecs VaryByParameters(IParameters parameters, NoParamOrder protector = default, string names = default, bool caseSensitive = false);
-    //ICacheSpecs VaryByUser(int id);
-    //ICacheSpecs VaryByUser(ICmsUser user = default);
 
     #endregion
 
