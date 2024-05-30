765bcc9a45ccc3d91e0e09626f28623ca44b4c09
iJungleboy <daniel.mettler@2sic.com>
2024-05-30 15:38:01 +0200

    minor cleanup / no change

--- Src/Sxc/ToSic.Sxc/Web/Internal/LightSpeed/LightSpeedDecorator.cs
+++ Src/Sxc/ToSic.Sxc/Web/Internal/LightSpeed/LightSpeedDecorator.cs
@@ -7,17 +7,9 @@ internal class LightSpeedDecorator(IEntity entity) : EntityBasedType(entity)
 {
     public static string NiceName = "LightSpeedOutputDecorator";
     public static string TypeNameId = "be34f64b-7d1f-4ad0-b488-dabbbb01a186";
-    //public const string FieldDurationUser = "DurationUsers";
-    //public const string FieldDurationEditor = "DurationEditors";
-    //public const string FieldDurationSysAdmin = "DurationSystemAdmin";
-    //public const string FieldByUrlParameters = "ByUrlParameters";
-    //public const string FieldUrlCaseSensitive = "UrlParametersCaseSensitive";
-
-    //internal LightSpeedDecorator(IEntity entity) : base(entity)
-    //{
-    //}
 
     public bool IsEnabled => GetThis(false);
+
     public bool? IsEnabledNullable => Get(nameof(IsEnabled), fallback: null as bool?);
 
     public int Duration => GetThis(0);
