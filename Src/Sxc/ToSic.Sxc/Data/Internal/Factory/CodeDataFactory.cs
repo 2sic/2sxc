a03f420b889f77984cff898d995fcbba584c8e54
iJungleboy <daniel.mettler@2sic.com>
2024-06-04 16:00:14 +0200

    CodeDataFactory - cache SiteCultures for language #3412

--- Src/Sxc/ToSic.Sxc/Data/Internal/Factory/CodeDataFactory.cs
+++ Src/Sxc/ToSic.Sxc/Data/Internal/Factory/CodeDataFactory.cs
@@ -1,12 +1,13 @@
 ﻿using ToSic.Eav.Code.InfoSystem;
 using ToSic.Eav.Context;
 using ToSic.Eav.Data.Build;
+using ToSic.Eav.Integration;
 using ToSic.Lib.DI;
 using ToSic.Lib.Helpers;
 using ToSic.Sxc.Adam.Internal;
 using ToSic.Sxc.Blocks.Internal;
 using ToSic.Sxc.Code.Internal;
-using ToSic.Sxc.Context;
+using ToSic.Sxc.Context.Internal;
 using ToSic.Sxc.Data.Internal.Wrapper;
 using ToSic.Sxc.Internal;
 using ToSic.Sxc.Services.Internal;
@@ -23,9 +24,10 @@ public partial class CodeDataFactory(
     LazySvc<DataBuilder> dataBuilderLazy,
     LazySvc<CodeDataWrapper> codeDataWrapper,
     Generator<CodeJsonWrapper> wrapJsonGenerator,
-    LazySvc<CodeInfoService> codeInfoSvc)
+    LazySvc<CodeInfoService> codeInfoSvc,
+    LazySvc<IZoneMapper> zoneMapper)
     : ServiceForDynamicCode("Sxc.AsConv",
-        connect: [codeDataServices, adamManager, contextOfAppLazy, dataBuilderLazy, codeDataWrapper, wrapJsonGenerator, codeInfoSvc])
+        connect: [codeDataServices, adamManager, contextOfAppLazy, dataBuilderLazy, codeDataWrapper, wrapJsonGenerator, codeInfoSvc, zoneMapper])
 {
     internal CodeInfoService CodeInfo => codeInfoSvc.Value;
 
@@ -37,8 +39,14 @@ public partial class CodeDataFactory(
         _compatibilityLevel = compatibility ?? _compatibilityLevel;
         _adamManager.Reset(adamManagerPrepared);
     }
-
     private ISite _siteOrNull;
+
+    private ISite SiteFromContextOrFallback => _siteFromContextOrFallback 
+        ??= (_CodeApiSvc?.CmsContext as CmsContext)?.CtxSite.Site
+            ?? _siteOrNull
+            ?? throw new("Tried getting site from context or fallback, neither returned anything useful. ");
+
+    private ISite _siteFromContextOrFallback;
     public int CompatibilityLevel => _priorityCompatibilityLevel ?? _compatibilityLevel;
     private int? _priorityCompatibilityLevel;
     private int _compatibilityLevel = CompatibilityLevels.CompatibilityLevel10;
@@ -57,8 +65,10 @@ public partial class CodeDataFactory(
 
     // If we don't have a DynCodeRoot, try to generate the language codes and compatibility
     // There are cases where these were supplied using SetFallbacks, but in some cases none of this is known
-    internal string[] Dimensions => _dimensions.Get(() => _CodeApiSvc?.CmsContext.SafeLanguagePriorityCodes()
-                                                          ?? _siteOrNull.SafeLanguagePriorityCodes());
+    internal string[] Dimensions => _dimensions.Get(() => SiteFromContextOrFallback.SafeLanguagePriorityCodes()
+        //_CodeApiSvc?.CmsContext.SafeLanguagePriorityCodes()
+        //?? _siteOrNull.SafeLanguagePriorityCodes()
+    );
     private readonly GetOnce<string[]> _dimensions = new();
 
     internal IBlock BlockOrNull => ((ICodeApiServiceInternal)_CodeApiSvc)?._Block;
@@ -69,4 +79,14 @@ public partial class CodeDataFactory(
         => wrapJsonGenerator.New().Setup(WrapperSettings.Dyn(true, true))
             .Json2Jacket(json, fallback: fallback);
 
+    /// <summary>
+    /// WIP, we need this in the GetAndConvertHelper, and want to make sure it's not executed on every entity used,
+    /// so for now we're doing this once only here.
+    /// </summary>
+    public List<string> SiteCultures => _siteCultures ??= zoneMapper.Value
+                                                              .CulturesEnabledWithState(SiteFromContextOrFallback)
+                                                              ?.Select(c => c.Code)
+                                                              .ToList()
+                                                          ?? [];
+    private List<string> _siteCultures;
 }
