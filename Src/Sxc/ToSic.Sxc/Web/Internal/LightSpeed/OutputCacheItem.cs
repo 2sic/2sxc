25aea934ba2fc195ee6231a207b676c8662b07e7
iJungleboy <daniel.mettler@2sic.com>
2024-06-06 16:48:52 +0200

    Improve LightSpeed CacheItem to have timestamp

--- Src/Sxc/ToSic.Sxc/Web/Internal/LightSpeed/OutputCacheItem.cs
+++ Src/Sxc/ToSic.Sxc/Web/Internal/LightSpeed/OutputCacheItem.cs
@@ -1,12 +1,11 @@
-﻿using ToSic.Eav.Apps.Internal.Insights;
-using ToSic.Lib.Data;
+﻿using ToSic.Eav.Caching;
 using ToSic.Lib.Memory;
 using ToSic.Sxc.Blocks.Internal.Render;
 
 namespace ToSic.Sxc.Web.Internal.LightSpeed;
 
 [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
-public class OutputCacheItem(IRenderResult data): ICanEstimateSize
+public class OutputCacheItem(IRenderResult data): ICanEstimateSize, ITimestamped
 {
     public IRenderResult Data => data;
 
@@ -15,8 +14,14 @@ public class OutputCacheItem(IRenderResult data): ICanEstimateSize
     /// This is only used in Dnn - might be solved with generics some time, but ATM this is just simpler
     /// </summary>
     public bool EnforcePre1025 = true;
+
 #endif
     public SizeEstimate EstimateSize(ILog log = default) 
         => (data as ICanEstimateSize)?.EstimateSize(log)
            ?? new SizeEstimate(0, 0, Unknown: true);
+
+    /// <summary>
+    /// Timestamp info to better analyze cache data
+    /// </summary>
+    long ITimestamped.CacheTimestamp { get; } = DateTime.Now.Ticks;
 }
