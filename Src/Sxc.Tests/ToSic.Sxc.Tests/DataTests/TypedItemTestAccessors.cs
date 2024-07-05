34ec573b60d913166b7a57111bf81ad588f5f487
iJungleboy <daniel.mettler@2sic.com>
2024-07-05 17:53:04 +0200

    minor unit tests

--- Src/Sxc.Tests/ToSic.Sxc.Tests/DataTests/TypedItemTestAccessors.cs
+++ Src/Sxc.Tests/ToSic.Sxc.Tests/DataTests/TypedItemTestAccessors.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections.Generic;
 using ToSic.Sxc.Data;
 
-namespace ToSic.Sxc.Tests.DataTests
+namespace ToSic.Sxc.Tests.DataTests;
+
+internal static class TypedItemTestAccessors
 {
-    internal static class TypedItemTestAccessors
-    {
-        public static IMetadata TestMetadata(this ITypedItem item) => item.Metadata;
+    public static IMetadata TestMetadata(this ITypedItem item) => item.Metadata;
 
-        public static bool TestContainsKey(this ITyped item, string key) => item.ContainsKey(key);
+    public static bool TestContainsKey(this ITyped item, string key) => item.ContainsKey(key);
 
-        public static IEnumerable<string> TestKeys(this ITyped item, IEnumerable<string> only = default) => item.Keys(only: only);
-    }
+    public static IEnumerable<string> TestKeys(this ITyped item, IEnumerable<string> only = default) => item.Keys(only: only);
 }
