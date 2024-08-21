3473b83cb7d265e3a8de846bf9f5dec2498a2fc1
iJungleboy <daniel.mettler@2sic.com>
2024-08-21 07:59:04 +0200

    minor draft interfaces, not yet in use

---
+++ Src/Sxc/ToSic.Sxc/Data/Publishing/ILifecycle.cs
@@ -0,0 +1,15 @@
+﻿namespace ToSic.Sxc.Data;
+
+/// <summary>
+/// Experimental 2dm - not done yet 2024-08
+/// 2dm Experimental - trying to get the lifecycle info into ITypedItem
+/// 
+/// Idea is to provide versioning information for items - different for original item, latest item, etc.
+///
+/// See IVersion / ILifecycle - not yet in use.
+/// </summary>
+internal interface ILifecycle
+{
+    public IVersion Created { get; }
+    public IVersion Modified { get; }
+}
