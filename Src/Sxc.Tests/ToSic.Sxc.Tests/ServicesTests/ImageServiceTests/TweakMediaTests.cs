9a786526ada18fe836b1a69777954a8400eae855
iJungleboy <daniel.mettler@2sic.com>
2024-10-29 16:50:27 +0100

    minor fix tests

--- Src/Sxc.Tests/ToSic.Sxc.Tests/ServicesTests/ImageServiceTests/TweakMediaTests.cs
+++ Src/Sxc.Tests/ToSic.Sxc.Tests/ServicesTests/ImageServiceTests/TweakMediaTests.cs
@@ -11,14 +11,14 @@ public class TweakMediaTests
     private static ITweakMedia ResizeOnly() => new TweakMedia(null, null, new(null, null, 100, 100, 2.5, 0.5, null, "not-based-on-anything"), null, null, null, null);
 
     [TestMethod]
-    public void Width() => Assert.AreEqual(42, (ResizeOnly().Resize(null, tweak: t => t.Width(42)) as TweakMedia).ResizeSettings.Width);
+    public void Width() => Assert.AreEqual(42, (ResizeOnly().Resize(null as string, tweak: t => t.Width(42)) as TweakMedia).ResizeSettings.Width);
 
     [TestMethod]
     public void ResizeChangeMakesCopy()
     {
         var x = ResizeOnly();
-        var y = x.Resize(null, tweak: t => t.Width(42));
-        var z = x.Resize(null, tweak: t => t.Factor(0.75));
+        var y = x.Resize(null as string, tweak: t => t.Width(42));
+        var z = x.Resize(null as string, tweak: t => t.Factor(0.75));
         Assert.AreNotEqual(x, y);
         Assert.AreNotEqual(x, z);
         Assert.AreNotEqual(y, z);
