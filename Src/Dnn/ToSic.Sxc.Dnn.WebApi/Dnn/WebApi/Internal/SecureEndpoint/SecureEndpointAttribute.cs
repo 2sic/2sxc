d65af5442d2690b69494252c9de51738dd14a0c6
Tonći Vatavuk <tonci@sistemi.hr>
2024-11-14 17:35:24 +0100

    use System.Text.Json for deserialization

--- Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Internal/SecureEndpoint/SecureEndpointAttribute.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.WebApi/Dnn/WebApi/Internal/SecureEndpoint/SecureEndpointAttribute.cs
@@ -1,11 +1,14 @@
-﻿using Newtonsoft.Json;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 using System.Web.Http.Controllers;
 using System.Web.Http.Dependencies;
 using System.Web.Http.Filters;
 using ToSic.Eav.Security.Encryption;
+using ToSic.Eav.Serialization;
 
 namespace ToSic.Sxc.Dnn.WebApi.Internal.SecureEndpoint
 {
@@ -24,12 +27,10 @@ namespace ToSic.Sxc.Dnn.WebApi.Internal.SecureEndpoint
                     return;
                 }
 
-                var encryptedData = JsonConvert.DeserializeObject<EncryptedData>(encryptedDataJson);
-                if (encryptedData?.Data == null)
-                {
-                    filterContext.Response = request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid encrypted data.");
+                var encryptedData = JsonSerializer.Deserialize<EncryptedData>(encryptedDataJson, options: JsonOptions.SafeJsonForHtmlAttributes);
+                // "duck typing" check
+                if (encryptedData.Version == 1 && encryptedData.Data is null && encryptedData.Key is null && encryptedData.Iv is null) // 
                     return;
-                }
 
                 // Determine the parameter name and type dynamically.
                 // Find parameter that has FromBody attribute on it,
@@ -47,10 +48,13 @@ namespace ToSic.Sxc.Dnn.WebApi.Internal.SecureEndpoint
                 try
                 {
                     // Validate that decrypted data is valid JSON
-                    var formData = JsonConvert.DeserializeObject(decryptedData, parameter.ParameterType);
+                    var formData = JsonSerializer.Deserialize(decryptedData, parameter.ParameterType, options: JsonOptions.SafeJsonForHtmlAttributes);
 
                     // Replace the request content with the deserialized object
                     filterContext.ActionArguments[parameter.ParameterName] = formData;
+                    request.Content = new StreamContent(new MemoryStream(Encoding.UTF8.GetBytes(decryptedData)));
+                    request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    //filterContext.Request.Content = new ObjectContent(parameter.ParameterType, formData, parameter.Configuration.Formatters.JsonFormatter);
                 }
                 catch (JsonException)
                 {
