96bf22145fa1ee006dd4c6eb3b8a72f64a61adb7
iJungleboy <daniel.mettler@2sic.com>
2024-11-27 14:49:18 +0100

    Update JsApi.cs
    correct json encoding - use System.Text.Json

--- Src/Sxc/ToSic.Sxc/Web/Internal/JsContext/JsApi.cs
+++ Src/Sxc/ToSic.Sxc/Web/Internal/JsContext/JsApi.cs
@@ -1,6 +1,7 @@
 ﻿// ReSharper disable InconsistentNaming
 #pragma warning disable IDE1006
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ToSic.Sxc.Web.Internal.JsContext;
 
@@ -14,57 +15,46 @@ public class JsApi
     public const string MetaName = "_jsApi";
     public const string ExtensionPlaceholder = "e.x.t";
 
+    [JsonPropertyName("platform")]
     public string platform { get; set; }
+
+    [JsonPropertyName("page")]
     public int page { get; set; }
+
+    [JsonPropertyName("root")]
     public string root { get; set; }
+
+    [JsonPropertyName("api")]
     public string api { get; set; }
+
+    [JsonPropertyName("appApi")]
     public string appApi { get; set; }
+
+    [JsonPropertyName("uiRoot")]
     public string uiRoot { get; set; }
+
+    [JsonPropertyName("rvtHeader")]
     public string rvtHeader { get; set; }
+
+    [JsonPropertyName("rvt")]
     public string rvt { get; set; }
+
+    [JsonPropertyName("dialogQuery")]
     public string dialogQuery { get; set; }
 
     // TODO: should probably get a nicer name, like "publicKey" or "publicKeyForEncryption"
     // ...but change would require JS changes as well
+
+    [JsonPropertyName("secureEndpointPublicKey")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string secureEndpointPublicKey { get; set; }
 
     /// <summary>
     /// Debug information while we're developing the on-module info
     /// </summary>
+    [JsonIgnore]
     public string source => "module JsApi";
 
-    public static string JsApiJson(JsApi jsApi) =>
-        "{"
-        + JsonProperty(nameof(JsApi.platform), jsApi.platform) + ","
-        + $"\"{nameof(JsApi.page)}\": {jsApi.page},"
-        + JsonProperty(nameof(JsApi.root), jsApi.root) + ","
-        + JsonProperty(nameof(JsApi.api), jsApi.api) + ","
-        + JsonProperty(nameof(JsApi.appApi), jsApi.appApi) + ","
-        + JsonProperty(nameof(JsApi.uiRoot), jsApi.uiRoot) + ","
-        + JsonProperty(nameof(JsApi.rvtHeader), jsApi.rvtHeader) + ","
-        + JsonProperty(nameof(JsApi.rvt), jsApi.rvt) + ","
-        + JsonProperty(nameof(JsApi.dialogQuery), jsApi.dialogQuery) + ","
-        + JsonPropertyOrEmpty(nameof(jsApi.secureEndpointPublicKey), jsApi.secureEndpointPublicKey)
-        + "}";
+    public static string JsApiJson(JsApi jsApi) => JsonSerializer.Serialize(jsApi);
 
-    private static string JsonPropertyOrEmpty(string key, string value)
-        => string.IsNullOrEmpty(value) ? string.Empty : JsonProperty(key, value);
-
-    /// <summary>
-    /// build string for json property
-    /// "key: 'value'"
-    /// </summary>
-    /// <param name="key"></param>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    private static string JsonProperty(string key, string value)
-        => $"\"{key}\": \"{JsonValue(value)}\"";
-
-    /// <summary>
-    /// build string for json encoded value
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    private static string JsonValue(string value)
-        => string.IsNullOrEmpty(value) ? string.Empty : JsonEncodedText.Encode(value).ToString();
 }
