ba0436e3b07e5a615bf955a3e4951d2751cfa147
Tonći Vatavuk <tonci@sistemi.hr>
2024-12-11 17:45:31 +0100

    Scope ModuleService data by ModuleId for each module rendering in Oqtane Interactive Server

--- Src/Sxc/ToSic.Sxc/Web/Internal/PageService/PageService_turnOn.cs
+++ Src/Sxc/ToSic.Sxc/Web/Internal/PageService/PageService_turnOn.cs
@@ -26,9 +26,26 @@ partial class PageService
 
         // then generate the turn-on and add to module state
         var tag = turnOn.Value.Run(runOrSpecs, require: require, data: data, args: args, addContext: addContext);
+
+#if NETCOREAPP
+        // In the Oqtane Interactive Server, the Dependency Injection (DI) session scope is bound to the first HTTP request of the user's browser session,
+        // and it does not change during subsequent SignalR communications (until a full page reload).
+        // As a result, scoped services have the same instance for all 2sxc module instances across all pages during a user's browser session.
+        // To prevent conflicts, we need to add the ModuleId to the ModuleService to scope its functionality to each module rendering.
+        moduleService.Value.AddToMore(tag, noDuplicates: noDuplicates == true, moduleId: GetModuleId());
+#else
         moduleService.Value.AddToMore(tag, noDuplicates: noDuplicates == true);
+#endif
 
         // Then return empty string
         return l.ReturnAsOk(null);
     }
+
+#if NETCOREAPP
+    /// <summary>
+    /// Retrieves the Module ID for the current module rendering context.
+    /// </summary>
+    /// <returns>The resolved Module ID as an integer.</returns>
+    private int GetModuleId() => _CodeApiSvc.CmsContext.Module.Id;
+#endif
 }
