80801afddb482e97f0b9940e5d73b19106ce93aa
iJungleboy <daniel.mettler@2sic.com>
2025-01-04 12:11:39 +0100

    ToSic.Sxc.Apps - reorg files (internal)

---
+++ Src/Sxc/ToSic.Sxc/Apps/App/readme.md
@@ -0,0 +1,4 @@
+# App object in ToSic.Sxc.Apps
+
+This is actually an internal object, but because it used to be in the docs as Public,
+we have to keep it public.
--- Src/Sxc/ToSic.Sxc/Apps/IAppTyped_TSettings_TResources.cs
+++ Src/Sxc/ToSic.Sxc/Apps/IAppTyped_TSettings_TResources.cs
@@ -18,42 +18,6 @@ public interface IAppTyped<out TSettings, out TResources> :
     where TSettings : class, ITypedItem, ITypedItemWrapper16, new()
     where TResources : class, ITypedItem, ITypedItemWrapper16, new()
 {
-    //#region Properties basically from Eav.Apps.IApp
-
-    ///// <inheritdoc cref="Eav.Apps.IApp.Name"/>
-    //string Name { get; }
-
-    ///// <inheritdoc cref="Eav.Apps.IApp.Data"/>
-    //IAppDataTyped Data { get; }
-
-    //#endregion
-
-    //#region From IAppTyped
-    ///// <inheritdoc cref="IAppTyped.GetQuery"/>
-    //IDataSource GetQuery(
-    //    string name = default,
-    //    NoParamOrder noParamOrder = default,
-    //    IDataSourceLinkable attach = default,
-    //    object parameters = default
-    //);
-
-
-    ///// <inheritdoc cref="IApp.Configuration"/>
-    //IAppConfiguration Configuration { get; }
-
-    //#endregion
-
-    //#region Paths / Urls
-
-    //IFolder Folder { get; }
-
-    ///// <inheritdoc cref="IAppTyped.FolderAdvanced"/>
-    //IFolder FolderAdvanced(NoParamOrder noParamOrder = default, string location = default);
-
-    //IFile Thumbnail { get; }
-
-    //#endregion
-
     /// <summary>
     /// All the app settings which are custom for each app.
     /// These are typed - typically to AppCode.Data.AppSettings
