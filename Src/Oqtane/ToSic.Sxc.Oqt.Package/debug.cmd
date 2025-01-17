fdd399c21dd170b8585502be83c4c5495b55674a
Tonći Vatavuk <tonci@sistemi.hr>
2025-01-17 10:45:47 +0100

    remove hardcoded TargetFramework path from debug.cmd build script and use variable to reduce amount of work after each change of .net version

--- Src/Oqtane/ToSic.Sxc.Oqt.Package/debug.cmd
+++ Src/Oqtane/ToSic.Sxc.Oqt.Package/debug.cmd
@@ -1,44 +1,51 @@
 @ECHO off
 ECHO(
 ECHO the build folder (Debug or DebugOqtane) must be passed in as a parameter
-REM Displaying the parameters received
+
 SET BuildFolder=%1
 SET OqtaneTarget=%2
+SET ModuleTargetFramework=%3
+REM ModuleTargetFramework is sometimes older than OqtaneTargetFramework to preserve compatibility with older Oqtane versions.
+REM Change the OqtaneTargetFramework to TargetFramework from Oqtane.Server.csproj
+SET OqtaneTargetFramework=net9.0
 
+REM Displaying the parameters received
 ECHO BuildFolder=%BuildFolder%
 ECHO OqtaneTarget=%OqtaneTarget%
+ECHO ModuleTargetFramework=%ModuleTargetFramework%
+ECHO OqtaneTargetFramework=%OqtaneTargetFramework%
 
-SET OqtaneBin=%OqtaneTarget%bin\%BuildFolder%\net9.0
+SET OqtaneBin=%OqtaneTarget%bin\%BuildFolder%\%OqtaneTargetFramework%
 SET PackageName=ToSic.Sxc.Oqtane
 SET BuildTarget=%OqtaneTarget%wwwroot\Modules\%PackageName%
 ECHO The target folder is: %OqtaneBin%
 
 ECHO(
 ECHO 2sxc Oqtane - Client
-XCOPY "..\ToSic.Sxc.Oqt.Client\bin\%BuildFolder%\net9.0\ToSic.*.dll" "%OqtaneBin%\" /Y
-XCOPY "..\ToSic.Sxc.Oqt.Client\bin\%BuildFolder%\net9.0\ToSic.*.pdb" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Client\bin\%BuildFolder%\%ModuleTargetFramework%\ToSic.*.dll" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Client\bin\%BuildFolder%\%ModuleTargetFramework%\ToSic.*.pdb" "%OqtaneBin%\" /Y
 
 ECHO(
 ECHO 2sxc Oqtane - Server
-XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\net9.0\ToSic.*.dll" "%OqtaneBin%\" /Y
-XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\net9.0\ToSic.*.pdb" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\%ModuleTargetFramework%\ToSic.*.dll" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\%ModuleTargetFramework%\ToSic.*.pdb" "%OqtaneBin%\" /Y
 
 ECHO(
 ECHO 2sxc Oqtane - Server Framework DLLs
-XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\net9.0\Microsoft.AspNetCore.Mvc.Razor.*.dll" "%OqtaneBin%\" /Y
-XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\net9.0\Microsoft.AspNetCore.Razor.*" "%OqtaneBin%\" /Y
-XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\net9.0\Microsoft.CodeAnalys*.*" "%OqtaneBin%\" /Y
-XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\net9.0\Microsoft.Extensions.DependencyModel.dll" "%OqtaneBin%\" /Y
-XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\net9.0\System.Runtime.Caching.dll" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\%ModuleTargetFramework%\Microsoft.AspNetCore.Mvc.Razor.*.dll" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\%ModuleTargetFramework%\Microsoft.AspNetCore.Razor.*" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\%ModuleTargetFramework%\Microsoft.CodeAnalys*.*" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\%ModuleTargetFramework%\Microsoft.Extensions.DependencyModel.dll" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\%ModuleTargetFramework%\System.Runtime.Caching.dll" "%OqtaneBin%\" /Y
 
 ECHO(
 ECHO 2sxc Oqtane - 3rd party deps
-XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\net9.0\CsvHelper.dll" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Server\bin\%BuildFolder%\%ModuleTargetFramework%\CsvHelper.dll" "%OqtaneBin%\" /Y
 
 ECHO(
 ECHO 2sxc Oqtane - Shared
-XCOPY "..\ToSic.Sxc.Oqt.Shared\bin\%BuildFolder%\net9.0\ToSic.*.dll" "%OqtaneBin%\" /Y
-XCOPY "..\ToSic.Sxc.Oqt.Shared\bin\%BuildFolder%\net9.0\ToSic.*.pdb" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Shared\bin\%BuildFolder%\%ModuleTargetFramework%\ToSic.*.dll" "%OqtaneBin%\" /Y
+XCOPY "..\ToSic.Sxc.Oqt.Shared\bin\%BuildFolder%\%ModuleTargetFramework%\ToSic.*.pdb" "%OqtaneBin%\" /Y
 
 ECHO(
 ECHO 2sxc Oqtane - Client Assets
@@ -68,7 +75,7 @@ REM robocopy /mir "..\ToSic.Sxc.Oqt.Server\wwwroot\Modules\ToSic.Sxc.Oqtane\syst
 
 ECHO(
 ECHO Copy ImportExport instructions
-robocopy /mir "..\ToSic.Sxc.Oqt.Server\Content\2sxc\system\ImportExport\ " "%OqtaneTarget%\Content\2sxc\system\ImportExport\ "
+robocopy /mir "..\..\Dnn\ToSic.Sxc.Dnn\ImportExport\ " "%OqtaneTarget%\Content\2sxc\system\ImportExport\ "
 
 ECHO(
 ECHO Copy the data folders
