b850a43596123c8c3cbe068385838056f12749e6
iJungleboy <daniel.mettler@2sic.com>
2025-01-17 09:38:47 +0100

    minor docs / hide obsolete API "Factory"

--- Src/Dnn/ToSic.Sxc.Dnn.Core/Compatibility/Eav/ToSic.Eav.Factory.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.Core/Compatibility/Eav/ToSic.Eav.Factory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿#if NETFRAMEWORK
+using Microsoft.Extensions.DependencyInjection;
 using ToSic.Eav.Code.Infos;
 using ToSic.Sxc.Dnn;
 using static ToSic.Eav.Code.Infos.CodeInfoObsolete;
@@ -11,20 +12,18 @@ namespace ToSic.Eav;
 ///
 /// If possible avoid using this, as it's a workaround for code which is outside the normal Dependency Injection and therefor a bad pattern.
 /// </summary>
-[PublicApi("Careful - obsolete!")]
+[PrivateApi("Up to v19 used to PublicApi(Careful - obsolete!)")]
 [Obsolete("Deprecated, please use Dnn 9 DI instead https://go.2sxc.org/brc-13-eav-factory")]
 [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
 public class Factory
 {
     [Obsolete("Not used any more, but keep for API consistency in case something calls ActivateNetCoreDi")]
-    [PrivateApi]
     public delegate void ServiceConfigurator(IServiceCollection service);
 
     /// <summary>
     /// Dependency Injection resolver with a known type as a parameter.
     /// </summary>
     /// <typeparam name="T">The type / interface we need.</typeparam>
-    [PrivateApi]
     [Obsolete("Please use standard Dnn 9.4+ Dnn DI instead https://go.2sxc.org/brc-13-eav-factory")]
     [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
     public static T Resolve<T>()
@@ -35,3 +34,4 @@ public class Factory
 
     private static readonly ICodeInfo WarnObsolete = V13To17("ToSic.Eav.Factory.Resolve<T>", "https://go.2sxc.org/brc-13-eav-factory");
 }
+#endif
