0de05f2df86a0f1e5f0d311f35936d8d308faecc
iJungleboy <daniel.mettler@2sic.com>
2025-03-13 15:50:38 +0100

    prepare setup for Sxc.System tests

---
+++ Src/ToSic.Sxc.SystemTests/StartupSxcWithDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ToSic.Eav.Integration;
+using ToSic.Eav.StartUp;
+using ToSic.Lib;
+using ToSic.Sxc.Startup;
+
+#pragma warning disable CA1822
+
+namespace ToSic.Sxc;
+
+// NOTE: QUITE A FEW DUPLICATES OF THIS - may want to consolidate
+
+/// <summary>
+/// A Startup helper for tests which need Dependency-Injection setup for EAV Core.
+/// </summary>
+/// <remarks>
+/// Use by adding this kind of attribute to your test class:
+/// `[Startup(typeof(TestStartupEavCore))]`
+/// </remarks>
+public class StartupSxcWithDb
+{
+    /// <summary>
+    /// Startup helper
+    /// </summary>
+    public virtual void ConfigureServices(IServiceCollection services) =>
+        services
+            // 2sxc core
+            .AddSxcCore()
+            .AddEavEverything();
+            
+}
