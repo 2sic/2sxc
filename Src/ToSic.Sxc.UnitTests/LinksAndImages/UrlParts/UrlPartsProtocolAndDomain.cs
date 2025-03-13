f3fbe7c84285558882c047e3fdb084df7f871ffb
iJungleboy <daniel.mettler@2sic.com>
2025-03-13 14:48:50 +0100

    Sxc.UnitTests - moved another 300 or so to xUnit

---
+++ Src/ToSic.Sxc.UnitTests/LinksAndImages/UrlParts/UrlPartsProtocolAndDomain.cs
@@ -0,0 +1,57 @@
+﻿namespace ToSic.Sxc.Tests.LinksAndImages;
+
+
+public class UrlPartsProtocolAndDomain : UrlPartsTestBase
+{
+        
+
+    [Fact]
+    public void ProtocolDetection()
+    {
+        void VerifyProtocol(string url, string protocol, string domain)
+        {
+            var urlParts = UrlParts(url);
+            Equal(protocol, urlParts.Protocol);
+            Equal(domain, urlParts.Domain);
+            Equal(url ?? "", urlParts.ToLink());//, "round trip should be identical except for null"); 
+        }
+        VerifyProtocol(null, "", "");
+        VerifyProtocol("", "", "");
+        VerifyProtocol("https:", "", "");
+        VerifyProtocol("//abc.com", "//", "abc.com");
+        VerifyProtocol("///abc.com", "//", "");
+        VerifyProtocol("ftp://daniel@abc.com/q", "ftp://", "daniel@abc.com");
+        VerifyProtocol("http://xyz.com", "http://", "xyz.com");
+        VerifyProtocol("http//", "", "");
+        VerifyProtocol("https://", "https://", "");
+        VerifyProtocol("futureprot://", "futureprot://", "");
+        VerifyProtocol("https://abc.com/", "https://", "abc.com");
+        VerifyProtocol("http://abc/home", "http://", "abc");
+        VerifyProtocol("http/test//", "", "");
+        VerifyProtocol("../../test", "", "");
+        VerifyProtocol("/test/abc?test", "", "");
+    }
+
+    /// <summary>
+    /// Verify Root Replacement
+    /// </summary>
+    private void VerRepRoot(string expected, string url, string post)
+    {
+        var urlParts = UrlParts(url);
+        urlParts.ReplaceRoot(post);
+        Equal(expected, urlParts.ToLink());
+    }
+
+    [Fact] public void RepRootEmptyOrigUsesReplacement() => VerRepRoot("//abc.org", "", "//abc.org");
+    [Fact] public void RepRootOnlyDomainWorks() => VerRepRoot("//abc.org", "", "abc.org");
+    [Fact] public void RepRootPreserveOrigProtocolIfNotNew() => VerRepRoot("https://abc.org/", "https://xyz/", "abc.org");
+    [Fact] public void RepRootTakeNewProtocolIfGiven() => VerRepRoot("//abc.org/", "https://xyz/", "//abc.org");
+    [Fact] public void RepRootSkipInvalidProtocol() => VerRepRoot("https://xyz/", "https://xyz/", "ftp:");
+    [Fact] public void RepRootUseStandaloneProtocol() => VerRepRoot("ftp://xyz/", "https://xyz/", "ftp://");
+
+    // TODO
+    // VARIOUS combinations of ToLink
+
+
+
+}
---
+++ Src/ToSic.Sxc.UnitTests/LinksAndImages/UrlParts/UrlPartsTrivial.cs
@@ -0,0 +1,44 @@
+﻿namespace ToSic.Sxc.Tests.LinksAndImages;
+
+
+public class UrlPartsTrivial: UrlPartsTestBase
+{
+
+    [Fact]
+    public void TrivialUrls()
+    {
+        VerifyUrlOnly("https://www.xyz.org/something.jpg");
+        VerifyUrlOnly("test");
+        VerifyUrlOnly("test.jpg");
+        VerifyUrlOnly("test.x.y.z.jpg");
+    }
+        
+
+    private void VerifyUrlOnly(string url)
+    {
+        var urlParts = UrlParts(url);
+        Equal(url, urlParts.Url);
+        Equal(url, urlParts.ToLink());
+        Equal(string.Empty, urlParts.Query);
+        Equal(string.Empty, urlParts.Fragment);
+    }
+
+    [Fact]
+    public void UrlsWithFragments()
+    {
+        VerifyUrlAndFragmentOnly("test#17", "test", "17");
+        VerifyUrlAndFragmentOnly("test.jpg#abc=def", "test.jpg", "abc=def");
+        VerifyUrlAndFragmentOnly("/test.jpg#", "/test.jpg", "");
+        VerifyUrlAndFragmentOnly("test#first=1&message=hello?", "test", "first=1&message=hello?");
+    }
+
+    private void VerifyUrlAndFragmentOnly(string url, string pathExp, string fragmentExp)
+    {
+        var urlParts = UrlParts(url);
+        Equal(url, urlParts.Url);
+        Equal(pathExp, urlParts.Path);
+        Equal(string.Empty, urlParts.Query);
+        Equal(fragmentExp, urlParts.Fragment);
+    }
+
+}
