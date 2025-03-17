38566e374d076b6fbadf3058821225f2a3018ad6
iJungleboy <daniel.mettler@2sic.com>
2025-03-17 08:39:14 +0100

    CmsService update unit tests

--- Src/ToSic.Sxc.SystemTests/ServicesTests/CmsService/StringWysiwygTests.cs
+++ Src/ToSic.Sxc.SystemTests/ServicesTests/CmsService/StringWysiwygTests.cs
@@ -10,59 +10,48 @@ namespace ToSic.Sxc.ServicesTests.CmsService;
 public class StringWysiwygTests(
     CodeApiService codeApiSvc,
     ContentTypeFactory contentTypeFactory,
-    CmsServiceTestData cmsTestData,
+    DataForCmsServiceTests dataForCmsTests,
     CodeDataFactory cdf,
     IImageService imageSvc, // needed to inject into CodeApiSvc
     ITestOutputHelper output
     )
 {
     /// <summary>
-    /// Must get the desired service through codeApiSvc, because
-    /// - the class is internal
-    /// - it needs to have a parent CodeApiService for sub-dependencies
+    /// Swap the image service to one which doesn't know about the app (so it won't get settings etc.)
     /// </summary>
-#if NETCOREAPP
-    [field: System.Diagnostics.CodeAnalysis.AllowNull, System.Diagnostics.CodeAnalysis.MaybeNull]
-#endif
-    private CmsServiceStringWysiwyg StringWysiwygParser => field ??= new Func<CmsServiceStringWysiwyg>(()=>
+    private void InitCodeApiSvc() => codeApiSvc.ReplaceServiceInCache(imageSvc);
+
+    /// <summary>
+    /// Must get service through codeApiSvc, because the class is internal & it needs to have a parent CodeApiService for sub-dependencies
+    /// </summary>
+    private CmsServiceStringWysiwyg GetStringWysiwygParser() => codeApiSvc.GetService<CmsServiceStringWysiwyg>();
+
+    // TODO: needs a lot more tests, such as with / without paths, etc.
+
+    /// <summary>
+    /// This test is almost identical to the <see cref="HtmlImgToPictureHelperTests"/> but going through one more layer of objects.
+    /// The primary test should be there, this should just confirm that these simple setups create the same result.
+    /// </summary>
+    /// <param name="conversion"></param>
+    [Theory, MemberData(nameof(DataForImgConversionTest.ImageConversions), MemberType = typeof(DataForImgConversionTest))]
+    public void ImageTagOnlyHasSameResultAsHtmlImgToPictureHelper(ImgConversionTest conversion)
     {
-        // Swap the image service to one which doesn't know about the app (so it won't get settings etc.)
-        codeApiSvc.ReplaceServiceInCache(imageSvc);
-        return codeApiSvc.GetService<CmsServiceStringWysiwyg>();
-    })();
+        InitCodeApiSvc();
+        var parser = GetStringWysiwygParser();
 
-    // needs a lot more tests, such as with / without paths, etc.
-
-    public static TheoryData<ImgConversionTest> ImageConversions =>
-    [
-        new() { AName = "Basic img.png" },
-        new() { AName = "Basic with path", ImgNameAndFolder = "some-path/img.png" },
-        new() { AName = "jpg with path", ImgNameAndFolder = "some-path/img.jpg", MimeType = "image/jpeg" },
-        new()
-        {
-            AName = "Extensive with tst.jpg",
-            OriginalHtml = "<img src='tst.jpg' data-cmsid='file:tst.jpg' class='img-fluid' loading='lazy' alt='description' width='1230' height='760' style='width:auto;'>",
-            Expected = "<picture class='img-fluid'><source type='image/jpeg' srcset='tst.jpg?w=1230'><img src='tst.jpg?w=1230' loading='lazy' height='760' alt='description' class='img-fluid' style='width:auto;'></picture>",
-        }
-    ];
-
-    [Theory]
-    [MemberData(nameof(ImageConversions))]
-    public void Test(ImgConversionTest conversion)
-    {
         var ctWithHtmlField = contentTypeFactory.Create(typeof(MockHtmlContentType));
 
         var attribute = ctWithHtmlField.Attributes
             .First(a => a.Name == nameof(MockHtmlContentType.SomeHtml));
 
-        var data = cmsTestData.TstDataEntity("hello", conversion.OriginalHtml, ctWithHtmlField);
+        var data = dataForCmsTests.TstDataEntity("hello", conversion.Original, ctWithHtmlField);
         var typed = cdf.AsItem(data);
         var field = typed.Field(nameof(MockHtmlContentType.SomeHtml));
 
-        var folder = CmsServiceTestData.GenerateFolderWithTestPng();
-        StringWysiwygParser.Init(field, ctWithHtmlField, attribute, folder, false, null);
+        var folder = DataForCmsServiceTests.GenerateFolderWithTestPng();
+        parser.Init(field, ctWithHtmlField, attribute, folder, false, null);
 
-        var result = StringWysiwygParser.HtmlForStringAndWysiwyg(conversion.OriginalHtml);
+        var result = parser.HtmlForStringAndWysiwyg(conversion.Original);
 
         NotNull(result);
 
