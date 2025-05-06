0da5665f47c453c7ed9d3c6d5e4572dd29509e00
iJungleboy <daniel.mettler@2sic.com>
2025-05-06 11:59:52 +0200

    minor copyright year

--- Src/Sxc/ToSic.Sxc/Properties/SharedAssemblyInfo.cs
+++ Src/Sxc/ToSic.Sxc/Properties/SharedAssemblyInfo.cs
@@ -9,5 +9,5 @@ public static class SharedAssemblyInfo
     public const string AssemblyVersion = ToSic.Eav.SharedAssemblyInfo.AssemblyVersion;
     public const string Company = ToSic.Eav.SharedAssemblyInfo.Company;
     public const string SxcProduct = "2sxc CMS- and Meta-Module for Dnn and Oqtane";
-    public const string SxcCopyright = "Copyright MIT © 2sic 2024";
+    public const string SxcCopyright = "Copyright MIT © 2sic 2025";
 }
