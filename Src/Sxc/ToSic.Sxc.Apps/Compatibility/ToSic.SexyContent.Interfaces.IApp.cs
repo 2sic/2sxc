be5b76b589b05d590af6f4d1c3bce5f273dd7b6e
iJungleboy <daniel.mettler@2sic.com>
2025-05-14 07:55:02 +0200

    #TryToDropOld... minor

--- Src/Sxc/ToSic.Sxc.Apps/Compatibility/ToSic.SexyContent.Interfaces.IApp.cs
+++ Src/Sxc/ToSic.Sxc.Apps/Compatibility/ToSic.SexyContent.Interfaces.IApp.cs
@@ -5,30 +5,30 @@
 // #TryToDropOldIApp: 2025-05-14 2dm v20 try to remove this for now
 
 
-#if NETFRAMEWORK
-// ReSharper disable once CheckNamespace
-namespace ToSic.SexyContent.Interfaces
-{
-    /// <summary>
-    /// This is the old interface with the "wrong" namespace
-    /// We'll probably need to keep it alive so old code doesn't break
-    /// But this interface shouldn't be enhanced or documented publicly
-    /// </summary>
+//#if NETFRAMEWORK
+//// ReSharper disable once CheckNamespace
+//namespace ToSic.SexyContent.Interfaces
+//{
+//    /// <summary>
+//    /// This is the old interface with the "wrong" namespace
+//    /// We'll probably need to keep it alive so old code doesn't break
+//    /// But this interface shouldn't be enhanced or documented publicly
+//    /// </summary>
 
-    [ShowApiWhenReleased(ShowApiMode.Never)]
-    public interface IApp: Eav.Apps.IApp
-    {
-        dynamic Configuration { get;  }
+//    [ShowApiWhenReleased(ShowApiMode.Never)]
+//    public interface IApp: Eav.Apps.IApp
+//    {
+//        dynamic Configuration { get;  }
 
-        dynamic Settings { get;  }
+//        dynamic Settings { get;  }
 
-        dynamic Resources { get;  }
+//        dynamic Resources { get;  }
 
-        string Path { get; }
+//        string Path { get; }
 
-        string PhysicalPath { get; }
+//        string PhysicalPath { get; }
 
-        string Thumbnail { get; }
-    }
-}
-#endif
+//        string Thumbnail { get; }
+//    }
+//}
+//#endif
--- Src/Sxc/ToSic.Sxc.Apps/Compatibility/ToSic.SexyContent.Interfaces.IAppData.cs
+++ Src/Sxc/ToSic.Sxc.Apps/Compatibility/ToSic.SexyContent.Interfaces.IAppData.cs
@@ -1,12 +1,13 @@
-﻿#if NETFRAMEWORK
-
-// 2025-05-14 2dm
+﻿// 2025-05-14 2dm
 // This was introduced to ensure compatibility with the old namespace
 // but AFAIK all old code was never typed, so it would probably never have been using this interface anyhow!
 //
 // #TryToDropOldIAppData: 2025-05-14 2dm v20 try to remove this for now
 
-// ReSharper disable once CheckNamespace
+//#if NETFRAMEWORK
+
+
+//// ReSharper disable once CheckNamespace
 //namespace ToSic.SexyContent.Interfaces
 //{
 //    // Note: we can't just remove this.
@@ -14,7 +15,7 @@
 //    // So even if it looks unused, it could still be used
 //    [Obsolete("please use the Eav.Apps.Interfaces.IAppData instead")]
 //    [ShowApiWhenReleased(ShowApiMode.Never)]
-//    public interface IAppData: Eav.Apps.IAppData { }
+//    public interface IAppData : Eav.Apps.IAppData { }
 //}
 
-#endif
+//#endif
