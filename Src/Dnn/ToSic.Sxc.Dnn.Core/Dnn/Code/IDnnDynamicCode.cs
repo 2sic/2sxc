658c9fae622193cbaf8ef7598b476b1a5c78cd6b
iJungleboy <daniel.mettler@2sic.com>
2025-05-15 22:30:30 +0200

    minor

--- Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn/Code/IDnnDynamicCode.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.Core/Dnn/Code/IDnnDynamicCode.cs
@@ -5,6 +5,4 @@
 /// It's important, because if 2sxc also runs on other CMS platforms, then the Dnn Context won't be available, so it's in a separate interface.
 /// </summary>
 [PublicApi]
-public interface IDnnDynamicCode : Sxc.Code.IDynamicCode, IHasDnn
-{
-}
+public interface IDnnDynamicCode : Sxc.Code.IDynamicCode, IHasDnn;
