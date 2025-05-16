0fbcc85fde4bcbae1c9a404277a982929327a5ca
iJungleboy <daniel.mettler@2sic.com>
2025-05-16 07:53:22 +0200

    drop old ToSic.Sxc project

--- Src/Sxc/ToSic.Sxc/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-// Global using directives
-
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using ToSic.Lib.Documentation;
-global using ToSic.Lib.Logging;
--- Src/Sxc/ToSic.Sxc/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//[assembly: AssemblyCopyright("Copyright © 2sic 2022")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
