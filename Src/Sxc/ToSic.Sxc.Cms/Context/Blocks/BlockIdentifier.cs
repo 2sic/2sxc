cf69494e3ccd0338efe81152d2e1d721ffaebe03
iJungleboy <daniel.mettler@2sic.com>
2025-05-28 11:49:52 +0200

    Reorg Eav.Apps (flatten folders)

---
+++ Src/Sxc/ToSic.Sxc.Cms/Context/Blocks/BlockIdentifier.cs
@@ -0,0 +1,21 @@
+﻿namespace ToSic.Eav.Cms.Internal;
+
+/// <inheritdoc />
+[ShowApiWhenReleased(ShowApiMode.Never)]
+public class BlockIdentifier(int zoneId, int appId, string appNameId, Guid guid, Guid viewOverride)
+    : IBlockIdentifier
+{
+    /// <inheritdoc />
+    public int ZoneId { get; } = zoneId;
+
+    /// <inheritdoc />
+    public int AppId { get; } = appId;
+
+    public string AppNameId { get; } = appNameId;
+
+    /// <inheritdoc />
+    public Guid Guid { get; } = guid;
+
+    /// <inheritdoc />
+    public Guid PreviewView { get; } = viewOverride;
+}
