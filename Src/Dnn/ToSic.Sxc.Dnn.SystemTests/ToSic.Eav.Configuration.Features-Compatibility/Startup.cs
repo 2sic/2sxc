cb026ec0d1665b1f88ddc9eeaccbd433b86bc2dd
iJungleboy <daniel.mettler@2sic.com>
2025-06-02 15:33:49 +0200

    extract Eav.Apps.Persistence

--- Src/Dnn/ToSic.Sxc.Dnn.SystemTests/ToSic.Eav.Configuration.Features-Compatibility/Startup.cs
+++ Src/Dnn/ToSic.Sxc.Dnn.SystemTests/ToSic.Eav.Configuration.Features-Compatibility/Startup.cs
@@ -38,6 +38,7 @@ public class Startup
             // Apps
             .AddEavWork()
             .AddEavApps()
+            .AddEavAppsPersistence()
 
             // SQL Server
             .AddRepositoryAndEfc()
