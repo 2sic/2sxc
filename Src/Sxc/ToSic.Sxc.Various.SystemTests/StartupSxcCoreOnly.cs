5053107ef4a83b8f522c61ab9cc17e3864564cb1
iJungleboy <daniel.mettler@2sic.com>
2025-06-04 09:37:24 +0200

    almost done dropping Eav.Core

--- Src/Sxc/ToSic.Sxc.Various.SystemTests/StartupSxcCoreOnly.cs
+++ Src/Sxc/ToSic.Sxc.Various.SystemTests/StartupSxcCoreOnly.cs
@@ -59,5 +59,5 @@ public class StartupSxcCoreOnly
             .AddSxcAppsFallbackServices()
             .AddEavDataBuildFallbacks()
             .AddEavDataFallbacks()
-            .AddEavCoreLibAndSysFallbackServices();
+            .AddAllLibAndSysFallbacks();
 }
