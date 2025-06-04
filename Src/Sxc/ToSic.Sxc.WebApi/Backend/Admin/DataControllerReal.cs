7beaa087c275668482bca678051431a274e4d497
iJungleboy <daniel.mettler@2sic.com>
2025-06-04 09:13:00 +0200

    move some constants

--- Src/Sxc/ToSic.Sxc.WebApi/Backend/Admin/DataControllerReal.cs
+++ Src/Sxc/ToSic.Sxc.WebApi/Backend/Admin/DataControllerReal.cs
@@ -69,7 +69,7 @@ public class DataControllerReal(
         if (fileName != fileNameSafe) l.A($"File name sanitized:'{fileName}' => '{fileNameSafe}'");
 
         var appPaths = appPathSvc.Get(appWorkCtxSvc.Context(appId).AppReader, site);
-        var filePath = Path.Combine(appPaths.PhysicalPath, FolderConstants.AppDataProtectedFolder, FsDataConstants.BundlesFolder, fileNameSafe);
+        var filePath = Path.Combine(appPaths.PhysicalPath, FolderConstants.AppDataProtectedFolder, AppDataFoldersConstants.BundlesFolder, fileNameSafe);
 
         if (!File.Exists(filePath))
             return l.ReturnFalse($"File not found: {filePath}");
