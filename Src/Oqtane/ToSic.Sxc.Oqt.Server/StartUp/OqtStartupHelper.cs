78e3ee59926d5594a459bb425845ce73c040cd2c
Tonći Vatavuk <tonci@sistemi.hr>
2025-06-11 20:10:39 +0200

    Oqtane 6.1.3 compatibility fixes #3643

--- Src/Oqtane/ToSic.Sxc.Oqt.Server/StartUp/OqtStartupHelper.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/StartUp/OqtStartupHelper.cs
@@ -1,11 +1,11 @@
-﻿using static ToSic.Sxc.Oqt.Server.WebApi.OqtWebApiConstants;
+﻿//using static ToSic.Sxc.Oqt.Server.WebApi.OqtWebApiConstants;
 
-namespace ToSic.Sxc.Oqt.Server.StartUp;
+//namespace ToSic.Sxc.Oqt.Server.StartUp;
 
-internal static class OqtStartupHelper
-{
+//internal static class OqtStartupHelper
+//{
 
-    public static bool IsSxcEndpoint(string path) => SxcEndpointPathRegex.IsMatch(path);
+//    public static bool IsSxcEndpoint(string path) => SxcEndpointPathRegex.IsMatch(path);
 
-    public static bool IsSxcDialog(string path) => SxcDialogs.Any(p => path.Contains(p.url, StringComparison.OrdinalIgnoreCase));
-}
+//    public static bool IsSxcDialog(string path) => SxcDialogs.Any(p => path.Contains(p.url, StringComparison.OrdinalIgnoreCase));
+//}
