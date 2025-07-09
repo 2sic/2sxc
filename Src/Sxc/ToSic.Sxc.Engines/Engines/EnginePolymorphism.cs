f5dcab77ed3049f9d5f5376c23ec7d5033077d6f
iJungleboy <daniel.mettler@2sic.com>
2025-07-09 14:02:39 +0200

    fix issue with edition button dissapearing #3475

--- Src/Sxc/ToSic.Sxc.Engines/Engines/EnginePolymorphism.cs
+++ Src/Sxc/ToSic.Sxc.Engines/Engines/EnginePolymorphism.cs
@@ -2,6 +2,7 @@
 using ToSic.Eav.Environment.Sys.ServerPaths;
 using ToSic.Sxc.Blocks.Sys.Views;
 using ToSic.Sxc.Polymorphism.Sys;
+using ToSic.Razor.Blade;
 
 namespace ToSic.Sxc.Engines;
 
@@ -27,32 +28,40 @@ public class EnginePolymorphism(PolymorphConfigReader polymorphism, IServerPaths
 
         // Case #1 where edition is between root and path
         // like subPath = "View.cshtml" and there is a "bs5/View.cshtml"
-        var newPath = PolymorphTestPathAndSetIfFound(view, root, edition, subPath);
+        (var newPath, view) = PolymorphTestPathAndSetIfFound(view, root, edition, subPath);
         if (newPath != null)
             return l.Return((newPath, edition), $"found edition {edition}");
 
         // Case #2 where edition _replaces_ an edition in the current path
         // like subPath ="bs5/View.cshtml" and there is a "bs4/View.cshtml"
         l.A("tried inserting path, will check if sub-path");
-        var pathWithoutFirstFolder = subPath.After("/");
+        var pathWithoutFirstFolder = Text.After(subPath, "/");
+        
+        // If there was no root path, exit now.
         if (pathWithoutFirstFolder.IsEmpty())
             return l.ReturnNull("view is not in subfolder, so no edition to replace, stopping now");
-        newPath = PolymorphTestPathAndSetIfFound(view, root, edition, pathWithoutFirstFolder);
-        if (newPath != null)
-            return l.Return((newPath, edition), $"edition {edition} up one path");
 
+        (newPath, view) = PolymorphTestPathAndSetIfFound(view, root, edition, pathWithoutFirstFolder);
+        if (newPath != null)
+        {
+            return l.Return((newPath, edition), $"edition {edition} up one path");
+        }
+
+        // Since obviously something tried to get/set an edition
+        // we'll still register it on the view so the UI knows about it.
+        view.Edition = subPath.Before("/");
         return l.ReturnNull($"edition {edition} not found");
     }
 
-    private string? PolymorphTestPathAndSetIfFound(IView view, string root, string edition, string subPath)
+    private (string? Edition, IView View) PolymorphTestPathAndSetIfFound(IView view, string root, string edition, string subPath)
     {
-        var l = Log.Fn<string>($"root: {root}; edition: {edition}; subPath: {subPath}");
+        var l = Log.Fn<(string?, IView)>($"root: {root}; edition: {edition}; subPath: {subPath}");
         var fullPathForTest = Path.Combine(root, edition, subPath).ToAbsolutePathForwardSlash();
         if (!File.Exists(serverPaths.FullAppPath(fullPathForTest)))
-            return l.ReturnNull("not found");
+            return l.Return((null, view),"not found");
         view.Edition = edition;
         view.EditionPath = Path.Combine(edition, subPath).ToAbsolutePathForwardSlash();
-        return l.Return(fullPathForTest, $"edition {edition}");
+        return l.Return((fullPathForTest, view), $"edition {edition}");
     }
 
 }
