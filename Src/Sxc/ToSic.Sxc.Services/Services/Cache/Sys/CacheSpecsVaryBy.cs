cfe9a99b8699b64552e4980d7ee97d38035a2fec
iJungleboy <daniel.mettler@2sic.com>
2025-08-14 19:38:21 +0200

    Improve insights on CacheSpecsVaryBy

--- Src/Sxc/ToSic.Sxc.Services/Services/Cache/Sys/CacheSpecsVaryBy.cs
+++ Src/Sxc/ToSic.Sxc.Services/Services/Cache/Sys/CacheSpecsVaryBy.cs
@@ -1,10 +1,13 @@
-﻿namespace ToSic.Sxc.Services.Cache.Sys;
+﻿using ToSic.Sys.Caching;
+using ToSic.Sys.Memory;
+
+namespace ToSic.Sxc.Services.Cache.Sys;
 
 /// <summary>
 /// Internal configuration to know what we're varying by.
 /// This is used to determine which parameters will be used to pre-check the cache.
 /// </summary>
-public record CacheSpecsVaryBy
+public record CacheSpecsVaryBy: ICanEstimateSize, ITimestamped
 {
     public bool ByPage { get; init; }
     public bool ByModule { get; init; }
@@ -18,4 +21,11 @@ public record CacheSpecsVaryBy
             CacheSpecConstants.ByUser => this with { ByUser = true },
             _ => this
         };
+
+    public SizeEstimate EstimateSize(ILog? log = default) => new(
+        sizeof(bool) * 3 // ByPage, ByModule, ByUser
+    );
+
+    long ITimestamped.CacheTimestamp { get; } = DateTime.Now.Ticks;
+
 }
