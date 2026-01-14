93a5153039d65b7f12ed9b53e18c8bb66c3879ad
iJungleboy <daniel.mettler@2sic.com>
2026-01-14 15:14:16 +0100

    version bump 21.00.03

--- Src/Oqtane/ToSic.Sxc.Oqt.Shared/Properties/EavSharedAssemblyInfo.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Shared/Properties/EavSharedAssemblyInfo.cs
@@ -9,7 +9,7 @@ public static class SharedAssemblyInfo
     // This is because the version etc. may be compiled into Oqtane DLLs
     // which will run in the browser, and shouldn't have to include these DLLs to work
     // If it's a constant, the value will be added to the compiled code, so no real dependency will exist at runtime
-    public const string AssemblyVersion = "21.00.02";
+    public const string AssemblyVersion = "21.00.03";
     public const string Company = "2sic internet solutions GmbH, Switzerland";
     //public const string EavProduct = "2sic 2sxc Oqtane System";
     //public const string EavCopyright = "Copyright MIT © 2sic 2025";
