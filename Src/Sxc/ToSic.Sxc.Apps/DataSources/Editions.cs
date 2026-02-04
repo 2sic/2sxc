dcd1fc3947df9b2d491b6a8b739df8a9d2b763d1
iJungleboy <daniel.mettler@2sic.com>
2026-02-04 15:41:17 +0100

    improve data sources

--- Src/Sxc/ToSic.Sxc.Apps/DataSources/Editions.cs
+++ Src/Sxc/ToSic.Sxc.Apps/DataSources/Editions.cs
@@ -1,68 +1,63 @@
 ﻿using ToSic.Eav.Apps.Sys.AppJson;
 using ToSic.Eav.Data.Raw.Sys;
-using ToSic.Eav.Data.Sys;
-using ToSic.Eav.DataSource.Sys;
 using ToSic.Eav.DataSource.VisualQuery;
-using ToSic.Eav.WebApi.Sys.Dto;
 
+namespace ToSic.Sxc.DataSources;
 
-namespace ToSic.Sxc.Backend.DataSources
+[PrivateApi]
+[VisualQuery(
+    ConfigurationType = "",
+    NameId = "6cce259b-bf0c-4752-b451-a3eb04825350",
+    //HelpLink = "https://go.2sxc.org/ds-sites",
+    //Icon = DataSourceIcons.Globe,
+    NiceName = "Editions",
+    Type = DataSourceType.System,
+    Audience = Audience.System,
+    DataConfidentiality = DataConfidentiality.System,
+    UiHint = "Editions in this application")]
+
+public class Editions : CustomDataSource
 {
-    [PublicApi]
-    [VisualQuery(
-        ConfigurationType = "",
-        NameId = "6cce259b-bf0c-4752-b451-a3eb04825350",
-        //HelpLink = "https://go.2sxc.org/ds-sites",
-        //Icon = DataSourceIcons.Globe,
-        NiceName = "Editions",
-        Type = DataSourceType.System,
-        UiHint = "Editions in this application")]
-
-    public class Editions : CustomDataSource
+    public Editions(Dependencies services, LazySvc<IAppJsonConfigurationService> appJsonService)
+        : base(services, logName: "CDS.Editions", connect: [appJsonService])
     {
-        public Editions(Dependencies services, LazySvc<IAppJsonConfigurationService> appJsonService) : base(services, logName: "CDS.Editions", connect: [appJsonService])
+        ProvideOutRaw(() => GetList(appJsonService.Value), options: () => new()
         {
-            _appJsonService = appJsonService;
-            ProvideOutRaw(GetList, options: () => new()
-            {
-                AutoId = false,
-                TitleField = "Name",
-                TypeName = "Edition",
-            });
+            AutoId = false,
+            TitleField = "Name",
+            TypeName = "Edition",
+        });
+    }
+
+    private IEnumerable<IRawEntity> GetList(IAppJsonConfigurationService appJsonService)
+    {
+        var l = Log.Fn<IEnumerable<IRawEntity>>();
+        var appJson = appJsonService.GetAppJson(AppId);
+
+        // ReSharper disable once ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
+        if (appJson?.Editions?.Count > 0)
+        {
+            l.A($"has editions in app.json: {appJson.Editions.Count}");
+            var list = appJson.Editions
+                .Select(s => new RawEntity(new()
+                {
+                    //{ AttributeNames.NameIdNiceName, s.Key },
+                    { "Name", s.Key },
+                    { "Description", s.Value.Description },
+                    { "IsDefault", s.Value.IsDefault },
+                }))
+                .ToList();
+            return l.Return(list, $"{list.Count}");
         }
 
-        private readonly LazySvc<IAppJsonConfigurationService> _appJsonService;
-
-        private IEnumerable<IRawEntity> GetList()
+        // default data
+        var rootEdition = new RawEntity(new()
         {
-            var l = Log.Fn<IEnumerable<IRawEntity>>();
-            var appJson = _appJsonService.Value.GetAppJson(AppId);
-
-            // ReSharper disable once ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
-            if (appJson?.Editions?.Count > 0)
-            {
-                l.A($"has editions in app.json: {appJson.Editions.Count}");
-                var list = appJson.Editions
-                    .Select(s => new RawEntity(new()
-                    {
-                        { AttributeNames.NameIdNiceName, s.Key },
-                        { "Name", s.Value.Description },
-                        { "Description", s.Value.Description },
-                        { "IsDefault", s.Value.IsDefault },
-                    }))
-                    .ToList();
-                return l.Return(list, $"{list.Count}");
-            }
-
-            // default data
-            var rootEdition = new RawEntity(new()
-            {
-                { AttributeNames.NameIdNiceName, "" },
-                { "Name", "" },
-                { "Description", "Root edition" },
-                { "IsDefault", true },
-            });
-            return l.Return([rootEdition], $"editions are not specified, so using default edition data");
-        }
+            //{ AttributeNames.NameIdNiceName, "" },
+            { "Name", "" },
+            { "Description", "Root edition" },
+            { "IsDefault", true },
+        });
+        return l.Return([rootEdition], $"editions are not specified, so using default edition data");
     }
 }
--- Src/Sxc/ToSic.Sxc.Code.Generate/DataSources/Generators.cs
+++ Src/Sxc/ToSic.Sxc.Code.Generate/DataSources/Generators.cs
@@ -5,52 +5,49 @@ using ToSic.Eav.DataSource.VisualQuery;
 using ToSic.Sxc.Code.Generate.Sys;
 using ToSic.Sys.DI;
 
-namespace ToSic.Sxc.DataSources
+namespace ToSic.Sxc.DataSources;
+
+[PrivateApi]
+[VisualQuery(
+    ConfigurationType = "",
+    NameId = "f512e44b-5b34-4a32-bfe3-d46d46800a7f",
+    //HelpLink = "https://go.2sxc.org/ds-sites",
+    //Icon = DataSourceIcons.Globe,
+    NiceName = "Generators",
+    Type = DataSourceType.System,
+    Audience = Audience.System,
+    DataConfidentiality = DataConfidentiality.System,
+    UiHint = "Generators in this system")]
+public class Generators: CustomDataSource
 {
-    [PublicApi]
-    [VisualQuery(
-        ConfigurationType = "",
-        NameId = "f512e44b-5b34-4a32-bfe3-d46d46800a7f",
-        //HelpLink = "https://go.2sxc.org/ds-sites",
-        //Icon = DataSourceIcons.Globe,
-        NiceName = "Generators",
-        Type = DataSourceType.System,
-        UiHint = "Generators in this system")]
-
-    public class Generators: CustomDataSource
+    public Generators(Dependencies services, LazySvc<IEnumerable<IFileGenerator>> generators)
+        : base(services, logName: "CDS.Generators", connect: [generators])
     {
-        public Generators(Dependencies services, LazySvc<IEnumerable<IFileGenerator>> generators) : base(services, logName: "CDS.Generators", connect: [generators])
+        ProvideOutRaw(() => GetList(generators.Value), options: () => new()
         {
-            _generators = generators;
-            ProvideOutRaw(GetList, options: () => new()
-            {
-                AutoId = true,
-                TitleField = "Name",
-                TypeName = "Generator",
-            });
-        }
-
-        private readonly LazySvc<IEnumerable<IFileGenerator>> _generators;
-
-        private IEnumerable<IRawEntity> GetList()
-        {
-            var l = Log.Fn<IEnumerable<IRawEntity>>();
-            var fileGenerators = _generators.Value;
-            var list = fileGenerators
-                .Select(g => new RawEntity(new()
-                {
-                    { AttributeNames.NameIdNiceName, g.NameId },
-                    { "Name", g.Name },
-                    { "Version", g.Version },
-                    { "Description", g.Description },
-                    { "DescriptionHtml", g.DescriptionHtml },
-                    { "OutputLanguage", g.OutputLanguage },
-                    { "OutputType", g.OutputType },
-                }))
-                .ToList();
-
-            return l.Return(list, $"{list.Count}");
-        }
+            AutoId = true,
+            TitleField = "Name",
+            TypeName = "Generator",
+        });
     }
 
+    private IEnumerable<IRawEntity> GetList(IEnumerable<IFileGenerator> fileGenerators)
+    {
+        var l = Log.Fn<IEnumerable<IRawEntity>>();
+        //var fileGenerators = _generators.Value;
+        var list = fileGenerators
+            .Select(g => new RawEntity(new()
+            {
+                { AttributeNames.NameIdNiceName, g.NameId },
+                { nameof(g.Name), g.Name },
+                { nameof(g.Version), g.Version },
+                { nameof(g.Description), g.Description },
+                { nameof(g.DescriptionHtml), g.DescriptionHtml },
+                { nameof(g.OutputLanguage), g.OutputLanguage },
+                { nameof(g.OutputType), g.OutputType },
+            }))
+            .ToList();
+
+        return l.Return(list, $"{list.Count}");
+    }
 }
