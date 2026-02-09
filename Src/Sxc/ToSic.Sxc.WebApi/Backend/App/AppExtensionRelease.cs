4cd29fd6d2277701951340fb04f29a3f682a75f7
iJungleboy <daniel.mettler@2sic.com>
2026-02-09 15:23:21 +0100

    correct app extension export

---
+++ Src/Sxc/ToSic.Sxc.WebApi/Backend/App/AppExtensionRelease.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+using ToSic.Eav.Models;
+
+namespace ToSic.Sxc.Backend.App;
+
+[ModelSpecs(ContentType = ContentTypeId)]
+internal record AppExtensionRelease: ModelOfEntityCore
+{
+    public const string ContentTypeId = "1d91a2c0-4d5d-4197-8cb8-d4bfebf72f15";
+    public const string ContentTypeName = "AppExtensionRelease";
+    private const string DefaultVersion = "00.00.01";
+
+    [JsonPropertyName("releaseNotes")]
+    public string ReleaseNotes => GetThis("");
+
+    [JsonPropertyName("version")]
+    public string Version => GetThis(DefaultVersion);
+
+    [JsonPropertyName("isBreaking")]
+    public bool IsBreaking => GetThis(false);
+}
