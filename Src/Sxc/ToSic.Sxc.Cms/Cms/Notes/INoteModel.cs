66507818f851f6da1e7d0fd206fcacf5941f1460
iJungleboy <daniel.mettler@2sic.com>
2026-02-16 17:32:01 +0100

    Introducing the model for notes

---
+++ Src/Sxc/ToSic.Sxc.Cms/Cms/Notes/INoteModel.cs
@@ -0,0 +1,43 @@
+﻿using ToSic.Sxc.Cms.Notes.Sys;
+
+namespace ToSic.Sxc.Cms.Notes;
+
+/// <summary>
+/// Note on anything - such as a site, page, entity, etc.
+/// </summary>
+/// <remarks>
+/// History
+/// 
+/// * Introduced in v21.02
+/// </remarks>
+[ModelSpecs(Use = typeof(NoteModelOfEntity))]
+[InternalApi_DoNotUse_MayChangeWithoutNotice("WIP v21.02")]
+public interface INoteModel : IModelOfData
+{
+    /// <summary>
+    /// The note ID.
+    /// </summary>
+    int Id { get; }
+
+    /// <summary>
+    /// The note GUID.
+    /// </summary>
+    Guid Guid { get; }
+
+    /// <summary>
+    /// The note creation date/time.
+    /// </summary>
+    DateTime Created { get; }
+
+    /// <summary>
+    /// The note modification date/time.
+    /// </summary>
+    DateTime Modified { get; }
+
+    /// <summary>
+    /// The note.
+    /// </summary>
+    string? Note { get; }
+
+    string? NoteType { get; }
+}
---
+++ Src/Sxc/ToSic.Sxc.Cms/Cms/Notes/Sys/NoteModelOfEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace ToSic.Sxc.Cms.Notes.Sys;
+
+[PrivateApi]
+[ShowApiWhenReleased(ShowApiMode.Never)]
+[ModelSpecs(ContentType = ContentTypeNameId)]
+internal record NoteModelOfEntity : ModelOfEntityCore, INoteModel
+{
+    public const string ContentTypeNameId = "5e958dc6-2922-4d68-835c-7b9711538b12";
+    internal const string TypeName = "Note";
+
+    public int Id => Entity.EntityId;
+    public Guid Guid => Entity.EntityGuid;
+    public DateTime Created => Entity.Created;
+    public DateTime Modified => Entity.Modified;
+
+    public string? Note => GetThis<string>(null);
+
+    public string? NoteType => GetThis(fallback: "note");
+}
