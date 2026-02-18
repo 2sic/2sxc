4c675ba75652d0c37cb38bcfff0427650bfe23bd
iJungleboy <daniel.mettler@2sic.com>
2026-02-18 13:53:19 +0100

    Delete PolymorphismDto.cs

--- Src/Sxc/ToSic.Sxc.WebApi/Backend/Views/PolymorphismDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ToSic.Sxc.Backend.Views;
-
-public class PolymorphismDto
-{
-    public int? Id { get; set; }
-    public string? Resolver { get; set; }
-    public string? TypeName { get; set; }
-}
