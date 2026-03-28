61e0f1af9c9dad8e34ab894693c6464a37afa1d0
iJungleboy <daniel.mettler@2sic.com>
2026-03-28 17:13:47 +0100

    improve lightspeed stats to later show compressed/uncompressed sizes

--- Src/Sxc/ToSic.Sxc.LightSpeed/Web.Sys.LightSpeed/LightSpeedStats.cs
+++ Src/Sxc/ToSic.Sxc.LightSpeed/Web.Sys.LightSpeed/LightSpeedStats.cs
@@ -10,19 +10,28 @@ namespace ToSic.Sxc.Web.Sys.LightSpeed;
 [ShowApiWhenReleased(ShowApiMode.Never)]
 public class LightSpeedStats(MemoryCacheService memoryCacheService) : ServiceBase(SxcLogName + ".LightSpeedStats", connect: [memoryCacheService])
 {
-    public Dictionary<int, int> ItemsCount => All
-        .GroupBy(i => i.AppId)
-        .ToDictionary(
-            g => g.Key,
-            g => g.Count()
-        );
+    private MemorySizeEstimator Estimator => field ??= new(Log);
 
-    public Dictionary<int, int> Size => All
-        .GroupBy(i => i.AppId)
-        .ToDictionary(
-            g => g.Key,
-            g => new MemorySizeEstimator(Log).EstimateMany(g.ToArray<object>()).Total
-        );
+    public Dictionary<int, LightSpeedStat> AppsWithCount =>
+        All
+            .GroupBy(i => i.AppId)
+            .ToDictionary(
+                g => g.Key,
+                g =>
+                {
+                    var size = Estimator.EstimateMany(g.ToArray<object>()).Total;
+                    var compressed = g.Where(i => (i?.Data as IOptimizeMemory)?.UseCompression == true);
+                    var compressedSize = Estimator.EstimateMany(compressed.ToArray<object>()).Total;
+                    return new LightSpeedStat(g.Count(), size, compressedSize, size - compressedSize);
+                });
+
+
+    //public Dictionary<int, int> Size => All
+    //    .GroupBy(i => i.AppId)
+    //    .ToDictionary(
+    //        g => g.Key,
+    //        g => Estimator.EstimateMany(g.ToArray<object>()).Total
+    //    );
 
     private List<OutputCacheItem> All => _all.Get(() => MemoryCache.Default
         .Where(pair => pair.Key.StartsWith(OutputCacheKeys.GlobalCacheKeyModuleRoot) || pair.Key.StartsWith(OutputCacheKeys.GlobalCacheKeyPartialRoot))
@@ -32,3 +41,5 @@ public class LightSpeedStats(MemoryCacheService memoryCacheService) : ServiceBas
     )!;
     private readonly GetOnce<List<OutputCacheItem>> _all = new();
 }
+
+public record LightSpeedStat(int Count, long Total, long Compressed, long Uncompressed);
--- Src/Sxc/ToSic.Sxc.WebApi/Backend/Sys/Insights/InsightsLightSpeed.cs
+++ Src/Sxc/ToSic.Sxc.WebApi/Backend/Sys/Insights/InsightsLightSpeed.cs
@@ -1,4 +1,5 @@
-﻿using ToSic.Eav.Sys;
+﻿using ToSic.Eav.Apps.Assets.Sys;
+using ToSic.Eav.Sys;
 using ToSic.Eav.Sys.Insights;
 using ToSic.Eav.Sys.Insights.HtmlHelpers;
 using ToSic.Sxc.Web.Sys.LightSpeed;
@@ -17,29 +18,38 @@ internal class InsightsLightSpeed(LightSpeedStats lightSpeedStats, IAppReaderFac
         var msg = H1("LightSpeed Stats").ToString();
         try
         {
-            var countStats = lightSpeedStats.ItemsCount;
-            var sizeStats = lightSpeedStats.Size;
+            var countStats = lightSpeedStats.AppsWithCount;
+            //var sizeStats = lightSpeedStats.Size;
             msg += P($"Apps in Cache: {countStats.Count}");
             msg += "<table id='table'>"
-                   + InsightsHtmlTable.HeadFields(["#", "ZoneId", "AppId", "Name", "Items in Cache", "Ca. Memory Use", "NameId"])
+                   + InsightsHtmlTable.HeadFields(["#", "ZoneId", "AppId", "Name", "Items in Cache", "Ca. Memory Use", "Uncompressed", "Compressed", "NameId"])
                    + "<tbody>";
             var count = 0;
             var totalItems = 0;
             var totalMemory = 0L;
             foreach (var cacheItem in countStats)
             {
-                var appSpecs = (cacheItem.Key != 0) ? appReader.Get(cacheItem.Key).Specs : null;
+                var appSpecs = (cacheItem.Key != 0)
+                    ? appReader.Get(cacheItem.Key).Specs
+                    : null;
+
                 msg += InsightsHtmlTable.RowFields([
                     ++count,
+                    // ZoneId, AppId, Name
                     SpecialField.Right(appSpecs?.ZoneId ?? 0),
                     SpecialField.Right(cacheItem.Key),
                     appSpecs?.Name ?? "unknown",
-                    SpecialField.Right(cacheItem.Value),
-                    SpecialField.Right(sizeStats.TryGetValue(cacheItem.Key, out var size) ? ByteToKByte(size) : EavConstants.NullNameId),
+
+                    // Count, Size, Uncompressed, Compressed
+                    SpecialField.Right(cacheItem.Value.Count),
+                    SpecialField.Right(new SizeInfo(cacheItem.Value.Total).ToString()),
+                    SpecialField.Right(new SizeInfo(cacheItem.Value.Uncompressed).ToString()),
+                    SpecialField.Right(new SizeInfo(cacheItem.Value.Compressed).ToString()),
+
                     appSpecs?.NameId ?? "unknown"
                 ]);
-                totalItems += cacheItem.Value;
-                totalMemory += size;
+                totalItems += cacheItem.Value.Count;
+                totalMemory += cacheItem.Value.Total;
             }
             msg += "</tbody>";
             msg += "<tfoot>";
@@ -68,13 +78,13 @@ internal class InsightsLightSpeed(LightSpeedStats lightSpeedStats, IAppReaderFac
     private string ByteToKByte(long bytes)
     {
         const int kb = 1024;
-        if (bytes < kb) return bytes + "b";
+        if (bytes < kb) return bytes + " b";
 
         const int mb = kb * kb;
         if (bytes < 10 * mb)
-            return ((double)bytes / kb).ToAposString() + "kb";
+            return ((double)bytes / kb).ToAposString() + " kb";
 
-        return ((double)bytes / mb).ToAposString() + "mb";
+        return ((double)bytes / mb).ToAposString() + " mb";
     }
 
 }
