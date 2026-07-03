12092b36d5e7b8ec93a5b9c6c6dc4a90ad2a0e7a
iJungleboy <daniel.mettler@2sic.com>
2026-07-03 17:09:43 +0200

    disable unused code for future cleanup

--- Src/Oqtane/ToSic.Sxc.Oqt.Server/WebApi/Admin/ApiExplorerController.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/WebApi/Admin/ApiExplorerController.cs
@@ -1,83 +1,86 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Oqtane.Shared;
-using System.Reflection;
-using ToSic.Eav.Apps.Sys;
-using ToSic.Eav.Environment.Sys.ServerPaths;
-using ToSic.Eav.WebApi.Sys.ApiExplorer;
-using ToSic.Sxc.Code.Sys.HotBuild;
-using ToSic.Sxc.Oqt.Server.Code.Sys;
-using ToSic.Sxc.Oqt.Server.Context;
-using ToSic.Sxc.Oqt.Server.Controllers;
-using ToSic.Sxc.Oqt.Server.Controllers.AppApi;
-using ToSic.Sxc.Oqt.Server.Plumbing;
-using ToSic.Sxc.Oqt.Server.Run;
-using ToSic.Sxc.Polymorphism.Sys;
-using ToSic.Sxc.WebApi.Sys;
-using ToSic.Sys.Utils;
-using RealController = ToSic.Eav.WebApi.Sys.ApiExplorer.ApiExplorerControllerReal;
+// 2026-07-03 2dm - seems unused now
 
-namespace ToSic.Sxc.Oqt.Server.WebApi.Admin;
 
-// Release routes
-[Route(OqtWebApiConstants.ApiRootNoLanguage + $"/{AreaRoutes.Admin}")]
-[Route(OqtWebApiConstants.ApiRootPathOrLang + $"/{AreaRoutes.Admin}")]
-[Route(OqtWebApiConstants.ApiRootPathAndLang + $"/{AreaRoutes.Admin}")]
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using Oqtane.Shared;
+//using System.Reflection;
+//using ToSic.Eav.Apps.Sys;
+//using ToSic.Eav.Environment.Sys.ServerPaths;
+//using ToSic.Eav.WebApi.Sys.ApiExplorer;
+//using ToSic.Sxc.Code.Sys.HotBuild;
+//using ToSic.Sxc.Oqt.Server.Code.Sys;
+//using ToSic.Sxc.Oqt.Server.Context;
+//using ToSic.Sxc.Oqt.Server.Controllers;
+//using ToSic.Sxc.Oqt.Server.Controllers.AppApi;
+//using ToSic.Sxc.Oqt.Server.Plumbing;
+//using ToSic.Sxc.Oqt.Server.Run;
+//using ToSic.Sxc.Polymorphism.Sys;
+//using ToSic.Sxc.WebApi.Sys;
+//using ToSic.Sys.Utils;
+//using RealController = ToSic.Eav.WebApi.Sys.ApiExplorer.ApiExplorerControllerReal;
 
-[ValidateAntiForgeryToken]
-//[DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
-[Authorize(Roles = RoleNames.Admin)]
+//namespace ToSic.Sxc.Oqt.Server.WebApi.Admin;
 
-[ShowApiWhenReleased(ShowApiMode.Never)]
-public class ApiExplorerController() : OqtStatefulControllerBase(RealController.LogSuffix)
-{
-    private RealController Real => GetService<RealController>();
-    private Generator<Compiler> Compiler => GetService<Generator<Compiler>>();
+//// Release routes
+//[Route(OqtWebApiConstants.ApiRootNoLanguage + $"/{AreaRoutes.Admin}")]
+//[Route(OqtWebApiConstants.ApiRootPathOrLang + $"/{AreaRoutes.Admin}")]
+//[Route(OqtWebApiConstants.ApiRootPathAndLang + $"/{AreaRoutes.Admin}")]
 
-    // @2rb 2026-06-19: Replaced by System.AppWebApiControllerDetails and System.AppWebApiControllerEndpoints DataSources.
-    //[HttpGet]
-    //public IActionResult Inspect(string path)
-    //{
-    //    // Make sure the Scoped ResponseMaker has this controller context
-    //    CtxHlp.SetupResponseMaker();
-    //    return Real.Inspect(path, GetCompiledAssembly);
-    //}
+//[ValidateAntiForgeryToken]
+////[DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Admin)]
+//[Authorize(Roles = RoleNames.Admin)]
 
-    private Assembly GetCompiledAssembly(string path)
-    {
-        // get path from root
-        var alias = GetService<SiteState>()?.Alias ?? GetService<AliasResolver>().Alias;
-        var siteId = GetService<OqtSiteGroup>().GetPrimaryLocalizationSiteId(alias.SiteId);
-        var tenantId = alias.TenantId;
-        var appFolder = GetService<AppFolderLookupForWebApi>().GetAppFolder();
-        var pathFromRoot = OqtServerPaths.GetAppApiPath(tenantId, siteId, appFolder, path);
+//[ShowApiWhenReleased(ShowApiMode.Never)]
+//public class ApiExplorerController() : OqtStatefulControllerBase(RealController.LogSuffix)
+//{
+//    private RealController Real => GetService<RealController>();
+//    private Generator<Compiler> Compiler => GetService<Generator<Compiler>>();
 
-        // Figure out the current edition
-        var blockOrNull = CtxHlp.BlockOptional;
-        var edition = blockOrNull
-            .NullOrGetWith(b => GetService<PolymorphConfigReader>().UseViewEditionOrGet(b));
+//    // @2rb 2026-06-19: Replaced by System.AppWebApiControllerDetails and System.AppWebApiControllerEndpoints DataSources.
+//    //[HttpGet]
+//    //public IActionResult Inspect(string path)
+//    //{
+//    //    // Make sure the Scoped ResponseMaker has this controller context
+//    //    CtxHlp.SetupResponseMaker();
+//    //    return Real.Inspect(path, GetCompiledAssembly);
+//    //}
 
-        var runtimeKey = blockOrNull?.Context.AppReaderRequired?.Specs.RuntimeKey;
-        var spec = new HotBuildSpec(blockOrNull?.AppId ?? KnownAppsConstants.AppIdEmpty, edition: edition, appName: blockOrNull?.AppOrNull?.Name, runtimeKey: runtimeKey);
+//    private Assembly GetCompiledAssembly(string path)
+//    {
+//        // get path from root
+//        var alias = GetService<SiteState>()?.Alias ?? GetService<AliasResolver>().Alias;
+//        var siteId = GetService<OqtSiteGroup>().GetPrimaryLocalizationSiteId(alias.SiteId);
+//        var tenantId = alias.TenantId;
+//        var appFolder = GetService<AppFolderLookupForWebApi>().GetAppFolder();
+//        var pathFromRoot = OqtServerPaths.GetAppApiPath(tenantId, siteId, appFolder, path);
 
-        Log.A($"Controller path from root: {pathFromRoot}");
+//        // Figure out the current edition
+//        var blockOrNull = CtxHlp.BlockOptional;
+//        var edition = blockOrNull
+//            .NullOrGetWith(b => GetService<PolymorphConfigReader>().UseViewEditionOrGet(b));
 
-        // get full path
-        var oqtServerPaths = GetService<IServerPaths>();
-        var apiFile = oqtServerPaths.FullContentPath(pathFromRoot);
+//        var runtimeKey = blockOrNull?.Context.AppReaderRequired?.Specs.RuntimeKey;
+//        var spec = new HotBuildSpec(blockOrNull?.AppId ?? KnownAppsConstants.AppIdEmpty, edition: edition, appName: blockOrNull?.AppOrNull?.Name, runtimeKey: runtimeKey);
 
-        if (!System.IO.File.Exists(apiFile))
-            throw new($"Error: can't find controller file: {pathFromRoot}");
+//        Log.A($"Controller path from root: {pathFromRoot}");
 
-        // get dll name
-        var controllerFolder = pathFromRoot.Substring(0, pathFromRoot.LastIndexOf(@"\"));
-        var dllName = AppApiDynamicRouteValueTransformer.GetDllName(controllerFolder, apiFile);
+//        // get full path
+//        var oqtServerPaths = GetService<IServerPaths>();
+//        var apiFile = oqtServerPaths.FullContentPath(pathFromRoot);
 
-        return Compiler.New().Compile(apiFile, dllName, spec).Assembly;
-    }
+//        if (!System.IO.File.Exists(apiFile))
+//            throw new($"Error: can't find controller file: {pathFromRoot}");
 
-    // 2rb: 2026-06-12: Replaced by AppWebApiControllers Datasource
-    //[HttpGet]
-    //[JsonFormatter(Casing = Casing.Camel)]
-    //public AllApiFilesDto AppApiFiles(int appId) => Real.AppApiFiles(appId);
-}
+//        // get dll name
+//        var controllerFolder = pathFromRoot.Substring(0, pathFromRoot.LastIndexOf(@"\"));
+//        var dllName = AppApiDynamicRouteValueTransformer.GetDllName(controllerFolder, apiFile);
+
+//        return Compiler.New().Compile(apiFile, dllName, spec).Assembly;
+//    }
+
+//    // 2rb: 2026-06-12: Replaced by AppWebApiControllers Datasource
+//    //[HttpGet]
+//    //[JsonFormatter(Casing = Casing.Camel)]
+//    //public AllApiFilesDto AppApiFiles(int appId) => Real.AppApiFiles(appId);
+//}
