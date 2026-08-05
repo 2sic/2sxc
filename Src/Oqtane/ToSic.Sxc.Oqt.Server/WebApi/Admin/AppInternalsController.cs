6d0023790dcd962c7c31143b1344c77c43a750fa
iJungleboy <daniel.mettler@2sic.com>
2026-08-05 18:09:39 +0200

    Comment out internal code Endpoints for AppInternals replaced by AppEnhancements

--- Src/Oqtane/ToSic.Sxc.Oqt.Server/WebApi/Admin/AppInternalsController.cs
+++ Src/Oqtane/ToSic.Sxc.Oqt.Server/WebApi/Admin/AppInternalsController.cs
@@ -1,26 +1,26 @@
-using Microsoft.AspNetCore.Mvc;
-using ToSic.Sxc.Oqt.Server.Controllers;
-using RealController = ToSic.Eav.WebApi.Sys.Admin.AppInternalsControllerReal;
+//using Microsoft.AspNetCore.Mvc;
+//using ToSic.Sxc.Oqt.Server.Controllers;
+//using RealController = ToSic.Eav.WebApi.Sys.Admin.AppInternalsControllerReal;
 
-namespace ToSic.Sxc.Oqt.Server.WebApi.Admin;
+//namespace ToSic.Sxc.Oqt.Server.WebApi.Admin;
 
-/// <summary>
-/// Proxy Class to the AppInternalsController (Web API Controller)
-/// </summary>
-[Route(OqtWebApiConstants.ApiRootNoLanguage + $"/{AreaRoutes.Admin}")]
-[Route(OqtWebApiConstants.ApiRootPathOrLang + $"/{AreaRoutes.Admin}")]
-[Route(OqtWebApiConstants.ApiRootPathAndLang + $"/{AreaRoutes.Admin}")]
-[ShowApiWhenReleased(ShowApiMode.Never)]
-public class AppInternalsController() : OqtStatefulControllerBase(RealController.LogSuffix)
-{
-    private RealController Real => GetService<RealController>();
+///// <summary>
+///// Proxy Class to the AppInternalsController (Web API Controller)
+///// </summary>
+//[Route(OqtWebApiConstants.ApiRootNoLanguage + $"/{AreaRoutes.Admin}")]
+//[Route(OqtWebApiConstants.ApiRootPathOrLang + $"/{AreaRoutes.Admin}")]
+//[Route(OqtWebApiConstants.ApiRootPathAndLang + $"/{AreaRoutes.Admin}")]
+//[ShowApiWhenReleased(ShowApiMode.Never)]
+//public class AppInternalsController() : OqtStatefulControllerBase(RealController.LogSuffix)
+//{
+//    private RealController Real => GetService<RealController>();
 
-    // Replaced by DataSource System.AppEnhancements through query System.SysData.
-    // Use app/auto/query/System.SysData/ with SysDataSource=System.AppEnhancements.
-    ///// <inheritdoc/>
-    //[HttpGet]
-    //[ValidateAntiForgeryToken]
-    //[Authorize(Roles = RoleNames.Admin)]
-    //public AppInternalsDto Get(int appId)
-    //    => Real.Get(appId);
-}
+//    // Replaced by DataSource System.AppEnhancements through query System.SysData.
+//    // Use app/auto/query/System.SysData/ with SysDataSource=System.AppEnhancements.
+//    ///// <inheritdoc/>
+//    //[HttpGet]
+//    //[ValidateAntiForgeryToken]
+//    //[Authorize(Roles = RoleNames.Admin)]
+//    //public AppInternalsDto Get(int appId)
+//    //    => Real.Get(appId);
+//}
