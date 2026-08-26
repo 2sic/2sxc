64333f9921b80e8df89fee3a126013d7856b6b79
iJungleboy <daniel.mettler@2sic.com>
2026-08-26 11:08:34 +0200

    #CleanUp2sxcEditInformation

--- Src/Sxc/ToSic.Sxc.Core/Edit.Sys/SxcEditSharedConstants.cs
+++ Src/Sxc/ToSic.Sxc.Core/Edit.Sys/SxcEditSharedConstants.cs
@@ -1,10 +1,25 @@
-﻿namespace ToSic.Sxc.Edit.Sys;
+﻿// #CleanUp2sxcEditInformation 2026-08-26 v22 2dm
+// Removing this completely.
+// Here's what I checked:
+// - There is a bit of old code in 2sxc-ui, mentioning compatibility with V9
+// - eav-ui does not use it
+// - no apps use it as of 2026-08 and no apps probably used this for at least 3 years
+// - IMHO previously there was still some use of this in the timelineJS a long time ago,
+//   so there may still be installed usages, which would then need some guidance for upgrading.
+//   
+//
+// But I hope that we can just kill this
+// Keep an eye on this.
 
-[ShowApiWhenReleased(ShowApiMode.Never)]
-public class SxcEditSharedConstants
-{
-    /// <summary>
-    /// Additional json-node for metadata in serialized entities, if user has edit rights
-    /// </summary>
-    public const string JsonEntityEditNodeName = "_2sxcEditInformation";
-}
+//namespace ToSic.Sxc.Edit.Sys;
+
+//[ShowApiWhenReleased(ShowApiMode.Never)]
+//public class SxcEditSharedConstants
+//{
+
+
+//    /// <summary>
+//    /// Additional json-node for metadata in serialized entities, if user has edit rights
+//    /// </summary>
+//    public const string JsonEntityEditNodeName = "_2sxcEditInformation";
+//}
